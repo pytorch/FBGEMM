895646cfe2b68e42a506c49217a635270d42bd09
James Reed <jamesreed@fb.com>
2018-12-06 11:33:39 -0800

    Fix duplicate symbols for thread local member variables (#43)
    Summary:
    Build would fail on my mac because these TLS variables were being instantiated in both the regular and AVX512 compilation units of GenerateKernel*.cc. This moves them into the regular version, and just lets the AVX512 version link to those
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/43
    
    Reviewed By: jianyuh
    
    Differential Revision: D13362476
    
    Pulled By: dskhudia
    
    fbshipit-source-id: e15ae957a38533df0565a1262267616dbd4ad88f

--- src/GenerateKernel.h
+++ src/GenerateKernel.h
@@ -139,16 +139,4 @@ class CodeGenBase {
       codeCache_; ///< JIT Code Cache for reuse.
 };
 
-template <typename TA, typename TB, typename TC, typename accT>
-thread_local asmjit::JitRuntime CodeGenBase<TA, TB, TC, accT>::rt_;
-
-template <typename TA, typename TB, typename TC, typename accT>
-thread_local asmjit::CodeHolder CodeGenBase<TA, TB, TC, accT>::code_;
-
-template <typename TA, typename TB, typename TC, typename accT>
-thread_local std::map<
-    std::tuple<bool, int, int>,
-    typename CodeGenBase<TA, TB, TC, accT>::jit_micro_kernel_fp>
-    CodeGenBase<TA, TB, TC, accT>::codeCache_;
-
 } // namespace fbgemm
