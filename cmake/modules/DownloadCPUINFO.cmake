b14f582ca614bb01b53e4c842c8d50e511454ade
Daya S Khudia <dskhudia@fb.com>
2019-05-13 11:24:34 -0700

    Back out "[FBGEMM][PR] switch from cmake downloads to git submodules"
    Summary: Original commit changeset: 9a33573ba34b
    
    Reviewed By: jianyuh
    
    Differential Revision: D15320950
    
    fbshipit-source-id: f6501b57346cc5e82fa2198dcf6b60b26cd4f7c6

---
+++ cmake/modules/DownloadCPUINFO.cmake
@@ -0,0 +1,16 @@
+cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+
+project(cpuinfo-download NONE)
+
+include(ExternalProject)
+
+ExternalProject_Add(cpuinfo
+  GIT_REPOSITORY https://github.com/pytorch/cpuinfo
+  GIT_TAG master
+  SOURCE_DIR "${FBGEMM_THIRDPARTY_DIR}/cpuinfo"
+  BINARY_DIR "${FBGEMM_BINARY_DIR}/cpuinfo"
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  TEST_COMMAND ""
+)
