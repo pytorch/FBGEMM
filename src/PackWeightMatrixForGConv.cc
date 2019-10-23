40ad9a8e33b42f5a49db59affa7c1841513d68f4
Daya Khudia <dskhudia@fb.com>
2019-10-23 11:25:13 -0700

    Packing/unpacking of weights for 3D conv
    Summary: Packing of weights for 3D groupwise convolutions.
    
    Reviewed By: jspark1105
    
    Differential Revision: D18074857
    
    fbshipit-source-id: 699a17b046f60e97e4939ee86cd9e401377cc9c1

--- src/PackWeightMatrixForGConv.cc
+++ src/PackWeightMatrixForGConv.cc
@@ -7,6 +7,7 @@
 #include <cpuinfo.h>
 #include <cassert>
 #include <iomanip>
+#include <numeric>
 #include "RefImplementations.h"
 #include "fbgemm/Fbgemm.h"
 
@@ -19,16 +20,16 @@ PackWeightMatrixForGConv<T, accT, SPATIAL_DIM>::PackWeightMatrixForGConv(
     const T* sdata,
     T* pdata)
     : trans_(trans), conv_param_(conv_param), sdata_(sdata) {
-  assert(SPATIAL_DIM == 2 && "3D conv not supported yet");
-
   if (!pdata) {
     bufAllocatedHere_ = true;
+    int kernel_prod = std::accumulate(
+        conv_param.K.begin(), conv_param.K.end(), 1, std::multiplies<int>());
     // we make it a multiple of 4
     int paddedICPerG = ((conv_param_.IC / conv_param_.G) + 3) / 4 * 4;
     pdata_ = static_cast<T*>(fbgemmAlignedAlloc(
         64,
-        conv_param_.G * conv_param_.K[0] * conv_param_.K[1] *
-            (conv_param_.OC / conv_param_.G) * paddedICPerG * sizeof(T)));
+        conv_param_.G * kernel_prod * (conv_param_.OC / conv_param_.G) *
+            paddedICPerG * sizeof(T)));
   } else {
     bufAllocatedHere_ = false;
     pdata_ = pdata;
@@ -66,55 +67,67 @@ int PackWeightMatrixForGConv<T, accT, SPATIAL_DIM>::numOfGroupsTogether(
 }
 
 /**
- * @brief Get the index of the unpacked data for a given <r, s, k, g, c, tr>
+ * @brief Get the index of the unpacked data
+ *        for a given <t, r, s, k, g, c, tr>
  *
- * Non-transposed: G (R S C/G) K/G
- * Transposed: G K/G (R S C/G)
+ * Non-transposed: G (T R S C/G) K/G
+ * Transposed: G K/G (T R S C/G)
  * Using inline as this will be called frequently
  */
 template <typename T, typename accT, int SPATIAL_DIM>
 inline int PackWeightMatrixForGConv<T, accT, SPATIAL_DIM>::unpacked_index_(
-    int r, int s, int k, int g, int c, bool tr) {
+    int t,
+    int r,
+    int s,
+    int k,
+    int g,
+    int c,
+    bool tr) {
   // Get the full dimensions
-  int R = conv_param_.K[0];
-  int S = conv_param_.K[1];
+  // Can't use T as varname because T is a template parameter.
+  int F = SPATIAL_DIM == 2 ? 1 : conv_param_.K[SPATIAL_DIM - 3];
+  int R = conv_param_.K[SPATIAL_DIM - 2];
+  int S = conv_param_.K[SPATIAL_DIM - 1];
   int G = conv_param_.G;
   int IC_per_G = conv_param_.IC / G;
   int OC_per_G = conv_param_.OC / G;
 
   int idx;
   if (tr) {
-    idx = (((g * OC_per_G + k) * R + r) * S + s) * IC_per_G + c;
+    idx = ((((g * OC_per_G + k) * F + t) * R + r) * S + s) * IC_per_G + c;
   } else {
-    idx = (((g * R + r) * S + s) * IC_per_G + c) * OC_per_G + k;
+    idx = ((((g * F + t) * R + r) * S + s) * IC_per_G + c) * OC_per_G + k;
   }
   return idx;
 }
 
 /**
- * @brief Get the index of the packed data for a given <r, s, k, g, c>
+ * @brief Get the index of the packed data for a given <t, r, s, k, g, c>
  *
  * The index may differ depending on IC_per_G.
  * Using inline as this will be called frequently
  */
 template <typename T, typename accT, int SPATIAL_DIM>
 inline int PackWeightMatrixForGConv<T, accT, SPATIAL_DIM>::packed_index_(
+    int t,
     int r,
     int s,
     int k,
     int g,
     int c) {
   // Get the full dimensions
-  int R = conv_param_.K[0];
-  int S = conv_param_.K[1];
+  // Can't use T as varname because T is a template parameter.
+  int F = SPATIAL_DIM == 2 ? 1 : conv_param_.K[SPATIAL_DIM - 3];
+  int R = conv_param_.K[SPATIAL_DIM - 2];
+  int S = conv_param_.K[SPATIAL_DIM - 1];
   int G = conv_param_.G;
   int IC_per_G = conv_param_.IC / G;
   int OC_per_G = conv_param_.OC / G;
   int paddedICPerG = (IC_per_G + 3) / 4 * 4;
 
-  int idx =
-      (((((g / GTogether_) * R + r) * S + s) * OC_per_G + k) * GTogether_ +
-       (g % GTogether_)) *
+  int idx = ((((((g / GTogether_) * F + t) * R + r) * S + s) * OC_per_G + k) *
+                 GTogether_ +
+             (g % GTogether_)) *
           paddedICPerG +
       c;
   return idx;
@@ -141,38 +154,43 @@ inline int PackWeightMatrixForGConv<T, accT, SPATIAL_DIM>::packed_index_(
 
 template <typename T, typename accT, int SPATIAL_DIM>
 void PackWeightMatrixForGConv<T, accT, SPATIAL_DIM>::pack_unpack_(
-    const T* src, T* dst, bool ispack) {
-  // filters are assumed to be in G RS C/G K/G format
-  int R = conv_param_.K[0];
-  int S = conv_param_.K[1];
+    const T* src,
+    T* dst,
+    bool ispack) {
+  // Can't use T as varname because T is a template parameter.
+  int F = SPATIAL_DIM == 2 ? 1 : conv_param_.K[SPATIAL_DIM - 3];
+  int R = conv_param_.K[SPATIAL_DIM - 2];
+  int S = conv_param_.K[SPATIAL_DIM - 1];
   int G = conv_param_.G;
   int IC_per_G = conv_param_.IC / G;
   int OC_per_G = conv_param_.OC / G;
   int paddedICPerG = (IC_per_G + 3) / 4 * 4;
 
-  // If transpose option is set, the weight matrix is in layout G K/G (R S C/G)
-  // instead of G (R S C/G) K/G
+  // If transpose option is set, the weight matrix is in layout G K/G (T R S
+  // C/G) instead of G (T R S C/G) K/G
   bool tr = (trans_ == matrix_op_t::Transpose);
   if (fbgemmOptimizedGConv(conv_param_)) {
     // currently only this case is supported
-    for (int r = 0; r < R; ++r) {
-      for (int s = 0; s < S; ++s) {
-        for (int k = 0; k < OC_per_G; ++k) {
-          for (int g = 0; g < G; ++g) {
-            for (int c = 0; c < IC_per_G; ++c) {
-              int p_idx = packed_index_(r, s, k, g, c);
-              int up_idx = unpacked_index_(r, s, k, g, c, tr);
-              // Pack: src (unpacked) -> dst (packed)
-              if (ispack) {
-                dst[p_idx] = src[up_idx];
-              } else {
-                dst[up_idx] = src[p_idx];
+    for (int t = 0; t < F; ++t) {
+      for (int r = 0; r < R; ++r) {
+        for (int s = 0; s < S; ++s) {
+          for (int k = 0; k < OC_per_G; ++k) {
+            for (int g = 0; g < G; ++g) {
+              for (int c = 0; c < IC_per_G; ++c) {
+                int p_idx = packed_index_(t, r, s, k, g, c);
+                int up_idx = unpacked_index_(t, r, s, k, g, c, tr);
+                // Pack: src (unpacked) -> dst (packed)
+                if (ispack) {
+                  dst[p_idx] = src[up_idx];
+                } else {
+                  dst[up_idx] = src[p_idx];
+                }
               }
-            }
-            if (ispack) {
-              for (int c = IC_per_G; c < paddedICPerG; ++c) {
-                int p_idx = packed_index_(r, s, k, g, c);
-                dst[p_idx] = 0;
+              if (ispack) {
+                for (int c = IC_per_G; c < paddedICPerG; ++c) {
+                  int p_idx = packed_index_(t, r, s, k, g, c);
+                  dst[p_idx] = 0;
+                }
               }
             }
           }
@@ -181,34 +199,42 @@ void PackWeightMatrixForGConv<T, accT, SPATIAL_DIM>::pack_unpack_(
     }
   } else {
     // For pack & transposed, call transposeConvWeights()
-    // G K/G (R S C/G) => G (R S C/G) K/G
+    // G K/G (T R S C/G) => G (T R S C/G) K/G
     if (tr) {
       if (ispack) {
         transposeConvWeights(conv_param_, src, dst);
       } else {
         // TODO: Wrap this as a inverseTransposeConvWeights()?
         // For unpack & transposed, call transposeConvWeights()
-        // G (R S C/G) K/G => G K/G (R S C/G)
-        for (int r = 0; r < R; ++r) {
-          for (int s = 0; s < S; ++s) {
-            for (int k = 0; k < OC_per_G; ++k) {
-              for (int g = 0; g < G; ++g) {
-                for (int c = 0; c < IC_per_G; ++c) {
-                  dst[(((g * OC_per_G + k) * R + r) * S + s)
-                    * IC_per_G + c] =
-                    src[(((g * R + r) * S + s) * IC_per_G + c)
-                    * OC_per_G + k];
+        // G (T R S C/G) K/G => G K/G (T R S C/G)
+        for (int t = 0; t < F; ++t) {
+          for (int r = 0; r < R; ++r) {
+            for (int s = 0; s < S; ++s) {
+              for (int k = 0; k < OC_per_G; ++k) {
+                for (int g = 0; g < G; ++g) {
+                  for (int c = 0; c < IC_per_G; ++c) {
+                    dst[((((g * OC_per_G + k) * F + t) * R + r) * S + s) *
+                            IC_per_G +
+                        c] =
+                        src[((((g * F + t) * R + r) * S + s) * IC_per_G + c) *
+                                OC_per_G +
+                            k];
+                  }
                 }
               }
             }
           }
         }
-      }  // end if(ispack)
+      } // end if(ispack)
     } else {
       // just copy the data for not supported cases
-      memcpy(dst, src,
-          G * R * S * OC_per_G * IC_per_G * sizeof(inpType));
-    } //end if(tr)
+      int kernel_prod = std::accumulate(
+          conv_param_.K.begin(),
+          conv_param_.K.end(),
+          1,
+          std::multiplies<int>());
+      memcpy(dst, src, G * kernel_prod * OC_per_G * IC_per_G * sizeof(inpType));
+    } // end if(tr)
   } // end if(fbgemmOptimizedGConv(conv_param_)
 }
 
