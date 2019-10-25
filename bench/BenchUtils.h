4dd4f005120b61022a6d16e6a8f6f6eb131a18c9
Jongsoo Park <jongsoo@fb.com>
2019-10-24 21:36:30 -0700

    move measureWithWarmup to fbgemm/bench/BenchUtils.h (#148)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/148
    
    measureWithWarmup in Alek's code should be useful for other benchmarks we have in fbgemm.
    
    Reviewed By: zlateski
    
    Differential Revision: D17902464
    
    fbshipit-source-id: ebb44dc8f5838d49af45c09f93d3f0a2ac870eb4

--- bench/BenchUtils.h
+++ bench/BenchUtils.h
@@ -5,8 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 #pragma once
+#include <chrono>
 #include <vector>
-#include "bench/AlignedVec.h"
+#include "AlignedVec.h"
 
 namespace fbgemm {
 
@@ -17,5 +18,42 @@ void llc_flush(std::vector<char>& llc);
 
 int fbgemm_get_num_threads();
 int fbgemm_get_thread_num();
+
+/**
+ * @params llc if not nullptr, flush llc
+ */
+template <class Fn>
+double measureWithWarmup(
+    Fn&& fn,
+    int warmupIterations,
+    int measuredIterations,
+    std::vector<char>* llc = nullptr) {
+  for (int i = 0; i < warmupIterations; ++i) {
+    if (llc) {
+      llc_flush(*llc);
+    }
+    fn();
+  }
+
+  double ttot = 0.0;
+  std::chrono::time_point<std::chrono::high_resolution_clock>
+      start = std::chrono::high_resolution_clock::now(),
+      end;
+
+  for (int i = 0; i < measuredIterations; ++i) {
+    if (llc) {
+      llc_flush(*llc);
+    }
+    start = std::chrono::high_resolution_clock::now();
+    fn();
+    end = std::chrono::high_resolution_clock::now();
+
+    auto dur =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    ttot += dur.count();
+  }
+
+  return ttot / 1e9 / measuredIterations;
+}
 
 } // namespace fbgemm
