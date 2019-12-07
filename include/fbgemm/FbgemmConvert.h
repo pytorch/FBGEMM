48c00442cc805a1fb0578cfc8a2c39645d3cb61c
Jianyu Huang <jianyuhuang@fb.com>
2019-12-07 01:40:08 -0800

    Migrate the FP16/BF16 <-> FP32 conversion routines into OSS (#203)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/203
    
    As Title says, we would like to move FP16/BF16 <-> FP32 conversion routines into OSS.
    
    Reviewed By: jspark1105
    
    Differential Revision: D18766898
    
    fbshipit-source-id: 2109327a9348947dfaae0dd13ea855d8b42cecc4

---
+++ include/fbgemm/FbgemmConvert.h
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#pragma once
+
+#include <stdexcept>
+#include "fbgemm/Types.h"
+#include "fbgemm/Utils.h"
+
+namespace fbgemm {
+
+typedef uint16_t bfloat16;
+
+/**
+ * @ Transform all entries in a matrix from fp32 to bfloat16: reference
+ * implementation.
+ *
+ */
+FBGEMM_API void FloatToBfloat16_ref(const float* src, bfloat16* dst, int size);
+
+/**
+ * @ Transform all entries in a matrix from bfloat16 to fp32: reference
+ * implementation.
+ *
+ */
+FBGEMM_API void Bfloat16ToFloat_ref(const bfloat16* src, float* dst, int size);
+
+/**
+ * @ Transform all entries in a matrix from fp32 to bfloat16: simd
+ * implementation.
+ *
+ */
+FBGEMM_API void FloatToBfloat16_simd(const float* src, bfloat16* dst, int size);
+
+/**
+ * @ Transform all entries in a matrix from bfloat16 to fp32: simd
+ * implementation.
+ *
+ */
+FBGEMM_API void Bfloat16ToFloat_simd(const bfloat16* src, float* dst, int size);
+
+/**
+ * @brief AVX2 implementation to convert fp32 numbers to bf16 numbers.
+ *
+ */
+void FloatToBfloat16_avx2(const float* src, bfloat16* dst, int size);
+
+/**
+ * @brief AVX512 implementation to convert fp32 numbers to bf16 numbers.
+ *
+ */
+void FloatToBfloat16_avx512(const float* src, bfloat16* dst, int size);
+
+/**
+ * @brief AVX2 implementation to convert bf16 numbers to fp32 numbers.
+ *
+ */
+void Bfloat16ToFloat_avx2(const bfloat16* src, float* dst, int size);
+
+/**
+ * @brief AVX512 implementation to convert bf16 numbers to fp32 numbers.
+ *
+ */
+void Bfloat16ToFloat_avx512(const bfloat16* src, float* dst, int size);
+
+/**
+ * @ Transform all entries in a matrix from fp32 to float16: reference
+ * implementation.
+ *
+ */
+FBGEMM_API void FloatToFloat16_ref(const float* src, float16* dst, int size);
+
+/**
+ * @ Transform all entries in a matrix from float16 to fp32: reference
+ * implementation.
+ *
+ */
+FBGEMM_API void Float16ToFloat_ref(const float16* src, float* dst, int size);
+
+/**
+ * @ Transform all entries in a matrix from fp32 to float16: simd
+ * implementation.
+ *
+ */
+FBGEMM_API void FloatToFloat16_simd(const float* src, float16* dst, int size);
+
+/**
+ * @ Transform all entries in a matrix from float16 to fp32: simd
+ * implementation.
+ *
+ */
+FBGEMM_API void Float16ToFloat_simd(const float16* src, float* dst, int size);
+
+/**
+ * @brief AVX2 implementation to convert fp32 numbers to fp16 numbers.
+ *
+ */
+void FloatToFloat16_avx2(const float* src, float16* dst, int size);
+
+/**
+ * @brief AVX512 implementation to convert fp32 numbers to fp16 numbers.
+ *
+ */
+void FloatToFloat16_avx512(const float* src, float16* dst, int size);
+
+/**
+ * @brief AVX2 implementation to convert fp16 numbers to fp32 numbers.
+ *
+ */
+void Float16ToFloat_avx2(const float16* src, float* dst, int size);
+
+/**
+ * @brief AVX512 implementation to convert fp16 numbers to fp32 numbers.
+ *
+ */
+void Float16ToFloat_avx512(const float16* src, float* dst, int size);
+
+}; // namespace fbgemm
