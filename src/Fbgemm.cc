0e2d4e93397b8bffc206bacdf147b3da9c90a15a
Daya Khudia <dskhudia@fb.com>
2019-12-11 17:26:56 -0800

    fbgemm API decorator only needed at declaration
    Summary: Title + MSVC errors out
    
    Reviewed By: jianyuh
    
    Differential Revision: D18937991
    
    fbshipit-source-id: eae1e065b774149ea1f12c04b69e317082e329bd

--- src/Fbgemm.cc
+++ src/Fbgemm.cc
@@ -204,7 +204,7 @@ void fbgemmPacked(
 }
 
 template <int SPATIAL_DIM>
-FBGEMM_API bool fbgemmOptimizedGConv(const conv_param_t<SPATIAL_DIM>& conv_p) {
+bool fbgemmOptimizedGConv(const conv_param_t<SPATIAL_DIM>& conv_p) {
   static_assert(SPATIAL_DIM >= 2, "Unsupported spatial dims");
   int C_per_G = conv_p.IC / conv_p.G;
   int K_per_G = conv_p.OC / conv_p.G;
