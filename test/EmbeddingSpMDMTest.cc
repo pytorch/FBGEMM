f9dcc41a0ac3706c5b731d1aa7d41bc8d936e30c
Jongsoo Park <jongsoo@fb.com>
2020-02-18 20:37:57 -0800

    clean up embedding spmdm tests (#300)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/300
    
    Refactor redundant code among various embedding spmdm tests. More careful testing of corner cases
    
    Reviewed By: jianyuh
    
    Differential Revision: D19943506
    
    fbshipit-source-id: 4eb8d55db7db729db65017c18c8ed0695c54516c

--- test/EmbeddingSpMDMTest.cc
+++ test/EmbeddingSpMDMTest.cc
@@ -12,6 +12,7 @@
 
 #include <gtest/gtest.h>
 
+#include "./EmbeddingSpMDMTestUtils.h"
 #include "fbgemm/Fbgemm.h"
 #include "fbgemm/FbgemmConvert.h"
 #include "fbgemm/Utils.h"
@@ -50,7 +51,8 @@ namespace {
 
 class EmbeddingSpMDMTest
     : public testing::TestWithParam<
-          tuple<bool, bool, bool, int, bool, bool, bool, bool>> {};
+          tuple<bool, bool, bool, int, bool, bool, EmbeddingSpMDMCornerCase>> {
+};
 }; // namespace
 
 vector<int> prefetch_distances = {0, 16, 1000000};
@@ -65,22 +67,24 @@ INSTANTIATE_TEST_CASE_P(
         ::testing::ValuesIn(prefetch_distances),
         ::testing::Bool(), // use_weight
         ::testing::Bool(), // normalize_by_lengths
-        ::testing::Bool(), // empty_indices
-        ::testing::Bool())); // out_of_bounds
+        ::testing::Values(
+            NONE,
+            EMPTY_INDICES,
+            OUT_OF_BOUND_INDICES,
+            UNMATCHED_NUM_INDICES_AND_LENGTHS_SUM)));
 
 TEST_P(EmbeddingSpMDMTest, basicTest) {
   vector<vector<int>> inputs(GetInputs_());
-  bool isFp16, isIndex64b, is_wt_positional, use_weight, normalize_by_lengths,
-      empty_indices, out_of_bounds;
+  bool isFp16, isIndex64b, is_wt_positional, use_weight, normalize_by_lengths;
   int prefetch;
+  EmbeddingSpMDMCornerCase corner_case;
   tie(isFp16,
       isIndex64b,
       is_wt_positional,
       prefetch,
       use_weight,
       normalize_by_lengths,
-      empty_indices,
-      out_of_bounds) = GetParam();
+      corner_case) = GetParam();
 
   for (auto input : inputs) {
     int batch_size = input[0];
@@ -104,50 +108,20 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
           embedding_table.size());
     }
 
-    // Generate lengths
-    uniform_int_distribution<int> length_distribution(
-        1, std::min(2 * average_len + 1, num_rows));
-    vector<int> lengths(batch_size);
-    for (int i = 0; i < batch_size; ++i) {
-      lengths[i] = empty_indices ? 0 : length_distribution(generator);
-    }
-
-    // Compute the number of indices
-    int lengths_sum = accumulate(lengths.begin(), lengths.end(), 0);
-    // cout << "lengths_sum " << lengths_sum << endl;
-
-    // Generate indices
+    vector<int> lengths;
     vector<int64_t> indices;
     vector<int32_t> indices_32;
-
-    // Generate indices
-    vector<int> container(num_rows);
-    for (int i = 0; i < batch_size; ++i) {
-      iota(container.begin(), container.end(), 0);
-      random_shuffle(container.begin(), container.end());
-      copy(
-          container.begin(),
-          container.begin() + lengths[i],
-          back_inserter(indices));
-    }
-    // use same indices for 32b and 64b
-    copy(begin(indices), end(indices), back_inserter(indices_32));
-    assert(indices.size() == lengths_sum);
-    assert(indices_32.size() == lengths_sum);
-    if (!empty_indices && out_of_bounds) {
-      int idx = uniform_int_distribution<int>(0, lengths_sum - 1)(generator);
-      indices_32[idx] = indices[idx] = num_rows;
-    }
-    if (!empty_indices) {
-      // To make sure to exercise out-of-bound cases
-      indices_32[0] = indices[0] = num_rows - 1;
-    }
-
-    // Generate weights
-    vector<float> weights(lengths_sum);
-    for (int i = 0; i < lengths_sum; ++i) {
-      weights[i] = embedding_distribution(generator);
-    }
+    vector<float> weights;
+    int lengths_sum = GenerateLengthsIndicesWeights(
+        lengths,
+        indices,
+        indices_32,
+        weights,
+        batch_size,
+        num_rows,
+        embedding_dim,
+        average_len,
+        corner_case);
 
     vector<float> output_sls_ref(batch_size * embedding_dim);
     vector<float> output_slws_ref(output_sls_ref.size()),
@@ -165,7 +139,7 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
             lengths_sum,
             num_rows,
             embedding_table_fp16.data(),
-            empty_indices ? nullptr : indices.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             normalize_by_lengths,
@@ -183,7 +157,7 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
             lengths_sum,
             num_rows,
             embedding_table_fp16.data(),
-            empty_indices ? nullptr : indices.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             output.data());
@@ -194,7 +168,7 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
             lengths_sum,
             num_rows,
             embedding_table.data(),
-            empty_indices ? nullptr : indices.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             normalize_by_lengths,
@@ -212,7 +186,7 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
             lengths_sum,
             num_rows,
             embedding_table.data(),
-            empty_indices ? nullptr : indices.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             output.data());
@@ -225,7 +199,7 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
             lengths_sum,
             num_rows,
             embedding_table_fp16.data(),
-            empty_indices ? nullptr : indices_32.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices_32.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             normalize_by_lengths,
@@ -243,7 +217,7 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
             lengths_sum,
             num_rows,
             embedding_table_fp16.data(),
-            empty_indices ? nullptr : indices_32.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices_32.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             output.data());
@@ -254,7 +228,7 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
             lengths_sum,
             num_rows,
             embedding_table.data(),
-            empty_indices ? nullptr : indices_32.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices_32.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             normalize_by_lengths,
@@ -272,7 +246,7 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
             lengths_sum,
             num_rows,
             embedding_table.data(),
-            empty_indices ? nullptr : indices_32.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices_32.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             output.data());
@@ -282,6 +256,10 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
     // Check correctness
     EXPECT_EQ(success, success_ref)
         << "Reference and JIT impl did not both succeed";
+    if (corner_case == OUT_OF_BOUND_INDICES ||
+        corner_case == UNMATCHED_NUM_INDICES_AND_LENGTHS_SUM) {
+      EXPECT_EQ(success, false);
+    }
     if (success) {
       for (int i = 0; i < output.size(); ++i) {
         EXPECT_EQ(output[i], output_ref[i])
@@ -294,17 +272,16 @@ TEST_P(EmbeddingSpMDMTest, basicTest) {
 
 TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
   vector<vector<int>> inputs(GetInputs_());
-  bool isFp16, isIndex64b, is_wt_positional, use_weight, normalize_by_lengths,
-      empty_indices, out_of_bounds;
+  bool isFp16, isIndex64b, is_wt_positional, use_weight, normalize_by_lengths;
   int prefetch;
+  EmbeddingSpMDMCornerCase corner_case;
   tie(isFp16,
       isIndex64b,
       is_wt_positional,
       prefetch,
       use_weight,
       normalize_by_lengths,
-      empty_indices,
-      out_of_bounds) = GetParam();
+      corner_case) = GetParam();
 
   constexpr float sparsity = 0.7;
 
@@ -314,8 +291,14 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
     int embedding_dim = input[2];
     int average_len = input[3];
 
+    // Create mapping table for rowwise sparsity
+    vector<int64_t> mapping_table;
+    vector<int32_t> mapping_table_32;
+    int num_compressed_rows = CreateMappingTableForRowWiseSparsity(
+        mapping_table, mapping_table_32, num_rows, sparsity);
+
     // Create embedding table
-    vector<float> embedding_table(num_rows * embedding_dim);
+    vector<float> embedding_table(num_compressed_rows * embedding_dim);
     default_random_engine generator;
     normal_distribution<float> embedding_distribution;
     for (int i = 0; i < embedding_table.size(); ++i) {
@@ -330,69 +313,20 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
           embedding_table.size());
     }
 
-    // Create mapping table for rowwise sparsity
-    vector<int64_t> mapping_table(num_rows);
-    bernoulli_distribution row_prune_dist(sparsity);
-    int num_compressed_rows = 0;
-    for (int i = 0; i < num_rows; ++i) {
-      if (row_prune_dist(generator)) {
-        // pruned
-        mapping_table[i] = -1;
-      } else {
-        mapping_table[i] = num_compressed_rows;
-        ++num_compressed_rows;
-      }
-    }
-    vector<int32_t> mapping_table_32;
-    copy(
-        mapping_table.begin(),
-        mapping_table.end(),
-        back_inserter(mapping_table_32));
-
-    // Generate lengths
-    uniform_int_distribution<int> length_distribution(
-        1, std::min(2 * average_len + 1, num_rows));
-    vector<int> lengths(batch_size);
-    for (int i = 0; i < batch_size; ++i) {
-      lengths[i] = empty_indices ? 0 : length_distribution(generator);
-    }
-
-    // Compute the number of indices
-    int lengths_sum = accumulate(lengths.begin(), lengths.end(), 0);
-    // cout << "lengths_sum " << lengths_sum << endl;
-
-    // Generate indices
+    vector<int> lengths;
     vector<int64_t> indices;
     vector<int32_t> indices_32;
-
-    // Generate indices
-    vector<int> container(num_rows);
-    for (int i = 0; i < batch_size; ++i) {
-      iota(container.begin(), container.end(), 0);
-      random_shuffle(container.begin(), container.end());
-      copy(
-          container.begin(),
-          container.begin() + lengths[i],
-          back_inserter(indices));
-    }
-    // use same indices for 32b and 64b
-    copy(begin(indices), end(indices), back_inserter(indices_32));
-    assert(indices.size() == lengths_sum);
-    assert(indices_32.size() == lengths_sum);
-    if (!empty_indices && out_of_bounds) {
-      int idx = uniform_int_distribution<int>(0, lengths_sum - 1)(generator);
-      indices_32[idx] = indices[idx] = num_rows;
-    }
-    if (!empty_indices) {
-      // To make sure to exercise out-of-bound cases
-      indices_32[0] = indices[0] = num_rows - 1;
-    }
-
-    // Generate weights
-    vector<float> weights(lengths_sum);
-    for (int i = 0; i < lengths_sum; ++i) {
-      weights[i] = embedding_distribution(generator);
-    }
+    vector<float> weights;
+    int lengths_sum = GenerateLengthsIndicesWeights(
+        lengths,
+        indices,
+        indices_32,
+        weights,
+        batch_size,
+        num_rows,
+        embedding_dim,
+        average_len,
+        corner_case);
 
     vector<float> output_sls_ref(batch_size * embedding_dim);
     vector<float> output_slws_ref(output_sls_ref.size()),
@@ -410,7 +344,7 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
             lengths_sum,
             num_rows,
             embedding_table_fp16.data(),
-            empty_indices ? nullptr : indices.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices.data(),
             mapping_table.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
@@ -429,7 +363,7 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
             lengths_sum,
             num_rows,
             embedding_table_fp16.data(),
-            empty_indices ? nullptr : indices.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             output.data(),
@@ -441,7 +375,7 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
             lengths_sum,
             num_rows,
             embedding_table.data(),
-            empty_indices ? nullptr : indices.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices.data(),
             mapping_table.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
@@ -460,7 +394,7 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
             lengths_sum,
             num_rows,
             embedding_table.data(),
-            empty_indices ? nullptr : indices.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             output.data(),
@@ -474,7 +408,7 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
             lengths_sum,
             num_rows,
             embedding_table_fp16.data(),
-            empty_indices ? nullptr : indices_32.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices_32.data(),
             mapping_table_32.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
@@ -493,7 +427,7 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
             lengths_sum,
             num_rows,
             embedding_table_fp16.data(),
-            empty_indices ? nullptr : indices_32.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices_32.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             output.data(),
@@ -505,7 +439,7 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
             lengths_sum,
             num_rows,
             embedding_table.data(),
-            empty_indices ? nullptr : indices_32.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices_32.data(),
             mapping_table_32.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
@@ -524,7 +458,7 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
             lengths_sum,
             num_rows,
             embedding_table.data(),
-            empty_indices ? nullptr : indices_32.data(),
+            corner_case == EMPTY_INDICES ? nullptr : indices_32.data(),
             lengths.data(),
             use_weight ? weights.data() : nullptr,
             output.data(),
@@ -535,6 +469,10 @@ TEST_P(EmbeddingSpMDMTest, rowwiseSparseTest) {
     // Check correctness
     EXPECT_EQ(success, success_ref)
         << "Reference and JIT impl did not both succeed";
+    if (corner_case == OUT_OF_BOUND_INDICES ||
+        corner_case == UNMATCHED_NUM_INDICES_AND_LENGTHS_SUM) {
+      EXPECT_EQ(success, false);
+    }
     if (success) {
       for (int i = 0; i < output.size(); ++i) {
         EXPECT_EQ(output[i], output_ref[i])
