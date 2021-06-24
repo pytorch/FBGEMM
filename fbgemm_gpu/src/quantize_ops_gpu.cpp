63d29b2f4545ec9d2034a07a642ef98b304fa3a2
Rick Weyrauch <weyrauch@fb.com>
2021-06-24 08:39:05 -0700

    Move FusedNBitRowwiseQuantizedSBHalfToFloat and FloatToFusedNBitRowwiseQuantizedSBHalf to FBGEMM_GPU
    Summary: Move the FusedNBit operators from PyTorch internal ops into FBGEMM_GPU directory to open source.
    
    Differential Revision: D29332354
    
    fbshipit-source-id: 38824f62e369c20bca0f25139ff2eac770598f72

--- fbgemm_gpu/src/quantize_ops_gpu.cpp
+++ fbgemm_gpu/src/quantize_ops_gpu.cpp
@@ -16,4 +16,8 @@ TORCH_LIBRARY_IMPL(fb, CUDA, m) {
       "FloatToFused8BitRowwiseQuantized", _float_to_fused8bitrowwise_gpu);
   DISPATCH_TO_CUDA(
       "Fused8BitRowwiseQuantizedToFloat", _fused8bitrowwise_to_float_gpu);
+  DISPATCH_TO_CUDA(
+      "FloatToFusedNBitRowwiseQuantizedSBHalf", _float_to_fusednbitrowwise_gpu);
+  DISPATCH_TO_CUDA(
+      "FusedNBitRowwiseQuantizedSBHalfToFloat", _fusednbitrowwise_to_float_gpu);
 }
