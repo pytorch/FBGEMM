5dc753c0d2599a4c09b986d0298da6af440e7ce1
Jianyu Huang <jianyuhuang@fb.com>
2021-06-30 23:43:54 -0700

    Add quantize workflow for inference table batched embeddings (#641)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/641
    
    Add the conversion workflow from FP32 trained embedding tables to FP16/INT8/INT4/INT2 quantized embedding tables during GPU inference.
    - Convert FP32 -> FP16/INT8/INT4/INT2 weights and replace the FP32 op with IntN op.
    
    TODO:
    - ~~move "FusedNBitRowwiseQuantizedSBHalfToFloat" to fbgemm_gpu;~~
    - quantize according the inference per-feature precisions (The current Diff only quantize all tables to FP16 or INT8 or INT4 or INT2);
    - ~~CPU supports/tests.~~
    
    Reviewed By: yinghai
    
    Differential Revision: D29252193
    
    fbshipit-source-id: 984617875d619eb37fb317c78e82e0a744b32390

--- fbgemm_gpu/test/split_table_batched_embeddings_test.py
+++ fbgemm_gpu/test/split_table_batched_embeddings_test.py
@@ -1608,8 +1608,7 @@ class SplitTableBatchedEmbeddingsTest(unittest.TestCase):
                 SparseType.FP16,
             ]
         ),
-
-        cpu=st.booleans(),
+        cpu=st.booleans() if torch.cuda.is_available() else st.just(True),
     )
     @settings(verbosity=Verbosity.verbose, max_examples=MAX_EXAMPLES, deadline=None)
     def test_nbit_forward(
@@ -1622,8 +1621,8 @@ class SplitTableBatchedEmbeddingsTest(unittest.TestCase):
         weighted: bool,
         mixed: bool,
         pooling_mode: split_table_batched_embeddings_ops.PoolingMode,
-        cpu: bool,
         weights_ty: SparseType,
+        cpu: bool,
     ) -> None:
         assume(
             pooling_mode == split_table_batched_embeddings_ops.PoolingMode.SUM
@@ -1726,7 +1725,7 @@ class SplitTableBatchedEmbeddingsTest(unittest.TestCase):
                 comps = comps.reshape(E, D)
                 bs[t].weight.detach().copy_(torch.tensor(comps).cuda())
 
-            if weights_ty == SparseType.INT2:
+            elif weights_ty == SparseType.INT2:
                 (E, D_4) = np_weights.shape
                 D = D_4 * 4
 
@@ -1749,7 +1748,7 @@ class SplitTableBatchedEmbeddingsTest(unittest.TestCase):
                 bs[t].weight.detach().copy_(torch.tensor(comps).cuda())
 
 
-            if weights_ty == SparseType.INT8:
+            elif weights_ty == SparseType.INT8:
                 (E, D) = np_weights.shape
                 comps = np_weights.astype(np.float32) * scale_shift[:, 0].reshape(
                         -1, 1
@@ -1758,7 +1757,7 @@ class SplitTableBatchedEmbeddingsTest(unittest.TestCase):
                 )
                 bs[t].weight.detach().copy_(torch.tensor(comps).cuda())
 
-            if weights_ty == SparseType.FP16:
+            elif weights_ty == SparseType.FP16:
                 comps = bs[t].weight.detach().half().cpu().numpy().view(np.uint8)
                 weights.copy_(torch.tensor(comps))
 
