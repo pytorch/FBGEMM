e922280540acf7920ea21f99b8db064f89bb8c11
Jongsoo Park <jongsoo@fb.com>
2021-08-27 21:40:59 -0700

    add copyright header to embedding_forward_quantized_split_template.cu (#674)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/674
    
    As title
    
    Reviewed By: jianyuh
    
    Differential Revision: D30560690
    
    fbshipit-source-id: 0df17efa7142525dedf13237409accd7b9b3333d

--- fbgemm_gpu/codegen/embedding_forward_quantized_split_template.cu
+++ fbgemm_gpu/codegen/embedding_forward_quantized_split_template.cu
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 {% set wdesc =  "weighted" if weighted else "unweighted" %}
 #include "codegen/embedding_forward_template_helpers.cuh"
 
