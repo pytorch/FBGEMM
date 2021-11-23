64d01d06193e510d21f914d4d4e57b02ac08c843
Rick Weyrauch <weyrauch@fb.com>
2021-11-23 11:56:33 -0800

    Tidy up pytorch/FBGEMM copyright headers. (#765)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/765
    
    Resolve failing OSS requirements tests.
    
    Reviewed By: jspark1105
    
    Differential Revision: D32565418
    
    fbshipit-source-id: 9a5e52496cba142567a0f309b0798772f4d963ca

--- fbgemm_gpu/src/permute_pooled_embedding_ops.cu
+++ fbgemm_gpu/src/permute_pooled_embedding_ops.cu
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 #include <ATen/AccumulateType.h>
 #include <ATen/cuda/CUDAContext.h>
 #include <ATen/cuda/Exceptions.h>
