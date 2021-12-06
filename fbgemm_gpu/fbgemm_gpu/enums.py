b9a49acf2baf281b9d02101d67cf3daf4370b399
Stephan Uphoff <ups@fb.com>
2021-12-06 15:36:34 -0800

    FBGEMM Add uvm memadvise, move uvm testing to separate file, create enum registry
    Summary:
    Restructured UVM Storage to always use indirection. This unifies handling of newly allocated Tensor with Tensors generated through uvm_to_cpu() or uvm_to_device().
    
    Added the following ops:
    
    uvm_storage - Test if Storage uses UVM (Like is_uvm_tensor but also returns true for Tensors generated through uvm_to_cpu ).
    
    new_vanilla_managed_tensor - Allocate new Tensor with UVM storage
    (Like new_managed_tensor - but only allocates the storage and does not set residency or access hints. new_managed_tensor could be implemented by new_vanilla_managed_tensor followed by two calls to cuda_mem_advise)
    
    uvm_to_device - Generates new Tensor sharing existing UVM space on specified devices.
    
    cuda_mem_advise - Calls cudaMemAdvise on UVM Storage. The hint enum is generated in python (fbgemm,uvm) using data returned from C++ op.
    
    cudaMemPrefetchAsync:  Calls cudaMemPrefetchAsync on UVM Storage
    
    uvm_mem_advice_dont_fork : calls madvise(..MADV_DONTFORK) on the UVM storage. This is a workaround for an issue where the UVM kernel driver unmaps UVM storage pages from the page table on fork - causing slowdown on the next access from a CPU.
    
    Note:
    The enum framework used for cuda_mem_advise is generic and templated and can be used outside of UVM.
    
    Reviewed By: jianyuh
    
    Differential Revision: D32655606
    
    fbshipit-source-id: b5fd888137f93c7266c60b97ac8d9bb010ad832d

---
+++ fbgemm_gpu/fbgemm_gpu/enums.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import enum
+import typing
+from typing import Any, Callable, List, Tuple
+
+# Create enums in given namespace with information from query_op
+def create_enums(
+    namespace: typing.Dict[str, Any],
+    query_op: Callable[[], List[Tuple[str, List[Tuple[str, int]]]]],
+) -> None:
+    for enum_name, items in query_op():
+        # Create matching python enumeration
+        # pyre-ignore[19]
+        new_enum = enum.Enum(enum_name, items)
+        # and store it in the module
+        namespace[enum_name] = new_enum
--- fbgemm_gpu/src/cumem_utils.cu
+++ fbgemm_gpu/src/cumem_utils.cu
@@ -8,10 +8,15 @@
 #include <ATen/cuda/Exceptions.h>
 #include <c10/cuda/CUDAGuard.h>
 
+#include <sys/mman.h>
+
+#include "cumem_utils.h"
+#include "fbgemm_gpu/enum_utils.h"
 #include "fbgemm_gpu/fbgemm_cuda_utils.cuh"
 
 using namespace at;
-using namespace fbgemm_gpu;
+
+namespace fbgemm_gpu {
 
 // Freeing host/uvm memory with cudaFree[Host] requires a cuda context.
 // If a uvm tensor is released from an arbitrary thread without a context
@@ -34,7 +39,7 @@ struct CUDAManagedContext {
   ~CUDAManagedContext() {
     at::cuda::OptionalCUDAGuard device_guard;
     device_guard.set_index(cuda_device_);
-    AT_CUDA_CHECK(cudaFree(ptr_));
+    cudaFree(ptr_);
   }
 
   static void release(void* ptr) {
@@ -44,13 +49,13 @@ struct CUDAManagedContext {
 
 // Keep a reference to the UVM memory allocation from the associated
 // CPU Tensor to prevent lifetime issues (use after free)
-struct CUDAManagedCpuContext {
+struct CUDAManagedIndirectContext {
   Storage storage_;
 
-  CUDAManagedCpuContext(Storage storage) : storage_(std::move(storage)){};
+  CUDAManagedIndirectContext(Storage storage) : storage_(std::move(storage)){};
 
   static void release(void* ptr) {
-    delete static_cast<CUDAManagedCpuContext*>(ptr);
+    delete static_cast<CUDAManagedIndirectContext*>(ptr);
   }
 };
 
@@ -64,10 +69,11 @@ std::vector<int64_t> defaultStrides(IntArrayRef sizes) {
   }
   return strides;
 }
-} // namespace
 
 // Allocate the ATen Tensor with unified managed memory (UVM)
-Tensor new_managed_tensor(Tensor self, std::vector<std::int64_t> sizes) {
+Tensor new_managed_tensor_internal(
+    Tensor self,
+    std::vector<std::int64_t>& sizes) {
   at::cuda::OptionalCUDAGuard device_guard;
   device_guard.set_index(self.get_device());
 
@@ -76,16 +82,16 @@ Tensor new_managed_tensor(Tensor self, std::vector<std::int64_t> sizes) {
       at::detail::computeStorageNbytes(sizes, strides, self.dtype().itemsize());
   void* ptr;
   AT_CUDA_CHECK(cudaMallocManaged(&ptr, size_bytes));
-  // User hints with "preferred location": Here the kernel will page fault
-  // and generate direct mapping to data on the CPU.
-  AT_CUDA_CHECK(cudaMemAdvise(
-      ptr, size_bytes, cudaMemAdviseSetPreferredLocation, cudaCpuDeviceId));
-  // User hints with "accessed by": GPU will establish direct mapping of data
-  // in CPU memory, no page faults will be generated
-  AT_CUDA_CHECK(cudaMemAdvise(
-      ptr, size_bytes, cudaMemAdviseSetAccessedBy, at::cuda::current_device()));
-  C10_CUDA_KERNEL_LAUNCH_CHECK();
-  auto storage = Storage(
+
+  // The memory allocated above can be accessed from CUDA and CPU
+  // However Storage requires a specific device and we need to retain the cuda
+  // device for releasing the memory (see "Freeing host/uvm memory with cudaFree
+  // .." above. To access the memory from devices other then the one used for
+  // allocation we need a new Storage object (with the new device) referring to
+  // the original storage object. We force this indirection even for newly
+  // allocated Tensors for code unification.
+
+  auto real_storage = Storage(
       Storage::use_byte_size_t(),
       size_bytes,
       at::DataPtr(
@@ -95,32 +101,223 @@ Tensor new_managed_tensor(Tensor self, std::vector<std::int64_t> sizes) {
           {at::DeviceType::CUDA, self.device().index()}),
       nullptr, /* allocator */
       /*resizable=*/false);
-  return at::empty({0}, self.options()).set_(storage, 0, sizes, strides);
+
+  auto indirect_storage = Storage(
+      Storage::use_byte_size_t(),
+      size_bytes,
+      at::DataPtr(
+          ptr,
+          new CUDAManagedIndirectContext(real_storage),
+          &CUDAManagedIndirectContext::release,
+          {at::DeviceType::CUDA, self.device().index()}),
+      nullptr, /* allocator */
+      /*resizable=*/false);
+
+  return at::empty({0}, self.options())
+      .set_(indirect_storage, 0, sizes, strides);
+}
+} // namespace
+
+// Allocate a cuda Tensor with unified managed memory (UVM)
+// Then set the preferred data location to CPU (host memory)
+// And establish mappings on the cuda device to the host memory
+Tensor new_managed_tensor(Tensor self, std::vector<std::int64_t> sizes) {
+  at::cuda::OptionalCUDAGuard device_guard;
+  device_guard.set_index(self.get_device());
+
+  Tensor t = new_managed_tensor_internal(self, sizes);
+
+  void* ptr = t.data_ptr();
+  size_t size_bytes = t.storage().nbytes();
+
+  // Set preferred memory location to host memory
+  AT_CUDA_CHECK(cudaMemAdvise(
+      ptr, size_bytes, cudaMemAdviseSetPreferredLocation, cudaCpuDeviceId));
+  // User hints with "accessed by": GPU will establish direct mapping of data
+  // in CPU memory, no page faults will be generated
+  AT_CUDA_CHECK(cudaMemAdvise(
+      ptr, size_bytes, cudaMemAdviseSetAccessedBy, at::cuda::current_device()));
+  C10_CUDA_KERNEL_LAUNCH_CHECK();
+
+  return t;
 }
 
-// Check if a tensor is allocated with UVM or host-mapped memory
+// Allocate a cuda Tensor with unified managed memory (UVM) without the
+// additional steps taked by new_managed_tensor above
+Tensor new_vanilla_managed_tensor(
+    Tensor self,
+    std::vector<std::int64_t> sizes) {
+  at::cuda::OptionalCUDAGuard device_guard;
+  device_guard.set_index(self.get_device());
+
+  return new_managed_tensor_internal(self, sizes);
+}
+
+// Check if a tensor is allocated with UVM (CPU or GPU Tensor)
+bool uvm_storage(Tensor t) {
+  auto deleter = t.storage().data_ptr().get_deleter();
+  return deleter == &CUDAManagedIndirectContext::release;
+}
+
+// Check if a tensor is allocated with UVM but is not a CPU Tensor
 bool is_uvm_tensor(Tensor t) {
   if (t.device().is_cpu()) {
     return false;
   }
-  auto deleter = t.storage().data_ptr().get_deleter();
-  return deleter == &CUDAManagedContext::release;
+  return uvm_storage(t);
 }
 
 // Convert a UVM tensor to a CPU tensor
 Tensor uvm_to_cpu(Tensor t) {
   TORCH_CHECK(is_uvm_tensor(t));
   // Don't copy the storage - just keep a reference to the original storage
+  auto* tcontext =
+      t.storage().data_ptr().cast_context<CUDAManagedIndirectContext>(
+          &CUDAManagedIndirectContext::release);
+  TORCH_CHECK(tcontext != nullptr)
   auto storage = Storage(
       Storage::use_byte_size_t(),
       t.storage().nbytes(),
       at::DataPtr(
           t.data_ptr(),
-          new CUDAManagedCpuContext(t.storage()),
-          &CUDAManagedCpuContext::release,
+          new CUDAManagedIndirectContext(tcontext->storage_),
+          &CUDAManagedIndirectContext::release,
           {at::DeviceType::CPU}),
       nullptr, /* allocator */
       /*resizable=*/false);
   return at::empty({0}, t.options().device(Device::Type::CPU))
       .set_(std::move(storage), 0, t.sizes(), t.strides());
 }
+
+// Create a new UVM tensor sharing storage with t on the same device as
+// prototype
+Tensor uvm_to_device(Tensor t, Tensor prototype) {
+  TORCH_CHECK(is_uvm_tensor(t));
+  // Don't copy the storage - just keep a reference to the original storage
+  auto* tcontext =
+      t.storage().data_ptr().cast_context<CUDAManagedIndirectContext>(
+          &CUDAManagedIndirectContext::release);
+  TORCH_CHECK(tcontext != nullptr)
+
+  auto device = prototype.device();
+  auto storage = Storage(
+      Storage::use_byte_size_t(),
+      t.storage().nbytes(),
+      at::DataPtr(
+          t.data_ptr(),
+          new CUDAManagedIndirectContext(tcontext->storage_),
+          &CUDAManagedIndirectContext::release,
+          device),
+      nullptr, /* allocator */
+      /*resizable=*/false);
+  return at::empty({0}, t.options().device(device))
+      .set_(std::move(storage), 0, t.sizes(), t.strides());
+}
+
+namespace {
+int64_t uvm_get_guard_index(Tensor& t) {
+  TORCH_CHECK(uvm_storage(t));
+  int cuda_device_index;
+  if (t.is_cpu()) {
+    auto* tcontext =
+        t.storage().data_ptr().cast_context<CUDAManagedIndirectContext>(
+            &CUDAManagedIndirectContext::release);
+    TORCH_CHECK(tcontext != nullptr)
+    auto* ocontext =
+        tcontext->storage_.data_ptr().cast_context<CUDAManagedContext>(
+            &CUDAManagedContext::release);
+    TORCH_CHECK(ocontext != nullptr)
+    cuda_device_index = static_cast<int64_t>(ocontext->cuda_device_);
+  } else {
+    TORCH_CHECK(t.is_cuda());
+    cuda_device_index = t.get_device();
+  }
+  return cuda_device_index;
+}
+
+} // namespace
+
+void uvm_cuda_mem_advise(Tensor t, int64_t cudaMemoryAdvise) {
+  // Call cudaMemAdvise on vm tensor
+  // See cudaMemoryAdvise enum (automatically exported to python fbgemm_gpu.uvm namespace)
+  // for valid values and interface stub.
+  at::cuda::OptionalCUDAGuard device_guard;
+  int64_t cuda_device_index = uvm_get_guard_index(t);
+  int hint_device;
+  if (t.is_cpu()) {
+    hint_device = cudaCpuDeviceId;
+  } else {
+    TORCH_CHECK(t.is_cuda());
+    hint_device = static_cast<int>(cuda_device_index);
+  }
+
+  void* ptr = t.data_ptr();
+  size_t size_bytes = at::detail::computeStorageNbytes(
+      t.sizes(), t.strides(), t.dtype().itemsize());
+
+  device_guard.set_index(cuda_device_index);
+
+  AT_CUDA_CHECK(cudaMemAdvise(
+      ptr,
+      size_bytes,
+      static_cast<enum cudaMemoryAdvise>(cudaMemoryAdvise),
+      hint_device));
+  return;
+}
+
+void uvm_cuda_mem_prefetch_async(Tensor t, c10::optional<Tensor> device_t) {
+  // Call cudaMemPrefetchAsync on Tensor
+  at::cuda::OptionalCUDAGuard device_guard;
+  TORCH_CHECK(uvm_storage(t));
+  TORCH_CHECK(t.is_cuda() || (t.is_cpu() && device_t.has_value()));
+  TORCH_CHECK(!device_t.has_value() || device_t.value().is_cuda());
+
+  int prefetch_device =
+      (t.is_cpu()) ? cudaCpuDeviceId : static_cast<int>(t.get_device());
+
+  Tensor& context_t = device_t.has_value() ? device_t.value() : t;
+
+  void* ptr = t.data_ptr();
+  size_t size_bytes = at::detail::computeStorageNbytes(
+      t.sizes(), t.strides(), t.dtype().itemsize());
+
+  device_guard.set_index(context_t.get_device());
+
+  auto stream = at::cuda::getCurrentCUDAStream();
+
+  AT_CUDA_CHECK(cudaMemPrefetchAsync(ptr, size_bytes, prefetch_device, stream));
+
+  return;
+}
+
+void uvm_mem_advice_dont_fork(Tensor t) {
+  // During fork() the uvm driver is called to copy VMA for UVM space.
+  // The uvm driver then removes pmap entries for both child and parent.
+  // Re-establishing the mappings for the paretn is slow.
+  // This works around the issue by setting the UVM VMA to not be copied
+  // into the child.
+  TORCH_CHECK(uvm_storage(t));
+
+  void* ptr = t.data_ptr();
+  size_t size_bytes = at::detail::computeStorageNbytes(
+      t.sizes(), t.strides(), t.dtype().itemsize());
+
+  int result = madvise(ptr, size_bytes, MADV_DONTFORK);
+
+  TORCH_CHECK(result == 0)
+
+  return;
+}
+
+FBGEMM_GPU_ENUM_GLOGAL(uvm)
+
+FBGEMM_GPU_ENUM_REGISTER_START(uvm, cudaMemoryAdvise){
+    FBGEMM_GPU_ENUM_ITEM(cudaMemAdviseSetReadMostly),
+    FBGEMM_GPU_ENUM_ITEM(cudaMemAdviseUnsetReadMostly),
+    FBGEMM_GPU_ENUM_ITEM(cudaMemAdviseSetPreferredLocation),
+    FBGEMM_GPU_ENUM_ITEM(cudaMemAdviseUnsetPreferredLocation),
+    FBGEMM_GPU_ENUM_ITEM(cudaMemAdviseSetAccessedBy),
+    FBGEMM_GPU_ENUM_ITEM(cudaMemAdviseUnsetAccessedBy),
+} FBGEMM_GPU_ENUM_REGISTER_END
+
+} // namespace fbgemm_gpu
