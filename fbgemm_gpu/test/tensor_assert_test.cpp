a221ff89e7e1dcc87fa2446ce3eee7435c72fa61
Jongsoo Park <jongsoo@fb.com>
2021-12-24 13:55:35 -0800

    fix copyright header of tensor_assert_test.cpp (#831)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/831
    
    As title
    
    Reviewed By: rweyrauch
    
    Differential Revision: D33310866
    
    fbshipit-source-id: 1cbdee1d7c00f0e900faac570bac330866887b1c

--- fbgemm_gpu/test/tensor_assert_test.cpp
+++ fbgemm_gpu/test/tensor_assert_test.cpp
@@ -1,4 +1,10 @@
-// (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) Intel Corporation.
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 #include <ATen/ATen.h>
 #include <gtest/gtest.h>
