490ff3e691fc41dd97fd6698974ebae66fbe0401
Jongsoo Park <jongsoo@fb.com>
2022-01-02 15:29:18 -0800

    increase parallelism in batched unary embeddings backward (#842)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/842
    
    Sort indices and have each thread handle indices with the same values (called a run in the code)
    
    Reviewed By: jianyuh
    
    Differential Revision: D33381126
    
    fbshipit-source-id: aec1c0be619b9072f5a1f9273b66c03e5106ca02

--- fbgemm_gpu/src/sparse_ops.cu
+++ fbgemm_gpu/src/sparse_ops.cu
@@ -4,7 +4,6 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-#include "fbgemm_gpu/batched_unary_embedding_ops.cuh"
 #include "fbgemm_gpu/sparse_ops.cuh"
 #include "fbgemm_gpu/sparse_ops.h"
 #include "fbgemm_gpu/sparse_ops_utils.h"
@@ -23,7 +22,9 @@
 #include "fbgemm_gpu/cub_namespace_postfix.cuh"
 // clang-format on
 
+#include "fbgemm_gpu/embedding_backward_template_helpers.cuh"
 #include "fbgemm_gpu/fbgemm_cuda_utils.cuh"
+#include "fbgemm_gpu/split_embeddings_utils.cuh"
 
 using Tensor = at::Tensor;
 
@@ -932,6 +933,38 @@ Tensor reorder_batched_ad_indices_gpu(
   return reordered_cat_ad_indices;
 }
 
+// Forward kernel for batched unary embedding op
+template <typename scalar_t, typename index_t>
+__global__ void batched_unary_embeddings_forward_kernel(
+    const int32_t N,
+    const int32_t B,
+    const int32_t T,
+    const scalar_t* __restrict__ weight, // N * sum(E) * 1 (embedding dimension
+                                         // is 1)
+    const index_t* __restrict__ table_offsets,
+    const index_t* __restrict__ offsets,
+    const index_t* __restrict__ indices,
+    scalar_t* __restrict__ output // N * B * T
+) {
+  index_t sum_E = table_offsets[T];
+  int32_t b = blockIdx.x * blockDim.x + threadIdx.x;
+  if (b >= B) {
+    return;
+  }
+  int32_t t = blockIdx.y;
+  int32_t n = blockIdx.z;
+  index_t table_offset = table_offsets[t];
+  index_t indices_start = offsets[t * B + b];
+  index_t indices_end = offsets[t * B + b + 1];
+  int32_t L = indices_end - indices_start;
+  at::acc_type<scalar_t, true> sum = 0.0;
+  for (int32_t l = 0; l < L; ++l) {
+    auto idx = __ldg(&indices[indices_start + l]);
+    sum += weight[n * sum_E + table_offset + idx + 0];
+  }
+  output[(n * B + b) * T + t] = sum;
+}
+
 Tensor batched_unary_embeddings_forward_cuda(
     const Tensor& weight,
     const Tensor& table_offsets,
@@ -976,6 +1009,74 @@ Tensor batched_unary_embeddings_forward_cuda(
   return output;
 }
 
+// Backward kernel for batched unary embedding op
+// We sort input indices so we don't have race conditions, an approach similar
+// to the usual split table batched embedding backward.
+// We can think of the following alternatives but each with challenges:
+// 1) Assign output elements to different threads. Each thread scan all indices
+//    corresponding to the table it owns but only accumulate gradients when an
+//    index value matches with the output element it owns.
+//    A challenge is each thread need to binary search to map from [0 .. sum_E]
+//    to table id.
+// 2) Densify indices and offsets to create [B, sum_E] matrix. Then, do batched
+//    GEMM where ith GEMM multiplies [N, B] submatrix of grad_output with
+//    [B, E_i] submatrix where E_i is the num of embeddings of ith table.
+//    Concatenating the GEMM outputs will result in [N, B, T]
+//    A challenge is there's no available batched GEMM routine with varying K
+//    dimension.
+template <typename scalar_t, typename index_t>
+__global__ void batched_unary_embeddings_backward_kernel(
+    const int32_t N,
+    const int32_t B,
+    const int32_t T,
+    const scalar_t* __restrict__ grad_output, // [N * B * T]
+    const index_t* __restrict__ table_offsets,
+    scalar_t* __restrict__ grad_weight, // [N * sum_E * 1] (embedding
+                                        // dimension is 1)
+    const at::PackedTensorAccessor32<index_t, 1, at::RestrictPtrTraits>
+        sorted_linear_indices_run,
+    const int32_t* __restrict__ sorted_linear_indices_cumulative_run_lengths,
+    const int32_t* __restrict__ sorted_infos,
+    const int32_t* __restrict__ sorted_linear_indices_num_runs,
+    FixedDivisor fd) {
+  int32_t run_id = blockIdx.x * blockDim.x + threadIdx.x;
+  int32_t n = blockIdx.y;
+  if (n >= N) {
+    return;
+  }
+  if (run_id >= sorted_linear_indices_run.size(0)) {
+    return;
+  }
+  if (run_id >= sorted_linear_indices_num_runs[0]) {
+    return;
+  }
+  int64_t linear_index = sorted_linear_indices_run[run_id];
+  int32_t segment_start = sorted_linear_indices_cumulative_run_lengths[run_id];
+  int32_t segment_end =
+      sorted_linear_indices_cumulative_run_lengths[run_id + 1];
+  int32_t SL = segment_end - segment_start;
+
+  if (SL == 0) {
+    return;
+  }
+
+  // now, each segment corresponds to exactly one table `t` and row in
+  // that table (`idx`). Thus, we can hoist out some of the book-keeping.
+  auto info = sorted_infos[segment_start];
+  int t = fd.Div(info);
+
+  at::acc_type<scalar_t, true> grad_sum = 0.0;
+  for (int32_t sl = 0; sl < SL; ++sl) {
+    int32_t b = fd.Mod(sorted_infos[segment_start + sl]);
+    grad_sum += grad_output[(n * B + b) * T + t];
+  }
+
+  index_t table_offset = table_offsets[t];
+  index_t sum_E = table_offsets[T];
+  int64_t idx = linear_index - table_offset;
+  grad_weight[n * sum_E + table_offset + idx] = grad_sum;
+}
+
 Tensor batched_unary_embeddings_backward_cuda(
     const Tensor& grad_output,
     const Tensor& weight,
@@ -998,8 +1099,30 @@ Tensor batched_unary_embeddings_backward_cuda(
   TORCH_CHECK(N > 0);
   TORCH_CHECK(B > 0);
   TORCH_CHECK(T > 0);
-  int threads = std::min<int32_t>(N * T, 512);
-  dim3 blocks(cuda_calc_xblock_count(N * T, threads));
+
+  // weight: [N, sum_E]
+  // total_hash_size_bits = log2(sum_E)
+  int64_t total_hash_size_bits = log2(weight.numel() / N) + 1;
+
+  Tensor linear_indices, linear_indices_sorted;
+  Tensor infos_sorted;
+  Tensor sorted_linear_indices_run, sorted_linear_indices_run_lengths,
+      sorted_linear_indices_num_runs,
+      sorted_linear_indices_cumulative_run_lengths;
+  std::tie(
+      linear_indices,
+      linear_indices_sorted,
+      infos_sorted,
+      sorted_linear_indices_run,
+      sorted_linear_indices_run_lengths,
+      sorted_linear_indices_num_runs,
+      sorted_linear_indices_cumulative_run_lengths) =
+      transpose_embedding_input(
+          table_offsets, total_hash_size_bits, indices, offsets);
+
+  int threads = std::min<int32_t>(sorted_linear_indices_run.numel(), 512);
+  dim3 blocks(
+      cuda_calc_xblock_count(sorted_linear_indices_run.numel(), threads), N);
   auto grad_weight = at::zeros_like(weight);
   AT_DISPATCH_INDEX_TYPES(
       indices.type(), "batched_unary_embeddings_backward_kernel", ([&] {
@@ -1014,9 +1137,16 @@ Tensor batched_unary_embeddings_backward_cuda(
                       T,
                       grad_output.data_ptr<scalar_t>(),
                       table_offsets.data_ptr<index_t>(),
-                      offsets.data_ptr<index_t>(),
-                      indices.data_ptr<index_t>(),
-                      grad_weight.data_ptr<scalar_t>());
+                      grad_weight.data_ptr<scalar_t>(),
+                      sorted_linear_indices_run.packed_accessor32<
+                          index_t,
+                          1,
+                          at::RestrictPtrTraits>(),
+                      sorted_linear_indices_cumulative_run_lengths
+                          .data_ptr<int32_t>(),
+                      infos_sorted.data_ptr<int32_t>(),
+                      sorted_linear_indices_num_runs.data_ptr<int32_t>(),
+                      FixedDivisor(B));
               C10_CUDA_KERNEL_LAUNCH_CHECK();
             }));
       }));
