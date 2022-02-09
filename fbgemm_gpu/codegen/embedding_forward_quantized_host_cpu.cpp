08ffd63254e5986f9b64b51302ea977d426162e2
Jianyu Huang <jianyuhuang@fb.com>
2022-02-09 05:32:12 -0800

    Fix TorchRec cpu only unit tests (#921)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/921
    
    Continue the fix for int_nbit_split_embedding_codegen_lookup_function op.
    
    Reviewed By: colin2328
    
    Differential Revision: D34099495
    
    fbshipit-source-id: 8cfa18e2556ce5393a8893a8e36002f29ae1ff83

--- fbgemm_gpu/codegen/embedding_forward_quantized_host_cpu.cpp
+++ fbgemm_gpu/codegen/embedding_forward_quantized_host_cpu.cpp
@@ -120,6 +120,8 @@ Tensor pruned_array_lookup_cpu(
     Tensor index_remappings_offsets);
 
 TORCH_LIBRARY_FRAGMENT(fb, m) {
+  m.def(
+      "int_nbit_split_embedding_codegen_lookup_function(Tensor dev_weights, Tensor uvm_weights, Tensor weights_placements, Tensor weights_offsets, Tensor weights_tys, Tensor D_offsets, int total_D, int max_int2_D, int max_int4_D, int max_int8_D, int max_float16_D, int max_float32_D, Tensor indices, Tensor offsets, int pooling_mode, Tensor? indice_weights, int output_dtype=1, Tensor? lxu_cache_weights=None, Tensor? lxu_cache_locations=None) -> Tensor");
   m.impl(
       "int_nbit_split_embedding_codegen_lookup_function",
       torch::dispatch(
@@ -138,6 +140,8 @@ TORCH_LIBRARY_FRAGMENT(fb, m) {
 
   // CPU version of hashmap Lookup isn't used. For CPUs, we should use
   // PrunedMapCPU below.
+  m.def(
+      "pruned_hashmap_lookup(Tensor indices, Tensor offsets, Tensor hash_table, Tensor hash_table_offsets) -> Tensor");
   m.impl(
       "pruned_hashmap_lookup",
       torch::dispatch(
@@ -145,6 +149,8 @@ TORCH_LIBRARY_FRAGMENT(fb, m) {
           TORCH_FN(pruned_hashmap_lookup_unweighted_cpu)));
 
   // CPU version of array lookup.
+  m.def(
+      "pruned_array_lookup(Tensor indices, Tensor offsets, Tensor index_remappings, Tensor index_remappings_offsets) -> Tensor");
   m.impl(
       "pruned_array_lookup",
       torch::dispatch(
@@ -152,6 +158,8 @@ TORCH_LIBRARY_FRAGMENT(fb, m) {
 }
 
 TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
+  m.def(
+      "int_nbit_split_embedding_codegen_lookup_function(Tensor dev_weights, Tensor uvm_weights, Tensor weights_placements, Tensor weights_offsets, Tensor weights_tys, Tensor D_offsets, int total_D, int max_int2_D, int max_int4_D, int max_int8_D, int max_float16_D, int max_float32_D, Tensor indices, Tensor offsets, int pooling_mode, Tensor? indice_weights, int output_dtype=1, Tensor? lxu_cache_weights=None, Tensor? lxu_cache_locations=None) -> Tensor");
   m.impl(
       "int_nbit_split_embedding_codegen_lookup_function",
       torch::dispatch(
@@ -170,6 +178,8 @@ TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
 
   // CPU version of hashmap Lookup isn't used. For CPUs, we should use
   // PrunedMapCPU below.
+  m.def(
+      "pruned_hashmap_lookup(Tensor indices, Tensor offsets, Tensor hash_table, Tensor hash_table_offsets) -> Tensor");
   m.impl(
       "pruned_hashmap_lookup",
       torch::dispatch(
@@ -177,6 +187,8 @@ TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
           TORCH_FN(pruned_hashmap_lookup_unweighted_cpu)));
 
   // CPU version of array lookup.
+  m.def(
+      "pruned_array_lookup(Tensor indices, Tensor offsets, Tensor index_remappings, Tensor index_remappings_offsets) -> Tensor");
   m.impl(
       "pruned_array_lookup",
       torch::dispatch(
