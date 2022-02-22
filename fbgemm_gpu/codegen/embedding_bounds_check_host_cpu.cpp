ab3ca6647d3f4be25423c5f997256a8a219fb762
Rostyslav Geyyer <grostyslav@fb.com>
2022-02-21 19:21:25 -0800

    Add DISPATCH_TO_CPU macro and refactor (#935)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/935
    
    Add a DISPATCH_TO_CPU and refactor instead of m.impl dispatching to CPU
    
    Reviewed By: jianyuh
    
    Differential Revision: D34189518
    
    fbshipit-source-id: acc4e2be071679cec897da7d8844204c2d1839e9

--- fbgemm_gpu/codegen/embedding_bounds_check_host_cpu.cpp
+++ fbgemm_gpu/codegen/embedding_bounds_check_host_cpu.cpp
@@ -9,6 +9,7 @@
 #include <ATen/core/op_registration/op_registration.h>
 #include <torch/script.h>
 #include "fbgemm_gpu/embedding_common.h"
+#include "fbgemm_gpu/sparse_ops_utils.h"
 
 using Tensor = at::Tensor;
 
@@ -109,10 +110,7 @@ TORCH_LIBRARY_FRAGMENT(fb, m) {
   // or DCE'd, etc.
   m.def(
       "bounds_check_indices(Tensor rows_per_table, Tensor(a!) indices, Tensor(a!) offsets, int bounds_check_mode, Tensor(a!) warning) -> ()");
-  m.impl(
-      "bounds_check_indices",
-      torch::dispatch(
-          c10::DispatchKey::CPU, TORCH_FN(bounds_check_indices_cpu)));
+  DISPATCH_TO_CPU("bounds_check_indices", bounds_check_indices_cpu);
 }
 
 TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
@@ -120,8 +118,5 @@ TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
   // or DCE'd, etc.
   m.def(
       "bounds_check_indices(Tensor rows_per_table, Tensor(a!) indices, Tensor(a!) offsets, int bounds_check_mode, Tensor(a!) warning) -> ()");
-  m.impl(
-      "bounds_check_indices",
-      torch::dispatch(
-          c10::DispatchKey::CPU, TORCH_FN(bounds_check_indices_cpu)));
+  DISPATCH_TO_CPU("bounds_check_indices", bounds_check_indices_cpu);
 }
