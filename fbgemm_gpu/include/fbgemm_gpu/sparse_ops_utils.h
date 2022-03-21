3c9e6aa5c8c89bc6df0218085e9fdeb95a419484
Jianyu Huang <jianyuhuang@fb.com>
2022-03-21 16:09:30 -0700

    hot fix on compiler issue

--- fbgemm_gpu/include/fbgemm_gpu/sparse_ops_utils.h
+++ fbgemm_gpu/include/fbgemm_gpu/sparse_ops_utils.h
@@ -265,22 +265,13 @@ constexpr uint32_t cuda_calc_block_count(
 
 // Used in jagged_tensor_ops.cu and jagged_tensor_ops_cpu.cpp
 #define JAGGED_TENSOR_DISPATCH_DIMS()                                         \
-  AT_DISPATCH_INDEX_TYPES(x_offsets[0].scalar_type(), "jagged_indices", [&] { \
+  AT_DISPATCH_INDEX_TYPES(x_offsets[0].scalar_type(), "jagged_indices", [=] { \
     switch (num_jagged_dim) {                                                 \
       case 1:                                                                 \
         INVOKE_KERNEL_WITH_DIM(1);                                            \
         break;                                                                \
       case 2:                                                                 \
         INVOKE_KERNEL_WITH_DIM(2);                                            \
-        break;                                                                \
-      case 3:                                                                 \
-        INVOKE_KERNEL_WITH_DIM(3);                                            \
-        break;                                                                \
-      case 4:                                                                 \
-        INVOKE_KERNEL_WITH_DIM(4);                                            \
-        break;                                                                \
-      case 5:                                                                 \
-        INVOKE_KERNEL_WITH_DIM(5);                                            \
         break;                                                                \
       default:                                                                \
         TORCH_CHECK(                                                          \
