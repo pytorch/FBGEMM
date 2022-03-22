fedbac509d76e5856f873a3825fbb800db3d1abb
Chao Gu <guchao@fb.com>
2022-03-22 13:34:56 -0700

    Add a new TBE input combine op with fused input padding support. (#983)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/983
    
    This new TBE input combine op adds padding to input before combining them.
    
    Reviewed By: jackm321
    
    Differential Revision: D34849460
    
    fbshipit-source-id: 293e6ff942303b6cd58a412c1244b096486b861c

--- fbgemm_gpu/src/input_combine_cpu.cpp
+++ fbgemm_gpu/src/input_combine_cpu.cpp
@@ -214,6 +214,93 @@ std::tuple<Tensor, Tensor, Tensor> tbe_input_combine_with_length_cpu(
   return {combined_indices, combined_lengths, at::empty({0})};
 }
 
+// Similar to tbe_input_combine_cpu, but padding all the offsets
+// to the size specified by batch_size.
+
+std::tuple<Tensor, Tensor, Tensor> padding_fused_tbe_input_combine_cpu(
+    const std::vector<Tensor>& indices_list,
+    const std::vector<Tensor>& offsets_list,
+    const std::vector<Tensor>& per_sample_weights,
+    const Tensor& include_last_offsets,
+    int64_t batch_size) {
+  TORCH_CHECK(indices_list.size() > 0);
+  TORCH_CHECK(offsets_list.size() == indices_list.size());
+  TORCH_CHECK(per_sample_weights.size() == indices_list.size());
+  TORCH_CHECK(
+      static_cast<uint64_t>(include_last_offsets.numel()) ==
+      indices_list.size());
+  auto include_last_offsets_acc = include_last_offsets.accessor<bool, 1>();
+  int64_t total_indices = 0;
+  int64_t total_offsets = 1 + batch_size * indices_list.size();
+  bool need_weights = false;
+  bool pin_memory = false;
+  if (at::Context::hasCUDA() && at::getNumGPUs() > 0) {
+    pin_memory = true;
+  }
+
+  for (size_t i = 0; i < indices_list.size(); i++) {
+    TORCH_CHECK(
+        indices_list[i].dtype() == c10::kInt ||
+        indices_list[i].dtype() == c10::kLong);
+    TORCH_CHECK(
+        offsets_list[i].dtype() == c10::kInt ||
+        offsets_list[i].dtype() == c10::kLong);
+    TORCH_CHECK(indices_list[i].ndimension() == 1);
+    TORCH_CHECK(offsets_list[i].ndimension() == 1);
+    TORCH_CHECK(indices_list[i].is_contiguous());
+    TORCH_CHECK(offsets_list[i].is_contiguous());
+    total_indices += indices_list[i].numel();
+
+    if (per_sample_weights[i].numel() > 0) {
+      TORCH_CHECK(per_sample_weights[i].ndimension() == 1);
+      TORCH_CHECK(per_sample_weights[i].numel() == indices_list[i].numel());
+      TORCH_CHECK(per_sample_weights[i].is_contiguous());
+      need_weights = true;
+    }
+  }
+
+  auto combined_indices =
+      _cat_int_tensors(indices_list, total_indices, pin_memory);
+
+  auto combined_offsets = at::empty(
+      {total_offsets},
+      at::TensorOptions()
+          .dtype(c10::kInt)
+          .device(offsets_list[0].device())
+          .pinned_memory(pin_memory));
+
+  auto combined_offsets_acc = combined_offsets.accessor<int32_t, 1>();
+  int32_t offset = 0;
+  size_t offsets_acc_idx = 0;
+  combined_offsets_acc[offsets_acc_idx++] = 0;
+
+  for (size_t i = 0; i < offsets_list.size(); i++) {
+    AT_DISPATCH_INDEX_TYPES(
+        offsets_list[i].scalar_type(), "tbe_input_offsets_", [&] {
+          auto offsets_acc = offsets_list[i].accessor<index_t, 1>();
+          int64_t offsets_size =
+              offsets_list[i].numel() - (include_last_offsets_acc[i] ? 1 : 0);
+          for (int64_t j = 1; j < offsets_size; j++) {
+            combined_offsets_acc[offsets_acc_idx++] =
+                offset + static_cast<int32_t>(offsets_acc[j]);
+          }
+          offset += static_cast<int32_t>(indices_list[i].numel());
+          for (int64_t j = offsets_size; j <= batch_size; j++) {
+            combined_offsets_acc[offsets_acc_idx++] = offset;
+          }
+        });
+  }
+
+  if (need_weights) {
+    return {
+        std::move(combined_indices),
+        std::move(combined_offsets),
+        _cat_per_sample_weights_list(
+            per_sample_weights, indices_list, total_indices, pin_memory)};
+  }
+  return {combined_indices, combined_offsets, at::empty({0})};
+}
+
 } // namespace fbgemm_gpu
 
 TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
@@ -221,8 +308,13 @@ TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
       "tbe_input_combine(Tensor[] indices_list, Tensor[] offsets_list, Tensor[] per_sample_weights, Tensor include_last_offsets) -> (Tensor, Tensor, Tensor)");
   m.def(
       "tbe_input_combine_with_length(Tensor[] indices_list, Tensor[] lengths_list, Tensor[] per_sample_weights) -> (Tensor, Tensor, Tensor)");
+  m.def(
+      "padding_fused_tbe_input_combine(Tensor[] indices_list, Tensor[] offsets_list, Tensor[] per_sample_weights, Tensor include_last_offsets, int batch_size) -> (Tensor, Tensor, Tensor)");
   DISPATCH_TO_CPU("tbe_input_combine", fbgemm_gpu::tbe_input_combine_cpu);
   DISPATCH_TO_CPU(
       "tbe_input_combine_with_length",
       fbgemm_gpu::tbe_input_combine_with_length_cpu);
+  DISPATCH_TO_CPU(
+      "padding_fused_tbe_input_combine",
+      fbgemm_gpu::padding_fused_tbe_input_combine_cpu);
 }
