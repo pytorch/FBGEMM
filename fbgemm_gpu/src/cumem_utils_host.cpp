b0ff5f6b66b664c4bb61450204f02bbbbc9883be
Jianyu Huang <jianyuhuang@fb.com>
2022-03-29 12:27:47 -0700

    Only keep fbgemm namespace for torch.ops (#1009)
    Summary: Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1009
    
    Differential Revision: D35104996
    
    fbshipit-source-id: 474aaab6769d34ab938165e7103c24ad9204d9e2

--- fbgemm_gpu/src/cumem_utils_host.cpp
+++ fbgemm_gpu/src/cumem_utils_host.cpp
@@ -15,36 +15,6 @@ using Tensor = at::Tensor;
 
 namespace fbgemm_gpu {
 
-TORCH_LIBRARY_FRAGMENT(fb, m) {
-  m.def("is_uvm_tensor(Tensor t) -> bool", TORCH_FN(is_uvm_tensor));
-  m.def("uvm_storage(Tensor t) -> bool", TORCH_FN(uvm_storage));
-  m.def(
-      "uvm_to_device(Tensor self, Tensor prototype) -> Tensor",
-      TORCH_FN(uvm_to_device));
-  m.def("uvm_to_cpu(Tensor t) -> Tensor");
-  DISPATCH_TO_CUDA("uvm_to_cpu", uvm_to_cpu);
-  m.def("new_managed_tensor(Tensor self, int[] sizes) -> Tensor");
-  DISPATCH_TO_CUDA("new_managed_tensor", new_managed_tensor);
-  m.def("new_vanilla_managed_tensor(Tensor self, int[] sizes) -> Tensor");
-  DISPATCH_TO_CUDA("new_vanilla_managed_tensor", new_vanilla_managed_tensor);
-  m.def(
-      "cuda_mem_advise(Tensor t, int advice) -> ()",
-      TORCH_FN(uvm_cuda_mem_advise));
-  m.def(
-      "cuda_mem_prefetch_async(Tensor t, Tensor? device_t) -> ()",
-      TORCH_FN(uvm_cuda_mem_prefetch_async));
-  m.def(
-      "uvm_mem_advice_dont_fork(Tensor t) -> ()",
-      TORCH_FN(uvm_mem_advice_dont_fork));
-
-  m.def("uvm_to_cpu_clone(Tensor t) -> Tensor", TORCH_FN(uvm_to_cpu_clone));
-
-#ifndef __HIP_PLATFORM_HCC__
-  // FIXME: some advanced "cudaMemAdvise" flags are not supported by HIP.
-  m.def(FBGEMM_GPU_ENUM_OP(uvm, fbgemm_gpu_uvm_enum_query));
-#endif
-}
-
 TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
   m.def("is_uvm_tensor(Tensor t) -> bool", TORCH_FN(is_uvm_tensor));
   m.def("uvm_storage(Tensor t) -> bool", TORCH_FN(uvm_storage));
