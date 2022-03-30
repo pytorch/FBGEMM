a61476345db057c13753e9afd29823290c87cbeb
Tareq Malas <tmalas@fb.com>
2022-03-29 21:16:07 -0700

    Moved floatToBfloat16Quantized and FloatToBfloat16Quantized ops to OSS
    Summary: moved the internal FP32 -> BF16 and BF16 -> FP32 conversion ops into fbgemm_gpu OSS
    
    Differential Revision: D35127015
    
    fbshipit-source-id: 722ee8616be577da5116b96e35daaed47c821e78

--- fbgemm_gpu/src/sparse_ops_gpu.cpp
+++ fbgemm_gpu/src/sparse_ops_gpu.cpp
@@ -173,4 +173,8 @@ TORCH_LIBRARY_IMPL(fbgemm, CUDA, m) {
       fbgemm_gpu::generic_histogram_binning_calibration_by_feature_cuda);
   DISPATCH_TO_CUDA("segment_sum_csr", fbgemm_gpu::segment_sum_csr_cuda);
   DISPATCH_TO_CUDA("lengths_range", fbgemm_gpu::lengths_range_cuda);
+  DISPATCH_TO_CUDA(
+      "Bfloat16QuantizedToFloat", fbgemm_gpu::_bfloat16_to_float_gpu);
+  DISPATCH_TO_CUDA(
+      "FloatToBfloat16Quantized", fbgemm_gpu::_float_to_bfloat16_gpu);
 }
