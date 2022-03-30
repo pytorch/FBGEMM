0221cd8813ab8385c32b5ddfa83b0dc4bae73b55
Zhanghan Wang <hankzhwang@gmail.com>
2022-03-29 20:23:58 -0700

    split cpu parts in permute_pooled_embedding_ops for cpu_only (#987)
    Summary:
    As specified in [CMakeLists.txt](https://github.com/pytorch/FBGEMM/blob/c3a26e17e7c514041ae3d08f39d0e19063614869/fbgemm_gpu/CMakeLists.txt#L239), "src/permute_pooled_embedding_ops_gpu.cpp" will only compile when "NOT FBGEMM_CPU_ONLY", which means the method "permute_pooled_embs_auto_grad" won't be generated when --cpu_only. However, this method is used by torchrec's column_wise sharding.
    
    The pr mentioned in https://github.com/pytorch/FBGEMM/pull/950 cannot work because of not using m.def to define permute_pooled_embs_auto_grad.
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/987
    
    Reviewed By: jianyuh
    
    Differential Revision: D34984810
    
    Pulled By: geyyer
    
    fbshipit-source-id: f3730cc69f760a414e8a9dfcf4f843a545b15756

--- fbgemm_gpu/include/fbgemm_gpu/permute_pooled_embedding_ops.h
+++ fbgemm_gpu/include/fbgemm_gpu/permute_pooled_embedding_ops.h
@@ -23,4 +23,18 @@ at::Tensor permute_pooled_embs_gpu(
     const at::Tensor& permute_list,
     const at::Tensor& inv_offset_dim_list,
     const at::Tensor& inv_permute_list);
+
+at::Tensor permute_pooled_embs_auto_grad_cpu(
+    const at::Tensor& pooled_embs,
+    const at::Tensor& offset_dim_list,
+    const at::Tensor& permute_list,
+    const at::Tensor& inv_offset_dim_list,
+    const at::Tensor& inv_permute_list);
+
+at::Tensor permute_pooled_embs_auto_grad_gpu(
+    const at::Tensor& pooled_embs,
+    const at::Tensor& offset_dim_list,
+    const at::Tensor& permute_list,
+    const at::Tensor& inv_offset_dim_list,
+    const at::Tensor& inv_permute_list);
 } // namespace fbgemm_gpu
