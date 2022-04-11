f18c0842f201fdbd74aa2352b555e6bfc3792975
Max Podkorytov <maxdp@fb.com>
2022-04-11 12:19:03 -0700

    add out variant for `fbgemm::tbe_input_combine_with_length` (#1038)
    Summary: Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1038
    
    Reviewed By: mikeiovine
    
    Differential Revision: D35435231
    
    fbshipit-source-id: 9ad27b194fe19c634caf17519277791ff1cf7af6

--- fbgemm_gpu/include/fbgemm_gpu/input_combine.h
+++ fbgemm_gpu/include/fbgemm_gpu/input_combine.h
@@ -25,4 +25,18 @@ padding_fused_tbe_input_combine_cpu(
     const at::Tensor& include_last_offsets,
     int64_t batch_size);
 
+void tbe_input_combine_with_length_cpu_out(
+    at::Tensor& combined_indices,
+    at::Tensor& combined_lengths,
+    at::Tensor& combined_per_sample_weights,
+    const std::vector<at::Tensor>& indices_list,
+    const std::vector<at::Tensor>& lengths_list,
+    const std::vector<at::Tensor>& per_sample_weights);
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor>
+tbe_input_combine_with_length_cpu(
+    const std::vector<at::Tensor>& indices_list,
+    const std::vector<at::Tensor>& lengths_list,
+    const std::vector<at::Tensor>& per_sample_weights);
+
 } // namespace fbgemm_gpu
