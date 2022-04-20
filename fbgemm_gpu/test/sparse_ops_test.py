Fix test_batched_dense_vec_jagged_2d_mul for half precision (#1060)
Summary:
Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1060

Fix broken test

torch.bmm is not implemented for half on CPU backend. This test uses torch.bmm to compute a reference. So convert it up to float for the reference calc.

Reviewed By: jianyuh

Differential Revision: D35756271

fbshipit-source-id: 85c572aad39d3dd15734d9aeca6d09b1dc55fd2e

fbgemm_gpu/test/sparse_ops_test.py
@@ -2035,14 +2035,21 @@ class SparseOpsTest(unittest.TestCase):
             [max_L],
         )  # [B, N, H * D]
 
-        output_ref = torch.bmm(
-            dense.unsqueeze(1),
+        bmm_arg1 = dense.unsqueeze(1)
+        bmm_arg2 = (
             padded_values.reshape(B, max_L, H, D)
             .transpose(1, 2)
-            .reshape(B * H, max_L, D),
-        ).squeeze(
+            .reshape(B * H, max_L, D)
+        )
+        # torch.bmm not implemented for Half on CPU
+        if dtype == torch.half and use_cpu:
+            bmm_arg1 = bmm_arg1.float()
+            bmm_arg2 = bmm_arg2.float()
+        output_ref = torch.bmm(bmm_arg1, bmm_arg2).squeeze(
             1
         )  # [B H, 1, N] x [B H, N, D] = [B H, 1, D]
+        if dtype == torch.half and use_cpu:
+            output_ref = output_ref.half()
         output = torch.ops.fbgemm.batched_dense_vec_jagged_2d_mul(
             dense, values, offsets
         )