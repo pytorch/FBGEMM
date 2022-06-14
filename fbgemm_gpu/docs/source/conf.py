beb65635af6ee651871b80fd3a795110d1dcb218
Svetlana Karslioglu <svekars@fb.com>
2022-06-14 15:30:41 -0700

    update

--- fbgemm_gpu/docs/source/conf.py
+++ fbgemm_gpu/docs/source/conf.py
@@ -18,7 +18,7 @@ import sys
 
 import pytorch_sphinx_theme
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../../fbgemm_gpu"))
 
 # -- Project information -----------------------------------------------------
 
@@ -35,7 +35,16 @@ release = "0.1.2"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc"]
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
