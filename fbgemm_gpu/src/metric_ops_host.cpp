f4425015e0340b0fe3d13f81b0a032e2a2e1ecae
Sarunya Pumma <sarunya@fb.com>
2022-06-23 13:53:24 -0700

    Add batch AUC CUDA kernel (#1153)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1153
    
    This diff adds the fused-batch-AUC CUDA kernel.  For each AUC
    computation, we fuse all operations except sort into a single kernel
    to avoid launching many small kernels and reduce data movement from/to
    global memory.  We also leverage shared memory for storing
    intermediate data.
    
    For multiple AUC computation (i.e., multiple tasks), we batch them
    into a single kernel to avoid kernel launching overhead and increase
    GPU resource utilization.  (We note that normally a single AUC
    computation does not fully utilize the GPU resources).
    
    Reviewed By: jspark1105
    
    Differential Revision: D36784281
    
    fbshipit-source-id: acc35f7fdcd8b876b2c8062b51a75a0e93a8b501

---
+++ fbgemm_gpu/src/metric_ops_host.cpp
@@ -0,0 +1,18 @@
+#include <ATen/core/op_registration/op_registration.h>
+#include <torch/library.h>
+
+#include "fbgemm_gpu/sparse_ops_utils.h"
+#include "metric_ops.h"
+
+namespace fbgemm_gpu {
+
+TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
+  m.def(
+      "batch_auc(int num_tasks, Tensor indices, Tensor laebls, Tensor weights) -> Tensor");
+}
+
+TORCH_LIBRARY_IMPL(fbgemm, CUDA, m) {
+  DISPATCH_TO_CUDA("batch_auc", fbgemm_gpu::batch_auc);
+}
+
+} // namespace fbgemm_gpu
