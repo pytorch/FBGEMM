8b6e1d559ac6db3b9c7e4f88c049f722bfb06d0e
liligwu <lili.cce.jlu@gmail.com>
2022-07-15 15:30:20 -0700

    Add ROCm runner (#1193)
    Summary:
    - The runner is tested in ROCmSoftwarePlatform fbgemm public repo https://github.com/ROCmSoftwarePlatform/FBGEMM/runs/7240711126?check_suite_focus=true
    
    - We added a script (.jenkins/rocm/build_and_test.sh) that runs the build and unit tests. shintaro-iwasaki, please let us know if it is a proper place to hold the script.
    
    ~- The hardcoded data type in `split_table_batched_embeddings_ops.py` triggers the `expect ... but got ... ` error.~
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1193
    
    Reviewed By: sryap
    
    Differential Revision: D37797401
    
    Pulled By: shintaro-iwasaki
    
    fbshipit-source-id: 825d50854fc8e1c367277aabff4fd5f0c16d4fd4

---
+++ .jenkins/rocm/build_and_test.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# exit immediately on failure, or if an undefined variable is used
+set -eux
+
+FBGEMM_REPO_DIR=${1:-/workspace/FBGEMM}
+
+git config --global --add safe.directory "$FBGEMM_REPO_DIR"
+git config --global --add safe.directory "$FBGEMM_REPO_DIR/third_party/asmjit"
+git config --global --add safe.directory "$FBGEMM_REPO_DIR/third_party/cpuinfo"
+git config --global --add safe.directory "$FBGEMM_REPO_DIR/third_party/googletest"
+git config --global --add safe.directory "$FBGEMM_REPO_DIR/third_party/hipify_torch"
+
+# Install dependencies
+apt-get update --allow-insecure-repositories && \
+  apt-get install -y --allow-unauthenticated \
+  git \
+  jq \
+  sshfs \
+  sshpass \
+  unzip
+
+apt-get install -y locales
+locale-gen en_US.UTF-8
+
+pip3 install click
+pip3 install jinja2
+pip3 install ninja
+pip3 install scikit-build
+pip3 install --upgrade hypothesis
+pip3 install --pre torch torchvision --extra-index-url https://download.pytorch.org/whl/nightly/rocm5.1.1/
+
+pip3 list
+
+# Build fbgemm_gpu
+cd "$FBGEMM_REPO_DIR/fbgemm_gpu"
+MAX_JOBS="$(nproc)"
+export MAX_JOBS
+export PYTORCH_ROCM_ARCH="gfx908"
+python setup.py build develop
+
+export FBGEMM_TEST_WITH_ROCM=1
+
+# Test fbgemm_gpu
+cd test
+
+python batched_unary_embeddings_test.py --verbose
+python input_combine_test.py --verbose
+python jagged_tensor_ops_test.py --verbose
+python layout_transform_ops_test.py --verbose
+python merge_pooled_embeddings_test.py --verbose
+python metric_ops_test.py --verbose
+python permute_pooled_embedding_modules_test.py --verbose
+python quantize_ops_test.py --verbose
+python sparse_ops_test.py --verbose
+python split_embedding_inference_converter_test.py --verbose
+python split_table_batched_embeddings_test.py --verbose
+python uvm_test.py --verbose
