a443ae7f3ae3f6a513c2c4469967f713560c5698
Jianyu Huang <jianyuhuang@fb.com>
2022-08-29 23:19:42 -0700

    move stacked jagged op
    Differential Revision: D39124669
    
    fbshipit-source-id: fc63db986f72e85909d5cc8a016ca4acf260eee3

--- fbgemm_gpu/src/sparse_ops_gpu.cpp
+++ fbgemm_gpu/src/sparse_ops_gpu.cpp
@@ -99,51 +99,6 @@ Tensor lookup_batched_unary_embedding_function(
       weight, table_offsets, offsets, indices)[0];
 }
 
-class StackedJagged2DToDenseGPUOp
-    : public torch::autograd::Function<StackedJagged2DToDenseGPUOp> {
- public:
-  static torch::autograd::variable_list forward(
-      torch::autograd::AutogradContext* ctx,
-      Tensor values,
-      Tensor lengths,
-      const std::vector<int64_t>& offset_per_key,
-      const std::vector<int64_t>& max_lengths_per_key) {
-    int64_t total_L = values.size(0);
-    ctx->saved_data["B"] = lengths.size(1);
-    ctx->saved_data["D"] = values.size(1);
-    ctx->saved_data["total_L"] = total_L;
-    ctx->saved_data["offset_per_key"] = offset_per_key;
-
-    auto [padded_values_per_key, offsets_tensor_per_key] =
-        stacked_jagged_2d_to_dense_forward_cuda(
-            values, lengths, offset_per_key, max_lengths_per_key);
-    ctx->saved_data["offsets_tensor_per_key"] = offsets_tensor_per_key;
-
-    return padded_values_per_key;
-  }
-
-  static torch::autograd::variable_list backward(
-      torch::autograd::AutogradContext* ctx,
-      torch::autograd::variable_list grad_outputs) {
-    auto B = ctx->saved_data["B"].toInt();
-    auto D = ctx->saved_data["D"].toInt();
-    auto total_L = ctx->saved_data["total_L"].toInt();
-    auto offset_per_key = ctx->saved_data["offset_per_key"].toIntVector();
-    auto offsets_tensor_per_key =
-        ctx->saved_data["offsets_tensor_per_key"].toTensorVector();
-
-    using torch::autograd::Variable;
-    auto grad_values = stacked_jagged_2d_to_dense_backward_cuda(
-        B, D, total_L, grad_outputs, offsets_tensor_per_key, offset_per_key);
-    return {
-        grad_values,
-        Variable(), // lengths
-        Variable(), // offset_per_key
-        Variable() // max_lengths_per_key
-    };
-  }
-};
-
 class IndexSelectDim0GPUOp
     : public torch::autograd::Function<IndexSelectDim0GPUOp> {
  public:
@@ -210,20 +165,6 @@ class IndexSelectDim0GPUOp
   }
 };
 
-std::vector<Tensor> stacked_jagged_2d_to_dense_gpu(
-    Tensor values,
-    Tensor lengths,
-    const std::vector<int64_t>& offset_per_key,
-    const std::vector<int64_t>& max_lengths_per_key) {
-  TENSOR_ON_CUDA_GPU(values);
-  TENSOR_ON_CUDA_GPU(lengths);
-  TENSORS_ON_SAME_DEVICE(values, lengths);
-  TORCH_CHECK(values.dim() == 2);
-  TORCH_CHECK(lengths.dim() == 2);
-  return StackedJagged2DToDenseGPUOp::apply(
-      values, lengths, offset_per_key, max_lengths_per_key);
-}
-
 Tensor pack_segments_cuda(
     const Tensor& t_in,
     const Tensor& lengths,
@@ -277,18 +218,6 @@ TORCH_LIBRARY_IMPL(fbgemm, CUDA, m) {
   DISPATCH_TO_CUDA(
       "batched_unary_embeddings",
       fbgemm_gpu::lookup_batched_unary_embedding_function);
-  DISPATCH_TO_CUDA("jagged_1d_to_dense", fbgemm_gpu::jagged_1d_to_dense_gpu);
-  DISPATCH_TO_CUDA("jagged_2d_to_dense", fbgemm_gpu::jagged_2d_to_dense_gpu);
-  DISPATCH_TO_CUDA(
-      "stacked_jagged_1d_to_dense", fbgemm_gpu::stacked_jagged_1d_to_dense_gpu);
-  DISPATCH_TO_CUDA(
-      "stacked_jagged_2d_to_dense", fbgemm_gpu::stacked_jagged_2d_to_dense_gpu);
-  DISPATCH_TO_CUDA(
-      "stacked_jagged_2d_to_dense_forward",
-      fbgemm_gpu::stacked_jagged_2d_to_dense_forward_cuda);
-  DISPATCH_TO_CUDA(
-      "stacked_jagged_2d_to_dense_backward",
-      fbgemm_gpu::stacked_jagged_2d_to_dense_backward_cuda);
   DISPATCH_TO_CUDA(
       "histogram_binning_calibration",
       fbgemm_gpu::histogram_binning_calibration_cuda);
