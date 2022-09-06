f0a9440d6583c4240806052bd56ae0f4588654d2
Paul Saab <ps@fb.com>
2022-09-06 02:55:22 -0700

    fbgemm building aarch64 using simde (#1271)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1271
    
    fbgemm building aarch64 using simde
    
    Reviewed By: pallab-zz, suphoff
    
    Differential Revision: D39175324
    
    fbshipit-source-id: 7331fe5d43a9ff455a9348451c790ad2322187a7

--- src/FbgemmBfloat16Convert.cc
+++ src/FbgemmBfloat16Convert.cc
@@ -43,9 +43,12 @@ namespace fbgemm {
 void FloatToBfloat16_simd(const float* src, bfloat16* dst, size_t size) {
   // Run time CPU detection
   if (cpuinfo_initialize()) {
+#ifndef __aarch64__
     if (fbgemmHasAvx512Support()) {
       FloatToBfloat16_avx512(src, dst, size);
-    } else if (fbgemmHasAvx2Support()) {
+    } else
+#endif
+        if (fbgemmHasAvx2Support()) {
       FloatToBfloat16_avx2(src, dst, size);
     } else {
       FloatToBfloat16_ref(src, dst, size);
@@ -59,9 +62,12 @@ void FloatToBfloat16_simd(const float* src, bfloat16* dst, size_t size) {
 void Bfloat16ToFloat_simd(const bfloat16* src, float* dst, size_t size) {
   // Run time CPU detection
   if (cpuinfo_initialize()) {
+#ifndef __aarch64__
     if (fbgemmHasAvx512Support()) {
       Bfloat16ToFloat_avx512(src, dst, size);
-    } else if (fbgemmHasAvx2Support()) {
+    } else
+#endif
+        if (fbgemmHasAvx2Support()) {
       Bfloat16ToFloat_avx2(src, dst, size);
     } else {
       Bfloat16ToFloat_ref(src, dst, size);
