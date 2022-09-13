467eed48ac8597e680f97aa8d740a1e71d782ec9
Gopi Vinod Avvari <vinodavvari2022@fb.com>
2022-09-12 19:14:08 -0700

    testing alternative documentation approach

--- fbgemm_gpu/docs/source/conf.py
+++ fbgemm_gpu/docs/source/conf.py
@@ -19,7 +19,14 @@ import sys
 
 import pytorch_sphinx_theme
 
-sys.path.insert(0, os.path.abspath("../.."))
+
+for dir_i in os.listdir("../.."):
+    if dir_i == "fbgemm_gpu":
+        continue
+    possible_dir = os.path.join("../..", dir_i)
+    if os.path.isdir(possible_dir):
+        sys.path.insert(0, possible_dir)
+
 # Doxygen
 subprocess.call("doxygen Doxyfile.in", shell=True)
 
