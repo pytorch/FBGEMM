3d6249ecb085c2a1129c16b267a62ca89fed24bf
Gopi Vinod Avvari <vinodavvari2022@fb.com>
2022-09-12 19:29:46 -0700

    modified conf

--- fbgemm_gpu/docs/source/conf.py
+++ fbgemm_gpu/docs/source/conf.py
@@ -20,12 +20,12 @@ import sys
 import pytorch_sphinx_theme
 
 
-for dir_i in os.listdir("../.."):
-    if dir_i == "fbgemm_gpu":
-        continue
-    possible_dir = os.path.join("../..", dir_i)
-    if os.path.isdir(possible_dir):
-        sys.path.insert(0, possible_dir)
+# for dir_i in os.listdir("../.."):
+#     if dir_i == "fbgemm_gpu":
+#         continue
+#     possible_dir = os.path.join("../..", dir_i)
+#     if os.path.isdir(possible_dir):
+#         sys.path.insert(0, possible_dir)
 
 # Doxygen
 subprocess.call("doxygen Doxyfile.in", shell=True)
