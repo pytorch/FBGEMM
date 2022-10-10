7b5d0500383a17809276edeabdb5a60bfd9460ee
Jongsoo Park <jongsoo@meta.com>
2022-10-09 22:57:54 -0700

    copyright headers (#1387)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1387
    
    As title
    
    Reviewed By: jianyuh
    
    Differential Revision: D40213760
    
    fbshipit-source-id: 65985450a05ef79c90dfdb4e028f730d2c69f8f1

--- fbgemm_gpu/fbgemm_gpu/_fbgemm_gpu_docs.py
+++ fbgemm_gpu/fbgemm_gpu/_fbgemm_gpu_docs.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 import fbgemm_gpu
 import fbgemm_gpu.split_table_batched_embeddings_ops
 import torch
