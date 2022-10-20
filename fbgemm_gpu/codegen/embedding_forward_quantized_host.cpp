2ce6cba40b006d35cf5f8cf934de2a37e0439d46
Jaewon Lee <jaewon@meta.com>
2022-10-20 13:52:47 -0700

    Placeholder/Template -- Create a wrapper to UVM kernel in FB-only FBGEMM (#1404)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1404
    
    Creating header files so that the corresponding TBE functions can be referred from various other projects.
    
    Differential Revision: D40424002
    
    fbshipit-source-id: 55005b6affa815266ed35a8c9063947881b2d303

--- fbgemm_gpu/codegen/embedding_forward_quantized_host.cpp
+++ fbgemm_gpu/codegen/embedding_forward_quantized_host.cpp
@@ -4,6 +4,7 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+#include "embedding_forward_quantized_host.h"
 #include <ATen/ATen.h>
 #include <ATen/TypeDefault.h>
 #include <ATen/core/op_registration/op_registration.h>
