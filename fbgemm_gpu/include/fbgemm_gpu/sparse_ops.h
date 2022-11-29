e4f6ed87c209ab25e457026598664aec8519bb65
Mengchi Zhang <mengchi@meta.com>
2022-11-28 23:47:33 -0800

    jagged_dense_elementwise_mul add Meta and Autograd backend (#1469)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1469
    
    Add Meta and Autograd backend for jagged_dense_elementwise_mul for dynamo and AOT autograd.
    
    A more proper way of doing Autograd to make inductor working for jagged ops. Also added Meta tensors so we're not mercy of arbitrary zero inputs fed into by default.
    
    Reviewed By: jianyuh, jiawenliu64
    
    Differential Revision: D41383597
    
    fbshipit-source-id: 8c9e573f7215b09c7dbb52a98b3501c78ac5106a

--- fbgemm_gpu/include/fbgemm_gpu/sparse_ops.h
+++ fbgemm_gpu/include/fbgemm_gpu/sparse_ops.h
@@ -402,6 +402,12 @@ jagged_dense_dense_elementwise_add_jagged_output_autograd(
     const at::Tensor& y_0,
     const at::Tensor& y_1);
 
+std::tuple<at::Tensor, std::vector<at::Tensor>>
+jagged_dense_elementwise_mul_autograd(
+    const at::Tensor& x_values,
+    const std::vector<at::Tensor>& x_offsets,
+    const at::Tensor& y);
+
 ///@ingroup sparse-data-cpu
 at::Tensor jagged_2d_to_dense_forward_cpu(
     at::Tensor values,
