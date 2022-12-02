6120f217ab270e61615d568c1336578ffd69f29d
Jianyu Huang <jianyuhuang@meta.com>
2022-12-02 05:03:37 -0800

    Open source TBE inplace update operators (#1480)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1480
    
    Move embedding_inplace_* from fbsource/fbcode/deeplearning/fbgemm/fbgemm_gpu/fb/src/ to fbsource/fbcode/deeplearning/fbgemm/fbgemm_gpu/src/ and refactor the related unit test, benchmark, etc.
    
    Reviewed By: jspark1105, airidas-meta
    
    Differential Revision: D41545741
    
    fbshipit-source-id: 125f81ca8267064641c37696ca73bb7eff99b6ab

---
+++ fbgemm_gpu/src/embedding_inplace_update.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <ATen/ATen.h>
+#include <ATen/core/TensorAccessor.h>
+#include <torch/torch.h>
+
+#include "fbgemm_gpu/embedding_common.h"
+#include "fbgemm_gpu/sparse_ops_utils.h"
+
+using Tensor = at::Tensor;
+
+namespace fbgemm_gpu {
+
+/**
+ * Embedding tables inplace updates with absolute values (idempotent guarantee)
+ *
+ * dev_weights: the loaded tables on device in TBE format
+ * uvm_weights: the loaded tables on UVM in TBE format
+ * weights_placements: placements for each table
+ * weights_offsets: physical offsets for each table
+ * weights_tys: weight types for each table
+ * D_offsets: table dimensions
+ * update_weights: new update weights tensor in TBE format
+ * update_table_idx: table indices for every new row
+ * update_row_idx: row indices for every new row
+ * update_offsets: offsets of new update weights
+ * row_alignment: alignment byte for embedding row
+ * lxu_cache_weights: the loaded cache weights
+ * lxu_cache_locations: the loaded cache location info
+ *
+ * it's guaranteed from upper service level that each row of table will
+ * only receive one update at a time.
+ *
+ * This function has embedding update parameters (update_weights,
+ * update_table_idx, updata_offsets) on the host, while embedding weights
+ * are on the CUDA devices.
+ */
+void embedding_inplace_update_host_weight_cuda(
+    Tensor dev_weights,
+    Tensor uvm_weights,
+    Tensor weights_placements,
+    Tensor weights_offsets,
+    Tensor weights_tys,
+    Tensor D_offsets,
+    Tensor update_weights,
+    const std::vector<int32_t>& update_table_idx,
+    const std::vector<int32_t>& update_row_idx,
+    const int64_t row_alignment,
+    c10::optional<Tensor> lxu_cache_weights = c10::nullopt,
+    c10::optional<Tensor> lxu_cache_locations = c10::nullopt);
+
+void embedding_inplace_update_cuda(
+    Tensor dev_weights,
+    Tensor uvm_weights,
+    Tensor weights_placements,
+    Tensor weights_offsets,
+    Tensor weights_tys,
+    Tensor D_offsets,
+    Tensor update_weights,
+    Tensor update_table_idx,
+    Tensor update_row_idx,
+    Tensor update_offsets,
+    const int64_t row_alignment,
+    c10::optional<Tensor> lxu_cache_weights = c10::nullopt,
+    c10::optional<Tensor> lxu_cache_locations = c10::nullopt);
+
+} // namespace fbgemm_gpu
