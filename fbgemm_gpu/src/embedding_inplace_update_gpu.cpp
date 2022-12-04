6f29153e0fed94784edae01fe79bc16002f2f146
Jianyu Huang <jianyuhuang@meta.com>
2022-12-04 12:42:46 -0800

    Follow up on BC issue for open sourcing TBE inplace update op (#1492)
    Summary: Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1492
    
    Reviewed By: jspark1105
    
    Differential Revision: D41717190
    
    fbshipit-source-id: d8cfc23aa1947ea74fd82f76190deb1ca2a7f9ba

--- fbgemm_gpu/src/embedding_inplace_update_gpu.cpp
+++ fbgemm_gpu/src/embedding_inplace_update_gpu.cpp
@@ -8,7 +8,7 @@
 #include <ATen/core/op_registration/op_registration.h>
 #include <ATen/cuda/CUDAContext.h>
 #include <torch/library.h>
-#include "embedding_inplace_update.h"
+#include "fbgemm_gpu/embedding_inplace_update.h"
 
 TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
   DISPATCH_TO_CUDA(
