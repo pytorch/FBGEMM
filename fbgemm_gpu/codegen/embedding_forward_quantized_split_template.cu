6f66bf864c82b3cb620ccef973d7a4f934ea4ca9
Jianyu Huang <jianyuhuang@meta.com>
2023-01-11 19:42:40 -0800

    add pruning/index_remapping support for inplace update (#1529)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1529
    
    Add pruning support (w/ index remapping) in the sparse inplace update TBE op.
    
    Reviewed By: jspark1105
    
    Differential Revision: D42038192
    
    fbshipit-source-id: 8a7c1e64d61831f5fe3ba27b91b5da57795bae8b

--- fbgemm_gpu/codegen/embedding_forward_quantized_split_template.cu
+++ fbgemm_gpu/codegen/embedding_forward_quantized_split_template.cu
@@ -552,6 +552,36 @@ __global__ __launch_bounds__(kMaxThreads) void int_nbit_split_embedding_codegen_
 }
 {% endif %}
 
+{% if not weighted %}
+template <typename index_t>
+__global__ __launch_bounds__(kMaxThreads) void int_nbit_split_embedding_codegen_forward_pruned_array_lookup_from_row_idx_kernel(
+    const at::PackedTensorAccessor32<index_t, 1, at::RestrictPtrTraits> update_row_indices,
+    const at::PackedTensorAccessor32<int32_t, 1, at::RestrictPtrTraits> update_table_indices,
+    const at::PackedTensorAccessor32<int32_t, 1, at::RestrictPtrTraits> index_remappings,
+    const at::PackedTensorAccessor32<int64_t, 1, at::RestrictPtrTraits> index_remappings_offsets,
+    at::PackedTensorAccessor32<index_t, 1, at::RestrictPtrTraits> dense_indices) {
+
+  const int64_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx >= update_row_indices.size(0)) {
+    return;
+  }
+  const int table_idx = update_table_indices[idx];
+  const auto row_idx = update_row_indices[idx];
+
+  const int64_t index_remappings_start = index_remappings_offsets[table_idx];
+  const int64_t index_remappings_end = index_remappings_offsets[table_idx + 1];
+  const int64_t capacity = index_remappings_end - index_remappings_start;
+
+  if (capacity > 0) {
+    dense_indices[idx] = index_remappings[index_remappings_start + row_idx];
+  } else {
+    dense_indices[idx] = row_idx;
+  }
+}
+{% endif %}
+
+
+
 }
 
 {% for nobag in [True, False] %}
@@ -1049,5 +1079,53 @@ Tensor pruned_array_lookup_cuda(
   C10_CUDA_KERNEL_LAUNCH_CHECK();
   return dense_indices;
 }
+
+Tensor pruned_array_lookup_from_row_idx_cuda(
+    Tensor update_row_indices,
+    Tensor update_table_indices,
+    Tensor index_remappings,
+    Tensor index_remappings_offsets) {
+
+  TENSOR_ON_CUDA_GPU(update_row_indices);
+  TENSOR_ON_CUDA_GPU(update_table_indices);
+  TENSOR_ON_CUDA_GPU(index_remappings);
+  TENSOR_ON_CUDA_GPU(index_remappings_offsets);
+
+  at::cuda::OptionalCUDAGuard device_guard;
+  device_guard.set_index(update_table_indices.get_device());
+  auto dense_indices = at::empty_like(update_row_indices);
+  const int32_t T = index_remappings_offsets.size(0) - 1;
+
+  const auto num_indices = update_row_indices.numel();
+  if (num_indices == 0) {
+    return dense_indices;
+  }
+
+  TORCH_CHECK(index_remappings.size(0) < std::numeric_limits<int64_t>::max());
+  TORCH_CHECK(update_row_indices.dim() == 1, "Tensor dim: ", update_row_indices.dim());
+  TORCH_CHECK(update_table_indices.dim() == 1, "Tensor dim: ", update_table_indices.dim());
+  TORCH_CHECK(index_remappings.dim() == 1, "Tensor dim: ", index_remappings.dim());
+  TORCH_CHECK(index_remappings_offsets.dim() == 1, "Tensor dim: ", index_remappings_offsets.dim());
+  TORCH_CHECK(dense_indices.dim() == 1, "Tensor dim: ", dense_indices.dim());
+  constexpr size_t kForwardMaxThreads = 256;
+
+  AT_DISPATCH_INDEX_TYPES(
+      update_row_indices.scalar_type(), "embedding_inplace_update_kernel", [&] {
+        nbit::int_nbit_split_embedding_codegen_forward_pruned_array_lookup_from_row_idx_kernel<<<
+            nbit::div_round_up(num_indices, kForwardMaxThreads),
+            kForwardMaxThreads,
+            0,
+            at::cuda::getCurrentCUDAStream()>>>(
+                update_row_indices.packed_accessor32<index_t, 1, at::RestrictPtrTraits>(),
+                update_table_indices.packed_accessor32<int32_t, 1, at::RestrictPtrTraits>(),
+                index_remappings.packed_accessor32<int32_t, 1, at::RestrictPtrTraits>(),
+                index_remappings_offsets.packed_accessor32<int64_t, 1, at::RestrictPtrTraits>(),
+                dense_indices.packed_accessor32<index_t, 1, at::RestrictPtrTraits>()
+        );
+        C10_CUDA_KERNEL_LAUNCH_CHECK();
+      });
+  return dense_indices;
+}
 {% endif %}
+
                                     // clang-format on
