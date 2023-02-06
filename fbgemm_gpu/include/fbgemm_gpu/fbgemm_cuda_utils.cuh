bc6f6b2c63c8b0cafee935df15afc6a4f9142c98
Li Li <li.li3@amd.com>
2023-02-06 15:20:05 +0000

    address Lint indentation issues

--- fbgemm_gpu/include/fbgemm_gpu/fbgemm_cuda_utils.cuh
+++ fbgemm_gpu/include/fbgemm_gpu/fbgemm_cuda_utils.cuh
@@ -1398,17 +1398,17 @@ struct __align__(32) half16 {
 };
 
 #ifdef __HIP_PLATFORM_HCC__
-  using __nv_bfloat16=hip_bfloat16;
+using __nv_bfloat16=hip_bfloat16;
 
-  typedef struct __align__(4) {
-    uint16_t x;
-    uint16_t y;
-  } __nv_bfloat162_raw;
+typedef struct __align__(4) {
+  uint16_t x;
+  uint16_t y;
+} __nv_bfloat162_raw;
 
-  struct __align__(4) __nv_bfloat162 {
-    __nv_bfloat16 x;
-    __nv_bfloat16 y;
-  };
+struct __align__(4) __nv_bfloat162 {
+  __nv_bfloat16 x;
+  __nv_bfloat16 y;
+};
 #endif
 
 #if !(                                                  \
@@ -1477,17 +1477,17 @@ DEVICE_INLINE half16 to_half16(float_16 v) {
 }
 
 #ifdef __HIP_PLATFORM_HCC__
-  // the descriptions of __float2bfloat16 and __float2bfloat16_rn are identical
-  // https://docs.nvidia.com/cuda/cuda-math-api/group__CUDA__MATH____BFLOAT16__MISC.html#group__CUDA__MATH____BFLOAT16__MISC
-  static __host__ __device__ __nv_bfloat16 __float2bfloat16(float f){
-    __nv_bfloat16 output;
-    return output.round_to_bfloat16(f);
-  }
+// the descriptions of __float2bfloat16 and __float2bfloat16_rn are identical
+// https://docs.nvidia.com/cuda/cuda-math-api/group__CUDA__MATH____BFLOAT16__MISC.html#group__CUDA__MATH____BFLOAT16__MISC
+static __host__ __device__ __nv_bfloat16 __float2bfloat16(float f){
+  __nv_bfloat16 output;
+  return output.round_to_bfloat16(f);
+}
 
-  static __host__ __device__ __nv_bfloat16 __float2bfloat16_rn(float f){
-    __nv_bfloat16 output;
-    return output.round_to_bfloat16(f);
-  }
+static __host__ __device__ __nv_bfloat16 __float2bfloat16_rn(float f){
+  __nv_bfloat16 output;
+  return output.round_to_bfloat16(f);
+}
 #endif
 
 #if !(                                                  \
