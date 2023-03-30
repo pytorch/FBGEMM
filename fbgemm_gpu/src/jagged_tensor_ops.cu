4ee46036ae77cc5664e9416da78753afcf101556
Rengan Xu <renganxu@meta.com>
2023-03-29 22:05:47 -0700

    jagged_softmax backward optimization (#1662)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1662
    
    This diff optimizes jagged_softmax backward with more efficient reduction from cub library
    
    Reviewed By: brad-mengchi
    
    Differential Revision: D44205819
    
    fbshipit-source-id: cd1d7a886d6ba68201dc1ad782c2e8cde7ff706b

--- fbgemm_gpu/src/jagged_tensor_ops.cu
+++ fbgemm_gpu/src/jagged_tensor_ops.cu
@@ -1968,35 +1968,76 @@ Tensor jagged_softmax_forward(
   return output;
 }
 
-template <typename index_t, typename scalar_t>
+template <const int THREADS_PER_BLOCK, typename index_t, typename scalar_t>
 __global__ __launch_bounds__(kMaxThreads) void jagged_softmax_backward_kernel(
     const at::PackedTensorAccessor32<scalar_t, 2> grad_output,
     const at::PackedTensorAccessor32<scalar_t, 2> output,
     const at::PackedTensorAccessor32<index_t, 1> offsets,
     at::PackedTensorAccessor32<scalar_t, 2> grad_input,
     const int max_L) {
-  const int B = offsets.size(0) - 1;
-  const int D = grad_output.size(1);
+  const auto B = offsets.size(0) - 1;
+  const auto D = grad_output.size(1);
 
-  const int b_begin = blockIdx.x * blockDim.y + threadIdx.y;
-  const int b_step = gridDim.x * blockDim.y;
-  for (int b = b_begin; b < B; b += b_step) {
-    const int row_start = offsets[b];
-    const int row_end = offsets[b + 1];
-    const int length = min(row_end - row_start, max_L);
-    if (length != 0) {
-      // TODO: use shared memory and better reduction
-      for (int d = threadIdx.x; d < D; d += blockDim.x) {
-        scalar_t sum_value = grad_output[row_start][d] * output[row_start][d];
-        for (int l = 1; l < length; ++l) {
-          sum_value += grad_output[row_start + l][d] * output[row_start + l][d];
+  // Specialize BlockReduce type for our thread block
+  typedef cub::BlockReduce<scalar_t, THREADS_PER_BLOCK> BlockReduceT;
+
+  // Allocate shared memory for BlockReduce
+  __shared__ typename BlockReduceT::TempStorage temp_storage;
+
+  __shared__ scalar_t sum_value;
+
+  const auto tid = threadIdx.x;
+  for (auto b = blockIdx.y; b < B; b += gridDim.y) {
+    const index_t row_start = offsets[b];
+    const index_t row_end = offsets[b + 1];
+    const auto length = min(row_end - row_start, (index_t)max_L);
+
+    if (length > 0) {
+      const auto num_l_blocks =
+          (length + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+
+      for (auto d = blockIdx.x; d < D; d += gridDim.x) {
+        if (tid == 0) {
+          sum_value = 0;
         }
 
-        for (int l = 0; l < length; ++l) {
-          grad_input[row_start + l][d] =
-              (grad_output[row_start + l][d] - sum_value) *
-              output[row_start + l][d];
+        // Loop through all blocks to calculate the sum value
+        // Each block has its own sum, and sum_value is the sum value across all
+        // blocks
+        for (auto bk_l = 0; bk_l < num_l_blocks; bk_l++) {
+          const auto l = bk_l * blockDim.x + tid;
+          scalar_t thread_val = 0;
+          if (l < length) {
+            thread_val =
+                grad_output[row_start + l][d] * output[row_start + l][d];
+          }
+
+          // Collectively compute the block-wide sum reduction
+          scalar_t block_sum_value = BlockReduceT(temp_storage).Sum(thread_val);
+          __syncthreads();
+
+          if (tid == 0) {
+            sum_value += block_sum_value;
+          }
         }
+
+        // The sum_value was updated by thread 0 in the last loop, sync here to
+        // make sure the next loop uses the updated sum_value
+        __syncthreads();
+
+        for (auto bk_l = 0; bk_l < num_l_blocks; bk_l++) {
+          const auto l = bk_l * blockDim.x + tid;
+          if (l < length) {
+            grad_input[row_start + l][d] =
+                (grad_output[row_start + l][d] - sum_value) *
+                output[row_start + l][d];
+          }
+        }
+
+        // The sum_value will be reinitialized by thread 0 in the
+        // next d iteration, sync here to make sure the last loop still uses the
+        // reduced value before reinitialization
+        __syncthreads();
       }
     }
   }
@@ -2014,14 +2055,13 @@ Tensor jagged_softmax_backward(
   at::cuda::OptionalCUDAGuard device_guard;
   device_guard.set_index(grad_output.get_device());
 
-  const int B = offsets.numel() - 1;
-  const int D = grad_output.size(1);
+  const auto B = offsets.numel() - 1;
+  const auto D = grad_output.size(1);
   auto grad_input = at::empty_like(grad_output);
 
   if (B > 0 && D > 0) {
-    const int block_dim_x =
-        std::min(div_round_up(D, kWarpSize) * kWarpSize, kMaxThreads);
-    const int block_dim_y = kMaxThreads / block_dim_x;
+    constexpr int THREADS_PER_BLOCK = 128;
+    const dim3 grid(D, std::min((int32_t)B, (int32_t)kMaxBlockYDim), 1);
 
     AT_DISPATCH_INDEX_TYPES(
         offsets.scalar_type(), "jagged_softmax_backward_kernel_1", [&] {
@@ -2031,9 +2071,12 @@ Tensor jagged_softmax_backward(
               grad_output.scalar_type(),
               "jagged_softmax_backward_kernel_2",
               [&] {
-                jagged_softmax_backward_kernel<index_t, scalar_t>
-                    <<<div_round_up(B, block_dim_y),
-                       dim3(block_dim_x, block_dim_y),
+                jagged_softmax_backward_kernel<
+                    THREADS_PER_BLOCK,
+                    index_t,
+                    scalar_t>
+                    <<<grid,
+                       THREADS_PER_BLOCK,
                        0,
                        at::cuda::getCurrentCUDAStream()>>>(
                         grad_output.packed_accessor32<scalar_t, 2>(),
