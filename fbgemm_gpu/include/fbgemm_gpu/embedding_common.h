fe85c98918087031d6fbe01f5edfb4d99f2a7349
Luoshang Pan <pls331@meta.com>
2023-03-30 12:55:47 -0700

    make tbe bounds check mode configurable at model loading time & fix loader dependency for AtomicCounter (#1664)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1664
    [XLForamt V2|Loading] make tbe bounds check mode configurable at model loading time
    
    hg status:? _run_on_acc_0.txt
    ? a.txt
    ? core_1677205474_devgpu003.nao2.facebook.com_3477120.nvcudmp
    ```
    
    Differential Revision: D44391390
    
    fbshipit-source-id: 95df5ab8082fb2ce4f5ca870fc485ae76a4c323b

--- fbgemm_gpu/include/fbgemm_gpu/embedding_common.h
+++ fbgemm_gpu/include/fbgemm_gpu/embedding_common.h
@@ -8,6 +8,7 @@
 #include <ATen/ATen.h>
 #include <c10/macros/Macros.h>
 #include <cstdint>
+#include <string>
 
 namespace fbgemm_gpu {
 
@@ -38,6 +39,19 @@ enum class BoundsCheckMode : uint8_t {
   WARNING = 1,
   IGNORE = 2,
 };
+
+inline std::string getBoundsCheckModeName(BoundsCheckMode mode) {
+  switch (mode) {
+    case BoundsCheckMode::FATAL:
+      return "FATAL";
+    case BoundsCheckMode::WARNING:
+      return "WARNING";
+    case BoundsCheckMode::IGNORE:
+      return "IGNORE";
+    default:
+      return "UNDEFINED";
+  }
+}
 
 inline at::ScalarType getScalarType(SparseType dtype) {
   switch (dtype) {
