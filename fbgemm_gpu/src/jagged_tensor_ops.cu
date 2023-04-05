eeae864e91bb3ad2d74419006c63d58b359f7f8a
Benson Ma <bensonma415@meta.com>
2023-04-04 17:20:39 -0700

    wippp

--- fbgemm_gpu/src/jagged_tensor_ops.cu
+++ fbgemm_gpu/src/jagged_tensor_ops.cu
@@ -1475,11 +1475,10 @@ void jagged_jagged_elementwise_dense_output_(
     }                                                                         \
     jagged_jagged_elementwise_dense_output_kernel_<NUM_JAGGED_DIM, index_t>   \
         <<<blocks, threads, 0, at::cuda::getCurrentCUDAStream()>>>(           \
-            x_values.packed_accessor32<scalar_t, 2, at::RestrictPtrTraits>(), \
+            MAKE_PACKED_TENSOR_ACCESSOR_32(x_values, scalar_t, 2),            \
             x_offset_ptrs,                                                    \
-            y_values.packed_accessor32<scalar_t, 2, at::RestrictPtrTraits>(), \
-            output_reshaped                                                   \
-                .packed_accessor32<scalar_t, 3, at::RestrictPtrTraits>(),     \
+            MAKE_PACKED_TENSOR_ACCESSOR_32(y_values, scalar_t, 2),            \
+            MAKE_PACKED_TENSOR_ACCESSOR_32(output_reshaped, scalar_t, 3),     \
             jagged_dims_tensor,                                               \
             f,                                                                \
             padding_value);                                                   \
@@ -1724,10 +1723,15 @@ Tensor batched_dense_vec_jagged_2d_mul_forward(
                        dim3(block_dim_x, block_dim_y),
                        0,
                        at::cuda::getCurrentCUDAStream()>>>(
+                        // MAKE_PACKED_TENSOR_ACCESSOR_32(v, scalar_t, 2),
+                        // MAKE_PACKED_TENSOR_ACCESSOR_32(a_values, scalar_t, 2),
+                        // MAKE_PACKED_TENSOR_ACCESSOR_32(a_offsets, index_t, 1),
+                        // MAKE_PACKED_TENSOR_ACCESSOR_32(output, scalar_t, 2)
                         v.packed_accessor32<scalar_t, 2>(),
                         a_values.packed_accessor32<scalar_t, 2>(),
                         a_offsets.packed_accessor32<index_t, 1>(),
-                        output.packed_accessor32<scalar_t, 2>());
+                        output.packed_accessor32<scalar_t, 2>()
+                       );
                 C10_CUDA_KERNEL_LAUNCH_CHECK();
               });
         });
