ac478ad28f0b3e0d42bbe55754412fa7173563f6
Benson Ma <bensonma415@meta.com>
2023-05-11 23:26:07 -0700

    Enable `LICENSELINT` in FBGEMM, pt. 2 (#1761)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1761
    
    - Run license-lint on fbgemm source files
    
    Reviewed By: shintaro-iwasaki
    
    Differential Revision: D45796375
    
    fbshipit-source-id: 80256aa2f4bae527873f1c5ad93e8a07c27e581b

--- include/fbgemm/FbgemmFP16.h
+++ include/fbgemm/FbgemmFP16.h
@@ -1,9 +1,11 @@
 /*
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  * All rights reserved.
+ *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 #pragma once
 
 // WARNING: this is a legacy fp16 fbgemm implementation and will soon be
