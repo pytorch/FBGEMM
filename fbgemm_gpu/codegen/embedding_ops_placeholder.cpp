2ae07d00cadcbe86ce46cc667204533777633d89
Benson Ma <bensonma415@meta.com>
2023-05-17 18:51:41 -0700

    Create separate targets for training and inference (#1757)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1757
    
    - Create separate targets for training and inference
    
    - Redefine the old `embedding_ops` and `embedding_ops` as an empty
    target with `exported_defs` pointing to the new split targets
    
    Reviewed By: sryap
    
    Differential Revision: D45687293
    
    fbshipit-source-id: 2adfaee5d0bdd075164749db9ebe7fc032c626ee

---
+++ fbgemm_gpu/codegen/embedding_ops_placeholder.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/*
+    This is placeholder code to force compilation and generation of an
+    `libdeeplearning_fbgemm_fbgemm_gpu_codegen_embedding_ops.so` file, which
+    allows downstream PyTorch code to contlinue loading the `embedding_ops“
+    and `embedding_ops_cpu` (now-)shim targets correctly.
+*/
+namespace fbgemm_gpu {}
