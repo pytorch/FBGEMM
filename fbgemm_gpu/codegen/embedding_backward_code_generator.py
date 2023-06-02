1c6a13ff341ea0f33f69600cdd37f94974097161
Benson Ma <bensonma415@meta.com>
2023-06-01 20:52:25 -0700

    Remove CPU support for lars_sgd (#1774)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1774
    
    This diff is a part of FBGEMM TBE optimizer deprecation plan (see
    https://github.com/pytorch/FBGEMM/discussions/1727)
    
    Changes include:
    - Setting `has_cpu_support` of `lars_sgd` to
      `False` in the code gen script
    - Updating `codegen/TARGETS` and `CMakeLists.txt`
    
    See D45835048 for how the changes affect the code generation
    
    Reviewed By: csmiler
    
    Differential Revision: D45845181
    
    fbshipit-source-id: eb3b2239ffe9ec6e1e9d78ddd0fd937be11198c9

--- fbgemm_gpu/codegen/embedding_backward_code_generator.py
+++ fbgemm_gpu/codegen/embedding_backward_code_generator.py
@@ -1399,7 +1399,7 @@ def lars_sgd() -> None:
         split_weight_update=split_weight_update,
         split_post_update="",
         split_weight_update_cpu=split_weight_update_cpu,
-        has_cpu_support=True,
+        has_cpu_support=False,
         has_gpu_support=True,
         has_vbe_support=False,
     )
