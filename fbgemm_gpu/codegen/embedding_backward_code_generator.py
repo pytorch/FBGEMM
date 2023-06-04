202c2f6ba59309177abc90b333ccae9f3ffd1c8c
Benson Ma <bensonma415@meta.com>
2023-06-03 18:25:05 -0700

    Remove CPU support for partial_rowwise_adam (#1773)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1773
    
    This diff is a part of FBGEMM TBE optimizer deprecation plan (see
    https://github.com/pytorch/FBGEMM/discussions/1727)
    
    Changes include:
    - Setting `has_cpu_support` of `partial_rowwise_adam` to
      `False` in the code gen script
    - Updating `codegen/TARGETS` and `CMakeLists.txt`
    
    See D45835048 for how the changes affect the code generation
    
    Reviewed By: csmiler
    
    Differential Revision: D45845400
    
    fbshipit-source-id: 10e63c9b5a8b27cde75638424fc506658343af7a

--- fbgemm_gpu/codegen/embedding_backward_code_generator.py
+++ fbgemm_gpu/codegen/embedding_backward_code_generator.py
@@ -1337,7 +1337,7 @@ def partial_rowwise_adam() -> None:
         split_weight_update=split_weight_update,
         split_post_update="",
         split_weight_update_cpu=split_weight_update_cpu,
-        has_cpu_support=True,
+        has_cpu_support=False,
         has_gpu_support=True,
         has_vbe_support=False,
     )
