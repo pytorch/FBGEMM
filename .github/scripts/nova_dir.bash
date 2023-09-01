489ccfcf34f9656822ae10d692050caaee842637
Supadchaya Puangpontip <supadchaya@fb.com>
2023-09-01 09:41:42 -0700

    Use Nova workflow to host all published wheel files at PyTorch site (#1958)
    Summary:
    
    To alleviate CUDA version mismatch issues, we publish fbgemm-gpu-nightly with different CUDA versions. This diff uses Nova workflow host the published wheel files at PyTorch site instead.
    
    https://github.com/pytorch/FBGEMM/issues/1947
    
    Differential Revision: D48487102

---
+++ .github/scripts/nova_dir.bash
@@ -0,0 +1,14 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+## Workaround for nova workflow to build wheels from fbgemm_gpu folder
+FBGEMM_DIR="/__w/FBGEMM/FBGEMM"
+FBGEMM_REPO="$FBGEMM_DIR/$REPOSITORY"
+export FBGEMM_REPO
+working_dir=$(pwd)
+BUILD_FROM_NOVA=1
+export BUILD_FROM_NOVA
+if [[ "$working_dir" == "$FBGEMM_REPO" ]]; then cd fbgemm_gpu || echo "Failed to cd fbgemm_gpu from $(pwd)"; fi
