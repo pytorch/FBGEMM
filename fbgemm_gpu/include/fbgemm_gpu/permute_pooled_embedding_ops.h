0fffa91fc9e6f134734d8b3da9309ef3af791dea
Abdul Zainul-Abedin <azainul@meta.com>
2023-09-08 07:58:44 -0700

    Add permute_duplicate_pooled_embeddings op for CPU (#1939)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/1939
    
    This diff builds ontop of the pervious diff and adds support for permute_duplicate_pooled_embeddings for CPU.
    
    # Background
    Currently permute_pooled_embs_gpu does not support duplicates in a permutation, this poses a problem with passing the same embeddings to multiple modules. This doc proposes a solution to allow duplicate subsets in the resultant permutation.
    
    # Details
    The required implementation of permute_duplicate_pooled_embs_gpu should support a subset being repeated. This is represented by having duplicates in the permute list. This also results in the output list size being greater than the input list.
    
    Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
    Offset_dims: [0,  2,  5,  6, 10]
    Permute: [3, 0, 2, 1, 3]
    
    Output:  [6, 7, 8, 9, 0, 1, 5, 2, 3, 4, 6, 7, 8, 9]
    
    Reviewed By: sryap
    
    Differential Revision: D48305145
    
    fbshipit-source-id: e308338f105eb95f8066f554b8143dfb4524c7e6

--- fbgemm_gpu/include/fbgemm_gpu/permute_pooled_embedding_ops.h
+++ fbgemm_gpu/include/fbgemm_gpu/permute_pooled_embedding_ops.h
@@ -11,6 +11,22 @@
 #include <ATen/ATen.h>
 
 namespace fbgemm_gpu {
+
+at::Tensor permute_pooled_embs_cpu_impl(
+    const at::Tensor& pooled_embs, // [B_local][Sum_T_global(D)]
+    const at::Tensor& offset_dim_list,
+    const at::Tensor& permute_list,
+    const at::Tensor& inv_offset_dim_list,
+    const at::Tensor& inv_permute_list,
+    const bool& allow_duplicates);
+
+at::Tensor permute_duplicate_pooled_embs_cpu(
+    const at::Tensor& pooled_embs, // [B_local][Sum_T_global(D)]
+    const at::Tensor& offset_dim_list,
+    const at::Tensor& permute_list,
+    const at::Tensor& inv_offset_dim_list,
+    const at::Tensor& inv_permute_list);
+
 at::Tensor permute_pooled_embs_cpu(
     const at::Tensor& pooled_embs, // [B_local][Sum_T_global(D)]
     const at::Tensor& offset_dim_list,
