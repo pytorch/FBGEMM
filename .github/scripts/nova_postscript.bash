9aac51f135080b978277374974aba726d6c10569
Supadchaya Puangpontip <supadchaya@meta.com>
2023-09-11 19:40:13 -0700

    [FBGEMM] Fix nova

--- .github/scripts/nova_postscript.bash
+++ .github/scripts/nova_postscript.bash
@@ -16,7 +16,9 @@ export GITHUB_ENV
 echo "Current working directory: $(pwd)"
 # shellcheck disable=SC1091
 # shellcheck source=.github/scripts/setup_env.bash
-. $PRELUDE; install_fbgemm_gpu_wheel "${BUILD_ENV_NAME}" fbgemm_gpu/dist/*.whl
+. ${PRELUDE};
+
+install_fbgemm_gpu_wheel "${BUILD_ENV_NAME}" fbgemm_gpu/dist/*.whl
 
 # Test with PyTest
 echo "Current working directory: $(pwd)"
@@ -24,8 +26,6 @@ CPU_GPU=${CU_VERSION}
 if [ "${CU_VERSION}" != 'cpu' ]; then
     CPU_GPU=""
 fi
-python3 -c "import torch; print("cuda.is_available() ", torch.cuda.is_available()); print ("device_count() ",torch.cuda.device_count());"
-# shellcheck disable=SC1091
-# shellcheck source=.github/scripts/setup_env.bash
-cd "${FBGEMM_REPO}" || echo "Failed to cd to ${FBGEMM_REPO}"
-. $PRELUDE; cd fbgemm_gpu/test || { echo "Failed to cd to fbgemm_gpu/test from $(pwd)"; exit 1; }; run_fbgemm_gpu_tests "${BUILD_ENV_NAME}" "${CPU_GPU}"
+$CONDA_RUN python3 -c "import torch; print('cuda.is_available() ', torch.cuda.is_available()); print ('device_count() ',torch.cuda.device_count());"
+cd "${FBGEMM_REPO}/fbgemm_gpu/test" || { echo "Failed to cd to fbgemm_gpu/test from $(pwd)"; };
+run_fbgemm_gpu_tests "${BUILD_ENV_NAME}" "${CPU_GPU}"
