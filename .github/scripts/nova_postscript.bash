67a5b9f0d966eff59a64f34e523f6fb2ec843c23
Supadchaya Puangpontip <supadchaya@meta.com>
2023-09-11 18:15:24 -0700

    [FBGEMM] Fix Nova

--- .github/scripts/nova_postscript.bash
+++ .github/scripts/nova_postscript.bash
@@ -6,23 +6,26 @@
 # LICENSE file in the root directory of this source tree.
 
 echo "Current working directory: $(pwd)"
-cd "$FBGEMM_REPO" || echo "Failed to cd to $FBGEMM_REPO"
-PRELUDE=.github/scripts/setup_env.bash
+cd "${FBGEMM_REPO}" || echo "Failed to cd to ${FBGEMM_REPO}"
+PRELUDE="${FBGEMM_REPO}/.github/scripts/setup_env.bash"
 BUILD_ENV_NAME=base
+GITHUB_ENV=TRUE
+export GITHUB_ENV
 
 # Install FBGEMM_GPU Nightly
 echo "Current working directory: $(pwd)"
 # shellcheck disable=SC1091
 # shellcheck source=.github/scripts/setup_env.bash
-. $PRELUDE; install_fbgemm_gpu_package "$BUILD_ENV_NAME" fbgemm_gpu/dist/*.whl
+. $PRELUDE; install_fbgemm_gpu_package "${BUILD_ENV_NAME}" fbgemm_gpu/dist/*.whl
 
 # Test with PyTest
 echo "Current working directory: $(pwd)"
-CPU_GPU=$CU_VERSION
-if [ "$CU_VERSION" != 'cpu' ]; then
+CPU_GPU=${CU_VERSION}
+if [ "${CU_VERSION}" != 'cpu' ]; then
     CPU_GPU=""
 fi
-python3 -c "import torch; print(torch.cuda.is_available()); print (torch.cuda.device_count());"
+python3 -c "import torch; print("cuda.is_available() ", torch.cuda.is_available()); print ("device_count() ",torch.cuda.device_count());"
 # shellcheck disable=SC1091
 # shellcheck source=.github/scripts/setup_env.bash
-. $PRELUDE; cd fbgemm_gpu/test || { echo "Failed to cd to fbgemm_gpu/test from $(pwd)"; exit 1; }; run_fbgemm_gpu_tests "$BUILD_ENV_NAME" "$CPU_GPU"
+cd "${FBGEMM_REPO}" || echo "Failed to cd to ${FBGEMM_REPO}"
+. $PRELUDE; cd fbgemm_gpu/test || { echo "Failed to cd to fbgemm_gpu/test from $(pwd)"; exit 1; }; run_fbgemm_gpu_tests "${BUILD_ENV_NAME}" "${CPU_GPU}"
--- .github/scripts/nova_prescript.bash
+++ .github/scripts/nova_prescript.bash
@@ -6,78 +6,66 @@
 # LICENSE file in the root directory of this source tree.
 
 echo "Current working directory: $(pwd)"
-cd "$FBGEMM_REPO" || echo "Failed to cd to $FBGEMM_REPO"
-PRELUDE=.github/scripts/setup_env.bash
+cd "${FBGEMM_REPO}" || echo "Failed to cd to ${FBGEMM_REPO}"
+PRELUDE="${FBGEMM_REPO}/.github/scripts/setup_env.bash"
+echo $PRELUDE
 BUILD_ENV_NAME=base
 echo "----- conda env list -----"
 conda env list
 echo "--------------------------"
-export PATH="$PATH:/usr/sbin:/sbin"
-echo "CU_VERSION = $CU_VERSION"
-echo "PYTHON_VERSION = $PYTHON_VERSION"
+export PATH="${PATH}:/usr/sbin:/sbin"
+echo "CU_VERSION = ${CU_VERSION}"
+echo "PYTHON_VERSION = ${PYTHON_VERSION}"
 echo "python3 --version = $(python3 --version)"
-echo "ARCH = $ARCH"
+echo "ARCH = ${ARCH}"
 echo "---------------------------"
+# shellcheck disable=SC1091
+# shellcheck source=.github/scripts/setup_env.bash
+. ${PRELUDE};
+
 ## Display System Info
-# shellcheck disable=SC1091
-# shellcheck source=.github/scripts/setup_env.bash
-. $PRELUDE; print_system_info
-# shellcheck source=.github/scripts/setup_env.bash
+print_system_info
+
 ## Display GPU Info
-# shellcheck disable=SC1091
-# shellcheck source=.github/scripts/setup_env.bash
-. $PRELUDE; print_gpu_info
+print_gpu_info
+
 ## Install C/C++ Compilers
-# shellcheck disable=SC1091
-# shellcheck source=.github/scripts/setup_env.bash
-. $PRELUDE; install_cxx_compiler "$BUILD_ENV_NAME"
+install_cxx_compiler "${BUILD_ENV_NAME}"
+
 ## Install Build Tools
-# shellcheck disable=SC1091
-# shellcheck source=.github/scripts/setup_env.bash
-. $PRELUDE; install_build_tools "$BUILD_ENV_NAME"
+install_build_tools "${BUILD_ENV_NAME}"
+
 ## Install cuDNN
-# shellcheck disable=SC1091
-# shellcheck source=.github/scripts/setup_env.bash
-CPU_GPU=$CU_VERSION
-if [ "$CU_VERSION" != 'cpu' ]; then
-    cuda_version_num=$(echo "$CU_VERSION" | cut -c 3-)
-    # shellcheck disable=SC1091
-    # shellcheck source=.github/scripts/setup_env.bash
-    . $PRELUDE; install_cudnn "$BUILD_ENV_NAME" "$(pwd)/build_only/cudnn" "$cuda_version_num"
+CPU_GPU=${CU_VERSION}
+if [ "${CU_VERSION}" != 'cpu' ]; then
+    install_cudnn "${BUILD_ENV_NAME}" "$(pwd)/build_only/cudnn" "${CU_VERSION}"
     echo "-------- Finding NVML_LIB_PATH -----------"
-    NVML_LIB_PATH=$(find "$FBGEMM_DIR" -name libnvidia-ml.so) || echo "libnvidia-ml.so not found in $FBGEMM_DIR!"
-    echo "NVML_LIB_PATH = $NVML_LIB_PATH"
-    [[ $NVML_LIB_PATH = "" ]] && NVML_LIB_PATH=$(find "$CUDA_HOME" -name libnvidia-ml.so) || echo "libnvidia-ml.so not found in $CUDA_HOME!"
-    [[ $NVML_LIB_PATH = "" ]] && NVML_LIB_PATH=$(find "$CONDA_PREFIX" -name libnvidia-ml.so) || echo "libnvidia-ml.so not found in $CONDA_PREFIX!"
-    export NVML_LIB_PATH
+    echo "NVML_LIB_PATH = ${NVML_LIB_PATH}"
+    [[ ${NVML_LIB_PATH} = "" ]] && (NVML_LIB_PATH=$(find "${FBGEMM_DIR}" -name libnvidia-ml.so) || echo "libnvidia-ml.so not found in ${FBGEMM_DIR}!")
+    [[ ${NVML_LIB_PATH} = "" ]] && (NVML_LIB_PATH=$(find "${CONDA_PREFIX}" -name libnvidia-ml.so) || echo "libnvidia-ml.so not found in ${CONDA_PREFIX}!")
+    echo "NVML_LIB_PATH = ${NVML_LIB_PATH}"
     echo "------------------------------------------"
     CPU_GPU="cuda"
 fi
-## Prepare FBGEMM_GPU Build
-# shellcheck disable=SC1091
-# shellcheck source=.github/scripts/setup_env.bash
-. $PRELUDE; cd fbgemm_gpu || { echo "Failed to cd to fbgemm_gpu/test from $(pwd)"; exit 1; }; prepare_fbgemm_gpu_build "$BUILD_ENV_NAME"
+
+cd "${FBGEMM_REPO}/fbgemm_gpu" || { echo "Failed to cd to fbgemm_gpu from $(pwd)"; }
+prepare_fbgemm_gpu_build "${BUILD_ENV_NAME}"
 
 # reset NOVA flag to run setup.py
 BUILD_FROM_NOVA=0
 export BUILD_FROM_NOVA
 
 ## Build FBGEMM_GPU Nightly
-cd "$FBGEMM_REPO" || echo "Failed to cd to $FBGEMM_REPO from $(pwd)"
-if [[ $CHANNEL == "" ]]; then CHANNEL="nightly"; fi
+cd "${FBGEMM_REPO}/fbgemm_gpu " || echo "Failed to cd to ${FBGEMM_REPO}/fbgemm_gpu from $(pwd)"
+if [[ ${CHANNEL} == "" ]]; then CHANNEL="nightly"; fi #set nightly by default
 echo "----------------------------------------------"
-echo ". $PRELUDE; build_fbgemm_gpu_package $BUILD_ENV_NAME $CHANNEL $CPU_GPU"
-# shellcheck disable=SC1091
-# shellcheck source=.github/scripts/setup_env.bash
-. $PRELUDE; cd fbgemm_gpu || echo "Failed to cd to fbgemm_gpu from $(pwd)"; build_fbgemm_gpu_package "$BUILD_ENV_NAME" "$CHANNEL" "$CPU_GPU"
+echo "build_fbgemm_gpu_package ${BUILD_ENV_NAME} ${CHANNEL} ${CPU_GPU}"
+build_fbgemm_gpu_package "${BUILD_ENV_NAME}" "${CHANNEL}" "${CPU_GPU}"
 echo "----------------------------------------------"
-
-# ## For debugging - preventing the container from terminating
-# for ((i=1; i<=10; i++)); do sleep 30m; echo "30 mins hold"; done
 
 ## Temporary workaround - copy dist/ to root repo for smoke test
 echo "Copying dist folder to root repo.."
-(cp -r dist "$FBGEMM_REPO") && (echo "dist folder has been copied to $FBGEMM_REPO") || echo "Failed to copy dist/ folder to $FBGEMM_REPO"
+(cp -r "${FBGEMM_REPO}/fbgemm_gpu/dist" "${FBGEMM_REPO}") && (echo "dist folder has been copied to ${FBGEMM_REPO}") || echo "Failed to copy dist/ folder to ${FBGEMM_REPO}"
 echo "----------------------------------"
-ls -al "$FBGEMM_REPO/dist"
+ls -al "${FBGEMM_REPO}/dist"
 echo "----------------------------------"
