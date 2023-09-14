b7a00290641d51597341480fe925f25d9b708198
Supadchaya Puangpontip <supadchaya@meta.com>
2023-09-14 04:11:05 -0700

    Use Nova workflow to host all published wheel files at PyTorch site (#2016)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2016
    
    To alleviate CUDA version mismatch issues, we aim to publish fbgemm-gpu-nightly with different CUDA versions. This diff uses Nova workflow and will host the published wheels at PyTorch site instead.
    https://github.com/pytorch/FBGEMM/issues/1947
    
    Reviewed By: q10
    
    Differential Revision: D49258503
    
    fbshipit-source-id: a06d095b0c03df62d8cea8fb8db1b5018c9a9dd7

---
+++ .github/scripts/nova_postscript.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+echo "Current working directory: $(pwd)"
+cd "${FBGEMM_REPO}" || echo "Failed to cd to ${FBGEMM_REPO}"
+PRELUDE="${FBGEMM_REPO}/.github/scripts/setup_env.bash"
+BUILD_ENV_NAME=base
+GITHUB_ENV=TRUE
+export GITHUB_ENV
+
+# Install FBGEMM_GPU Nightly
+echo "Current working directory: $(pwd)"
+# shellcheck disable=SC1091
+# shellcheck source=.github/scripts/setup_env.bash
+. "${PRELUDE}";
+
+install_fbgemm_gpu_wheel "${BUILD_ENV_NAME}" fbgemm_gpu/dist/*.whl
+
+# Test with PyTest
+echo "Current working directory: $(pwd)"
+CPU_GPU="${CU_VERSION}"
+if [ "${CU_VERSION}" != 'cpu' ]; then
+    CPU_GPU=""
+fi
+$CONDA_RUN python3 -c "import torch; print('cuda.is_available() ', torch.cuda.is_available()); print ('device_count() ',torch.cuda.device_count());"
+cd "${FBGEMM_REPO}/fbgemm_gpu/test" || { echo "Failed to cd to fbgemm_gpu/test from $(pwd)"; };
+run_fbgemm_gpu_tests "${BUILD_ENV_NAME}" "${CPU_GPU}"
