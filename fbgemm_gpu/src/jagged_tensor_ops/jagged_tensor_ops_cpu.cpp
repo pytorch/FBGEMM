f8b3e30201a2e5aec15aca88afff88d517c8d109
Supadchaya Puangpontip <supadchaya@meta.com>
2023-09-22 17:06:44 -0700

    Fix jagged_test_index_select_2d that hangs in OSS and revert skip tests (#2036)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2036
    
    Before c++20, `std::atomic_flag` is initialized to an unspecified state, hence the loop `while (lock.test_and_set(std::memory_order_acquire)` is never broken and causes the test to hang in OSS. This diff properly initializes the `std::atomic_flag`.
    
    Reviewed By: q10, sryap
    
    Differential Revision: D49528661
    
    fbshipit-source-id: ba2213cb9bf8c0abbd1e169db03f0e32dd2a7ebb

--- fbgemm_gpu/src/jagged_tensor_ops/jagged_tensor_ops_cpu.cpp
+++ fbgemm_gpu/src/jagged_tensor_ops/jagged_tensor_ops_cpu.cpp
@@ -1151,6 +1151,13 @@ void jagged_index_add_2d_kernel(
   const auto num_cols = input.size(1);
   // Allocate one lock per row
   std::atomic_flag* locks = new std::atomic_flag[output.size(0)];
+  // Initialize all locks since before c++20 std::atomic_flag is initialized to
+  // an unspecified state.
+  // https://en.cppreference.com/w/cpp/atomic/atomic_flag/atomic_flag
+  for (auto i = 0; i < output.size(0); i++) {
+    locks[i].clear();
+  }
+
   at::parallel_for(0, num_dense_input_rows, 0, [&](int64_t start, int64_t end) {
     for (const auto dense_input_offset : c10::irange(start, end)) {
       int index_pos;
