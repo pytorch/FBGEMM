4c60f91b22aec7d043f995475b29a86d1df867b5
cyy <cyyever@outlook.com>
2023-10-05 01:11:32 -0700

    Fix inconsistent dll linkage warning (#2059)
    Summary: Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2059
    
    Reviewed By: sryap
    
    Differential Revision: D49938275
    
    Pulled By: q10
    
    fbshipit-source-id: 3110c89af9d80e64f2a4715eed0d3e2f93f0e3c8

--- include/fbgemm/FbgemmFPCommon.h
+++ include/fbgemm/FbgemmFPCommon.h
@@ -73,6 +73,7 @@ FBGEMM_API void cblas_gemm_compute(
     int thread_id = 0,
     int num_threads = 1);
 
+#if defined(FBGEMM_EXPORTS)
 // autotuned kernel splits for various cases m = 1:mb_max
 template <typename T>
 void cblas_gemm_compute(
@@ -252,6 +253,7 @@ void cblas_gemm_compute(
     }
   }
 }
+#endif
 
 #undef FBGEMM_USE_REF_KERNEL
 } // namespace fbgemm
