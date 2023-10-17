daf9042e6da9a6348f62323bbcc356cea7d1444c
Richard Zou <rzou@meta.com>
2023-10-17 15:44:01 -0700

    Turn on generate_opcheck_tests for jagged_tensor_ops_test.py (#2073)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2073
    
    This PR turns on automatically generated opcheck tests for
    jagged_tensor_ops_test.py. I reverted the original one because the fbgemm OSS
    CI failures were real.
    
    Reviewed By: ezyang
    
    Differential Revision: D50241452
    
    fbshipit-source-id: 98192cbbb7d0e4e72c3cafd3c85ab6ad3a166da1

--- fbgemm_gpu/test/test_utils.py
+++ fbgemm_gpu/test/test_utils.py
@@ -4,13 +4,14 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import inspect
 import math
 import os
 import struct
 import subprocess
 import unittest
 from functools import wraps
-from typing import Any, Callable, List, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import hypothesis.strategies as st
 import numpy as np
@@ -200,6 +201,102 @@ def cpu_and_maybe_gpu() -> st.SearchStrategy[List[torch.device]]:
     return st.sampled_from(
         [torch.device("cpu")] + ([torch.device("cuda")] if gpu_available else [])
     )
+
+
+def has_optests() -> bool:
+    return torch.__version__ >= "2.2.*"
+
+
+class optests:
+    # Usage examples:
+    #
+    # @generate_opcheck_tests
+    # class MyOpTest(unittest.TestCase):
+    #     ...
+    #
+    # @generate_opcheck_tests(additional_decorators={})
+    # class MyOpTest(unittest.TestCase):
+    #     ...
+    #
+    @staticmethod
+    # pyre-ignore[3]
+    def generate_opcheck_tests(
+        test_class: Optional[unittest.TestCase] = None,
+        *,
+        # pyre-ignore[24]: Generic type `Callable` expects 2 type parameters.
+        additional_decorators: Optional[Dict[str, Callable]] = None,
+    ):
+        if additional_decorators is None:
+            additional_decorators = {}
+
+        def decorator(test_class: unittest.TestCase) -> unittest.TestCase:
+            if not has_optests():
+                return test_class
+            import torch.testing._internal.optests as optests
+            from torch._utils_internal import get_file_path_2
+
+            filename = inspect.getfile(test_class)
+            failures_dict_path = get_file_path_2(
+                "", os.path.dirname(filename), "failures_dict.json"
+            )
+            optests.generate_opcheck_tests(
+                test_class,
+                ["fb", "fbgemm"],
+                failures_dict_path,
+                # pyre-ignore[6]
+                additional_decorators,
+                [
+                    "test_schema",
+                    "test_autograd_registration",
+                    "test_faketensor",
+                    "test_aot_dispatch_static",
+                    "test_aot_dispatch_dynamic",
+                ],
+            )
+            return test_class
+
+        if test_class is None:
+            return decorator
+        else:
+            return decorator(test_class)
+
+    @staticmethod
+    def is_inside_opcheck_mode() -> bool:
+        if not has_optests():
+            return False
+
+        import torch.testing._internal.optests as optests
+
+        return optests.is_inside_opcheck_mode()
+
+    @staticmethod
+    # pyre-ignore[3]
+    def dontGenerateOpCheckTests(reason: str):
+        if not has_optests():
+            return lambda fun: fun
+        import torch.testing._internal.optests as optests
+
+        return optests.dontGenerateOpCheckTests(reason)
+
+
+# Version of torch.autograd.gradcheck that works with generate_opcheck_tests.
+# The problem with just torch.autograd.gradcheck is that it results in
+# very slow tests when composed with generate_opcheck_tests.
+def gradcheck(
+    # pyre-ignore[24]: Generic type `Callable` expects 2 type parameters.
+    f: Callable,
+    # pyre-ignore[2]
+    inputs: Union[torch.Tensor, Tuple[Any, ...]],
+    *args: Any,
+    **kwargs: Any,
+) -> None:
+    if optests.is_inside_opcheck_mode():
+        if isinstance(inputs, torch.Tensor):
+            f(inputs)
+        else:
+            f(*inputs)
+        return
+    torch.autograd.gradcheck(f, inputs, *args, **kwargs)
 
 
 def cpu_only() -> st.SearchStrategy[List[torch.device]]:
