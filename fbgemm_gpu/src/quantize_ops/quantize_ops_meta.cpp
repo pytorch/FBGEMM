7854b9df6311b2d60cf145011d3327471efcdc2d
William Wen <williamwen@meta.com>
2023-11-30 11:11:53 -0800

    Get fbgemm::FloatToFP8RowwiseQuantized opcheck tests passing
    Summary: Title
    
    Reviewed By: zou3519
    
    Differential Revision: D51692611

--- fbgemm_gpu/src/quantize_ops/quantize_ops_meta.cpp
+++ fbgemm_gpu/src/quantize_ops/quantize_ops_meta.cpp
@@ -49,10 +49,28 @@ Tensor FP8rowwise_to_float_meta(
   }
 }
 
+Tensor FloatToFP8RowwiseQuantized_meta(const Tensor& input, bool forward) {
+  TORCH_CHECK(input.is_contiguous(), "input must be contiguous");
+
+  const at::SymIntArrayRef input_sizes = input.sym_sizes();
+
+  const auto last_dim = input_sizes.size() - 1;
+  const at::SymInt ncols = input_sizes[last_dim];
+  const at::SymInt ncols_aligned = (ncols + 4 - 1) / 4 * 4;
+  const at::SymInt output_columns = ncols_aligned + 2 * sizeof(float);
+
+  auto output_dims = input_sizes.vec();
+  output_dims[last_dim] = output_columns;
+  return at::empty_symint(output_dims, input.options().dtype(at::kByte));
+}
+
 } // namespace fbgemm_gpu
 
 TORCH_LIBRARY_IMPL(fbgemm, Meta, m) {
   m.impl(
       "FP8RowwiseQuantizedToFloat",
       TORCH_FN(fbgemm_gpu::FP8rowwise_to_float_meta));
+  m.impl(
+      "FloatToFP8RowwiseQuantized",
+      TORCH_FN(fbgemm_gpu::FloatToFP8RowwiseQuantized_meta));
 }
