c877c30ba5df6fe80db36637ea6b61f22c6fa64b
Richard Zou <rzou@meta.com>
2023-11-30 11:00:04 -0800

    Make fbgemm::permute_1D_sparse_data, permute_2D_sparse_data pt2_compliant (#2180)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2180
    
    - We fix fbgemm::permute_1d_sparse_data's implementation to never return
      views. The schema for the operator promises that it is functional. This was
      causing flaky tests.
    - fbgemm::permute_2d_sparse_data already has that fix applied, so we just
      remove the skip
    
    Reviewed By: williamwen42
    
    Differential Revision: D51711257
    
    fbshipit-source-id: 388aa89d7b7be3036ec3a620eb1c239e2e02d0dc

--- fbgemm_gpu/src/sparse_ops/sparse_ops_cpu.cpp
+++ fbgemm_gpu/src/sparse_ops/sparse_ops_cpu.cpp
@@ -2730,9 +2730,11 @@ TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
   m.def(
       "permute_sparse_data(Tensor permute, Tensor lengths, Tensor values, Tensor? weights=None, SymInt? permuted_lengths_sum=None) -> (Tensor, Tensor, Tensor?)");
   m.def(
-      "permute_2D_sparse_data(Tensor permute, Tensor lengths, Tensor values, Tensor? weights=None, SymInt? permuted_lengths_sum=None) -> (Tensor, Tensor, Tensor?)");
+      "permute_2D_sparse_data(Tensor permute, Tensor lengths, Tensor values, Tensor? weights=None, SymInt? permuted_lengths_sum=None) -> (Tensor, Tensor, Tensor?)",
+      {PT2_COMPLIANT_TAG});
   m.def(
-      "permute_1D_sparse_data(Tensor permute, Tensor lengths, Tensor values, Tensor? weights=None, SymInt? permuted_lengths_sum=None) -> (Tensor, Tensor, Tensor?)");
+      "permute_1D_sparse_data(Tensor permute, Tensor lengths, Tensor values, Tensor? weights=None, SymInt? permuted_lengths_sum=None) -> (Tensor, Tensor, Tensor?)",
+      {PT2_COMPLIANT_TAG});
   m.def("invert_permute(Tensor permute) -> Tensor");
   m.def(
       "expand_into_jagged_permute(Tensor permute, Tensor input_offset, Tensor output_offset, SymInt output_size) -> Tensor");
