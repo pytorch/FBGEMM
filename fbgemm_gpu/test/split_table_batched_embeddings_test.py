c2f64ec1c734466be9f8e0e0e90c794dcd909c77
Sarunya Pumma <sarunya@fb.com>
2023-12-06 17:42:58 -0800

    Enable sequence TBE CPU via AVX (#2195)
    Summary:
    
    Instead of using the ref implementation for sequence embedding on CPU,
    this diff directs TBE to invoke the AVX implementation by forcing
    pooling factors of 1 (i.e., passing `at::arange(index_size)` as
    offfsets).  The performance gained from using the AVX implementation
    offsets the overhead incurs in creating the new offsets.
    
    Differential Revision: D51918878

--- fbgemm_gpu/test/split_table_batched_embeddings_test.py
+++ fbgemm_gpu/test/split_table_batched_embeddings_test.py
@@ -4562,11 +4562,10 @@ class SplitTableBatchedEmbeddingsTest(unittest.TestCase):
                 [
                     SparseType.FP32,
                     SparseType.FP16,
-                    # CPU sequence embedding does not support FP8/INT4/INT2 yet
-                    # SparseType.FP8,
+                    SparseType.FP8,
                     SparseType.INT8,
-                    # SparseType.INT4,
-                    # SparseType.INT2,
+                    SparseType.INT4,
+                    SparseType.INT2,
                 ]
             )
 
