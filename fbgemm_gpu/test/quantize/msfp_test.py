edd4de1c481c7194d66aeb454b29297cb347c93c
Benson Ma <bensonma415@meta.com>
2024-01-10 15:49:29 -0800

    Break up quantize ops tests (#2260)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2260
    
    - Break up quantize ops tests into multiple files for easier maintenance
    
    Reviewed By: jianyuh
    
    Differential Revision: D52646900
    
    fbshipit-source-id: dad1e22e65f19f89498f3f1afd0a9fd254baf721

---
+++ fbgemm_gpu/test/quantize/msfp_test.py
@@ -0,0 +1,63 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+import hypothesis.strategies as st
+import torch
+from hypothesis import given, HealthCheck, settings
+
+
+try:
+    # pyre-ignore[21]
+    from fbgemm_gpu import open_source  # noqa: F401
+
+    # pyre-ignore[21]
+    from test_utils import gpu_unavailable
+except Exception:
+    if torch.version.hip:
+        torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops_hip")
+    else:
+        torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops")
+
+    torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops_cpu")
+    from fbgemm_gpu.test.test_utils import gpu_unavailable
+
+
+class TestMSFPQuantizationConversion(unittest.TestCase):
+    @unittest.skipIf(*gpu_unavailable)
+    # pyre-ignore [56]: Invalid decoration, was not able to infer the type of argument
+    @given(
+        nrows=st.integers(min_value=0, max_value=100),
+        ncols=st.integers(min_value=0, max_value=100),
+    )
+    @settings(deadline=10000, suppress_health_check=[HealthCheck.filter_too_much])
+    def test_quantize_op(self, nrows: int, ncols: int) -> None:
+        ebits = 8
+        mbits = 7
+        bias = 127
+        max_pos = (1 << ((1 << ebits) - 2 - bias)) * (2 - 2 ** (-mbits))
+        min_pos = 2 ** (1 - bias - mbits)
+        bounding_box_size = 16
+        print("MSFP parameters", bounding_box_size, ebits, mbits, bias)
+        input_data = torch.rand(nrows, ncols).float()
+        quantized_data = torch.ops.fbgemm.FloatToMSFPQuantized(
+            input_data.cuda(),
+            bounding_box_size,
+            ebits,
+            mbits,
+            bias,
+            min_pos,
+            max_pos,
+        )
+        dequantized_data = torch.ops.fbgemm.MSFPQuantizedToFloat(
+            quantized_data.cuda(), ebits, mbits, bias
+        )
+        torch.testing.assert_close(dequantized_data.cpu(), input_data, rtol=1, atol=0)
+
+
+if __name__ == "__main__":
+    unittest.main()
