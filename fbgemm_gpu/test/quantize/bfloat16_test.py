3f1fe5d1f0344b0690f9830937e8e0ce06d520f6
Benson Ma <bensonma415@meta.com>
2024-01-31 23:18:29 -0800

    Fix Quantize tests (#2301)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2301
    
    - Fix Quantize tests to pass again
    
    Reviewed By: jspark1105
    
    Differential Revision: D53283205
    
    fbshipit-source-id: 35b5a5d20423620b1a6997bc7e7bd505d52306a5

--- fbgemm_gpu/test/quantize/bfloat16_test.py
+++ fbgemm_gpu/test/quantize/bfloat16_test.py
@@ -13,18 +13,7 @@ import numpy as np
 import torch
 from hypothesis import given, HealthCheck, settings
 
-
-try:
-    # pyre-ignore[21]
-    from fbgemm_gpu import open_source  # noqa: F401
-
-except Exception:
-    if torch.version.hip:
-        torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops_hip")
-    else:
-        torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops")
-
-    torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops_cpu")
+from . import common  # noqa E402
 
 
 class SparseNNOperatorsGPUTest(unittest.TestCase):
--- fbgemm_gpu/test/quantize/msfp_test.py
+++ fbgemm_gpu/test/quantize/msfp_test.py
@@ -10,20 +10,13 @@ import hypothesis.strategies as st
 import torch
 from hypothesis import given, HealthCheck, settings
 
+from . import common  # noqa E402
+from .common import open_source
 
-try:
-    # pyre-ignore[21]
-    from fbgemm_gpu import open_source  # noqa: F401
-
+if open_source:
     # pyre-ignore[21]
     from test_utils import gpu_unavailable
-except Exception:
-    if torch.version.hip:
-        torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops_hip")
-    else:
-        torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops")
-
-    torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops_cpu")
+else:
     from fbgemm_gpu.test.test_utils import gpu_unavailable
 
 
