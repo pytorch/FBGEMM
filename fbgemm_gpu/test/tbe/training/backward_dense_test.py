74eb809558024c5c5bb38ba5731fb8904b518215
Supadchaya Puangpontip <supadchaya@meta.com>
2024-02-05 18:58:14 -0800

    [WIP] Skip test_backward_dense

--- fbgemm_gpu/test/tbe/training/backward_dense_test.py
+++ fbgemm_gpu/test/tbe/training/backward_dense_test.py
@@ -31,13 +31,14 @@ from ..common import open_source
 
 if open_source:
     # pyre-ignore[21]
-    from test_utils import gpu_available, gradcheck, optests, TEST_WITH_ROCM
+    from test_utils import gpu_available, gradcheck, optests, TEST_WITH_ROCM, running_on_github
 else:
     from fbgemm_gpu.test.test_utils import (
         gpu_available,
         gradcheck,
         optests,
         TEST_WITH_ROCM,
+        running_on_github
     )
 
 
@@ -76,6 +77,7 @@ class BackwardDenseTest(unittest.TestCase):
         deadline=None,
         suppress_health_check=[HealthCheck.filter_too_much, HealthCheck.data_too_large],
     )
+    @unittest.skipIf(*running_on_github)
     def test_backward_dense(  # noqa C901
         self,
         T: int,
