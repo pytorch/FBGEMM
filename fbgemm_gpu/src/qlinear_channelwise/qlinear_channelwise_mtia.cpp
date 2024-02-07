44fc10a3c56f89a769d73cc24b68e82a6c951ca3
Jiyuan Zhang <jiyuanz@meta.com>
2024-02-06 17:12:24 -0800

    add dynamic quantize gemm benchmark [step 2: fp16->int8 quantize] (#2295)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2295
    
    - Register the 2nd step operator `qlinear_quant` into FX stack
    - Add FX Kernel benchmark for dynamic quantized gemm step-2
    - Use `quantize_step` parameter to differentiate different stages
    - Separate Net modules for step-2 vs step-1 --
    
    result:
    https://fb-my.sharepoint.com/:x:/g/personal/jiyuanz_meta_com/Ec94q-KgmslMtQ7nIYT4240BZUyWiK-iQvP1cBgzfgEDWg?e=DfP82U
    
    1K x 1K: 638 cycles (5.10 us) --> 411 GB/s
    2K x 2K: 1200 cycles (9.6 us) --> 873 GB/s
    
    As a reference:
    1K x 1K x 1K FP16 GEMM: 20.30 us
    2K x 2K x 2K FP16 GEMM:  127.80 us
    
    Reviewed By: charliezjw
    
    Differential Revision: D52136852
    
    fbshipit-source-id: 1f967549019e4662261ccdfd72a5b6e49d72120b

--- fbgemm_gpu/src/qlinear_channelwise/qlinear_channelwise_mtia.cpp
+++ fbgemm_gpu/src/qlinear_channelwise/qlinear_channelwise_mtia.cpp
@@ -24,6 +24,23 @@ static at::Tensor qlinear_channelwise(
   return x;
 }
 
+static at::Tensor qlinear_quant(
+    at::Tensor x,
+    at::Tensor weight,
+    at::Tensor bias,
+    at::Tensor input_scale,
+    at::Tensor weight_scale,
+    at::Tensor weight_zero_point,
+    at::Tensor relu) {
+  assert(x.options().dtype() == at::kHalf);
+  assert(weight.options().dtype() == at::kQInt8);
+  assert(bias.options().dtype() == at::kFloat);
+  assert(input_scale.options().dtype() == at::kFloat);
+  assert(weight_scale.options().dtype() == at::kFloat);
+  assert(weight_zero_point.options().dtype() == at::kQUInt8);
+  return x;
+}
+
 TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
   m.def(
       "qlinear_channelwise(Tensor x, Tensor weight, Tensor "
@@ -32,4 +49,13 @@ TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
   m.impl(
       "qlinear_channelwise",
       torch::dispatch(c10::DispatchKey::CPU, TORCH_FN(qlinear_channelwise)));
+
+  m.def(
+      "qlinear_quant(Tensor x, Tensor weight, Tensor "
+      "bias, Tensor input_scale, Tensor weight_scale, Tensor "
+      "weight_zero_point, Tensor relu) -> Tensor");
+
+  m.impl(
+      "qlinear_quant",
+      torch::dispatch(c10::DispatchKey::CPU, TORCH_FN(qlinear_quant)));
 }
