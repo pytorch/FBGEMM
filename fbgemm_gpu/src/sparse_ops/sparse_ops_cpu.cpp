3a944fb7d257277c3f1f2494cbf58f41aee37c1d
Joshua Deng <joshuadeng@meta.com>
2024-02-07 10:29:36 -0800

    remove d2h sync from keyed jagged index select (#2315)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2315
    
    Add option to provide output length for values if known to avoid D to H sync. For permutation cases using keyed jagged index select where batch size. == len(lengths), the output length will be known to be len(values).
    
    Reviewed By: sryap
    
    Differential Revision: D53461566
    
    fbshipit-source-id: c4eb3d099e4a28351924ebe67e8e421ab4e727bb

--- fbgemm_gpu/src/sparse_ops/sparse_ops_cpu.cpp
+++ fbgemm_gpu/src/sparse_ops/sparse_ops_cpu.cpp
@@ -2925,7 +2925,7 @@ TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
   m.def(
       "bottom_k_per_row(Tensor input, Tensor k_offsets, bool requires_unique) -> Tensor");
   m.def(
-      "keyed_jagged_index_select_dim1(Tensor values, Tensor lengths, Tensor offsets, Tensor indices, SymInt batch_size, Tensor? weights=None) -> Tensor[]");
+      "keyed_jagged_index_select_dim1(Tensor values, Tensor lengths, Tensor offsets, Tensor indices, SymInt batch_size, Tensor? weights=None, SymInt? selected_lengths_sum=None) -> Tensor[]");
 }
 
 TORCH_LIBRARY_IMPL(fbgemm, CPU, m) {
