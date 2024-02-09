533c4bb0d12b0f84358657be02871447787f975c
Benson Ma <bensonma415@meta.com>
2024-02-08 21:18:35 -0800

    Enable compilation by clang (#2323)
    Summary: Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2323
    
    Reviewed By: spcyppt
    
    Differential Revision: D53597686
    
    Pulled By: q10
    
    fbshipit-source-id: c53ad045e14fd05106dcea6c12dfd0d0212e53a0

--- .github/scripts/utils_build.bash
+++ .github/scripts/utils_build.bash
@@ -41,71 +41,93 @@ setup_bazel () {
 # Build Tools Setup Functions
 ################################################################################
 
-install_cxx_compiler () {
-  local env_name="$1"
-  local use_system_package_manager="$2"
-  if [ "$env_name" == "" ]; then
-    echo "Usage: ${FUNCNAME[0]} ENV_NAME [USE_YUM]"
-    echo "Example(s):"
-    echo "    ${FUNCNAME[0]} build_env     # Install C/C++ compilers through Conda"
-    echo "    ${FUNCNAME[0]} build_env 1   # Install C/C++ compilers through the system package manager"
-    return 1
+__extract_archname () {
+  export archname=""
+  if [ "$MACHINE_NAME_LC" = "x86_64" ]; then
+    export archname="64"
+  elif [ "$MACHINE_NAME_LC" = "aarch64" ] || [ "$MACHINE_NAME_LC" = "arm64" ]; then
+    export archname="aarch64"
   else
-    echo "################################################################################"
-    echo "# Install C/C++ Compilers"
-    echo "#"
-    echo "# [$(date --utc +%FT%T.%3NZ)] + ${FUNCNAME[0]} ${*}"
-    echo "################################################################################"
-    echo ""
+    export archname="$MACHINE_NAME_LC"
   fi
+}
 
-  test_network_connection || return 1
+__conda_install_glibc () {
+  # sysroot_linux-<arch> needs to be installed alongside the C/C++ compiler for GLIBC:
+  #   https://root-forum.cern.ch/t/error-timespec-get-has-not-been-declared-with-conda-root-package/45712/6
+  #   https://github.com/conda-forge/conda-forge.github.io/issues/1625
+  #   https://conda-forge.org/docs/maintainer/knowledge_base.html#using-centos-7
+  #   https://github.com/conda/conda-build/issues/4371
 
-  if [ "$use_system_package_manager" != "" ]; then
-    echo "[INSTALL] Installing C/C++ compilers through the system package manager ..."
-    install_system_packages gcc gcc-c++
+  # shellcheck disable=SC2155
+  local env_prefix=$(env_name_or_prefix "${env_name}")
 
-  else
-    # Install gxx_linux-<arch> from conda-forge instead of from anaconda channel.
-    # sysroot_linux-<arch> needs to be installed alongside this:
-    #
-    #   https://root-forum.cern.ch/t/error-timespec-get-has-not-been-declared-with-conda-root-package/45712/6
-    #   https://github.com/conda-forge/conda-forge.github.io/issues/1625
-    #   https://conda-forge.org/docs/maintainer/knowledge_base.html#using-centos-7
-    #   https://github.com/conda/conda-build/issues/4371
-    #
-    # NOTE: We install g++ 10.x instead of 11.x becaue 11.x builds binaries that
-    # reference GLIBCXX_3.4.29, which may not be available on systems with older
-    # versions of libstdc++.so.6 such as CentOS Stream 8 and Ubuntu 20.04
-    local archname=""
-    if [ "$MACHINE_NAME_LC" = "x86_64" ]; then
-      archname="64"
-    elif [ "$MACHINE_NAME_LC" = "aarch64" ] || [ "$MACHINE_NAME_LC" = "arm64" ]; then
-      archname="aarch64"
-    else
-      archname="$MACHINE_NAME_LC"
-    fi
+  echo "[INSTALL] Installing GLIBC (architecture = ${archname}) ..."
+  # shellcheck disable=SC2086
+  (exec_with_retries 3 conda install ${env_prefix} -c conda-forge -y "sysroot_linux-${archname}"=2.17) || return 1
+}
 
-    # shellcheck disable=SC2155
-    local env_prefix=$(env_name_or_prefix "${env_name}")
+__conda_install_gcc () {
+  # Install gxx_linux-<arch> from conda-forge instead of from anaconda channel.
+  #
+  # NOTE: We install g++ 10.x instead of 11.x becaue 11.x builds binaries that
+  # reference GLIBCXX_3.4.29, which may not be available on systems with older
+  # versions of libstdc++.so.6 such as CentOS Stream 8 and Ubuntu 20.04
 
-    echo "[INSTALL] Installing C/C++ compilers through Conda (architecture = ${archname}) ..."
-    # shellcheck disable=SC2086
-    (exec_with_retries 3 conda install ${env_prefix} -y "gxx_linux-${archname}"=10.4.0 "sysroot_linux-${archname}"=2.17 -c conda-forge) || return 1
+  # shellcheck disable=SC2155
+  local env_prefix=$(env_name_or_prefix "${env_name}")
 
-    # The compilers are visible in the PATH as `x86_64-conda-linux-gnu-cc` and
-    # `x86_64-conda-linux-gnu-c++`, so symlinks will need to be created
-    echo "[INSTALL] Setting the C/C++ compiler symlinks ..."
-    # shellcheck disable=SC2155,SC2086
-    local cc_path=$(conda run ${env_prefix} printenv CC)
-    # shellcheck disable=SC2155,SC2086
-    local cxx_path=$(conda run ${env_prefix} printenv CXX)
+  echo "[INSTALL] Installing GCC through Conda (architecture = ${archname}) ..."
+  # shellcheck disable=SC2086
+  (exec_with_retries 3 conda install ${env_prefix} -c conda-forge -y "gxx_linux-${archname}"=10.4.0 ) || return 1
 
-    print_exec ln -s "${cc_path}" "$(dirname "$cc_path")/cc"
-    print_exec ln -s "${cc_path}" "$(dirname "$cc_path")/gcc"
-    print_exec ln -s "${cxx_path}" "$(dirname "$cxx_path")/c++"
-    print_exec ln -s "${cxx_path}" "$(dirname "$cxx_path")/g++"
-  fi
+  # The compilers are visible in the PATH as `x86_64-conda-linux-gnu-cc` and
+  # `x86_64-conda-linux-gnu-c++`, so symlinks will need to be created
+  echo "[INSTALL] Setting the C/C++ compiler symlinks ..."
+  # shellcheck disable=SC2155,SC2086
+  local cc_path=$(conda run ${env_prefix} printenv CC)
+  # shellcheck disable=SC2155,SC2086
+  local cxx_path=$(conda run ${env_prefix} printenv CXX)
+
+  print_exec ln -s "${cc_path}" "$(dirname "$cc_path")/cc"
+  print_exec ln -s "${cc_path}" "$(dirname "$cc_path")/gcc"
+  print_exec ln -s "${cxx_path}" "$(dirname "$cxx_path")/c++"
+  print_exec ln -s "${cxx_path}" "$(dirname "$cxx_path")/g++"
+}
+
+__conda_install_clang () {
+  # shellcheck disable=SC2155
+  local env_prefix=$(env_name_or_prefix "${env_name}")
+
+  echo "[INSTALL] Installing Clang through Conda ..."
+  # shellcheck disable=SC2086
+  (exec_with_retries 3 conda install ${env_prefix} -c conda-forge -y clangxx libcxx llvm-openmp compiler-rt ) || return 1
+
+  # The compilers are visible in the PATH as `clang` and `clang++`, so symlinks
+  # will need to be created
+  echo "[INSTALL] Setting the C/C++ compiler symlinks ..."
+  # shellcheck disable=SC2155,SC2086
+  local cc_path=$(conda run ${env_prefix} which clang)
+  # shellcheck disable=SC2155,SC2086
+  local cxx_path=$(conda run ${env_prefix} which clang++)
+
+  print_exec ln -s "${cc_path}" "$(dirname "$cc_path")/cc"
+  print_exec ln -s "${cc_path}" "$(dirname "$cc_path")/gcc"
+  print_exec ln -s "${cxx_path}" "$(dirname "$cxx_path")/c++"
+  print_exec ln -s "${cxx_path}" "$(dirname "$cxx_path")/g++"
+
+  echo "[INSTALL] Updating LD_LIBRARY_PATH ..."
+  # shellcheck disable=SC2155,SC2086
+  local ld_library_path=$(conda run ${env_prefix} printenv LD_LIBRARY_PATH)
+  # shellcheck disable=SC2155,SC2086
+  local conda_prefix=$(conda run ${env_prefix} printenv CONDA_PREFIX)
+  # shellcheck disable=SC2086
+  print_exec conda env config vars set ${env_prefix} LD_LIBRARY_PATH="${ld_library_path}:${conda_prefix}/lib"
+}
+
+__compiler_post_install_checks () {
+  # shellcheck disable=SC2155
+  local env_prefix=$(env_name_or_prefix "${env_name}")
 
   # Check C/C++ compilers are visible
   (test_binpath "${env_name}" cc) || return 1
@@ -129,12 +151,48 @@ install_cxx_compiler () {
 
   # https://stackoverflow.com/questions/4991707/how-to-find-my-current-compilers-standard-like-if-it-is-c90-etc
   echo "[INFO] Printing the default version of the C standard used by the compiler ..."
-  print_exec "conda run ${env_prefix} cc -dM -E - | grep __STDC_VERSION__"
+  print_exec "conda run ${env_prefix} cc -dM -E - < /dev/null | grep __STDC_VERSION__"
 
   # https://stackoverflow.com/questions/2324658/how-to-determine-the-version-of-the-c-standard-used-by-the-compiler
   echo "[INFO] Printing the default version of the C++ standard used by the compiler ..."
-  print_exec "conda run ${env_prefix} c++ -dM -E -x c++ - | grep __cplusplus"
+  print_exec "conda run ${env_prefix} c++ -dM -E -x c++ - < /dev/null | grep __cplusplus"
+}
 
+install_cxx_compiler () {
+  env_name="$1"
+  local compiler="$2"
+  if [ "$env_name" == "" ]; then
+    echo "Usage: ${FUNCNAME[0]} ENV_NAME [USE_YUM]"
+    echo "Example(s):"
+    echo "    ${FUNCNAME[0]} build_env clang  # Install C/C++ compilers (clang)"
+    echo "    ${FUNCNAME[0]} build_env gcc    # Install C/C++ compilers (gcc)"
+    return 1
+  else
+    echo "################################################################################"
+    echo "# Install C/C++ Compilers"
+    echo "#"
+    echo "# [$(date --utc +%FT%T.%3NZ)] + ${FUNCNAME[0]} ${*}"
+    echo "################################################################################"
+    echo ""
+  fi
+
+  test_network_connection || return 1
+
+  # Extract the archname
+  __extract_archname
+
+  # Install GLIBC
+  __conda_install_glibc
+
+  # Install the C/C++ compiler
+  if [ "$compiler" == "clang" ]; then
+    __conda_install_clang
+  else
+    __conda_install_gcc
+  fi
+
+  # Run post-install checks
+  __compiler_post_install_checks
   echo "[INSTALL] Successfully installed C/C++ compilers"
 }
 
@@ -161,7 +219,8 @@ install_build_tools () {
 
   echo "[INSTALL] Installing build tools ..."
   # shellcheck disable=SC2086
-  (exec_with_retries 3 conda install ${env_prefix} -y \
+  (exec_with_retries 3 conda install ${env_prefix} -c conda-forge -y \
+    blas \
     click \
     cmake \
     hypothesis \
