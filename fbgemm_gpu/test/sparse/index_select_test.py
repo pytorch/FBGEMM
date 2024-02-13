2c519bef568e91f2f89373cc79d9ae6c07eb8614
Benson Ma <bensonma415@meta.com>
2024-02-12 18:53:12 -0800

    More flexible test retries (#2328)
    Summary:
    - Allow for re-running tests for only the failed test cases, to avoid OOM errors with large test suites
    
    - Re-enable the `test_backward_dense` test
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2328
    
    Reviewed By: sryap
    
    Differential Revision: D53688642
    
    Pulled By: q10
    
    fbshipit-source-id: 368698a3a2a5088c796ea188faa655a778b42b6b

--- fbgemm_gpu/test/sparse/index_select_test.py
+++ fbgemm_gpu/test/sparse/index_select_test.py
@@ -23,14 +23,13 @@ from .common import extend_test_class, open_source
 
 if open_source:
     # pyre-ignore[21]
-    from test_utils import gpu_available, skipIfRocm
+    from test_utils import gpu_available
 else:
     import fbgemm_gpu.sparse_ops  # noqa: F401, E402
-    from fbgemm_gpu.test.test_utils import gpu_available, skipIfRocm
+    from fbgemm_gpu.test.test_utils import gpu_available
 
 
 class IndexSelectTest(unittest.TestCase):
-    @skipIfRocm()
     @given(
         N=st.integers(1, 32),
         shape=st.one_of(
