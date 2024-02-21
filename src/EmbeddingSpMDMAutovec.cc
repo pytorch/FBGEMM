bab6fd5d850fc1bb43ba1102caafc7c2c48bff23
Wei Su <wsu@meta.com>
2024-02-21 11:46:28 -0800

    int4-TBE auto-vec optimization by increasing prefetching (#2325)
    Summary:
    
    Increase prefetching and reduce backend stall as is suggested by NVIDIA
    
    Reviewed By: helloguo
    
    Differential Revision: D53552699

--- src/EmbeddingSpMDMAutovec.cc
+++ src/EmbeddingSpMDMAutovec.cc
@@ -56,11 +56,25 @@ bool EmbeddingSpMDMNBit_autovec(
   if (input_stride == -1) {
     input_stride = div_up(block_size, num_elem_per_byte) + scale_bias_offset;
   }
+
+  // more prefetch: prefetch up to 16 rows from the embedding table. Increasing
+  // prefetching helps reduce backend stall and therefore enable vectorization
+  // reach better of its potential. 16 is tuned for Neoverse-V2.
+  constexpr int64_t max_initial_prefetch_rows = 16;
+  const int64_t prefetch_stride = std::min(max_initial_prefetch_rows, index_size);
+  for (int pf_idx = 0; pf_idx < prefetch_stride; ++pf_idx) {
+    do_prefetch(
+        reinterpret_cast<const char*>(input + input_stride * indices[pf_idx]),
+        0,
+        0);
+  }
+
   int64_t current = 0;
   const int64_t rounded_bs = round_up(block_size, num_elem_per_byte);
-  vector<float> buf(rounded_bs);
+  vector<float> fma_res(rounded_bs);
+  vector<uint8_t> quantized_buf(rounded_bs);
   for (int m = 0; m < output_size; ++m) {
-    memset(buf.data(), 0, sizeof(float) * rounded_bs);
+    memset(fma_res.data(), 0, sizeof(float) * rounded_bs);
     int len = use_offsets ? offsets_or_lengths[m + 1] - offsets_or_lengths[m]
                           : offsets_or_lengths[m];
     if (current + len > index_size) {
@@ -72,14 +86,15 @@ bool EmbeddingSpMDMNBit_autovec(
 #endif
     for (int i = 0; i < len; ++i) {
       int64_t idx = indices[current];
-      int64_t prefetch_idx =
-          indices[std::min(current + tile_size, index_size - 1)];
-      do_prefetch(
-          reinterpret_cast<const char*>(input + input_stride * prefetch_idx),
-          1);
       if (idx < 0 || idx >= data_size) {
         return false;
       }
+      int64_t prefetch_idx =
+          indices[std::min(current + prefetch_stride, index_size - 1)];
+      do_prefetch(
+          reinterpret_cast<const char*>(input + input_stride * prefetch_idx),
+          0,
+          0);
 
       const float16* scale_bias = reinterpret_cast<const float16*>(
           input + input_stride * idx +
@@ -95,15 +110,16 @@ bool EmbeddingSpMDMNBit_autovec(
 
       const int64_t offset =
           input_stride * idx + (scale_bias_last ? 0 : scale_bias_offset);
+      const uint8_t* input_row = input + offset;
       if (bit_rate == 4) {
-        const size_t halfbufsz = (block_size + 1) / 2;
-        assert(halfbufsz > 0);
+        const size_t halfbufsz = (rounded_bs + 1) / 2;
         for (size_t j = 0; j < halfbufsz; ++j) {
-          uint8_t tmp = input[offset + j];
-          float quantized1 = float(tmp & 0xf);
-          float quantized2 = float(tmp >> 4);
-          buf[j * 2] = std::fma(scale, quantized1, buf[j * 2] + bias);
-          buf[j * 2 + 1] = std::fma(scale, quantized2, buf[j * 2 + 1] + bias);
+          quantized_buf[j * 2] = input_row[j] & 0xf;
+          quantized_buf[j * 2 + 1] = (input_row[j] >> 4);
+        }
+        for (int j = 0; j < rounded_bs; ++j) {
+          fma_res[j] =
+              std::fma(scale, (float)quantized_buf[j], fma_res[j] + bias);
         }
       } else if (bit_rate == 2) {
         size_t qbufsz = (block_size + 3) / 4;
@@ -113,13 +129,16 @@ bool EmbeddingSpMDMNBit_autovec(
         for (size_t j = 0; j < qbufsz; ++j) {
           uint8_t tmp = input[offset + j];
           float quantized1 = float(tmp & mask1);
-          buf[j * 4] = std::fma(scale, quantized1, buf[j * 4] + bias);
+          fma_res[j * 4] = std::fma(scale, quantized1, fma_res[j * 4] + bias);
           float quantized2 = float((tmp & mask2) >> 2);
-          buf[j * 4 + 1] = std::fma(scale, quantized2, buf[j * 4 + 1] + bias);
+          fma_res[j * 4 + 1] =
+              std::fma(scale, quantized2, fma_res[j * 4 + 1] + bias);
           float quantized3 = float((tmp & mask3) >> 4);
-          buf[j * 4 + 2] = std::fma(scale, quantized3, buf[j * 4 + 2] + bias);
+          fma_res[j * 4 + 2] =
+              std::fma(scale, quantized3, fma_res[j * 4 + 2] + bias);
           float quantized4 = float(tmp >> 6);
-          buf[j * 4 + 3] = std::fma(scale, quantized4, buf[j * 4 + 3] + bias);
+          fma_res[j * 4 + 3] =
+              std::fma(scale, quantized4, fma_res[j * 4 + 3] + bias);
         }
       }
       ++current;
@@ -129,23 +148,23 @@ bool EmbeddingSpMDMNBit_autovec(
       float scale = 1.f / len;
 #pragma omp simd
       for (int j = 0; j < block_size; ++j) {
-        buf[j] *= scale;
+        fma_res[j] *= scale;
       }
     }
     if (std::is_same<OutType, float>::value) {
 #pragma omp simd
       for (int j = 0; j < block_size; ++j) {
-        out[j] = buf[j];
+        out[j] = fma_res[j];
       }
     } else if (std::is_same<OutType, uint16_t>::value && is_bf16_out) {
 #pragma omp simd
       for (int j = 0; j < block_size; ++j) {
-        out[j] = cpu_bf162float(buf[j]);
+        out[j] = cpu_bf162float(fma_res[j]);
       }
     } else {
 #pragma omp simd
       for (int j = 0; j < block_size; ++j) {
-        out[j] = cpu_half2float(buf[j]);
+        out[j] = cpu_half2float(fma_res[j]);
       }
     }
     out += output_stride;
