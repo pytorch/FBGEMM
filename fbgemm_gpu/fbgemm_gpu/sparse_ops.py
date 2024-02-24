dc209666f0ee78d404ed20b269fe17f7934074c2
Shen Li <shenli@meta.com>
2024-02-23 16:32:24 -0800

    Allow jagged_index_select to accept pre-computed output shape
    Summary: `jagged_index_select`'s CPU kernel API already accepts `num_dense_output_rows` as an argument. Generalize this to the CUDA kernel as well, which can to avoid a CPU-blocking `.item()` call in the CUDA kernel if users decided to pre-compute it.
    
    Reviewed By: sryap, zhangruiskyline
    
    Differential Revision: D54085880
    
    fbshipit-source-id: c3d839f5d340ec485acc1daeca3838cb382b5d2c

--- fbgemm_gpu/fbgemm_gpu/sparse_ops.py
+++ fbgemm_gpu/fbgemm_gpu/sparse_ops.py
@@ -208,14 +208,15 @@ def jagged_index_select_2d_forward_v2_abstract(
     indices: Tensor,
     input_offsets: Tensor,
     output_offsets: Tensor,
+    num_dense_output_rows: Optional[int] = None,
 ) -> Tensor:
     torch._check(values.device == indices.device)
     torch._check(values.device == input_offsets.device)
     torch._check(values.device == output_offsets.device)
     torch._check(values.dim() == 2)
-    num_dense_output_rows = torch.library.get_ctx().new_dynamic_size()
+    dynamic_num_dense_output_rows = torch.library.get_ctx().new_dynamic_size()
     num_cols = values.size(1)
-    return values.new_empty([num_dense_output_rows, num_cols])
+    return values.new_empty([dynamic_num_dense_output_rows, num_cols])
 
 
 @impl_abstract("fbgemm::jagged_index_add_2d_forward_v2")
