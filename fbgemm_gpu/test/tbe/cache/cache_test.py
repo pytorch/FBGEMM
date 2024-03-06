bf0adea2228d8f949a515d93774fc617821f79a3
Levy Zhao <leiyuz@meta.com>
2024-03-05 20:08:06 -0800

    Measure the GPU time for prefetch stream to block computation stream
    Summary: Introduce AsyncSeriesTimer , a delayed timer to calculate CUDA events on FIFO order.
    
    Reviewed By: sryap
    
    Differential Revision: D54217908
    
    fbshipit-source-id: 37f1b5a6a52f3ce0474695bfdc3f4e5eef3ab581

--- fbgemm_gpu/test/tbe/cache/cache_test.py
+++ fbgemm_gpu/test/tbe/cache/cache_test.py
@@ -10,7 +10,7 @@
 # pyre-ignore-all-errors[56]
 
 import unittest
-from typing import Optional, Tuple
+from typing import cast, Optional, Tuple
 
 import hypothesis.strategies as st
 import numpy as np
@@ -40,6 +40,7 @@ from .cache_common import (
     generate_cache_tbes,
     gpu_unavailable,
     optests,
+    TestingStatsReporter,
     TestingStatsReporterConfig,
     VERBOSITY,
 )
@@ -205,6 +206,32 @@ class CacheTest(unittest.TestCase):
 
         assert_cache(output, output_ref, stochastic_rounding)
         self.assertTrue(torch.all(cc.lxu_cache_locking_counter == 0))
+
+        if prefetch_stream:
+            # We record timing info at batch 1, 3, 5
+
+            # But before, we need to wait until all backwards finished. Then
+            # force report for async timer
+            torch.cuda.synchronize()
+            assert cc.bwd_wait_prefetch_timer, "Timer must have been set"
+            cc.bwd_wait_prefetch_timer._lazy_report()
+
+            self.assertIsInstance(cc.stats_reporter, TestingStatsReporter)
+            stats_reporter = cast(TestingStatsReporter, cc.stats_reporter)
+            self.assertEqual(len(stats_reporter.reported_data), 3)
+            for step in [1, 3, 5]:
+                (
+                    rep_step,
+                    rep_event,
+                    rep_duration,
+                    rep_emb_id,
+                    rep_tbe_id,
+                ) = stats_reporter.reported_data.pop(0)
+                self.assertEqual(rep_step, step)
+                self.assertEqual(rep_event, "bwd_wait_for_prefetch")
+                self.assertGreaterEqual(float(rep_duration), 0)
+                self.assertEqual(rep_emb_id, "")
+                self.assertEqual(rep_tbe_id, "")
 
     @optests.dontGenerateOpCheckTests("Serial OOM")
     @unittest.skipIf(*gpu_unavailable)
