5e842e06699c4ab3489d1c70c06991e250de2463
Nathaniel Iskandar <niskandar@berkeley.edu>
2024-03-10 18:38:07 -0700

    Implemented FP32 Autovectorization; To Be Reviewed

--- src/EmbeddingSpMDMAutovec.cc
+++ src/EmbeddingSpMDMAutovec.cc
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#ifdef __linux__
+#ifdef __linux__ 
 
 #define FBGEMM_EXPORTS
 #include "./EmbeddingSpMDMAutovec.h"
@@ -152,6 +152,171 @@ bool EmbeddingSpMDMNBit_autovec(
   }
   return current == index_size;
 }
+
+
+template <typename InType, typename IndexType, typename OffsetType>
+bool EmbeddingSpMDMRowWiseSparse_autovec(
+    const int64_t block_size,
+    const int64_t output_size,
+    const int64_t index_size,
+    const int64_t uncompressed_data_size,
+    // const int64_t compressed_data_size,
+    const InType* input,
+    const IndexType* indices,
+    const int32_t* compressed_indices_table,
+    const OffsetType* offsets_or_lengths,
+    const float* weights, // optional, can be null for non-weighted sum
+    bool normalize_by_lengths,
+    float* out,
+    bool is_weight_positional,
+    bool use_offsets) {
+  bool is8bit = is_same<InType, uint8_t>::value;
+
+  if (is8bit) {
+    // block_size is the number of elements and fused_block_size is the size
+    // of an entire row, including scale and bias.
+    const auto scale_bias_offset = 2 * sizeof(float);
+    const int64_t fused_block_size = block_size + scale_bias_offset;
+    int64_t current = 0;
+    for (int m = 0; m < output_size; ++m) {
+      memset(out, 0, sizeof(float) * block_size);
+      int len = use_offsets ? offsets_or_lengths[m + 1] - offsets_or_lengths[m]
+                            : offsets_or_lengths[m];
+      if (current + len > index_size) {
+        return false;
+      }
+      for (int i = 0; i < len; ++i) {
+        IndexType uncompressed_idx = indices[current];
+        if (uncompressed_idx < 0 ||
+            uncompressed_idx >= uncompressed_data_size) {
+          return false;
+        }
+        IndexType idx = compressed_indices_table[uncompressed_idx];
+        if (idx == -1) {
+          ++current;
+          continue;
+        }
+        // if (idx < 0 || idx >= compressed_data_size) {
+        //   return false;
+        // }
+
+        const float* scale_bias = reinterpret_cast<const float*>(
+            input + fused_block_size * idx + block_size);
+
+        float weight = 1.0f;
+        if (weights) {
+          weight = weights[is_weight_positional ? i : current];
+        }
+        const float scale = weight * scale_bias[0];
+        const float bias = weight * scale_bias[1];
+
+        for (int j = 0; j < block_size; ++j) {
+          out[j] =
+              std::fma(scale, input[fused_block_size * idx + j], out[j] + bias);
+        }
+
+        ++current;
+      }
+      if (normalize_by_lengths && len) {
+        float scale = 1.f / len;
+        for (int j = 0; j < block_size; ++j) {
+          out[j] *= scale;
+        }
+      }
+      out += block_size;
+    }
+    return current == index_size;
+  } else {
+    // Reference implementation of FP32 SLS
+
+    // more prefetch: prefetch up to 16 rows from the embedding table. Increasing
+    // prefetching helps reduce backend stall and therefore enable vectorization
+    // reach better of its potential. 16 is tuned for Neoverse-V2.
+    constexpr int64_t max_initial_prefetch_rows = 16;
+    const int64_t prefetch_stride = std::min(max_initial_prefetch_rows, index_size);
+    for (int pf_idx = 0; pf_idx < prefetch_stride; ++pf_idx) {
+      do_prefetch(
+          reinterpret_cast<const char*>(input + input_stride * indices[pf_idx]),
+          0,
+          0);
+    }
+
+    int64_t current = 0;
+    for (int m = 0; m < output_size; ++m) {
+      memset(out, 0, sizeof(float) * block_size);
+      int len = use_offsets ? offsets_or_lengths[m + 1] - offsets_or_lengths[m]
+                            : offsets_or_lengths[m];
+      if (current + len > index_size) {
+        return false;
+      }
+
+      constexpr int tile_size = 4;
+      #if _OPENMP >= 202011
+      #pragma omp tile sizes(tile_size)
+      #endif
+      for (int i = 0; i < len; ++i) {
+        IndexType uncompressed_idx = indices[current];
+        if (uncompressed_idx < 0 ||
+            uncompressed_idx >= uncompressed_data_size) {
+          return false;
+        }
+        IndexType idx = compressed_indices_table[uncompressed_idx];
+        if (idx == -1) {
+          ++current;
+          continue;
+        }
+        // if (idx < 0 || idx >= compressed_data_size) {
+        //   return false;
+        // }
+
+        int64_t prefetch_idx =
+          indices[std::min(current + prefetch_stride, index_size - 1)];
+      do_prefetch(
+          reinterpret_cast<const char*>(input + input_stride * prefetch_idx),
+          0,
+          0);
+
+
+        float w = 1.f;
+        if (weights) {
+          w = weights[is_weight_positional ? i : current];
+        }
+
+        if(is_same<InType, float16>::value){
+          #pragma omp simd
+          for (int j = 0; j < block_size; ++j) {
+            const InType* inptr = input + block_size * idx + j;
+            out[j] = std::fma(
+                w,
+                cpu_half2float(*inptr),
+                out[j]);
+          }    
+        } else {
+          #pragma omp simd
+          for (int j = 0; j < block_size; ++j) {
+            const InType* inptr = input + block_size * idx + j;
+            out[j] = std::fma(
+                w,
+                *inptr,
+                out[j]);
+          }
+        }
+        
+        ++current;
+      }
+      if (normalize_by_lengths && len) {
+        float scale = 1.f / len;
+        #pragma omp simd
+        for (int j = 0; j < block_size; ++j) {
+          out[j] *= scale;
+        }
+      }
+      out += block_size;
+    }
+    return current == index_size;
+  }
+}
+
 
 #define INSTANTIATE_SPMDM_BASE(INDEX_TYPE, OFFSET_TYPE, OUT_TYPE) \
   template FBGEMM_API bool EmbeddingSpMDMNBit_autovec(            \
