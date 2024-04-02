437110c63fb66a17a1027b8a960387468fb0c41a
Benson Ma <bensonma415@meta.com>
2024-04-02 10:31:23 -0700

    Refactor the codegen directory, pt 5 (#2472)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2472
    
    - Break out forward code generation from `codegen/genscript/generate_embedding_optimizer.py`
    
    Reviewed By: sryap
    
    Differential Revision: D55603000
    
    fbshipit-source-id: 36bf8ae2e419500d32cdb8b941d5b645256c04b6

---
+++ fbgemm_gpu/codegen/genscript/generate_forward_split.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+# flake8: noqa F401
+
+import sys
+from typing import List
+
+try:
+    from .common import CodeTemplate
+except ImportError:
+    # pyre-ignore[21]
+    from common import CodeTemplate
+
+
+class ForwardSplitGenerator:
+    @staticmethod
+    def render_forward_templates(
+        template_filepath: str,
+        filename_format: str,
+        dense_options: List[bool],
+        nobag_options: List[bool],
+        vbe_options: List[bool],
+    ) -> None:
+        template = CodeTemplate.load(template_filepath)
+        for dense in dense_options:
+            for weighted in [True, False]:
+                for nobag in nobag_options:
+                    for vbe in vbe_options:
+                        if (not nobag or (not weighted and not vbe)) and (
+                            not dense or not vbe
+                        ):
+                            dense_desc = f"{ 'dense' if dense else 'split'}"
+                            weight_desc = (
+                                f"{ 'weighted' if weighted else 'unweighted' }"
+                            )
+                            nobag_desc = f"{ '_nobag' if nobag else '' }"
+                            vbe_desc = f"{ '_vbe' if vbe else '' }"
+
+                            template.write(
+                                filename_format.format(
+                                    f"{ dense_desc }_{ weight_desc }{ nobag_desc }{ vbe_desc }"
+                                ),
+                                dense=dense,
+                                weighted=weighted,
+                                nobag=nobag,
+                                vbe=vbe,
+                                is_index_select=False,
+                            )
+
+    @staticmethod
+    def generate_pt2_wrappers() -> None:
+        # Generate PT2 forward wrapper (CUDA)
+        CodeTemplate.load(
+            "embedding_split_host_pt2_cuda_wrapper_template.cpp",
+        ).write(
+            f"gen_embedding_forward_split_pt2_cuda_wrapper.cpp",
+            has_gpu_support=True,
+            is_forward=True,
+            has_vbe_support=True,
+        )
+
+        # Generate PT2 forward wrapper (CPU)
+        CodeTemplate.load(
+            "embedding_split_host_pt2_cpu_wrapper_template.cpp",
+        ).write(
+            f"gen_embedding_forward_split_pt2_cpu_wrapper.cpp",
+            has_cpu_support=True,
+            is_forward=True,
+        )
+
+    @staticmethod
+    def generate_small_kernels() -> None:
+        # Generate the small kernels (for nobag only) for the forward splits
+        template = CodeTemplate.load(
+            "embedding_forward_split_kernel_nobag_small_template.cu"
+        )
+        for dense in [True, False]:
+            wdesc = f"{ 'dense' if dense else 'split' }"
+            template.write(
+                f"gen_embedding_forward_{wdesc}_unweighted_nobag_kernel_small.cu",
+                dense=dense,
+                is_index_select=False,
+            )
+
+    @staticmethod
+    def generate_kernels() -> None:
+        # Generate the CUDA host code
+        ForwardSplitGenerator.render_forward_templates(
+            "embedding_forward_split_template.cu",
+            "gen_embedding_forward_{}_codegen_cuda.cu",
+            dense_options=[True, False],
+            nobag_options=[False],  # nobag is not used
+            vbe_options=[True, False],
+        )
+
+        # Generate the meta kernels
+        ForwardSplitGenerator.render_forward_templates(
+            "embedding_forward_split_meta_template.cpp",
+            "gen_embedding_forward_{}_codegen_meta.cpp",
+            dense_options=[True, False],
+            nobag_options=[False],  # nobag is not used
+            vbe_options=[True, False],
+        )
+
+        # Generate the CUDA kernels
+        ForwardSplitGenerator.render_forward_templates(
+            "embedding_forward_split_kernel_template.cu",
+            "gen_embedding_forward_{}_kernel.cu",
+            dense_options=[True, False],
+            nobag_options=[True, False],
+            vbe_options=[True, False],
+        )
+
+        # Generate the v2 CUDA kernels
+        ForwardSplitGenerator.render_forward_templates(
+            "embedding_forward_split_kernel_v2_template.cu",
+            "gen_embedding_forward_{}_v2_kernel.cu",
+            dense_options=[False],  # dense is not supported
+            nobag_options=[False],  # nobag is not supported
+            vbe_options=[False],  # vbe is not supported
+        )
+
+    @staticmethod
+    def generate() -> None:
+        ForwardSplitGenerator.generate_kernels()
+        ForwardSplitGenerator.generate_small_kernels()
+        ForwardSplitGenerator.generate_pt2_wrappers()
+
+
+def main() -> None:
+    ForwardSplitGenerator.generate()
+
+
+if __name__ == "__main__":
+    print(f"[FORWARD SPLIT GENERATOR] {sys.argv}")
+    main()
