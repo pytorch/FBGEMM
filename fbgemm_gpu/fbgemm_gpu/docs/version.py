f214708d899bb3e7bb69b8496aa9a128dd9aee2c
Christy Huang <hchristy1234@gmail.com>
2024-04-15 00:46:54 -0700

    switching mechanism for fp8 autovec-ref

---
+++ fbgemm_gpu/fbgemm_gpu/docs/version.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+__version__: str = "2024.4.15"
