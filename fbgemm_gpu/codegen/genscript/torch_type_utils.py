e64a166c9ab520bd08b98d7d3b3825d443451962
Sarunya Pumma <sarunya@meta.com>
2024-04-22 15:06:16 -0700

    Fix linter error in torch_type_utils.py (#2528)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2528
    
    As title
    
    Reviewed By: spcyppt
    
    Differential Revision: D56428934
    
    fbshipit-source-id: e27cf470e22cc37c567732444aeff626abcc99fe

--- fbgemm_gpu/codegen/genscript/torch_type_utils.py
+++ fbgemm_gpu/codegen/genscript/torch_type_utils.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 ######################################################################
-## PyTorch type utils
+# PyTorch type utils
 ######################################################################
 
 from dataclasses import dataclass
