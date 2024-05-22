0d6cd2b6b7d4893d3edf18d78510b5a5968ae4b4
Benson Ma <bensonma415@meta.com>
2024-05-22 11:25:38 -0700

    Fix Triton OSS installation (#2618)
    Summary:
    - Fix Triton OSS installation to install PyTorch-Triton instead
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2618
    
    Reviewed By: spcyppt
    
    Differential Revision: D57653228
    
    Pulled By: q10
    
    fbshipit-source-id: 2a8fedcb93ceb410d31488cba531d8760d6c0d7c

--- .github/scripts/utils_triton.bash
+++ .github/scripts/utils_triton.bash
@@ -8,12 +8,14 @@
 
 # shellcheck disable=SC1091,SC2128
 . "$( dirname -- "$BASH_SOURCE"; )/utils_base.bash"
+# shellcheck disable=SC1091,SC2128
+. "$( dirname -- "$BASH_SOURCE"; )/utils_pip.bash"
 
 ################################################################################
 # Triton Setup Functions
 ################################################################################
 
-install_triton () {
+install_triton_gitmodule () {
   local env_name="$1"
   local triton_version="$2"
   if [ "$env_name" == "" ]; then
@@ -24,7 +26,7 @@ install_triton () {
     return 1
   else
     echo "################################################################################"
-    echo "# Build + Install Triton"
+    echo "# Build + Install Triton (gitmodule)"
     echo "#"
     echo "# [$(date --utc +%FT%T.%3NZ)] + ${FUNCNAME[0]} ${*}"
     echo "################################################################################"
@@ -42,7 +44,7 @@ install_triton () {
     (print_exec git checkout "${triton_version}") || return 1
   fi
 
-  echo "[BUILD] Installing Triton ..."
+  echo "[BUILD] Installing Triton from gitmodule ..."
   # shellcheck disable=SC2086
   (exec_with_retries 3 conda run --no-capture-output ${env_prefix} python -m pip install -e .) || return 1
 
@@ -50,5 +52,31 @@ install_triton () {
   (test_python_import_package "${env_name}" triton) || return 1
 
   cd - || return 1
-  echo "[INSTALL] Successfully installed Triton ${triton_version}"
+  echo "[INSTALL] Successfully installed Triton ${triton_version} from gitmodule"
+}
+
+install_triton_pip () {
+  local env_name="$1"
+  if [ "$env_name" == "" ]; then
+    echo "Usage: ${FUNCNAME[0]} ENV_NAME"
+    echo "Example(s):"
+    echo "    ${FUNCNAME[0]} build_env"
+    return 1
+  else
+    echo "################################################################################"
+    echo "# Install PyTorch (PyTorch PIP)"
+    echo "#"
+    echo "# [$(date --utc +%FT%T.%3NZ)] + ${FUNCNAME[0]} ${*}"
+    echo "################################################################################"
+    echo ""
+  fi
+
+  echo "[BUILD] Installing Triton from PIP ..."
+  # shellcheck disable=SC2086
+  install_from_pytorch_pip "${env_name}" pytorch-triton nightly/3.0.0+45fff310c8 || return 1
+
+  # shellcheck disable=SC2086
+  (test_python_import_package "${env_name}" triton) || return 1
+
+  echo "[INSTALL] Successfully installed PyTorch through PyTorch PIP"
 }
