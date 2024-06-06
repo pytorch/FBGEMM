edb27b17471f281ec332e83263d1a2c2b6a6ce92
Jie You <jyou@meta.com>
2024-06-06 15:26:21 -0700

    Move ITEP FBGEMM ops to OSS: Step 1 (#2690)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2690
    
    # Context and steps
    Due to torch package compatibility concern. We are copying files to OSS first, then deprecate and remove the `/fb` version when production workflows have migrated.
    
    Changelist:
    - Copy 3 files from `/fbcode/deeplearning/fbgemm/fbgemm_gpu/fb/src` to `fbcode/deeplearning/fbgemm/fbgemm_gpu/src/intraining_embedding_pruning_ops/`
    
    This diff also a benchmark script to be using the new OSS FBGEMM Kernel.
    
    Reviewed By: PaulZhang12
    
    Differential Revision: D58217535
    
    fbshipit-source-id: 9736fb20a0cf72d02fc804e40984249088d67b27

---
+++ fbgemm_gpu/src/intraining_embedding_pruning_ops/intraining_embedding_pruning_gpu.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <ATen/ATen.h>
+#include <ATen/core/op_registration/op_registration.h>
+#include <ATen/cuda/CUDAContext.h>
+#include <torch/library.h>
+#include "fbgemm_gpu/intraining_embedding_pruning.h"
+
+TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
+  m.def(
+      "init_address_lookup(Tensor address_lookups, Tensor buffer_offsets, Tensor emb_sizes) -> ()");
+  DISPATCH_TO_CUDA("init_address_lookup", fbgemm_gpu::init_address_lookup_cuda);
+  m.def(
+      "prune_embedding_tables(int iter, int pruning_interval, Tensor address_lookups, Tensor row_utils, Tensor buffer_offsets, Tensor emb_sizes) -> (Tensor, Tensor, int)");
+  DISPATCH_TO_CUDA(
+      "prune_embedding_tables", fbgemm_gpu::prune_embedding_tables_cuda);
+  // row_util can be updated in remap_indices_update_utils
+  m.def(
+      "remap_indices_update_utils("
+      "    int iter, "
+      "    Tensor buffer_idx, "
+      "    Tensor feature_lengths, "
+      "    Tensor feature_offsets, "
+      "    Tensor values, "
+      "    Tensor address_lookup, "
+      "    Tensor(a!) row_util, "
+      "    Tensor buffer_offsets, "
+      "    Tensor[]? full_values_list=None"
+      ") -> Tensor");
+  DISPATCH_TO_CUDA(
+      "remap_indices_update_utils",
+      fbgemm_gpu::remap_indices_update_utils_cuda);
+}
