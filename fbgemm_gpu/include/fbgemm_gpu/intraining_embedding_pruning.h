34403978dac830baa4e7a9a6ba6b72b7306845cc
Jie You <jyou@meta.com>
2024-06-06 12:14:21 -0700

    Move ITEP FBGEMM ops to OSS: Step 1 (#2690)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2690
    
    # Context and steps
    Due to torch package compatibility concern. We are copying files to OSS first, then deprecate and remove the `/fb` version when production workflows have migrated.
    
    Changelist:
    - Copy 3 files from `/fbcode/deeplearning/fbgemm/fbgemm_gpu/fb/src` to `fbcode/deeplearning/fbgemm/fbgemm_gpu/src/intraining_embedding_pruning_ops/`
    
    This diff also a benchmark script to be using the new OSS FBGEMM Kernel.
    
    Reviewed By: PaulZhang12
    
    Differential Revision: D58217535

---
+++ fbgemm_gpu/include/fbgemm_gpu/intraining_embedding_pruning.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <ATen/ATen.h>
+#include <ATen/core/TensorAccessor.h>
+#include <torch/torch.h>
+#include <cstdint>
+
+#include "fbgemm_gpu/embedding_common.h"
+#include "fbgemm_gpu/sparse_ops_utils.h"
+
+using Tensor = at::Tensor;
+
+namespace fbgemm_gpu {
+
+/*
+ * In-training embedding pruning util functions
+ */
+
+void init_address_lookup_cuda(
+    Tensor address_lookups,
+    Tensor buffer_offsets,
+    Tensor emb_sizes);
+
+std::tuple<Tensor, Tensor, int64_t> prune_embedding_tables_cuda(
+    int64_t iter,
+    int64_t pruning_interval,
+    Tensor address_lookups,
+    Tensor row_utils,
+    Tensor buffer_offsets,
+    Tensor emb_sizes);
+
+Tensor remap_indices_update_utils_cuda(
+    const int64_t iter,
+    const Tensor& buffer_idx,
+    const Tensor& feature_lengths,
+    const Tensor& feature_offsets,
+    const Tensor& values,
+    const Tensor& address_lookup,
+    Tensor& row_util,
+    const Tensor& buffer_offsets,
+    const std::optional<std::vector<Tensor>>& full_values_list);
+
+} // namespace fbgemm_gpu
