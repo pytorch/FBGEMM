ce92291f52b44d99b78ef40f5b6e7b80e3676aff
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-06-10 09:13:24 -0700

    Pyre Faster Type Checking By Default Mode Headers] [batch:1/188] [shard:3/N] (#2704)
    Summary: Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2704
    
    Reviewed By: connernilsen
    
    Differential Revision: D58348553
    
    fbshipit-source-id: 4f16abdcee6e6f19cc328973cbc91398a8d51d6b

--- fbgemm_gpu/fbgemm_gpu/tbe/cache/__init__.py
+++ fbgemm_gpu/fbgemm_gpu/tbe/cache/__init__.py
@@ -5,4 +5,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 from .split_embeddings_cache_ops import get_unique_indices_v2  # noqa: F401
