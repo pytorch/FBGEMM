8ae99879f1f7e73c7ea760c6f369e113a2edfa3f
Jiyuan Zhang <jiyuanz@meta.com>
2024-06-10 10:32:14 -0700

    step 3: int8gemm + dequantize kernel (#2705)
    Summary: Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2705
    
    Differential Revision: D52978388

--- fbgemm_gpu/src/qlinear_channelwise/qlinear_channelwise_mtia.cpp
+++ fbgemm_gpu/src/qlinear_channelwise/qlinear_channelwise_mtia.cpp
@@ -18,7 +18,9 @@ static at::Tensor qlinear_channelwise(
     at::Tensor input_scale,
     at::Tensor weight_scale,
     at::Tensor weight_zero_point,
+    at::Tensor y_off,
     at::Tensor relu) {
+  return x;
   // quantized linear function with
   // activation: per-tensor quantization,
   // weight: per-tensor quantization
@@ -166,14 +168,21 @@ static at::Tensor qlinear_dynamic(
   x = qlinear_quant(
       x, weight, bias, input_scale, weight_scale, weight_zero_point, relu);
   return qlinear_channelwise(
-      x, weight, bias, input_scale, weight_scale, weight_zero_point, relu);
+      x,
+      weight,
+      bias,
+      input_scale,
+      weight_scale,
+      weight_zero_point,
+      relu,
+      relu);
 }
 
 TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
   m.def(
       "qlinear_channelwise(Tensor x, Tensor weight, Tensor "
       "bias, Tensor input_scale, Tensor weight_scale, Tensor "
-      "weight_zero_point, Tensor relu) -> Tensor");
+      "weight_zero_point, Tensor y_off, Tensor relu) -> Tensor");
   m.impl(
       "qlinear_channelwise",
       torch::dispatch(c10::DispatchKey::CPU, TORCH_FN(qlinear_channelwise)));
