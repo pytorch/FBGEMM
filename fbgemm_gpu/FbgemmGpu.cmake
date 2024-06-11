7d6d4900f988de23d81652f6e2cd3a64f5743e87
Shikai Li <shikaili@fb.com>
2024-06-11 14:46:59 -0700

    Add cudnn frontend dependency.

--- fbgemm_gpu/FbgemmGpu.cmake
+++ fbgemm_gpu/FbgemmGpu.cmake
@@ -32,6 +32,7 @@ set(fbgemm_sources_include_directories
   ${THIRDPARTY}/cutlass/include
   ${THIRDPARTY}/cutlass/tools/util/include
   ${THIRDPARTY}/json/include
+  ${THIRDPARTY}/cudnn-frontend/include
   ${NCCL_INCLUDE_DIRS})
 
 
@@ -41,6 +42,12 @@ set(fbgemm_sources_include_directories
 
 file(GLOB_RECURSE asmjit_sources
   "${CMAKE_CURRENT_SOURCE_DIR}/../external/asmjit/src/asmjit/*/*.cpp")
+
+set(third_party_include_directories
+  ${THIRDPARTY}/asmjit/src
+  ${THIRDPARTY}/cpuinfo/include
+  ${THIRDPARTY}/cutlass/include
+  ${THIRDPARTY}/cudnn-frontend/include)
 
 
 ################################################################################
