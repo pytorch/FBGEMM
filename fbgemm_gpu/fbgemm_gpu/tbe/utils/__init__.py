64bcdc38a490c74ad26c77d328ac6770517b296b
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-06-25 11:19:06 -0700

    Pyre Faster Type Checking By Default Mode Headers] [batch:27/245] [shard:4/N] (#2782)
    Summary: Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2782
    
    Reviewed By: connernilsen
    
    Differential Revision: D58992587
    
    fbshipit-source-id: c2799de5d81002e3581e72047512de38646e14aa

--- fbgemm_gpu/fbgemm_gpu/tbe/utils/__init__.py
+++ fbgemm_gpu/fbgemm_gpu/tbe/utils/__init__.py
@@ -5,6 +5,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 from .common import get_device, round_up, to_device  # noqa: F401
 from .offsets import b_indices, get_table_batched_offsets_from_dense  # noqa: F401
 from .quantize import fake_quantize_embs, quantize_embs  # noqa: F401
