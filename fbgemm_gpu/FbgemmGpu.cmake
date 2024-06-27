469119b1088e2ddca190177f3c5d0e4261a0137e
Benson Ma <bensonma415@meta.com>
2024-06-27 12:06:16 -0700

    [fbgemm_gpu] Fix the missing `libcuda` linking
    - Fix the missing `libcuda` linking issue that started with torch nightly 2.5.0.dev20240622 with the introduction of https://github.com/pytorch/pytorch/pull/128801

--- fbgemm_gpu/FbgemmGpu.cmake
+++ fbgemm_gpu/FbgemmGpu.cmake
@@ -683,12 +683,8 @@ set_target_properties(fbgemm_gpu_py PROPERTIES PREFIX "")
 # Link to PyTorch
 target_link_libraries(fbgemm_gpu_py
   ${TORCH_LIBRARIES}
-  ${NCCL_LIBRARIES})
-
-# Append -lcuda flag, since TORCH_LIBRARIES doesn't include this
-if(NOT FBGEMM_CPU_ONLY AND NOT USE_ROCM)
-  set_target_properties(fbgemm_gpu_py PROPERTIES LINK_FLAGS "-lcuda")
-endif()
+  ${NCCL_LIBRARIES}
+  ${CUDA_DRIVER_LIBRARIES})
 
 # Link to NVML
 if(NVML_LIB_PATH)
