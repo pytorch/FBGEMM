7ae9d7f9f588f979e18c071efb0f99e7d5a7677e
Benson Ma <bensonma415@meta.com>
2024-07-01 19:37:20 -0700

    Break up `fbgemm_cuda_utils.cuh`, pt 4 (#2799)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2799
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/3
    
    - Move `Vec4AccT` out of `fbgemm_cuda_utils.cuh` and into `utils/` subdirectory
    
    Reviewed By: spcyppt
    
    Differential Revision: D59240060
    
    fbshipit-source-id: f262175e3a815ea67a95afb0b25ae9c8899b5a09

--- fbgemm_gpu/include/fbgemm_gpu/embedding_forward_template_helpers.cuh
+++ fbgemm_gpu/include/fbgemm_gpu/embedding_forward_template_helpers.cuh
@@ -36,6 +36,7 @@
 #include "fbgemm_gpu/fbgemm_tensor_accessor.h"
 #include "fbgemm_gpu/sparse_ops_utils.h"
 #include "fbgemm_gpu/utils/fixed_divisor.cuh"
+#include "fbgemm_gpu/utils/vec4acc.cuh"
 
 #define SHFL_SYNC(val, srcLane) \
   shfl_sync(val, srcLane, kThreadGroupSize, shfl_sync_mask)
