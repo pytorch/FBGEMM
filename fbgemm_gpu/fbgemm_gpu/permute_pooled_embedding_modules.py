e7c1067e0da4369e21756cffaa69d9a066071829
Jianyu Huang <jianyuhuang@meta.com>
2024-07-11 19:36:05 -0700

    remove unused exception (#2832)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2832
    
    As title:
    https://www.internalfb.com/diff/D52531661?dst_version_fbid=1014237276957412&transaction_fbid=485161920675232
    
    Thanks nanoax for identify this issue!
    
    Reviewed By: nanoax
    
    Differential Revision: D59655439

--- fbgemm_gpu/fbgemm_gpu/permute_pooled_embedding_modules.py
+++ fbgemm_gpu/fbgemm_gpu/permute_pooled_embedding_modules.py
@@ -29,8 +29,6 @@ except Exception:
         torch.ops.load_library(
             "//deeplearning/fbgemm/fbgemm_gpu:permute_pooled_embedding_ops_gpu_cuda"
         )
-except OSError:
-    pass
 
 
 class PermutePooledEmbeddings:
