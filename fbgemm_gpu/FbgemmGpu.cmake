800da97d0ae14c06f109c7fe66e705ba5732066a
Minhua Chen <minhua@meta.com>
2024-07-31 03:51:55 -0700

    ensemble rowwise adagrad (fbgemm backend) (#2889)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/2889
    
    ensemble rowwise adagrad (fbgemm diff)
    
    Differential Revision: D60189486

--- fbgemm_gpu/FbgemmGpu.cmake
+++ fbgemm_gpu/FbgemmGpu.cmake
@@ -61,6 +61,7 @@ set(GPU_ONLY_OPTIMIZERS
     lamb
     partial_rowwise_adam
     partial_rowwise_lamb
+    ensemble_rowwise_adagrad
     lars_sgd
     none
     rowwise_adagrad_with_counter)
@@ -87,6 +88,7 @@ set(GPU_OPTIMIZERS ${COMMON_OPTIMIZERS} ${GPU_ONLY_OPTIMIZERS})
 set(VBE_OPTIMIZERS
     rowwise_adagrad
     rowwise_adagrad_with_counter
+    ensemble_rowwise_adagrad
     sgd
     dense)
 
