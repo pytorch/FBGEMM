add16a43e7d6fa564d5b441b21f2b6d7ad5853c8
Summer Deng <summerdeng@meta.com>
2024-08-05 10:39:34 -0700

    Add rounding mode options to mx_github (#34)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/FBGEMM/pull/34
    
    X-link: https://github.com/pytorch/FBGEMM/pull/2933
    
    We found the rounding mode when selecting the block scales in MX formats is critical to numeric accuracies. Josh has added rounding mode options in the fp32 to mx4 conversion kernels in D59562029. Here is to add the options to mx_github library which supports more mx formats. To use different options from the default 'floor' method in the cuda kernel,  need to specify custom_cuda=False in the mx specs.
    
    Reviewed By: jwfromm
    
    Differential Revision: D60682029
    
    fbshipit-source-id: 561d668b96cdcf02cd4b9673c3dffe523cf4d577

--- fbgemm_gpu/test/quantize/mx/common.py
+++ fbgemm_gpu/test/quantize/mx/common.py
@@ -407,6 +407,7 @@ def _round_mantissa(
 def _shared_exponents(
     A: torch.Tensor,
     method: str = "max",
+    rounding_mode: str = "even",
     axes: Optional[List[int]] = None,
     ebits: int = 0,
 ) -> torch.Tensor:
@@ -435,12 +436,41 @@ def _shared_exponents(
     else:
         raise Exception("Unrecognized shared exponent selection method %s" % (method))
 
-    # log2(shared_exp) and truncate to integer
-    shared_exp = torch.floor(
-        torch.log2(
-            shared_exp + FP32_MIN_NORMAL * (shared_exp == 0).type(shared_exp.dtype)
+    if rounding_mode == "even":
+        SBITS, EBITS_F32, MBITS_F32 = 1, 8, 23
+        shared_exp = shared_exp.to(torch.float32).view(torch.int32)
+        val_to_add = 1 << (MBITS_F32 - 1)
+        mask = ((1 << (EBITS_F32 + SBITS)) - 1) << MBITS_F32
+        shared_exp = (shared_exp + val_to_add) & mask
+        shared_exp = shared_exp.view(torch.float32)
+
+        shared_exp = torch.floor(
+            torch.log2(
+                shared_exp + FP32_MIN_NORMAL * (shared_exp == 0).type(shared_exp.dtype)
+            )
         )
-    )
+        """
+        roundup_idx = (shared_exp_old != shared_exp).nonzero()
+        if roundup_idx.numel() > 0:
+            idx = roundup_idx[0]
+            raise Exception(
+                f"{roundup_idx.numel() / len(shared_exp.shape) / shared_exp.numel() * 100}% exp round up: {idx=} {shared_exp.shape=} {(shared_exp - shared_exp_old)[idx]=}"
+            )
+        """
+    elif rounding_mode == "ceil":
+        shared_exp = torch.ceil(
+            torch.log2(
+                shared_exp + FP32_MIN_NORMAL * (shared_exp == 0).type(shared_exp.dtype)
+            )
+        )
+    elif rounding_mode == "floor":
+        shared_exp = torch.floor(
+            torch.log2(
+                shared_exp + FP32_MIN_NORMAL * (shared_exp == 0).type(shared_exp.dtype)
+            )
+        )
+    else:
+        raise Exception("Unrecognized rounding mode %s" % (rounding_mode))
 
     # Restrict to [-emax, emax] range
     if ebits > 0:
