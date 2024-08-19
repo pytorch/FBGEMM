ee3281403ba088398c2ff3b548b85831d0ba686f
Benson Ma <bensonma415@meta.com>
2024-08-19 13:24:05 -0700

    Move ops macros out of sparse_ops_utils.h (#3007)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3007
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/101
    
    - Move ops macros out of sparse_ops_utils.h
    
    Differential Revision: D61413333

--- fbgemm_gpu/include/fbgemm_gpu/utils/ops_utils.h
+++ fbgemm_gpu/include/fbgemm_gpu/utils/ops_utils.h
@@ -39,9 +39,43 @@ __builtin_ia32_serialize(void) {
  */
 #define DLL_PUBLIC __attribute__((visibility("default")))
 
+////////////////////////////////////////////////////////////////////////////////
+/// Op Dispatch Macros
+////////////////////////////////////////////////////////////////////////////////
+
 #define FBGEMM_OP_DISPATCH(DISPATCH_KEY, EXPORT_NAME, FUNC_NAME)               \
   TORCH_LIBRARY_IMPL(fbgemm, DISPATCH_KEY, m) {                                \
     m.impl(                                                                    \
         EXPORT_NAME,                                                           \
         torch::dispatch(c10::DispatchKey::DISPATCH_KEY, TORCH_FN(FUNC_NAME))); \
   }
+
+#define DISPATCH_TO_CUDA(name, function) \
+  m.impl(name, torch::dispatch(c10::DispatchKey::CUDA, TORCH_FN(function)))
+
+#define DISPATCH_TO_CPU(name, function) \
+  m.impl(name, torch::dispatch(c10::DispatchKey::CPU, TORCH_FN(function)))
+
+#define DISPATCH_TO_META(name, function) \
+  m.impl(name, torch::dispatch(c10::DispatchKey::Meta, TORCH_FN(function)))
+
+#define DISPATCH_TO_ALL(name, function) \
+  m.impl(name, torch::dispatch(c10::DispatchKey::CatchAll, TORCH_FN(function)))
+
+#define DISPATCH_TO_AUTOGRAD(name, function) \
+  m.impl(name, torch::dispatch(c10::DispatchKey::Autograd, TORCH_FN(function)))
+
+#define DISPATCH_TO_AUTOGRAD_CPU(name, function) \
+  m.impl(                                        \
+      name,                                      \
+      torch::dispatch(c10::DispatchKey::AutogradCPU, TORCH_FN(function)))
+
+#define DISPATCH_TO_AUTOGRAD_CUDA(name, function) \
+  m.impl(                                         \
+      name,                                       \
+      torch::dispatch(c10::DispatchKey::AutogradCUDA, TORCH_FN(function)))
+
+#define DISPATCH_TO_AUTOGRAD_META(name, function) \
+  m.impl(                                         \
+      name,                                       \
+      torch::dispatch(c10::DispatchKey::AutogradMETA, TORCH_FN(function)))
