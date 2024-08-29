c41d67646480fcf648bb2695f83d06221fa9933a
Joe Wang <wangj@meta.com>
2024-08-28 20:39:43 -0700

    parallelizing L2 cache lookup (#3032)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3032
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/130
    
    Change sets
    1. instead of allocate an intermediate tensor to collect the L2 cache miss info, we will do all the embeddings copy inside the originally provided tensor and mark related indices to -1
    2. paralizing the cache lookup logic using multiple cachelib pools which helps reduce the LRU contention
    3. fix cachelib->UVA tensor data copy bug(wrong offset)
    
    Reviewed By: ehsanardestani
    
    Differential Revision: D61417947
    
    fbshipit-source-id: 0363977bd48ca990139be314c930683d30bb574e

---
+++ fbgemm_gpu/include/fbgemm_gpu/split_embeddings_cache/kv_db_cpp_utils.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <folly/hash/Hash.h>
+#include <stddef.h>
+#include <stdint.h>
+
+namespace kv_db_utils {
+
+/// @ingroup embedding-ssd
+///
+/// @brief hash function used for SSD L2 cache and rocksdb sharding algorithm
+///
+/// @param id sharding key
+/// @param num_shards sharding range
+///
+/// @return shard id ranges from [0, num_shards)
+inline size_t hash_shard(int64_t id, size_t num_shards) {
+  auto hash = folly::hash::fnv64_buf(
+      reinterpret_cast<const char*>(&id), sizeof(int64_t));
+  __uint128_t wide = __uint128_t{num_shards} * hash;
+  return static_cast<size_t>(wide >> 64);
+}
+
+}; // namespace kv_db_utils
