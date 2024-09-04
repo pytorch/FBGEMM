e41aed3dbf4a88d0c7c19486783e5c7507a512fc
Benson Ma <bensonma415@meta.com>
2024-09-04 01:33:06 -0700

    Redefine FBGEMM targets with gpu_cpp_library [3/N] (#3067)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3067
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/162
    
    - Redefine `fbgemm_cuda_utils` target with `gpu_cpp_library`
    
    Reviewed By: jianyuh
    
    Differential Revision: D62056416
    
    fbshipit-source-id: 2b3f976e9a461cf7f6cf1d05630a698cee9dfda8

---
+++ fbgemm_gpu/src/placeholder.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/*
+    This is placeholder code to force compilation and generation of .so file.
+*/
+namespace fbgemm_gpu {}
