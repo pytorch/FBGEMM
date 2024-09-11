92f559f3209e65662e39392008aae1ea3ba19b35
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2024-09-10 19:02:26 -0700

    Add missing Pyre mode headers] [batch:23/423] [shard:5/N]
    Differential Revision: D62430535
    
    fbshipit-source-id: 9d3bbf85b8d02536ffaa347f0a0f88cafda5d0a3

--- fbgemm_gpu/fbgemm_gpu/split_table_batched_embeddings_ops_training_common.py
+++ fbgemm_gpu/fbgemm_gpu/split_table_batched_embeddings_ops_training_common.py
@@ -5,6 +5,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 from typing import List, Optional
 
 # @manual=//deeplearning/fbgemm/fbgemm_gpu/codegen:split_embedding_codegen_lookup_invokers
