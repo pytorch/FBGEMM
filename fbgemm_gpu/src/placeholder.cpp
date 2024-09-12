d136481318972a99fe02f14c0542429646192408
Benson Ma <bensonma415@meta.com>
2024-09-12 16:40:05 -0700

    Redefine FBGEMM targets with gpu_cpp_library [10/N] (#3131)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3131
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/218
    
    - Redefine `input_combine_*` targets using `gpu_cpp_library`
    
    Differential Revision: D62611707

--- fbgemm_gpu/src/placeholder.cpp
+++ fbgemm_gpu/src/placeholder.cpp
@@ -6,7 +6,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-/*
-    This is placeholder code to force compilation and generation of .so file.
-*/
+/// This is a placeholder source file that is used to force compilation and
+/// generation of an .SO file.
 namespace fbgemm_gpu {}
