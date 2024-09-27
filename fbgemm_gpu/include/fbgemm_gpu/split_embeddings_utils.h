d056aa3689380f7decad83c90bfc36f5dcf04195
Supadchaya Puangpontip <supadchaya@meta.com>
2024-09-26 23:18:38 -0700

    Add generate_vbe_metadata CPU fallback (#3183)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/279
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3183
    
    Add generate_vbe_metadata for CPU
    
    Reviewed By: q10
    
    Differential Revision: D63494418
    
    fbshipit-source-id: 3936e2546ccf4fb89632df5c49148141adbabe71

---
+++ fbgemm_gpu/include/fbgemm_gpu/split_embeddings_utils.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <ATen/ATen.h>
+
+// These values are adjusted in backward based on B and T
+constexpr int DEFAULT_INFO_NUM_BITS = 32;
+constexpr int DEFAULT_INFO_B_NUM_BITS = 26;
+constexpr uint32_t DEFAULT_INFO_B_MASK = (1u << DEFAULT_INFO_B_NUM_BITS) - 1;
+constexpr uint32_t MAX_T =
+    (1u << (DEFAULT_INFO_NUM_BITS - DEFAULT_INFO_B_NUM_BITS)) - 1;
+constexpr uint32_t MAX_B = (1u << DEFAULT_INFO_B_NUM_BITS) - 1;
+
+std::tuple<int64_t, int64_t>
+get_infos_metadata(at::Tensor unused, int64_t B, int64_t T);
+
+std::tuple<int32_t, uint32_t> adjust_info_B_num_bits(int32_t B, int32_t T);
+
+std::tuple<at::Tensor /*row_output_offsets*/, at::Tensor /*b_t_map*/>
+generate_vbe_metadata(
+    const at::Tensor& B_offsets,
+    const at::Tensor& B_offsets_rank_per_feature,
+    const at::Tensor& output_offsets_feature_rank,
+    const at::Tensor& D_offsets,
+    const int64_t D,
+    const bool nobag,
+    const int64_t max_B_feature_rank,
+    const int64_t info_B_num_bits,
+    const int64_t total_B);
