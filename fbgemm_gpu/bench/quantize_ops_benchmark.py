619abb07fd0a35e3092dbb50013490e55eb6a9e5
Benson Ma <bensonma415@meta.com>
2024-10-01 16:19:17 -0700

    Redefine FBGEMM targets with gpu_cpp_library [20B/N] (#3201)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3201
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/299
    
    - Combine `gpu_cpp_library` targets into `fbgemm_gpu:sparse_ops`
    
    Reviewed By: gag1jain
    
    Differential Revision: D63679169

--- fbgemm_gpu/bench/quantize_ops_benchmark.py
+++ fbgemm_gpu/bench/quantize_ops_benchmark.py
@@ -34,11 +34,7 @@ if open_source:
 else:
     from fbgemm_gpu.bench.bench_utils import benchmark_torch_function
 
-    if torch.version.hip:
-        torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops_hip")
-    else:
-        torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops")
-    torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops_cpu")
+    torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops")
 
 
 @click.group()
