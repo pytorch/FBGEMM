c39d7e87200458f608d6e288506cfedc6fe49799
Richard Barnes <rbarnes@meta.com>
2024-10-02 11:09:42 -0700

    c10::optional -> std::optional
    Summary: `c10::optional` is an alias for `std::optional`. Let's remove the alias and use the real thing.
    
    Reviewed By: palmje
    
    Differential Revision: D63409425
    
    fbshipit-source-id: 91d88c77c4b85d29091de7f9e22b5ee87d6eb171

--- fbgemm_gpu/src/ssd_split_embeddings_cache/ssd_split_table_batched_embeddings.cpp
+++ fbgemm_gpu/src/ssd_split_embeddings_cache/ssd_split_table_batched_embeddings.cpp
@@ -30,7 +30,7 @@ ssd_cache_populate_actions_cuda(
     bool gather_cache_stats,
     std::optional<Tensor> ssd_cache_stats,
     const bool lock_cache_line,
-    const c10::optional<Tensor>& lxu_cache_locking_counter);
+    const std::optional<Tensor>& lxu_cache_locking_counter);
 
 /// @ingroup embedding-ssd
 ///
