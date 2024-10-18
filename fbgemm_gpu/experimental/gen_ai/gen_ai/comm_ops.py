bc3254c1a913530d9f4e4d7bb0422f8f3dadef7a
Josh Fromm <jwfromm@meta.com>
2024-10-18 13:08:38 -0700

    Better Shape Function Registration (#3237)
    Summary:
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/338
    
    With the increasing use of torch.compile both internally and in open source, I wanted to take some time to think about how to best make our FBGEMM custom ops compile compatible. This diff attempts to implement a testable and scalable way of registering custom shape functions easily.
    
    `torch.compile` requires that custom operators have shape functions registered, as they are needed for tracing.
    
    In FBGEMM, we have inconsistently registered such shape functions. This diff attempts to clean up and register a good number of commonly used operators.
    
    Notably, pytorch allows two methods of registering shape functions for custom ops. In CPP, you can use a Meta function, or in python  you can use `register_fake`. It turns out `register_fake` is the recommended and more powerful approach. For example, it is needed for ops that cross devices (such as the car ops) and is needed when exporting a traced graph.
    
    This diff thus focuses on the register_fake method and converts a handful of Meta registrations to it. My hope is that this can provide an easily extensible way of registering shape functions for other kernel authors.
    
    Reviewed By: jianyuh, jiawenliu64
    
    Differential Revision: D64147797

---
+++ fbgemm_gpu/experimental/gen_ai/gen_ai/comm_ops.py
@@ -0,0 +1,79 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from typing import Optional
+
+import torch
+
+"""
+This file contains manual shape registrations for communication custom operators.
+These are needed for custom operators to be compatible with torch.compile.
+
+In some cases, fake tensor handling can be done by registering a meta implementation
+directly in cpp. However, for more complicated functions such as those that involve
+cross device synchronization, pytorch requires a full fake implementation be registered
+in python.
+"""
+
+
+@torch.library.register_fake("fbgemm::nccl_allreduce")
+def nccl_allreduce_abstract(
+    dst: torch.Tensor,
+    src: torch.Tensor,
+    bias: Optional[torch.Tensor] = None,
+    comm_idx: int = 0,
+) -> None:
+    return None
+
+
+@torch.library.register_fake("fbgemm::nccl_allgather")
+def nccl_allgather_abstract(
+    dst: torch.Tensor,
+    src: torch.Tensor,
+    comm_idx: int = 0,
+) -> None:
+    return None
+
+
+@torch.library.register_fake("fbgemm::nccl_alltoall")
+def nccl_alltoall_abstract(
+    dst: torch.Tensor,
+    src: torch.Tensor,
+    world_size: int,
+    comm_idx: int = 0,
+) -> None:
+    return None
+
+
+@torch.library.register_fake("fbgemm::nccl_reducescatter")
+def nccl_reducescatter_abstract(
+    dst: torch.Tensor,
+    src: torch.Tensor,
+    comm_idx: int = 0,
+) -> None:
+    return None
+
+
+@torch.library.register_fake("fbgemm::one_shot_car_allreduce")
+def one_shot_car_allreduce_abstract(
+    dst: torch.Tensor,
+    src: torch.Tensor,
+    bias: Optional[torch.Tensor] = None,
+    comm_idx: int = 0,
+) -> None:
+    return None
+
+
+@torch.library.register_fake("fbgemm::two_shot_car_allreduce")
+def two_shot_car_allreduce_abstract(
+    dst: torch.Tensor,
+    src: torch.Tensor,
+    bias: Optional[torch.Tensor] = None,
+    comm_idx: int = 0,
+) -> None:
+    return None
