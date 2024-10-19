4ba523c2a8cb638db25e0a2771ebc65da9a5d038
Josh Fromm <jwfromm@meta.com>
2024-10-18 20:05:41 -0700

    Better Shape Function Registration (#3237)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3237
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/338
    
    With the increasing use of torch.compile both internally and in open source, I wanted to take some time to think about how to best make our FBGEMM custom ops compile compatible. This diff attempts to implement a testable and scalable way of registering custom shape functions easily.
    
    `torch.compile` requires that custom operators have shape functions registered, as they are needed for tracing.
    
    In FBGEMM, we have inconsistently registered such shape functions. This diff attempts to clean up and register a good number of commonly used operators.
    
    Notably, pytorch allows two methods of registering shape functions for custom ops. In CPP, you can use a Meta function, or in python  you can use `register_fake`. It turns out `register_fake` is the recommended and more powerful approach. For example, it is needed for ops that cross devices (such as the car ops) and is needed when exporting a traced graph.
    
    This diff thus focuses on the register_fake method and converts a handful of Meta registrations to it. My hope is that this can provide an easily extensible way of registering shape functions for other kernel authors.
    
    Reviewed By: jianyuh, jiawenliu64
    
    Differential Revision: D64147797
    
    fbshipit-source-id: 73b25394b3d65730ddb5cb1614536c007f918638

---
+++ fbgemm_gpu/experimental/gen_ai/gen_ai/quantize_ops.py
@@ -0,0 +1,228 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from typing import Optional, Tuple
+
+import torch
+
+"""
+This file contains manual shape registrations for quantize custom operators.
+These are needed for custom operators to be compatible with torch.compile.
+
+In some cases, fake tensor handling can be done by registering a meta implementation
+directly in cpp. However, for more complicated functions such as those that involve
+cross device synchronization, pytorch requires a full fake implementation be registered
+in python.
+"""
+
+
+@torch.library.register_fake("fbgemm::f8f8bf16_blockwise")
+def f8f8bf16_blockwise_abstract(
+    XQ: torch.Tensor,
+    WQ: torch.Tensor,
+    x_scale: torch.Tensor,
+    w_scale: torch.Tensor,
+    block_m: int = 128,
+    block_n: int = 128,
+    block_k: int = 128,
+) -> torch.Tensor:
+    M = XQ.shape[0]
+    N = WQ.shape[0]
+    return torch.empty(
+        [M, N],
+        dtype=torch.bfloat16,
+    )
+
+
+@torch.library.register_fake("fbgemm::f8f8bf16_tensorwise")
+def f8f8bf16_tensorwise_abstract(
+    XQ: torch.Tensor,
+    WQ: torch.Tensor,
+    scale: float,
+    use_fast_accum: bool = True,
+) -> torch.Tensor:
+    M = XQ.shape[0]
+    N = WQ.shape[0]
+    return torch.empty(
+        [M, N],
+        dtype=torch.bfloat16,
+    )
+
+
+@torch.library.register_fake("fbgemm::f8f8bf16_rowwise")
+def f8f8bf16_rowwise_abstract(
+    XQ: torch.Tensor,
+    WQ: torch.Tensor,
+    x_scale: torch.Tensor,
+    w_scale: torch.Tensor,
+    bias: Optional[torch.Tensor] = None,
+    use_fast_accum: bool = True,
+    output: Optional[torch.Tensor] = None,
+) -> torch.Tensor:
+    M = XQ.shape[0]
+    N = WQ.shape[0]
+    return torch.empty(
+        [M, N],
+        dtype=torch.bfloat16,
+    )
+
+
+@torch.library.register_fake("fbgemm::quantize_fp8_per_tensor")
+def quantize_fp8_per_tensor_abstract(
+    input: torch.Tensor,
+    bs: Optional[torch.Tensor] = None,
+    scale_ub: Optional[torch.Tensor] = None,
+    stochastic_rounding: bool = False,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    if torch.version.hip:
+        fp8_dtype = torch.float8_e4m3fnuz
+    else:
+        fp8_dtype = torch.float8_e4m3fn
+    output = torch.empty_like(input, dtype=fp8_dtype)
+    scale = torch.empty([], dtype=torch.bfloat16)
+    return output, scale
+
+
+@torch.library.register_fake("fbgemm::quantize_fp8_per_row")
+def quantize_fp8_per_row_abstract(
+    input: torch.Tensor,
+    bs: Optional[torch.Tensor] = None,
+    scale_ub: Optional[torch.Tensor] = None,
+    output_dtype: Optional[torch.dtype] = None,
+    stochastic_rounding: bool = False,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    if torch.version.hip:
+        fp8_dtype = torch.float8_e4m3fnuz
+    else:
+        fp8_dtype = torch.float8_e4m3fn
+    output = torch.empty_like(input, dtype=fp8_dtype)
+    scale = torch.empty([], dtype=torch.bfloat16)
+    return output, scale
+
+
+@torch.library.register_fake("fbgemm::quantize_fp8_per_col")
+def quantize_fp8_per_col_abstract(
+    input: torch.Tensor,
+    bs: Optional[torch.Tensor] = None,
+    scale_ub: Optional[torch.Tensor] = None,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    if torch.version.hip:
+        fp8_dtype = torch.float8_e4m3fnuz
+    else:
+        fp8_dtype = torch.float8_e4m3fn
+    output = torch.empty_like(input, dtype=fp8_dtype)
+    scale = torch.empty([], dtype=torch.bfloat16)
+    return output, scale
+
+
+# The following operators are not supported on AMD.
+if not torch.version.hip:
+
+    @torch.library.register_fake("fbgemm::i8i8bf16")
+    def i8i8bf16_abstract(
+        XQ: torch.Tensor,
+        WQ: torch.Tensor,
+        scale: float,
+        split_k: int = 1,
+    ) -> torch.Tensor:
+        M = XQ.shape[0]
+        N = WQ.shape[0]
+        return torch.empty(
+            [M, N],
+            dtype=torch.bfloat16,
+        )
+
+    @torch.library.register_fake("fbgemm::f8f8bf16")
+    def f8f8bf16_abstract(
+        XQ: torch.Tensor,
+        WQ: torch.Tensor,
+        scale: torch.Tensor,
+        use_fast_accum: bool = True,
+    ) -> torch.Tensor:
+        M = XQ.shape[0]
+        N = WQ.shape[0]
+        return torch.empty(
+            [M, N],
+            dtype=torch.bfloat16,
+        )
+
+    @torch.library.register_fake("fbgemm::f8f8bf16_cublas")
+    def f8f8bf16_cublas_abstract(
+        A: torch.Tensor,
+        B: torch.Tensor,
+        Ainvs: Optional[torch.Tensor] = None,
+        Binvs: Optional[torch.Tensor] = None,
+        use_fast_accum: bool = True,
+        output: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        M = A.shape[0]
+        N = B.shape[0]
+        return torch.empty(
+            [M, N],
+            dtype=torch.bfloat16,
+        )
+
+    @torch.library.register_fake("fbgemm::f8f8bf16_rowwise_batched")
+    def f8f8bf16_rowwise_batched_abstract(
+        XQ: torch.Tensor,
+        WQ: torch.Tensor,
+        x_scale: torch.Tensor,
+        w_scale: torch.Tensor,
+        bias: Optional[torch.Tensor] = None,
+        use_fast_accum: bool = True,
+        output: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        M = XQ.shape[0]
+        N = WQ.shape[0]
+        return torch.empty(
+            [M, N],
+            dtype=torch.bfloat16,
+        )
+
+    @torch.library.register_fake("fbgemm::f8i4bf16_rowwise")
+    def f8i4bf16_rowwise_abstract(
+        XQ: torch.Tensor,
+        WQ: torch.Tensor,
+        x_scale: torch.Tensor,
+        w_scale: torch.Tensor,
+        w_zp: torch.Tensor,
+    ) -> torch.Tensor:
+        M = XQ.shape[0]
+        N = WQ.shape[0]
+        return torch.empty(
+            [M, N],
+            dtype=torch.bfloat16,
+        )
+
+    @torch.library.register_fake("fbgemm::bf16i4bf16_rowwise")
+    def bf16i4bf16_rowwise_abstract(
+        X: torch.Tensor,
+        WQ: torch.Tensor,
+        w_scale: torch.Tensor,
+        w_zp: torch.Tensor,
+    ) -> torch.Tensor:
+        M = X.shape[0]
+        N = WQ.shape[0]
+        return torch.empty(
+            [M, N],
+            dtype=torch.bfloat16,
+        )
+
+    @torch.library.register_fake("fbgemm::bf16i4bf16_rowwise_batched")
+    def bf16i4bf16_rowwise_batched_abstract(
+        X: torch.Tensor,
+        WQ: torch.Tensor,
+        w_scale: torch.Tensor,
+        w_zp: torch.Tensor,
+    ) -> torch.Tensor:
+        M = X.shape[0]
+        N = WQ.shape[0]
+        return torch.empty(
+            [M, N],
+            dtype=torch.bfloat16,
+        )
