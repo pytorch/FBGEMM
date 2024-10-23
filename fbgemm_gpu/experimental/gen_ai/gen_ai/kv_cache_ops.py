97d32bc8cfbcf12bd210be155aa2c055ccfe2c34
Benson Ma <bensonma415@meta.com>
2024-10-23 15:45:03 -0700

    Re-organize GenAI package init (copy of D64770837) (#3268)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3268
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/368
    
    - Re-organize GenAI package init (copy of D64770837)
    
    Reviewed By: jwfromm
    
    Differential Revision: D64795669
    
    fbshipit-source-id: 825e14e4dbc050403f47f4c6aa7b87664b35f38d

--- fbgemm_gpu/experimental/gen_ai/gen_ai/kv_cache_ops.py
+++ fbgemm_gpu/experimental/gen_ai/gen_ai/kv_cache_ops.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 #
@@ -7,11 +6,7 @@
 
 # pyre-strict
 
-# Load custom operator libraries and register shape functions.
-from . import (  # noqa: F401
-    attention_ops,
-    comm_ops,
-    gemm_ops,
-    kv_cache_ops,
-    quantize_ops,
-)
+from fbgemm_gpu.experimental.gen_ai.utils.loader import load_custom_library
+
+# Load all custom kv cache operators.
+load_custom_library("//deeplearning/fbgemm/fbgemm_gpu/experimental/gen_ai:kv_cache_ops")
