0fa56a37ab582fc1e6fda6f458b45ec696d7a433
Josh Fromm <joshwfromm@gmail.com>
2024-11-01 10:54:15 -0700

    FBEMM Cutlass 3.6 Compatibility (#402)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/FBGEMM/pull/402
    
    While we previously updated the ai_codesign/gen_ai copy of cutlass to 3.6, it wasnt actually being used in fbgemm.
    
    This diff properly enables it and does a bunch of forward compatibility fixes in our cutlass kernels.
    
    [Comparisons](https://docs.google.com/spreadsheets/d/1RC6wF5cJC4_IweBgY9Zj7kP2BTIWksSGZN4o0SNl_Mo/edit?usp=sharing) on correctness and speed show that outputs are effectively the same and there may be some small speedup.
    
    I found that the blockwise fp8 test was a bit flaky and adjusted the tolerance to prevent issues. I dont think its a cause for concern.
    
    Other tests seem to suggest everything is working as expected without regression.
    
    X-link: https://github.com/pytorch/FBGEMM/pull/3303
    
    Reviewed By: q10, jiawenliu64
    
    Differential Revision: D65296741
    
    Pulled By: jwfromm
    
    fbshipit-source-id: 2b6a3077b3061b2586201b9812ce33e5ac221955

--- fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/i8i8bf16.cu
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/i8i8bf16.cu
@@ -232,11 +232,11 @@ at::Tensor i8i8bf16sm90a_impl(
   using StrideOutput = typename Gemm::GemmKernel::StrideC;
 
   StrideInputA stride_a = cutlass::make_cute_packed_stride(
-      StrideInputA{}, cute::make_shape(M, K, cute::Int<1>{}));
+      StrideInputA{}, cute::make_shape(M, K, 1));
   StrideInputB stride_b = cutlass::make_cute_packed_stride(
-      StrideInputB{}, cute::make_shape(N, K, cute::Int<1>{}));
+      StrideInputB{}, cute::make_shape(N, K, 1));
   StrideOutput stride_output = cutlass::make_cute_packed_stride(
-      StrideOutput{}, cute::make_shape(N, M, cute::Int<1>{}));
+      StrideOutput{}, cute::make_shape(N, M, 1));
 
   typename Gemm::Arguments arguments{
       cutlass::gemm::GemmUniversalMode::kGemm,
