9844086caf62c4b389c24342160a75c183e7f82c
Josh Fromm <jwfromm@meta.com>
2024-11-04 23:46:50 -0800

    CK FP8 Batched Gemm Enablement (#3322)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3322
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/417
    
    Initial implementation of CK FP8 batched gemm with fused epilogue scaling.
    
    This diff doesnt include adding heuristics or tuning, which we will include in follow-up.
    
    At least for compute bound shapes though, performance looks incredibly promising:
    
    ```
    bf16_baseline sim: 0.000.
    bf16_baseline ms: 12.967.
    bf16_baseline TFLOPS: 508.744.
    bf16_baseline GB/s: 320.863.
    ck_rowwise sim: 4.344.
    ck_rowwise ms: 8.843.
    ck_rowwise TFLOPS: 745.999.
    ck_rowwise GB/s: 356.669.
    ck_rowwise_batched sim: 4.344.
    ck_rowwise_batched ms: 6.564.
    ck_rowwise_batched TFLOPS: 1005.036.
    ck_rowwise_batched GB/s: 480.516.
    ```
    
    Reviewed By: jiawenliu64
    
    Differential Revision: D64996347
    
    fbshipit-source-id: 16b9c31c545fde9fc55de2aa4d1b0c051dbfe293

---
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/ck_extensions/fp8_rowwise_batched/kernels/fp8_rowwise_batched_kernel_manifest.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <cstdlib>
+
+#include <ATen/ATen.h>
+
+// Default Kernel for now.
+at::Tensor
+fp8_rowwise_batched_256x256x256x128_16x16_8x8_8x32x1_8x32x1_1x32x1x8_8x8x1_1x2_intrawave_v3(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor Y);
+
+at::Tensor
+fp8_rowwise_batched_256x128x128x128_32x32_2x2_8x32x1_8x32x1_1x32x1x8_8x8x1_1x1_intrawave_v3(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor Y);
