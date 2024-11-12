e0292393e4d2c250bbd6feb9eba6bb0b36c5d965
Huanyu He <hhy@meta.com>
2024-11-11 18:44:40 -0800

    open-source SLL jagged_self_substraction_jagged_out (#3351)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3351
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/443
    
    as title, create a cpu op just for correctness not for performance.
    
    Differential Revision: D65786601

--- fbgemm_gpu/test/sll/triton_sll_test.py
+++ fbgemm_gpu/test/sll/triton_sll_test.py
@@ -13,6 +13,7 @@ import fbgemm_gpu.sll.triton_sll  # noqa F401
 
 import torch
 from hypothesis import given, settings, strategies as st
+from torch.testing._internal.optests import opcheck
 
 # pyre-ignore[16]: Module `fbgemm_gpu` has no attribute `open_source`
 open_source: bool = getattr(fbgemm_gpu, "open_source", False)
@@ -352,3 +353,77 @@ class TritonBmmTest(unittest.TestCase):
 
         assert torch.allclose(ref, ret)
         assert torch.equal(c_offsets, c_offsets_computed)
+
+    @unittest.skipIf(*gpu_unavailable)
+    # pyre-fixme[56]: Pyre was not able to infer the type of argument
+    @given(
+        B=st.integers(10, 100),
+        L=st.integers(1, 200),
+        device_type=st.sampled_from(["cpu", "cuda"]),
+        enable_pt2=st.booleans(),
+    )
+    @settings(deadline=None)
+    def test_triton_jagged_self_substraction_jagged_out(
+        self,
+        B: int,
+        L: int,
+        device_type: str,
+        enable_pt2: bool,
+    ) -> None:
+        device = torch.device(device_type)
+        torch.manual_seed(0)
+        lengths_a = torch.randint(1, L + 2, (B,), device=device)
+        offsets_a = torch.cat(
+            [
+                torch.tensor([0], dtype=torch.int32, device=device),
+                lengths_a.cumsum(dim=0),
+            ],
+            dim=0,
+        )
+
+        lengths_b = (lengths_a - 1) * (lengths_a - 1)
+
+        offsets_b = torch.cat(
+            [
+                torch.tensor([0], dtype=torch.int32, device=device),
+                lengths_b.cumsum(dim=0),
+            ],
+            dim=0,
+        )
+
+        jagged_A = torch.randint(
+            0, 100000000, (int(lengths_a.sum().item()),), device=device
+        )
+
+        def model(
+            jagged_A: torch.Tensor,
+            offsets_a: torch.Tensor,
+            offsets_b: torch.Tensor,
+            L: int,
+        ) -> torch.Tensor:
+            return torch.ops.fbgemm.sll_jagged_self_substraction_jagged_out(
+                jagged_A,
+                offsets_a,
+                offsets_b,
+                L,
+            )
+
+        if enable_pt2:
+            torch._dynamo.config.capture_dynamic_output_shape_ops = True
+            torch._dynamo.config.capture_scalar_outputs = True
+            opcheck(
+                torch.ops.fbgemm.sll_jagged_self_substraction_jagged_out,
+                (jagged_A, offsets_a, offsets_b, L),
+            )
+            model = torch.compile(model)
+
+        result = model(jagged_A, offsets_a, offsets_b, L)
+
+        for i in range(B):
+            if lengths_a[i] == 1:
+                continue
+
+            a = jagged_A[offsets_a[i] : offsets_a[i + 1]]
+            ref = a[:-1].unsqueeze(1) - a[1:].unsqueeze(0)
+
+            assert torch.equal(result[offsets_b[i] : offsets_b[i + 1]], ref.flatten())
