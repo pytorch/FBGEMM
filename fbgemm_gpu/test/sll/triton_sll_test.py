92ff8996959fe294388a929a5e800e3987864445
Huanyu He <hhy@meta.com>
2024-11-18 14:10:26 -0800

    open-source SLL jagged2_to_padded_dense (#3352)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/444
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3352
    
    as title, re-use the ref_function from the test for cpu op.
    
    Reviewed By: renganxu, brad-mengchi
    
    Differential Revision: D65787664
    
    fbshipit-source-id: 367a4c37b789c8c660d15b0d06a4801e1db9a850

--- fbgemm_gpu/test/sll/triton_sll_test.py
+++ fbgemm_gpu/test/sll/triton_sll_test.py
@@ -427,3 +427,76 @@ class TritonBmmTest(unittest.TestCase):
             ref = a[:-1].unsqueeze(1) - a[1:].unsqueeze(0)
 
             assert torch.equal(result[offsets_b[i] : offsets_b[i + 1]], ref.flatten())
+
+    @unittest.skipIf(*gpu_unavailable)
+    # pyre-fixme[56]: Pyre was not able to infer the type of argument
+    @given(
+        B=st.integers(1, 10),
+        max_L=st.integers(1, 100),
+        device_type=st.sampled_from(["cpu", "cuda"]),
+    )
+    @settings(deadline=None)
+    def test_jagged2_to_padded_dense(
+        self,
+        B: int,
+        max_L: int,
+        device_type: str,
+    ) -> None:
+        device = torch.device(device_type)
+
+        lengths = torch.randint(1, max_L + 1, (B,), device=device)
+        lengths_square = lengths * lengths
+        offsets = torch.cat(
+            [
+                torch.tensor([0], device=device, dtype=torch.int),
+                lengths_square.cumsum(dim=0),
+            ],
+            dim=0,
+        )
+
+        x = torch.rand(
+            int(lengths_square.sum().item()),
+            requires_grad=True,
+            device=device,
+        )
+
+        def ref_jagged2_to_padded_dense(
+            x: torch.Tensor, offsets: torch.Tensor, max_L: int, padding_value: float
+        ) -> torch.Tensor:
+            B = offsets.size(0) - 1
+            dense_output = torch.full(
+                (B, max_L, max_L),
+                padding_value,
+                dtype=x.dtype,
+                device=x.device,
+            )
+            for b in range(B):
+                begin = offsets[b]
+                end = offsets[b + 1]
+                Ni = int(torch.sqrt(end - begin))
+                if Ni == 0:
+                    continue
+                dense_output[b, 0:Ni, 0:Ni] = x[begin:end].view(Ni, Ni)
+
+            return dense_output
+
+        x_clone = (
+            x.detach().clone().requires_grad_()
+            if x.requires_grad
+            else x.detach().clone()
+        )
+        padding_value = 0.0
+        ref_out = ref_jagged2_to_padded_dense(x, offsets, max_L, padding_value)
+        test_out = torch.ops.fbgemm.sll_jagged2_to_padded_dense(
+            x_clone, offsets, max_L, padding_value
+        )
+        assert torch.allclose(ref_out, test_out)
+
+        # Backward pass
+        dout = torch.rand((B, max_L, max_L), dtype=x.dtype, device=x.device) * 0.1
+        test_out.backward(dout)
+        ref_out.backward(dout)
+
+        assert x.grad is not None
+        assert x_clone.grad is not None
+        assert torch.allclose(x.grad, x_clone.grad)
