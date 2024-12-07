ecf6555e62c497518d2878a8e97e36e1cb7b0b69
Benson Ma <bensonma415@meta.com>
2024-12-06 21:37:05 -0800

    Migrate `jagged_dense_elementwise_add` SLL ops to OSS (#3479)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3479
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/561
    
    - Migrate `jagged_dense_elementwise_add` SLL ops to OSS
    
    Differential Revision: D66901550

--- fbgemm_gpu/fbgemm_gpu/sll/cpu_sll.py
+++ fbgemm_gpu/fbgemm_gpu/sll/cpu_sll.py
@@ -965,3 +965,56 @@ def cpu_jagged_flash_attention_basic(
     )
 
     return jagged_O
+
+
+class JaggedDenseAddCPU(torch.autograd.Function):
+    @staticmethod
+    # pyre-fixme
+    def forward(
+        ctx: Any,  # pyre-ignore
+        x: torch.Tensor,
+        x_offsets: torch.Tensor,
+        y: torch.Tensor,
+        max_seq_len: int,
+    ) -> torch.Tensor:
+        ctx.save_for_backward(x_offsets)
+        ctx.max_seq_len = max_seq_len
+        # TODO: what should be the correct behavior when jagged values has length > max seq len?
+        # current behavior is to not truncate jagged values
+        # similar for backward grad_output
+        padded_x = torch.ops.fbgemm.jagged_to_padded_dense(
+            x,
+            [x_offsets],
+            max_lengths=[max_seq_len],
+            padding_value=0.0,
+        )  # [B, max_seq_len, D]
+        return torch.ops.fbgemm.dense_to_jagged(padded_x + y, [x_offsets])[0]
+
+    @staticmethod
+    # pyre-fixme
+    def backward(
+        ctx,  # pyre-ignore
+        grad_output: torch.Tensor,
+    ) -> Tuple[torch.Tensor, None, torch.Tensor, None]:
+        (offsets,) = ctx.saved_tensors
+        grad_dense = torch.ops.fbgemm.jagged_to_padded_dense(
+            grad_output, [offsets], [ctx.max_seq_len]
+        )
+        return grad_output, None, grad_dense, None
+
+
+def cpu_jagged_dense_elementwise_add(
+    x: torch.Tensor,
+    x_offsets: torch.Tensor,
+    y: torch.Tensor,
+    max_seq_len: int,
+    use_fbgemm_kernel: bool = True,
+) -> torch.Tensor:
+    # Force the CPU backend to use fbgemm kernel as it has better performance
+    use_fbgemm_kernel = True
+    if use_fbgemm_kernel:
+        return torch.ops.fbgemm.jagged_dense_elementwise_add_jagged_output(
+            x, [x_offsets], y
+        )[0]
+    else:
+        return JaggedDenseAddCPU.apply(x, x_offsets, y, max_seq_len)
