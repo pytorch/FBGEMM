009a7f9f1b5140bc3fc3de550aa7532dafbba41b
Benson Ma <bensonma415@meta.com>
2024-12-13 01:32:00 -0800

    Install pytorch-triton for the ROCm case (#3501)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/580
    
    - Install pytorch-triton for the ROCm case
    
    - Print installed version to confirm that the correct pytorch-triton is installed
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3501
    
    Reviewed By: brad-mengchi
    
    Differential Revision: D67160553
    
    Pulled By: q10
    
    fbshipit-source-id: 5f166e4465e3bf49dd0462a47bc9be023e878d1a

--- fbgemm_gpu/experimental/gemm/test/__init__.py
+++ fbgemm_gpu/experimental/gemm/test/__init__.py
@@ -5,13 +5,3 @@
 # LICENSE file in the root directory of this source tree.
 
 # pyre-strict
-
-from typing import Tuple
-
-import torch
-
-
-gpu_unavailable: Tuple[bool, str] = (
-    not torch.cuda.is_available() or torch.cuda.device_count() == 0,
-    "CUDA is not available or no GPUs detected",
-)
