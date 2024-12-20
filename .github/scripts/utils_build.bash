9f73c0b56f1fb41cc3b6945d802deed5b34f11c0
Benson Ma <bensonma415@meta.com>
2024-12-20 10:52:09 -0800

    [fbgemm_gpu] Explicitly update manylinux version
    - Set the --plat-name explicitly to `manylinux_2_28`

--- .github/scripts/utils_build.bash
+++ .github/scripts/utils_build.bash
@@ -298,6 +298,7 @@ install_build_tools () {
   #
   # shellcheck disable=SC2086
   (exec_with_retries 3 conda install ${env_prefix} -c conda-forge -y \
+    auditwheel \
     bazel \
     click \
     'cmake>=3.30' \
