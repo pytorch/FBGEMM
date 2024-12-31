2b1200c2194d79f73c4afc801e72797accf0e86d
Josh Fromm <jwfromm@meta.com>
2024-12-31 11:07:33 -0800

    Add Cutlass FP8 Grouped Gemm to Quantize Bench (#3530)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3530
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/610
    
    This small diff adds the new Cutlass fp8 grouped gemm operators to quantize bench for easier performance profiling. I also adjusted the groups argument to quantize_bench to make it a bit easier to specify input shapes.
    
    Reviewed By: jianyuh
    
    Differential Revision: D67610633

--- fbgemm_gpu/experimental/gen_ai/bench/quantize_bench.py
+++ fbgemm_gpu/experimental/gen_ai/bench/quantize_bench.py
@@ -10,6 +10,7 @@ import os
 from typing import Any, Dict, List, Optional, Tuple
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 import pandas as pd
 import seaborn as sns
@@ -80,7 +81,11 @@ def benchmark_grouped(
     for i in range(num_groups):
         out_ref.append(torch.matmul(A[i], B[i].t()))
     # Keep track of results.
-    results: Dict[str, Any] = {"M": m, "N": n, "K": k, "groups": num_groups}
+    # Only log all shapes in a group if they are unique.
+    log_m = m[0] if len(np.unique(m)) == 1 else m
+    log_n = n[0] if len(np.unique(n)) == 1 else n
+    log_k = k[0] if len(np.unique(k)) == 1 else k
+    results: Dict[str, Any] = {"M": log_m, "N": log_n, "K": log_k, "groups": num_groups}
     # Benchmark each operator.
     for quantize_op in quantize_ops:
         # If kernel filter is provided, skip kernels that arent requested.
@@ -269,16 +274,12 @@ def main(args: Any):
         kernels = None
 
     # Enumerate shapes to benchmark.
-    if args.grouped:
+    if args.grouped and not args.groups:
         # In grouped mode, M, N, and K represent the groups of a single gemm.
         assert args.M is not None and args.N is not None and args.K is not None
         M = [int(m) for m in args.M.strip().split(",")]
         N = [int(n) for n in args.N.strip().split(",")]
         K = [int(k) for k in args.K.strip().split(",")]
-        if args.groups:
-            M = M * int(args.groups)
-            N = N * int(args.groups)
-            K = K * int(args.groups)
         if args.B is None:
             B = [1] * len(M)
         else:
@@ -311,6 +312,13 @@ def main(args: Any):
                 K = [int(k) for k in args.K.strip().split(",")]
             # List all shapes for simplicity.
             MNK = list(itertools.product(B, M, N, K))
+    # When groups is provided transform shapes into grouped format.
+    if args.groups:
+        groups = int(args.groups)
+        MNK = [
+            [[b] * groups, [m] * groups, [n] * groups, [k] * groups]
+            for b, m, n, k in MNK
+        ]
 
     # Iterate over shapes and benchmark.
     benchmark_results = []
