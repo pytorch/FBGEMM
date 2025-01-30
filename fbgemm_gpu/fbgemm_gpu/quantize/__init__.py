e527b91f725f52f82f19013754e3f1b8256ec67d
Benson Ma <bensonma415@meta.com>
2025-01-29 21:09:05 -0800

    Fold ops registration code, pt 1 (#3634)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3634
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/710
    
    - Fold ops registration code into a single location
    
    Reviewed By: sryap
    
    Differential Revision: D68850095
    
    fbshipit-source-id: 19dca1649f136f75941d10711967a69a2faf8ff8

--- fbgemm_gpu/fbgemm_gpu/quantize/__init__.py
+++ fbgemm_gpu/fbgemm_gpu/quantize/__init__.py
@@ -6,65 +6,38 @@
 
 # pyre-strict
 
-import torch
-
 from fbgemm_gpu.quantize.quantize_ops import dequantize_mx, quantize_mx  # noqa F401
+from fbgemm_gpu.utils import TorchLibraryFragment
 
+lib = TorchLibraryFragment("fbgemm")
 
-def op_registeration(
-    lib,  # pyre-ignore[2]
-    op_name,  # pyre-ignore[2]
-    fn,  # pyre-ignore[2]
-    dispatch_key,  # pyre-ignore[2]
-) -> None:
+lib.define(
+    """quantize_mx(
+        Tensor input,
+        int scale_bits,
+        int elem_ebits,
+        int elem_mbits,
+        float elem_max_norm,
+        int mx_group_size,
+        int? rounding_mode = None
+    ) -> Tensor
     """
-    Registers an op with the given name and dispatch key only once.
+)
 
-    Args:
-        lib: torch.library  (e.g., torch.library.Library("fbgemm", "FRAGMENT"))
-        op_name: operator name
-        fn: function that's the operator implementation for the input dispatch key
-        dispatch_key: dispatch key that the function should be registered for (e.g., "CUDA")
-
-    Returns:
-        None
-
-    Example:
-        lib = torch.library.Library("fbgemm", "FRAGMENT")
-        lib.define(...)
-        op_registeration(lib, "quantize_mx", quantize_mx, "CUDA")
+lib.define(
+    """dequantize_mx(
+        Tensor input,
+        int mx_group_size
+    ) -> Tensor
     """
-    full_op_name = "fbgemm::" + op_name
-    if not torch._C._dispatch_has_kernel_for_dispatch_key(full_op_name, dispatch_key):
-        lib.impl(op_name, fn, dispatch_key)
+)
 
+lib.register(
+    "quantize_mx",
+    {"CUDA": quantize_mx, "CPU": quantize_mx},
+)
 
-lib = torch.library.Library("fbgemm", "FRAGMENT")
-
-if "fbgemm::quantize_mx" not in torch.library._defs:
-    lib.define(
-        """quantize_mx(
-            Tensor input,
-            int scale_bits,
-            int elem_ebits,
-            int elem_mbits,
-            float elem_max_norm,
-            int mx_group_size,
-            int? rounding_mode = None
-        ) -> Tensor
-        """
-    )
-
-if "fbgemm::dequantize_mx" not in torch.library._defs:
-    lib.define(
-        """dequantize_mx(
-            Tensor input,
-            int mx_group_size
-        ) -> Tensor
-        """
-    )
-
-op_registeration(lib, "quantize_mx", quantize_mx, "CUDA")
-op_registeration(lib, "quantize_mx", quantize_mx, "CPU")
-op_registeration(lib, "dequantize_mx", dequantize_mx, "CUDA")
-op_registeration(lib, "dequantize_mx", dequantize_mx, "CPU")
+lib.register(
+    "dequantize_mx",
+    {"CUDA": dequantize_mx, "CPU": dequantize_mx},
+)
