dfc6d113bb639dab6127ad91422500fcac27f2d6
Andrey Bokovoy <avbokovoy@gmail.com>
2025-01-31 09:24:54 +0000

    Add more PackedMode wrappers

--- fbgemm_gpu/codegen/inference/embedding_forward_quantized_split_nbit_kernel_template.cu
+++ fbgemm_gpu/codegen/inference/embedding_forward_quantized_split_nbit_kernel_template.cu
@@ -102,7 +102,8 @@ __global__ void {{ emb_weight_type.enum_name }}_split_embedding{{ "_nobag" if no
 
   for (uint32_t i = 0; i < OutputRowsPerThread; ++i) {
     const uint32_t packed_bag_idx = PackedMode ? (threadIdx.x % NumUint4LoadsPerRow) / uint4_loads_per_row : 0;
-    uint32_t b = min(static_cast<uint32_t>(bb * num_packed_bags * OutputRowsPerThread + i * num_packed_bags + packed_bag_idx), static_cast<uint32_t>(B - 1));
+    uint32_t b = PackedMode ? min(static_cast<uint32_t>(bb * num_packed_bags * OutputRowsPerThread + i * num_packed_bags + packed_bag_idx), static_cast<uint32_t>(B - 1))
+                            : min(static_cast<uint32_t>(bb * OutputRowsPerThread + i), static_cast<uint32_t>(B - 1));
     int32_t indices_start = offsets[t * B + b];
     int32_t indices_end = offsets[t * B + b + 1];
     indices_starts[i] = indices_start;
@@ -262,11 +263,11 @@ __global__ void {{ emb_weight_type.enum_name }}_split_embedding{{ "_nobag" if no
         // rationale: have scale/shift at start since these get loaded first
         // and then broadcasted around so it might speed up the first cache miss.
         {% if emb_weight_type.primitive_type == "INT" %}
-        half2 shift_scale = reinterpret_cast<const half2*>(row)[packed_bag_idx * uints_per_row];
+        half2 shift_scale = reinterpret_cast<const half2*>(row)[PackedMode ? packed_bag_idx * uints_per_row : 0];
         {% endif %}
 
         {% if weighted %}
-        float row_weight = buffers_indice_weights[warp_idx][i][input_row_idx][packed_bag_idx];
+        float row_weight = buffers_indice_weights[warp_idx][i][input_row_idx][PackedMode ? packed_bag_idx : 0];
         {% endif %}
 
         using scalar_t = {{ emb_weight_type.cpp_type_name }};
@@ -351,9 +352,16 @@ __global__ void {{ emb_weight_type.enum_name }}_split_embedding{{ "_nobag" if no
         }
         accumulators[i][j].mul(inv_L);
 
-        if (output_d >= 0 && output_d < D && (!PackedMode || packed_bag_idx < num_packed_bags)) {
-          const int num_valid_outputs = min(static_cast<int>(D - output_d), static_cast<int>({{ (32 // emb_weight_type.bit_width) }}));
-          accumulators[i][j].store(&output[b][D_start + output_d], num_valid_outputs);
+        if constexpr (PackedMode) {
+          if (output_d >= 0 && output_d < D && packed_bag_idx < num_packed_bags) {
+            const int num_valid_outputs = min(static_cast<int>(D - output_d), static_cast<int>({{ (32 // emb_weight_type.bit_width) }}));
+            accumulators[i][j].store(&output[b][D_start + output_d], num_valid_outputs);
+          }
+        } else {
+          if (output_d >= 0 && output_d < D) {
+            const int num_valid_outputs = min(static_cast<int>(D - output_d), static_cast<int>({{ (32 // emb_weight_type.bit_width) }}));
+            accumulators[i][j].store(&output[b][D_start + output_d], num_valid_outputs);
+          }
         }
 
       }
