5070be38569cfbe52e91af29b68dd88f8c40de6e
Benson Ma <bensonma415@meta.com>
2025-01-31 16:50:43 -0800

    Re-organize SLL ops, pt 2 (#3643)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3643
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/719
    
    - Re-organize `jagged_dense_flash_attention`
    
    Reviewed By: sryap
    
    Differential Revision: D68916405
    
    fbshipit-source-id: 688f41ae4b96684697af8538611f9f6a800e7ff2

--- fbgemm_gpu/fbgemm_gpu/sll/triton/__init__.py
+++ fbgemm_gpu/fbgemm_gpu/sll/triton/__init__.py
@@ -8,12 +8,22 @@
 # pyre-strict
 
 
-from fbgemm_gpu.sll.triton.multi_head_jagged_flash_attention import (  # noqa F401
-    multi_head_jagged_flash_attention,
-    MultiHeadJaggedFlashAttention,
+from fbgemm_gpu.sll.triton.jagged_dense_flash_attention import (  # noqa F401
+    jagged_dense_flash_attention,
+    JaggedDenseFlashAttention,  # noqa F401
 )
 
+from fbgemm_gpu.sll.triton.multi_head_jagged_flash_attention import (  # noqa F401
+    multi_head_jagged_flash_attention,
+    MultiHeadJaggedFlashAttention,  # noqa F401
+)
+
+# pyre-ignore[5]
 op_registrations = {
+    "sll_jagged_dense_flash_attention": {
+        "CUDA": jagged_dense_flash_attention,
+        "AutogradCUDA": jagged_dense_flash_attention,
+    },
     "sll_multi_head_jagged_flash_attention": {
         "CUDA": multi_head_jagged_flash_attention,
         "AutogradCUDA": multi_head_jagged_flash_attention,
