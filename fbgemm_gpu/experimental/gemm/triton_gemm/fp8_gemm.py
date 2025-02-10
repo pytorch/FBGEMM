37ea0d25ca5fcbb82095aa97512a83fb25fff38d
Shikai Li <shikaili@meta.com>
2025-02-10 14:04:09 -0800

    Moves utility functions into a standalone file. (#749)
    Summary:
    X-link: https://github.com/pytorch/FBGEMM/pull/3671
    
    Pull Request resolved: https://github.com/facebookresearch/FBGEMM/pull/749
    
    Moves functions to better modularize code.
    
    https://github.com/facebookresearch/FBGEMM/pull/749
    https://github.com/pytorch/FBGEMM/pull/3671
    
    Reviewed By: jianyuh
    
    Differential Revision: D69377391
    
    fbshipit-source-id: 510cf2cdae55fe950fb1df5448a5068bce5f7330

--- fbgemm_gpu/experimental/gemm/triton_gemm/fp8_gemm.py
+++ fbgemm_gpu/experimental/gemm/triton_gemm/fp8_gemm.py
@@ -6,7 +6,6 @@
 
 # pyre-unsafe
 import logging
-import sys
 from typing import List, Optional, Tuple, Union
 
 import torch
@@ -17,6 +16,10 @@ import triton.language as tl  # @manual
 from fbgemm_gpu.experimental.gemm.triton_gemm.matmul_perf_model import (
     early_config_prune,
     estimate_matmul_time,
+)
+from fbgemm_gpu.experimental.gemm.triton_gemm.utils import (
+    map_dtype_to_triton,
+    TmaAutoTuneHelper,
 )
 from torch._tensor import Tensor
 
@@ -57,28 +60,6 @@ def reinterpret_fp8_type(tensor: torch.Tensor, dtype: tl.dtype) -> TensorWrapper
         triton.TensorWrapper: fp8 tensor.
     """
     return tl_reinterpret(tensor, dtype=dtype)
-
-
-def map_dtype_to_triton(dtype: torch.dtype) -> tl.dtype:
-    """
-    Maps torch dtype to triton dtype.
-
-    Args:
-        dtype (torch.dtype): input dtype.
-
-    Returns:
-        tl.dtype: triton dtype.
-    """
-    if dtype == torch.float16:
-        return tl.float16
-    elif dtype == torch.bfloat16:
-        return tl.bfloat16
-    elif dtype == torch.float32:
-        return tl.float32
-    elif dtype == torch.int32:
-        return tl.int32
-    else:
-        raise ValueError(f"Unsupported dtype {dtype}")
 
 
 def init_to_zero(name):
@@ -1123,99 +1104,6 @@ def _kernel_matmul_fp8_row_tma_persistent_ws_cooperative(
                 acc += bias[None, :]
             acc = acc.to(c_dtype)
             tl._experimental_descriptor_store(C_ptr, acc, [offs_am, offs_bn])
-
-
-# check if we have the TMA version in Triton PR #4498 (https://github.com/triton-lang/triton/pull/4498).
-HAS_TMA_DESC = "nv_tma_desc_type" in dir(tl)
-
-if HAS_TMA_DESC:
-    print(
-        "TMA benchmarks will be running with experimental grid constant TMA descriptor.",
-        file=sys.stderr,
-    )
-else:
-    print(
-        "TMA benchmarks will be running without grid constant TMA descriptor.",
-        file=sys.stderr,
-    )
-
-
-class TmaAutoTuneHelper:
-
-    # duck typing wrapper to implement the same interface as TmaDescKernelParam in Triton PR #4498
-    class KernelParamWrapper:
-        def __init__(self, desc):
-            self.desc = desc
-
-        def tma_desc_cpu_ptr(self):
-            return self.desc.data_ptr()
-
-    TMA_SIZE = 128
-
-    def __init__(self):
-        self.fill_1d_tma_descriptor_inner = (
-            triton.runtime.driver.active.utils.fill_1d_tma_descriptor
-        )
-        self.fill_2d_tma_descriptor_inner = (
-            triton.runtime.driver.active.utils.fill_2d_tma_descriptor
-        )
-        if HAS_TMA_DESC:
-            self.descriptors = {}
-        else:
-            self.cuda_descriptors = {}
-
-    # Call this method outside of the lambda function for grid size
-    def init_tma_descriptor(self, name):
-        if HAS_TMA_DESC:
-            self.descriptors[name] = torch.empty(
-                TmaAutoTuneHelper.TMA_SIZE, device="cpu", dtype=torch.int8
-            )
-        else:
-            self.cuda_descriptors[name] = torch.empty(
-                TmaAutoTuneHelper.TMA_SIZE, device="cuda", dtype=torch.int8
-            )
-
-    # Call this method inside the lambda function for grid size
-    def fill_1d_tma_descriptor(self, name, ptr, dim, block_dim, element_size):
-        if HAS_TMA_DESC:
-            desc_x = self.descriptors[name]
-            assert desc_x.data_ptr() % 64 == 0
-            self.fill_1d_tma_descriptor_inner(
-                ptr, dim, block_dim, element_size, desc_x.data_ptr()
-            )
-        else:
-            desc_x = self.cuda_descriptors[name]
-            buf_x = torch.empty_like(desc_x, device="cpu", pin_memory=True)
-            self.fill_1d_tma_descriptor_inner(
-                ptr, dim, block_dim, element_size, buf_x.data_ptr()
-            )
-            desc_x.copy_(buf_x, non_blocking=True)
-
-    # Call this method inside the lambda function for grid size
-    def fill_2d_tma_descriptor(
-        self, name, ptr, dim1, dim0, block_dim1, block_dim0, element_size
-    ):
-        if HAS_TMA_DESC:
-            desc_x = self.descriptors[name]
-            assert desc_x.data_ptr() % 64 == 0
-            self.fill_2d_tma_descriptor_inner(
-                ptr, dim1, dim0, block_dim1, block_dim0, element_size, desc_x.data_ptr()
-            )
-        else:
-            desc_x = self.cuda_descriptors[name]
-            buf_x = torch.empty_like(desc_x, device="cpu", pin_memory=True)
-            self.fill_2d_tma_descriptor_inner(
-                ptr, dim1, dim0, block_dim1, block_dim0, element_size, buf_x.data_ptr()
-            )
-            desc_x.copy_(buf_x, non_blocking=True)
-
-    def get_tma_descriptor_kernel_param(self, name):
-        if HAS_TMA_DESC:
-            assert self.descriptors[name] is not None
-            return self.KernelParamWrapper(self.descriptors[name])
-        else:
-            assert self.cuda_descriptors[name] is not None
-            return self.cuda_descriptors[name]
 
 
 @torch._library.triton_op("triton::matmul_fp8_row", mutates_args=())
