324f06294f343589263699c5efbc228e1edc43a4
Andy Lugo <Andy.LugoReyes@amd.com>
2025-02-17 22:29:12 -0800

    Update ck
    Summary:
    Updates the CK version and re-implements kernel generation
    
    
    cc albanD
    
    X-link: https://github.com/pytorch/pytorch/pull/144799
    
    Reviewed By: jianyuh
    
    Differential Revision: D68613917
    
    Pulled By: xw285cornell

--- fbgemm_gpu/experimental/gen_ai/src/quantize/ck_extensions/fused_moe/fused_moesorting.hpp
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/ck_extensions/fused_moe/fused_moesorting.hpp
@@ -7,14 +7,15 @@
 #include "ck_tile/host.hpp"
 #include "ck_tile/ops/fused_moe.hpp"
 
-struct fused_moesorting_trait {
-  std::string index_type;
-  std::string weight_type; // currently always float
+struct fused_moesorting_trait
+{
+    std::string index_type;
+    std::string weight_type;   // currently always float
+    bool local_expert_masking; // if mask experts as local expert
 };
 
-struct fused_moesorting_args : public ck_tile::MoeSortingHostArgs {};
+struct fused_moesorting_args : public ck_tile::MoeSortingHostArgs
+{
+};
 
-float fused_moesorting(
-    fused_moesorting_trait t,
-    fused_moesorting_args a,
-    ck_tile::stream_config s);
+float fused_moesorting(fused_moesorting_trait t, fused_moesorting_args a, ck_tile::stream_config s);
