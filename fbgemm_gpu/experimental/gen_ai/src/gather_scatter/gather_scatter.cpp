a7e01e8a97545043615fb0ec54df54416a0052e4
Shikai Li <shikaili@meta.com>
2025-02-22 16:02:19 -0800

    `torch.ops.fbgemm.scatter_add_along_first_dim`. (#3720)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3720
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/804
    
    TMA based scatter_add operation optimized for large shapes.
    Hyperparameters could be finetuned for better performance. However, the expected headroom is small.
    
    Reviewed By: jianyuh
    
    Differential Revision: D69957147
    
    fbshipit-source-id: 17aefbeb379976b651e7abd123b885d48d479d1a

--- fbgemm_gpu/experimental/gen_ai/src/gather_scatter/gather_scatter.cpp
+++ fbgemm_gpu/experimental/gen_ai/src/gather_scatter/gather_scatter.cpp
@@ -15,13 +15,21 @@ namespace fbgemm_gpu {
 
 at::Tensor gather_along_first_dim(at::Tensor data, at::Tensor index);
 
+void scatter_add_along_first_dim(
+    at::Tensor dst,
+    at::Tensor src,
+    at::Tensor index);
+
 TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
-  m.set_python_module("fbgemm_gpu.experimental.gen_ai.gather");
+  m.set_python_module("fbgemm_gpu.experimental.gen_ai.gather_scatter");
   m.def("gather_along_first_dim(Tensor Data, Tensor Index) -> Tensor");
+  m.def(
+      "scatter_add_along_first_dim(Tensor Dst, Tensor Src, Tensor Index) -> ()");
 }
 
 TORCH_LIBRARY_IMPL(fbgemm, CUDA, m) {
   m.impl("gather_along_first_dim", gather_along_first_dim);
+  m.impl("scatter_add_along_first_dim", scatter_add_along_first_dim);
 }
 
 #endif
