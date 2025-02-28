8da003e82399a13b8380d990fe6fde51d42eb2ae
Shikai Li <shikaili@meta.com>
2025-02-28 15:36:46 -0800

    Reduce tuning.
    Summary:
    - Filtering reasonable block sizes based on input shape. In order to:
      - Avoid OoO in shared memory that results in compilation error.
      - Avoid excessive paddings that results in wasted computation.
      - Avoid excessive tiling that results in repeated HBM access.
      - Achieve ideal SM/CU utilization.
    
    Reviewed By: mxz297
    
    Differential Revision: D70377113

--- fbgemm_gpu/experimental/gemm/triton_gemm/grouped_gemm.py
+++ fbgemm_gpu/experimental/gemm/triton_gemm/grouped_gemm.py
@@ -6,6 +6,7 @@
 
 # pyre-unsafe
 
+import functools
 from typing import Optional
 
 import torch
@@ -14,6 +15,7 @@ import triton
 import triton.language as tl
 
 from fbgemm_gpu.experimental.gemm.triton_gemm import utils
+from triton.runtime import driver  # @manual
 
 
 _NV_CONFIGS = [
@@ -28,12 +30,11 @@ _NV_CONFIGS = [
         num_ctas=num_ctas,
     )
     for block_size_m in [64, 128]
-    for block_size_n in [128, 256]
-    for block_size_k in [128, 256]
+    for block_size_n in [64, 128, 256]
+    for block_size_k in [64, 128, 256]
     for num_stages in [3, 4]
     for num_warps in [4, 8]
     for num_ctas in [1]
-    if not (block_size_m == 64 and num_warps == 8)
 ]
 
 _AMD_CONFIGS = [
@@ -49,17 +50,82 @@ _AMD_CONFIGS = [
         num_warps=num_warps,
     )
     for block_size_m in [32, 64, 128]
-    for block_size_n in [128, 256]
+    for block_size_n in [32, 64, 128, 256]
     for block_size_k in [128, 256]
     for num_stages in [1, 2]
     for num_warps, waves_per_cu in [(4, 1), (8, 2), (16, 4)]
-    for matrix_instr_nonkdim in [16, 32]
+    for matrix_instr_nonkdim in [16]
 ]
+
+
+def early_config_prune(configs, named_args, dtsize=None, dtype=None, **kwargs):
+    device = torch.cuda.current_device()
+    # BLOCK_M, BLOCK_N, BLOCK_K, SPLIT_K, num_warps, num_stages
+    if dtsize is None:
+        dtsize = named_args["c_ptr"].element_size()
+    if dtype is None:
+        dtype = named_args["c_ptr"].dtype
+
+    pruned_configs = []
+    for config in configs:
+        kw = config.kwargs
+        BLOCK_M, BLOCK_N, BLOCK_K, num_stages = (
+            kw["BLOCK_SIZE_M"],
+            kw["BLOCK_SIZE_N"],
+            kw["BLOCK_SIZE_K"],
+            config.num_stages,
+        )
+        G, M, N, K = (
+            named_args["G"],
+            named_args["M_BUCKET"],
+            named_args["N"],
+            named_args["K"],
+        )
+
+        # 1. make sure we have enough smem
+        max_shared_memory = driver.active.utils.get_device_properties(device)[
+            "max_shared_mem"
+        ]
+        if torch.version.hip:
+            required_shared_memory = BLOCK_N * BLOCK_K * num_stages * dtsize
+        else:
+            required_shared_memory = (BLOCK_M + BLOCK_N) * BLOCK_K * num_stages * dtsize
+        if required_shared_memory > max_shared_memory:
+            continue
+
+        M_PER_GROUP = M // G
+        MIN_M_TILES = 32 if torch.version.hip else 64
+        # 2. make sure we don't load M tiles that are too big
+        if BLOCK_M > MIN_M_TILES and BLOCK_M > (M_PER_GROUP * 2):
+            continue
+        # 3. make sure we don't load N tiles that are too small
+        if BLOCK_M < 128 and BLOCK_M < (M_PER_GROUP // 2):
+            continue
+
+        num_sm = driver.active.utils.get_device_properties(device)[
+            "multiprocessor_count"
+        ]
+        N_TILES = N // BLOCK_N
+        MIN_N_TILES = 32 if torch.version.hip else 64
+        # 4. make sure we don't load N tiles that are too big
+        if BLOCK_N > MIN_N_TILES and M * N_TILES < num_sm:
+            continue
+        # 5. make sure we don't load N tiles that are too small
+        if BLOCK_N < 128 and M * N_TILES > 2 * num_sm:
+            continue
+        # 6. make sure K can be evenly divided
+        if K % BLOCK_K != 0:
+            continue
+
+        pruned_configs.append(config)
+
+    return pruned_configs
 
 
 @triton.autotune(
     configs=_AMD_CONFIGS if torch.version.hip else _NV_CONFIGS,
     key=["G", "M_BUCKET", "N", "K"],
+    prune_configs_by={"early_config_prune": early_config_prune},
 )
 @triton.jit
 def _kernel_grouped_gemm(
@@ -194,6 +260,11 @@ TT_FP8_DTYPE = tl.float8e4b8 if torch.version.hip else tl.float8e4nv
 @triton.autotune(
     configs=_AMD_CONFIGS if torch.version.hip else _NV_CONFIGS,
     key=["G", "M_BUCKET", "N", "K"],
+    prune_configs_by={
+        "early_config_prune": functools.partial(
+            early_config_prune, dtype=TT_FP8_DTYPE, dtsize=1
+        )
+    },
 )
 @triton.jit
 def _kernel_grouped_gemm_fp8_rowwise(
