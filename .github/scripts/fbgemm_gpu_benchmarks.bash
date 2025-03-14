488c0debc5d5aec30003626278285f8c83dedd87
Benson Ma <bensonma415@meta.com>
2025-03-13 19:45:47 -0700

    Add benchmark workflows (#3713)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/894
    
    - Add benchmark workflow for AMD TBE
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3713
    
    Reviewed By: spcyppt
    
    Differential Revision: D71129755
    
    Pulled By: q10
    
    fbshipit-source-id: 820d0c84f5e6a3999f7eb00639072fc7caddad20

---
+++ .github/scripts/fbgemm_gpu_benchmarks.bash
@@ -0,0 +1,98 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+# shellcheck disable=SC1091,SC2128
+. "$( dirname -- "$BASH_SOURCE"; )/utils_base.bash"
+
+################################################################################
+# FBGEMM_GPU Test Helper Functions
+################################################################################
+
+run_tbe_microbench () {
+  local env_name="$1"
+
+  __single_run() {
+    local cache_type="$1"
+    local embedding_location="$2"
+
+    echo "################################################################################"
+    echo "# Running Benchmark: (${cache_type}, ${embedding_location})"
+    echo "#"
+    echo "# [$(date --utc +%FT%T.%3NZ)] + ${FUNCNAME[0]} ${*}"
+    echo "################################################################################"
+    echo ""
+
+    # shellcheck disable=SC2155
+    local env_prefix=$(env_name_or_prefix "${env_name}")
+
+    if [ "$embedding_location" == "hbm" ]; then
+      local managed="device"
+    elif [ "$embedding_location" == "uvm" ]; then
+      local managed="managed"
+    fi
+
+    # Old TBE benchmark script
+    # shellcheck disable=SC2086
+    print_exec conda run --no-capture-output ${env_prefix} python tbe/split_table_batched_embeddings_benchmark.py device \
+      --batch-size 13107 \
+      --embedding-dim 256 \
+      --iters 400 \
+      --warmup-runs 50 \
+      --alpha 1.15 \
+      --bag-size 55 \
+      --weights-precision fp16 \
+      --cache-precision "${cache_type}" \
+      --output-dtype bf16 \
+      --managed="${managed}" \
+      --num-embeddings 10000000 \
+      --num-tables 1 \
+      --row-wise
+
+    # New TBE benchmark script
+    #
+    # Invoke `python tbe/tbe_training_benchmark.py device --help` for
+    # documentation on all available flags
+    # shellcheck disable=SC2086
+    print_exec conda run --no-capture-output ${env_prefix} python tbe/tbe_training_benchmark.py device \
+      --bench-iterations 400 \
+      --bench-warmup-iterations 50 \
+      --bench-num-requests 10 \
+      --tbe-batch-size 13107 \
+      --tbe-embedding-dim 256 \
+      --tbe-pooling-size 55 \
+      --tbe-num-embeddings 10000000 \
+      --tbe-num-tables 1 \
+      --weights-precision fp16 \
+      --cache-precision "${cache_type}" \
+      --output-dtype bf16 \
+      --managed="${managed}" \
+      --row-wise
+  }
+
+  pushd fbgemm_gpu/bench || return 1
+
+  local cache_types=(
+    # fp16
+    fp32
+  )
+
+  local embedding_locations=(
+    # uvm
+    hbm
+  )
+
+  for cache_type in "${cache_types[@]}"; do
+    for embedding_location in "${embedding_locations[@]}"; do
+      __single_run "${cache_type}" "${embedding_location}" || return 1
+      echo ""
+      echo ""
+    done
+  done
+
+  popd || return 1
+}
