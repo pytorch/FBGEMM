cc67beadb7aa3ceebdab9a0f167d4de68fdf1c15
Josh Fromm <jwfromm@meta.com>
2025-03-18 17:54:25 -0700

    Move DeepGemm scale transpose to quantize (#920)
    Summary:
    X-link: https://github.com/pytorch/FBGEMM/pull/3834
    
    Pull Request resolved: https://github.com/facebookresearch/FBGEMM/pull/920
    
    For more fair comparisons, we can move a bit of scale preprocessing needed by DeepGemm to the quantize step. This diff makes that change and also does a small update to triton to include the latest features.
    
    Reviewed By: jiawenliu64
    
    Differential Revision: D71328564
    
    fbshipit-source-id: bfc54f1856abb72df12f0583add58a328872b5e1

--- fbgemm_gpu/experimental/gen_ai/bench/quantize_ops.py
+++ fbgemm_gpu/experimental/gen_ai/bench/quantize_ops.py
@@ -42,6 +42,7 @@ except ImportError:
 try:
     from deep_gemm import (
         gemm_fp8_fp8_bf16_nt,
+        get_col_major_tma_aligned_tensor,
         m_grouped_gemm_fp8_fp8_bf16_nt_contiguous,
     )
 
@@ -799,6 +800,8 @@ class DeepGemmStacked(QuantizeOpBase):
 
     def quantize(self, x, wq, w_scale, m_indices):
         xq, x_scale = quantize_fp8_block(x, block_m=1, block_k=128)
+        # Pretranspose scales to deepgemm format.
+        x_scale = get_col_major_tma_aligned_tensor(x_scale)
         return xq, wq, x_scale, w_scale, m_indices
 
     def compute(self, xq, wq, x_scale, w_scale, m_indices):
@@ -846,6 +849,8 @@ class DeepGemmBlockwise(QuantizeOpBase):
 
     def quantize(self, x, wq, w_scale, out):
         xq, x_scale = quantize_fp8_block(x, block_m=1, block_k=128)
+        # Pretranspose scales to deepgemm format.
+        x_scale = get_col_major_tma_aligned_tensor(x_scale)
         return xq, wq, x_scale, w_scale, out
 
     def compute(self, xq, wq, x_scale, w_scale, out):
@@ -1071,7 +1076,13 @@ class TritonFP8RowwiseGemm(QuantizeOpBase):
 
     def compute(self, xq, wq, x_scale, w_scale, bias):
         return matmul_fp8_row(
-            xq, wq, x_scale, w_scale, bias=bias, fp8_fast_accum=self.fast_accum
+            xq,
+            wq,
+            x_scale,
+            w_scale,
+            bias=bias,
+            fp8_fast_accum=self.fast_accum,
+            use_warp_specialization=True,
         )
 
     def quantize_and_compute(self, x, w):
