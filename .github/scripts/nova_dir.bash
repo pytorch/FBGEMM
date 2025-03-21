ff53743f17958bafe777e6e16c7ff03212661443
Benson Ma <bensonma415@meta.com>
2025-03-21 15:51:15 -0700

    Update Nova CI configuration to support B200 (#3868)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/958
    
    - Update Nova CI configuration to support B200
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3868
    
    Reviewed By: spcyppt
    
    Differential Revision: D71646490
    
    Pulled By: q10
    
    fbshipit-source-id: d738eec6ee494d3b57ee952c562562f3d6d8db5f

--- .github/scripts/nova_dir.bash
+++ .github/scripts/nova_dir.bash
@@ -22,10 +22,13 @@ fi
 ## Overwrite existing ENV VAR in Nova
 if [[ "$CONDA_ENV" != "" ]]; then export CONDA_RUN="conda run --no-capture-output -p ${CONDA_ENV}" && echo "$CONDA_RUN"; fi
 
-if  [[ "$CU_VERSION" == "cu121" ]] ||
-    [[ "$CU_VERSION" == "cu124" ]] ||
-    [[ "$CU_VERSION" == "cu126" ]] ||
-    [[ "$CU_VERSION" == "cu128" ]]; then
+if  [[ "$CU_VERSION" == "cu128" ]]; then
+    export TORCH_CUDA_ARCH_LIST="7.0;8.0;9.0;9.0a;10.0;10.0a;12.0;12.0a"
+    echo "Set TORCH_CUDA_ARCH_LIST to: ${TORCH_CUDA_ARCH_LIST}"
+
+elif [[ "$CU_VERSION" == "cu126" ]] ||
+     [[ "$CU_VERSION" == "cu124" ]] ||
+     [[ "$CU_VERSION" == "cu121" ]]; then
     export TORCH_CUDA_ARCH_LIST="7.0;8.0;9.0;9.0a"
     echo "Set TORCH_CUDA_ARCH_LIST to: ${TORCH_CUDA_ARCH_LIST}"
 
