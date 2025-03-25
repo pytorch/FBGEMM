bd55eb624ecef7926451884502feb23bbd564b69
Benson Ma <bensonma415@meta.com>
2025-03-25 10:18:35 -0700

    Clean up stochastic rounding benchmarks (#3876)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3876
    
    - Clean up stochastic rounding benchmarks
    
    Reviewed By: sryap
    
    Differential Revision: D71766109
    
    fbshipit-source-id: 02207f40b79fda724b1bda72ead934cd19ad9cb8

--- fbgemm_gpu/bench/verify_fp16_stochastic_benchmark.cu
+++ fbgemm_gpu/bench/verify_fp16_stochastic_benchmark.cu
@@ -16,7 +16,10 @@
 #include <unistd.h>
 #include <chrono>
 #include <iostream>
-#include <vector>
+
+////////////////////////////////////////////////////////////////////////////////
+// Direct Stochastic Rounding Kernels
+////////////////////////////////////////////////////////////////////////////////
 
 __device__ half float_to_sto_half_direct(float w) {
   curandState_t state;
@@ -34,8 +37,18 @@ __device__ half float_to_sto_half_direct(float w) {
   const float n = (up_f32 - w);
   return rand > n / m ? up : down;
 }
+__global__ void convert_float_to_half_direct(half* dst, float* src, int size) {
+  const auto idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < size) {
+    dst[idx] = float_to_sto_half_direct(src[idx]);
+  }
+}
 
-__device__ float two_to_e(float X) {
+////////////////////////////////////////////////////////////////////////////////
+// Bitcarry Stochastic Rounding Kernels
+////////////////////////////////////////////////////////////////////////////////
+
+__inline__ __device__ float two_to_e(float X) {
   const float Y = 16777216 * X; // 2^24
   const float U = ((Y + X) - Y) * 0.5;
   return U == 0 ? X : U;
@@ -50,28 +63,6 @@ __device__ half float_to_sto_half_bitcarry(float w) {
   return __float2half_rz(Z);
 }
 
-__device__ half float_to_sto_half_shortrand(float w, uint8_t rand) {
-  const unsigned w_int = __float_as_uint(w);
-  const unsigned w_new = w_int + (rand << 5);
-  return __float2half_rz(__uint_as_float(w_new));
-}
-
-__device__ half float_to_sto_half_assemblefloat(float w, uint8_t rand) {
-  const unsigned w_int = __float_as_uint(w);
-  const unsigned assembles = (w_int & 0xff800000) | (rand << 5);
-  const unsigned subtract = (w_int & 0xff800000);
-  const float assemble_float =
-      __uint_as_float(assembles) - __uint_as_float(subtract);
-  return __float2half_rz(w + assemble_float);
-}
-
-__global__ void convert_float_to_half_direct(half* dst, float* src, int size) {
-  const auto idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx < size) {
-    dst[idx] = float_to_sto_half_direct(src[idx]);
-  }
-}
-
 __global__ void
 convert_float_to_half_bitcarry(half* dst, float* src, int size) {
   const auto idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -80,12 +71,35 @@ convert_float_to_half_bitcarry(half* dst, float* src, int size) {
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Shortrand Stochastic Rounding Kernels
+////////////////////////////////////////////////////////////////////////////////
+
+__device__ half float_to_sto_half_shortrand(float w, uint8_t rand) {
+  const unsigned w_int = __float_as_uint(w);
+  const unsigned w_new = w_int + (rand << 5);
+  return __float2half_rz(__uint_as_float(w_new));
+}
+
 __global__ void
 convert_float_to_half_shortrand(half* dst, float* src, uint8_t* r, int size) {
   const auto idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < size) {
     dst[idx] = float_to_sto_half_shortrand(src[idx], r[idx]);
   }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// AssembleFloat Stochastic Rounding Kernels
+////////////////////////////////////////////////////////////////////////////////
+
+__device__ half float_to_sto_half_assemblefloat(float w, uint8_t rand) {
+  const unsigned w_int = __float_as_uint(w);
+  const unsigned assembles = (w_int & 0xff800000) | (rand << 5);
+  const unsigned subtract = (w_int & 0xff800000);
+  const float assemble_float =
+      __uint_as_float(assembles) - __uint_as_float(subtract);
+  return __float2half_rz(w + assemble_float);
 }
 
 __global__ void convert_float_to_half_assemblefloat(
@@ -98,6 +112,10 @@ __global__ void convert_float_to_half_assemblefloat(
     dst[idx] = float_to_sto_half_assemblefloat(src[idx], r[idx]);
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// Random Data Generation
+////////////////////////////////////////////////////////////////////////////////
 
 void gen_data(float* d_f32_array, int test_size) {
   curandGenerator_t gen;
@@ -127,6 +145,10 @@ void gen_8bit_random(uint8_t* d_random_number, int test_size) {
   cudaFree(d_random_number_f32);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Cache Flusher
+////////////////////////////////////////////////////////////////////////////////
+
 __global__ void flush_gpu(char* d_flush, char* d_flush2, bool do_write) {
   const auto idx = blockIdx.x * blockDim.x + threadIdx.x;
   const char val = d_flush[idx];
@@ -135,34 +157,118 @@ __global__ void flush_gpu(char* d_flush, char* d_flush2, bool do_write) {
   }
 }
 
-void flush_cache(
-    std::vector<char> flush,
-    char* d_flush,
-    char* d_flush2,
-    int cache_size,
-    bool do_write = false) {
-  cudaMemcpy(d_flush, flush.data(), cache_size, cudaMemcpyHostToDevice);
-  const unsigned num_blocks = cache_size / 512;
-  flush_gpu<<<num_blocks, 512>>>(d_flush, d_flush2, do_write);
+class CacheFlusher {
+  size_t cache_size;
+
+  std::vector<char> h_flush;
+  char* d_flush;
+  char* d_flush2;
+
+ public:
+  CacheFlusher() {
+    // Use the first device to determine L2 cache size
+    cudaDeviceProp properties;
+    cudaGetDeviceProperties(&properties, 0);
+    cache_size = properties.l2CacheSize;
+
+    h_flush.assign(cache_size, 255);
+    cudaMalloc(&d_flush, cache_size * sizeof(char));
+    cudaMalloc(&d_flush2, cache_size * sizeof(char));
+  }
+
+  inline void flush(bool do_write = false) const {
+    // Force a copy from host to data1, and from data1 to data2 buffer, to flush
+    // the L2 cache
+    cudaMemcpy(d_flush, h_flush.data(), cache_size, cudaMemcpyHostToDevice);
+    const unsigned num_blocks = cache_size / 512;
+    flush_gpu<<<num_blocks, 512>>>(d_flush, d_flush2, do_write);
+    C10_CUDA_KERNEL_LAUNCH_CHECK();
+    cudaDeviceSynchronize();
+  }
+
+  ~CacheFlusher() {
+    if (d_flush) {
+      cudaFree(d_flush);
+    }
+
+    if (d_flush2) {
+      cudaFree(d_flush2);
+    }
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Host-Device Buffer Pair
+////////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+struct BufferPair {
+  std::vector<T> host;
+  T* device = nullptr;
+
+  BufferPair(size_t size) {
+    init(size);
+  }
+
+  void init(size_t size) {
+    free();
+    host.reserve(size);
+    cudaMalloc(&device, size * sizeof(T));
+  }
+
+  void free() {
+    if (device) {
+      cudaFree(device);
+    }
+  }
+
+  void syncToDevice() {
+    cudaMemcpy(
+        device, host.data(), host.size() * sizeof(T), cudaMemcpyHostToDevice);
+  }
+
+  void syncToHost() {
+    cudaMemcpy(
+        host.data(), device, host.size() * sizeof(T), cudaMemcpyDeviceToHost);
+  }
+
+  ~BufferPair() {
+    free();
+  }
+};
+
+template <typename KernelFunc, typename... Args>
+void time_kernel_run(
+    const std::string& description,
+    const KernelFunc& kernel,
+    dim3 grid,
+    dim3 block,
+    Args&&... args) {
+  std::cout << "[" << description << "] starting kernel run ..." << std::endl;
+
+  const auto start = std::chrono::high_resolution_clock::now();
+  kernel<<<grid, block>>>(std::forward<Args>(args)...);
   C10_CUDA_KERNEL_LAUNCH_CHECK();
+  cudaDeviceSynchronize();
+  const auto end = std::chrono::high_resolution_clock::now();
+
+  const auto e = cudaGetLastError();
+  if (e != cudaSuccess) {
+    std::cout << "[" << description
+              << "] CUDA Failure: " << cudaGetErrorString(e) << std::endl;
+    std::exit(-1);
+  }
+
+  const std::chrono::duration<double> time = end - start;
+  std::cout << "[" << description << "] " << time.count() << " sec(s)\n"
+            << std::endl;
+  return;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<float> f32_array;
-  std::vector<half> f16_direct_array;
-  std::vector<half> f16_bitcarry_array;
-  std::vector<half> f16_shortrand_array;
-  std::vector<half> f16_assemblefloat_array;
-  float* d_f32_array;
-  half* d_f16_direct_array;
-  half* d_f16_bitcarry_array;
-  half* d_f16_shortrand_array;
-  half* d_f16_assemblefloat_array;
   uint8_t* d_random_number;
 
-  std::vector<char> flush;
-  char* d_flush;
-  char* d_flush2;
+  const auto flusher = CacheFlusher();
 
   int test_size = 10;
   bool verbose = false;
@@ -179,147 +285,86 @@ int main(int argc, char* argv[]) {
   }
 
   std::cout << "Start stochastic algorithm tests with test_size = " << test_size
+            << "\n"
             << std::endl;
-  constexpr int cache_size = 40 * 1024 * 1024; // A100 40MB L2 cache
 
-  f32_array.reserve(test_size);
-  f16_direct_array.reserve(test_size);
-  f16_bitcarry_array.reserve(test_size);
-  f16_shortrand_array.reserve(test_size);
-  f16_assemblefloat_array.reserve(test_size);
-  cudaMalloc(&d_f32_array, test_size * sizeof(float));
-  cudaMalloc(&d_f16_direct_array, test_size * sizeof(half));
-  cudaMalloc(&d_f16_bitcarry_array, test_size * sizeof(half));
-  cudaMalloc(&d_f16_shortrand_array, test_size * sizeof(half));
-  cudaMalloc(&d_f16_assemblefloat_array, test_size * sizeof(half));
+  BufferPair<float> f32(test_size);
+  BufferPair<half> f16_direct(test_size), f16_bitcarry(test_size),
+      f16_shortrand(test_size), f16_assemblefloat(test_size);
+
   cudaMalloc(&d_random_number, test_size * sizeof(uint8_t));
 
-  flush.assign(cache_size, 255);
-  cudaMalloc(&d_flush, cache_size * sizeof(char));
-  cudaMalloc(&d_flush2, cache_size * sizeof(char));
-
-  gen_data(d_f32_array, test_size);
+  // Generate random data
+  gen_data(f32.device, test_size);
   gen_8bit_random(d_random_number, test_size);
 
   constexpr int block_size = 128;
   const int num_blocks = (test_size + block_size - 1) / block_size;
 
-  flush_cache(flush, d_flush, d_flush2, cache_size);
-  std::cout << "Starting algorithm direct..." << std::endl;
-  auto start = std::chrono::high_resolution_clock::now();
-  convert_float_to_half_direct<<<num_blocks, block_size>>>(
-      d_f16_direct_array, d_f32_array, test_size);
-  C10_CUDA_KERNEL_LAUNCH_CHECK();
-  cudaDeviceSynchronize();
-  auto end = std::chrono::high_resolution_clock::now();
-  cudaError_t e = cudaGetLastError();
-  if (e != cudaSuccess) {
-    std::cout << "Cuda failure: " << cudaGetErrorString(e) << std::endl;
-    exit(-1);
-  }
-  std::chrono::duration<double> time = end - start;
-  std::cout << "Direct stochastic algorithm runs: " << time.count() << " sec "
-            << std::endl;
+  flusher.flush();
+  time_kernel_run(
+      "Direct Stochastic Algorithm",
+      convert_float_to_half_direct,
+      num_blocks,
+      block_size,
+      f16_direct.device,
+      f32.device,
+      test_size);
 
-  flush_cache(flush, d_flush, d_flush2, cache_size);
-  std::cout << "Starting algorithm bitcarry..." << std::endl;
-  start = std::chrono::high_resolution_clock::now();
-  convert_float_to_half_bitcarry<<<num_blocks, block_size>>>(
-      d_f16_bitcarry_array, d_f32_array, test_size);
-  C10_CUDA_KERNEL_LAUNCH_CHECK();
-  cudaDeviceSynchronize();
-  end = std::chrono::high_resolution_clock::now();
-  e = cudaGetLastError();
-  if (e != cudaSuccess) {
-    std::cout << "Cuda failure: " << cudaGetErrorString(e) << std::endl;
-    exit(-1);
-  }
-  time = end - start;
-  std::cout << "Bitcarry stochastic algorithm runs: " << time.count() << " sec"
-            << std::endl;
+  flusher.flush();
+  time_kernel_run(
+      "Bitcarry Stochastic Algorithm",
+      convert_float_to_half_bitcarry,
+      num_blocks,
+      block_size,
+      f16_bitcarry.device,
+      f32.device,
+      test_size);
 
-  flush_cache(flush, d_flush, d_flush2, cache_size);
-  std::cout << "Starting algorithm shortrand..." << std::endl;
-  start = std::chrono::high_resolution_clock::now();
-  convert_float_to_half_shortrand<<<num_blocks, block_size>>>(
-      d_f16_shortrand_array, d_f32_array, d_random_number, test_size);
-  C10_CUDA_KERNEL_LAUNCH_CHECK();
-  cudaDeviceSynchronize();
-  end = std::chrono::high_resolution_clock::now();
-  e = cudaGetLastError();
-  if (e != cudaSuccess) {
-    std::cout << "Cuda failure: " << cudaGetErrorString(e) << std::endl;
-    exit(-1);
-  }
-  time = end - start;
-  std::cout << "Shortrand stochastic algorithm runs: " << time.count() << " sec"
-            << std::endl;
+  flusher.flush();
+  time_kernel_run(
+      "Shortrand Stochastic Algorithm",
+      convert_float_to_half_shortrand,
+      num_blocks,
+      block_size,
+      f16_shortrand.device,
+      f32.device,
+      d_random_number,
+      test_size);
 
-  flush_cache(flush, d_flush, d_flush2, cache_size);
-  std::cout << "Starting algorithm assemblefloat..." << std::endl;
-  start = std::chrono::high_resolution_clock::now();
-  convert_float_to_half_assemblefloat<<<num_blocks, block_size>>>(
-      d_f16_assemblefloat_array, d_f32_array, d_random_number, test_size);
-  C10_CUDA_KERNEL_LAUNCH_CHECK();
-  cudaDeviceSynchronize();
-  end = std::chrono::high_resolution_clock::now();
-  e = cudaGetLastError();
-  if (e != cudaSuccess) {
-    std::cout << "Cuda failure: " << cudaGetErrorString(e) << std::endl;
-    exit(-1);
-  }
-  time = end - start;
-  std::cout << "Assemblefloat stochastic algorithm runs: " << time.count()
-            << " sec" << std::endl;
+  flusher.flush();
+  time_kernel_run(
+      "AssembleFloat Stochastic Algorithm",
+      convert_float_to_half_assemblefloat,
+      num_blocks,
+      block_size,
+      f16_assemblefloat.device,
+      f32.device,
+      d_random_number,
+      test_size);
 
   if (verbose) {
-    cudaMemcpy(
-        f32_array.data(),
-        d_f32_array,
-        test_size * sizeof(float),
-        cudaMemcpyDeviceToHost);
-    cudaMemcpy(
-        f16_direct_array.data(),
-        d_f16_direct_array,
-        test_size * sizeof(half),
-        cudaMemcpyDeviceToHost);
-    cudaMemcpy(
-        f16_bitcarry_array.data(),
-        d_f16_bitcarry_array,
-        test_size * sizeof(half),
-        cudaMemcpyDeviceToHost);
-    cudaMemcpy(
-        f16_shortrand_array.data(),
-        d_f16_shortrand_array,
-        test_size * sizeof(half),
-        cudaMemcpyDeviceToHost);
-    cudaMemcpy(
-        f16_assemblefloat_array.data(),
-        d_f16_assemblefloat_array,
-        test_size * sizeof(half),
-        cudaMemcpyDeviceToHost);
+    f32.syncToHost();
+    f16_direct.syncToHost();
+    f16_bitcarry.syncToHost();
+    f16_shortrand.syncToHost();
+    f16_assemblefloat.syncToHost();
 
     for (int i = 0; i < test_size; i++) {
-      std::cout << std::hexfloat << f32_array[i] << ":\t(up:" << std::hexfloat
-                << __half2float(__float2half_ru(f32_array[i]))
+      std::cout << std::hexfloat << f32.host[i] << ":\t(up:" << std::hexfloat
+                << __half2float(__float2half_ru(f32.host[i]))
                 << "\tdown:" << std::hexfloat
-                << __half2float(__float2half_rd(f32_array[i]))
+                << __half2float(__float2half_rd(f32.host[i]))
                 << ") \tdirect: " << std::hexfloat
-                << __half2float(f16_direct_array[i])
+                << __half2float(f16_direct.host[i])
                 << "\tbitcarry: " << std::hexfloat
-                << __half2float(f16_bitcarry_array[i])
+                << __half2float(f16_bitcarry.host[i])
                 << " \tshortrand: " << std::hexfloat
-                << __half2float(f16_shortrand_array[i])
+                << __half2float(f16_shortrand.host[i])
                 << " \tassemblefloat: " << std::hexfloat
-                << __half2float(f16_assemblefloat_array[i]) << std::endl;
+                << __half2float(f16_assemblefloat.host[i]) << std::endl;
     }
   }
-
-  cudaFree(d_f32_array);
-  cudaFree(d_f16_direct_array);
-  cudaFree(d_f16_bitcarry_array);
-  cudaFree(d_f16_shortrand_array);
-  cudaFree(d_f16_assemblefloat_array);
 
   return 0;
 }
