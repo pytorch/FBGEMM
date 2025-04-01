3de10557e8278d35d43dd1becf30f49bb435cfd8
Sarunya Pumma <sarunya@meta.com>
2025-03-31 23:25:39 -0700

    Add a workaround for stochastic rounding for AMD GPUs
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/997
    
    This diff contains a workaround for the stochastic rounding issue for
    the AMD GPUs.
    
    Problem:
    
    `quantize_store` calls `nearest_rounding_vector` instead of
    `stochastic_rounding_vector` when stochastic rounding is used because
    the `StochasticRoundingRNGState` pointer is a nullptr
    (https://fburl.com/code/kna14icj)
    
    We found that the `WeightRow` constructor also gets a null
    `StochasticRoundingRNGState` pointer (https://fburl.com/code/vyq53lia)
    
    When `WeightRow` is instantiated, we confirm that
    `stochastic_rounding` is
    true.  `WeightRow` should receive `&state`, but instead it receives a
    nullptr. (https://fburl.com/code/o3kxgt4z)
    
    We suspect that the compiler might have optimized out the
    `StochasticRoundingRNGState` since it is only passed to `WeightRow`
    and not utilized anywhere else in the caller kernel.
    
    Workaround:
    
    We move the `StochasticRoundingRNGState` storage inside the
    `WeightRow` struct and pass a boolean to the `WeightRow` constructor
    instead.
    
    Differential Revision: D72201618

--- fbgemm_gpu/src/split_embeddings_cache/lru_cache_populate.cu
+++ fbgemm_gpu/src/split_embeddings_cache/lru_cache_populate.cu
@@ -123,12 +123,11 @@ __global__ __launch_bounds__(kMaxThreads) void lru_cache_insert_kernel(
           D_emb += kINT8QparamsBytes;
         }
 
-        StochasticRoundingRNGState state;
         auto weight_row = WeightRow<emb_t, cache_t, cache_t>(
             &weights[weights_offset_current + idx_current * D_emb + 0],
             &lxu_cache_weights[cache_set * kWarpSize + insert_slot][0],
             D_current,
-            stochastic_rounding ? &state : nullptr,
+            stochastic_rounding,
             &stochastic_rounding_philox_args,
             stoc_rounding_salt + l);
 
