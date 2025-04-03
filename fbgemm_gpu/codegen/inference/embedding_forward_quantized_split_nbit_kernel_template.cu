527dd43e4120202179d456515f59dfab2db669b3
Sarunya Pumma <sarunya@meta.com>
2025-04-03 15:51:42 -0700

    Partial revert D70855331 (#1013)
    Summary:
    X-link: https://github.com/pytorch/FBGEMM/pull/3925
    
    Pull Request resolved: https://github.com/facebookresearch/FBGEMM/pull/1013
    
    Original commit changeset: b32e59e07018
    
    Original Phabricator Diff: D70855331
    
    Revert the code on NVIDIA but keep it for AMD
    
    Differential Revision: D72400686
    
    fbshipit-source-id: ae2489060878af7df99fb648918f2a813be3932d

--- fbgemm_gpu/codegen/inference/embedding_forward_quantized_split_nbit_kernel_template.cu
+++ fbgemm_gpu/codegen/inference/embedding_forward_quantized_split_nbit_kernel_template.cu
@@ -212,11 +212,15 @@ __global__ void {{ emb_weight_type.enum_name }}_split_embedding{{ "_nobag" if no
             buffers[warp_idx][i][input_row_idx][row_load_idx] = data;
           }
           {% if weighted %}
+          {%- if is_rocm %}
           if (valid && row_load_idx == 0)  {
             // Use only one thread to load the index weight to prevent a race
             // condition when writing to the shared memory
             buffers_indice_weights[warp_idx][i][input_row_idx][packed_bag_load_idx] = indice_weights[indices_starts[i] + L_start + input_row_idx];
           }
+          {%- else %}
+          buffers_indice_weights[warp_idx][i][input_row_idx][packed_bag_load_idx] = valid ? indice_weights[indices_starts[i] + L_start + input_row_idx] : 0.0;
+          {%- endif %}
           {% endif %}
         }
       }
@@ -251,11 +255,15 @@ __global__ void {{ emb_weight_type.enum_name }}_split_embedding{{ "_nobag" if no
           cp_async_zfill_cg<sizeof(uint4)>(&buffers[warp_idx][i][input_row_idx][row_load_idx], &row[row_load_idx], valid);
         }
         {% if weighted %}
+        {%- if is_rocm %}
         if (valid && row_load_idx == 0) {
           // Use only one thread to load the index weight to prevent a race
           // condition when writing to the shared memory
           buffers_indice_weights[warp_idx][i][input_row_idx][packed_bag_load_idx] = indice_weights[indices_starts[i] + L_start + input_row_idx];
         }
+        {%- else %}
+        buffers_indice_weights[warp_idx][i][input_row_idx][packed_bag_load_idx] = valid ? indice_weights[indices_starts[i] + L_start + input_row_idx] : 0.0;
+        {%- endif %}
         {% endif %}
       }
       {%- if is_rocm %}
