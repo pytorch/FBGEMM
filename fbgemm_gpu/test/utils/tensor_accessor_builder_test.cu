80f566eec90354050b829d88f283f62037cb4502
Benson Ma <bensonma415@meta.com>
2025-04-08 20:52:26 -0700

    Better kernel launch utilities (#1009)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/FBGEMM/pull/1009
    
    X-link: https://github.com/pytorch/FBGEMM/pull/3914
    
    We have a few things to do as part of the boilerplate ritual of launching GPU kernels:
    
    - Perform host-side precondition checks on arguments namely tensors and tensor types
    - Perform host-side precondition checks on kernel launch parameters, i.e. whether grid size is too large, shared memory is too large, etc
    - Build `pta::PackedTensorAccessor` from tensors, and pass context into it
    - Perform host-side post-run checks for CUDA run errors.
    - Pass source context information around for consistent log and error messages
    - Reduce the pain with dealing with incorrect macro invocations
    
    The `TensorAccessorBuilder`, `SourceContext`, and `KernelLauncher` classes, together with their construction macros, cover these aspects and provide the space for future exansion of boilerplate rituals in a low to zero-cost abstraction, all while improving code ergonomics around their usage.
    
    Before:
    
    ```
    #ifdef FBGEMM_GPU_MEMCHECK
                  const auto func_name = "pruned_array_lookup_from_row_idx_kernel";
    #endif
                  pruned_array_lookup_from_row_idx_kernel<<<
                      nbit::div_round_up(num_indices, kForwardMaxThreads),
                      kForwardMaxThreads,
                      0,
                      at::cuda::getCurrentCUDAStream()>>>(
                      MAKE_PTA_WITH_NAME(
                          func_name, update_row_indices, index_t, 1, 32),
                      MAKE_PTA_WITH_NAME(
                          func_name, update_table_indices, int32_t, 1, 32),
                      MAKE_PTA_WITH_NAME(
                          func_name, index_remappings, remap_t, 1, 32),
                      MAKE_PTA_WITH_NAME(
                          func_name, index_remappings_offsets, int64_t, 1, 32),
                      MAKE_PTA_WITH_NAME(func_name, dense_indices, index_t, 1, 32));
                  C10_CUDA_KERNEL_LAUNCH_CHECK();
    ```
    
    After:
    ```
                  FBGEMM_LAUNCH_KERNEL(
                      (pruned_array_lookup_from_row_idx_kernel<index_t, remap_t>),
                      nbit::div_round_up(num_indices, kForwardMaxThreads),
                      kForwardMaxThreads,
                      PTA_B(update_row_indices, index_t, 1, 32),
                      PTA_B(update_table_indices, int32_t, 1, 32),
                      PTA_B(index_remappings, remap_t, 1, 32),
                      PTA_B(index_remappings_offsets, int64_t, 1, 32),
                      PTA_B(dense_indices, index_t, 1, 32));
    ```
    
    Reviewed By: sryap
    
    Differential Revision: D72095960
    
    fbshipit-source-id: eec20854ab178adbacd4d6163596b554481dfafa

---
+++ fbgemm_gpu/test/utils/tensor_accessor_builder_test.cu
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <ATen/ATen.h>
+#include <gtest/gtest.h>
+#include <torch/types.h> // @manual=//caffe2:torch-cpp-cpu
+
+// DISABLE compilation in FBGEMM_GPU_MEMCHECK mode as a test
+#ifdef FBGEMM_GPU_MEMCHECK
+#undef FBGEMM_GPU_MEMCHECK
+#endif
+
+#include "fbgemm_gpu/utils/tensor_accessor_builder.h"
+
+template <typename T>
+void test_ta_create_1(const at::Tensor& tensor) {
+  TA_B(tensor, T, 1, 64).build("test_ta_create");
+}
+
+template <size_t N>
+void test_ta_create_2(const at::Tensor& tensor) {
+  TA_B(tensor, float, N, 64).build("test_ta_create");
+}
+
+void test_ta_create_3(const at::Tensor& tensor) {
+  TA_B(tensor, float, 1, 64).build("test_ta_create");
+}
+
+TEST(TensorAccessorBuilderTest, test_ta_create) {
+  const auto tensor = torch::tensor(
+      {1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f}, torch::kFloat32);
+  // Test mismatched types
+  EXPECT_THROW({ test_ta_create_1<int32_t>(tensor); }, std::exception);
+  EXPECT_THROW({ test_ta_create_1<int64_t>(tensor); }, std::exception);
+  EXPECT_THROW({ test_ta_create_1<double>(tensor); }, std::exception);
+
+  // Test invalid dimensions
+  EXPECT_THROW({ test_ta_create_2<2>(tensor); }, std::exception);
+  EXPECT_THROW({ test_ta_create_2<3>(tensor); }, std::exception);
+  EXPECT_THROW({ test_ta_create_2<4>(tensor); }, std::exception);
+
+  // Test valid type and dimension
+  EXPECT_NO_THROW({ test_ta_create_3(tensor); });
+}
+
+template <typename T>
+void test_pta_create_1(const at::Tensor& tensor) {
+  PTA_B(tensor, T, 1, 64).build("test_pta_create");
+}
+
+template <size_t N>
+void test_pta_create_2(const at::Tensor& tensor) {
+  PTA_B(tensor, float, N, 64).build("test_pta_create");
+}
+
+void test_pta_create_3(const at::Tensor& tensor) {
+  PTA_B(tensor, float, 1, 64).build("test_pta_create");
+}
+
+TEST(TensorAccessorBuilderTest, test_pta_create) {
+  const auto tensor = torch::tensor(
+      {1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f}, torch::kFloat32);
+  // Test mismatched types
+  EXPECT_THROW({ test_pta_create_1<int32_t>(tensor); }, std::exception);
+  EXPECT_THROW({ test_pta_create_1<int64_t>(tensor); }, std::exception);
+  EXPECT_THROW({ test_pta_create_1<double>(tensor); }, std::exception);
+
+  // Test invalid dimensions
+  EXPECT_THROW({ test_pta_create_2<2>(tensor); }, std::exception);
+  EXPECT_THROW({ test_pta_create_2<3>(tensor); }, std::exception);
+  EXPECT_THROW({ test_pta_create_2<4>(tensor); }, std::exception);
+
+  // Test valid type and dimension
+  EXPECT_NO_THROW({ test_pta_create_3(tensor); });
+}
