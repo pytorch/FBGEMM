f0198054eea2b9169ad13dc1f83d62c2cd8ff7a2
Benson Ma <bensonma415@meta.com>
2025-04-10 10:43:07 -0700

    Better kernel launch utilities, pt 2 (#3947)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1032
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3947
    
    Following up on D72095960, add more kernel launch constraint checks
    
    Reviewed By: sryap
    
    Differential Revision: D72597771
    
    fbshipit-source-id: 59fe9e1836307e3f56e124e2ec2caf60b5065a44

--- fbgemm_gpu/include/fbgemm_gpu/utils/kernel_launcher.cuh
+++ fbgemm_gpu/include/fbgemm_gpu/utils/kernel_launcher.cuh
@@ -11,10 +11,10 @@
 #include <ATen/ATen.h>
 #include <c10/cuda/CUDAStream.h>
 
+#include "fbgemm_gpu/utils/device_properties.cuh"
 #include "fbgemm_gpu/utils/source_context.h"
 #include "fbgemm_gpu/utils/tensor_accessor_builder.h"
 
-#include <iostream>
 #include <type_traits>
 
 namespace fbgemm_gpu::utils {
@@ -77,32 +77,148 @@ struct KernelLauncher {
       const std::string_view& summary) noexcept
       : context(SourceContext(location, summary)) {}
 
+  constexpr inline void checkGridSizesInRange(
+      const cudaDeviceProp& properties,
+      const dim3& grid) const {
+    const auto grid_limits = properties.maxGridSize;
+
+    TORCH_CHECK(
+        grid.x > 0 && grid.x <= grid_limits[0],
+        context.description(),
+        ": grid.x value ",
+        grid.x,
+        " is not within the range (0, ",
+        grid_limits[0],
+        ")");
+
+    TORCH_CHECK(
+        grid.y > 0 && grid.y <= grid_limits[1],
+        context.description(),
+        ": grid.y value ",
+        grid.y,
+        " is not within the range (0, ",
+        grid_limits[1],
+        ")");
+
+    TORCH_CHECK(
+        grid.z > 0 && grid.z <= grid_limits[2],
+        context.description(),
+        ": grid.z value ",
+        grid.z,
+        " is not within the range (0, ",
+        grid_limits[2],
+        ")");
+  }
+
+  constexpr inline void checkBlockSizesInRange(
+      const cudaDeviceProp& properties,
+      const dim3& block) const {
+    const auto block_limits = properties.maxThreadsDim;
+
+    TORCH_CHECK(
+        block.x > 0 && block.x <= block_limits[0],
+        context.description(),
+        ": block.x value ",
+        block.x,
+        " is not within the range (0, ",
+        block_limits[0],
+        ")");
+
+    TORCH_CHECK(
+        block.y > 0 && block.y <= block_limits[1],
+        context.description(),
+        ": block.y value ",
+        block.y,
+        " is not within the range (0, ",
+        block_limits[1],
+        ")");
+
+    TORCH_CHECK(
+        block.z > 0 && block.z <= block_limits[2],
+        context.description(),
+        ": block.z value ",
+        block.z,
+        " is not within the range (0, ",
+        block_limits[2],
+        ")");
+  }
+
+  constexpr inline void checkThreadCountNotExceeded(
+      const cudaDeviceProp& properties,
+      const dim3& grid,
+      const dim3& block) const {
+    const uint64_t threads_per_block =
+        U64(block.x) * U64(block.y) * U64(block.z);
+
+    TORCH_CHECK(
+        threads_per_block <= properties.maxThreadsPerBlock,
+        context.description(),
+        ": Threads per block ",
+        threads_per_block,
+        " is greater than the limit of ",
+        properties.maxThreadsPerBlock);
+
+#if defined(USE_ROCM) || (defined(__CUDA_ARCH__) && (__CUDA_ARCH__ < 700))
+    // ROCm has a limit of 2^32 elements per kernel launch, but doesn't
+    // automatically work around problem like CUDA does (V100 or newer
+    // architectures), see:
+    //    https://github.com/ROCm/hip/issues/2253
+    const uint64_t total_threads = U64(grid.x) * U64(grid.y) * U64(grid.z) *
+        U64(block.x) * U64(block.y) * U64(block.z);
+
+    TORCH_CHECK(
+        total_threads < U64(std::numeric_limits<uint32_t>::max()),
+        context.description(),
+        ": Total number of threads ",
+        total_threads,
+        " is greater than the limit of 2^32");
+#endif
+  }
+
+  constexpr inline void checkSharedMemoryPerBlockNotExceeded(
+      const cudaDeviceProp& properties,
+      const size_t shared_mem_per_block) const {
+    const auto smem_limits = properties.sharedMemPerBlock;
+
+    TORCH_CHECK(
+        shared_mem_per_block >= 0 && shared_mem_per_block <= smem_limits,
+        context.description(),
+        ": Requested shared memory per block (",
+        shared_mem_per_block,
+        " bytes) is not within the range [0, ",
+        smem_limits,
+        "]");
+  }
+
   template <typename KernelFunc, typename... Args>
-  constexpr inline void launch_kernel(
+  inline void launch_kernel(
       const KernelFunc& kernel,
       const dim3 grid,
       const dim3 block,
-      const size_t Ns,
-      cudaStream_t stream,
-      Args&&... args) {
-    // TODO: Check smem size based on actual hardware
+      const size_t shared_mem_per_block,
+      const cudaStream_t stream,
+      Args&&... args) const {
+    // Fetch device properties from the stream information
+    const auto device = get_device_for_stream(stream);
+    const auto properties = get_device_properties(device);
 
-#ifdef USE_ROCM
-    // ROCm has a limit of 2^32 elements per kernel launch, but doens't
-    // automatically work around problem like CUDA does, see:
-    //  https://github.com/ROCm/hip/issues/2253
-    const uint64_t grid_size = U64(grid.x) * U64(grid.y) * U64(grid.z) *
-        U64(block.x) * U64(block.y) * U64(block.z);
-    TORCH_CHECK(
-        grid_size < U64(std::numeric_limits<uint32_t>::max()),
-        context.description(),
-        ": Kernel launch grid size ",
-        grid_size,
-        " is greater than the ROCm limit of 2^32");
-#endif
+    // Check that the grid sizes are within the range per the device associated
+    // with the compute stream
+    checkGridSizesInRange(properties, grid);
+
+    // Check that the grid sizes are within the range per the device associated
+    // with the compute stream
+    checkBlockSizesInRange(properties, block);
+
+    // Check that the thread count (per block and global) is not exceeded
+    checkThreadCountNotExceeded(properties, grid, block);
+
+    // Check that the shared memory allocation is within the range per the
+    // device associated with the compute stream
+    checkSharedMemoryPerBlockNotExceeded(properties, shared_mem_per_block);
 
     // Launch the kernel
-    kernel<<<grid, block, Ns, stream>>>(
+    kernel<<<grid, block, shared_mem_per_block, stream>>>(
         // Transform arguments to the kernel before forwarding them.
         transform_kernel_arg(context, std::forward<Args>(args))...);
 
