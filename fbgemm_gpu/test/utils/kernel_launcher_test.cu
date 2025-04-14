9a2ad5337aa7ee1f0272badc9e7f49012bc28243
Benson Ma <bensonma415@meta.com>
2025-04-14 13:12:34 -0700

    Incorporate Torch DSA (#3950)
    Summary:
    X-link: https://github.com/pytorch/pytorch/pull/151148
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1035
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3950
    
    - Incorporte the PyTorch DSA infrastructure into the FBGEMM kernel launcher
      utility
    
    Reviewed By: huydhn
    
    Differential Revision: D72759030

--- fbgemm_gpu/test/utils/kernel_launcher_test.cu
+++ fbgemm_gpu/test/utils/kernel_launcher_test.cu
@@ -7,7 +7,9 @@
  */
 
 #include <ATen/ATen.h>
+#include <c10/cuda/CUDADeviceAssertion.h>
 #include <cuda.h>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <torch/types.h> // @manual=//caffe2:torch-cpp-cpu
 
@@ -20,8 +22,27 @@ namespace fbgemm_gpu::utils {
 
 #define U32(x) static_cast<uint32_t>(x)
 
+using ::testing::HasSubstr;
+
+////////////////////////////////////////////////////////////////////////////////
+// Test Kernels
+////////////////////////////////////////////////////////////////////////////////
+
 template <typename T>
 __global__ void array_sum_kernel(T* C, const T* A, const T* B, size_t size) {
+  const auto idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < size) {
+    C[idx] = A[idx] + B[idx];
+  }
+}
+
+template <typename T>
+__global__ void array_sum_dsa_kernel(
+    T* C,
+    const T* A,
+    const T* B,
+    size_t size,
+    TORCH_DSA_KERNEL_ARGS) {
   const auto idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < size) {
     C[idx] = A[idx] + B[idx];
@@ -32,11 +53,18 @@ template <typename T>
 __global__ void tensor_sum_kernel(
     pta::PackedTensorAccessor64<T, 1, at::RestrictPtrTraits> C,
     const pta::PackedTensorAccessor64<T, 1, at::RestrictPtrTraits> A,
-    const pta::PackedTensorAccessor64<T, 1, at::RestrictPtrTraits> B) {
+    const pta::PackedTensorAccessor64<T, 1, at::RestrictPtrTraits> B,
+    TORCH_DSA_KERNEL_ARGS) {
   const auto idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < C.size(0)) {
     C[idx] = A[idx] + B[idx];
   }
+}
+
+__global__ void always_fail_assertion_kernel(
+    const int a,
+    TORCH_DSA_KERNEL_ARGS) {
+  CUDA_KERNEL_ASSERT2((a != a) && "This assertion should always fail");
 }
 
 auto sample_tensors(const long size) {
@@ -60,6 +88,10 @@ auto sample_tensors(const long size) {
 
   return std::make_tuple(A, B, C);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// Kernel Launcher Tests
+////////////////////////////////////////////////////////////////////////////////
 
 TEST(KernelLauncherTest, test_array_kernel_launch) {
   constexpr auto size = 1024;
@@ -87,6 +119,32 @@ TEST(KernelLauncherTest, test_array_kernel_launch) {
   });
 }
 
+TEST(KernelLauncherTest, test_array_kernel_launch_dsa) {
+  constexpr auto size = 1024;
+  auto A = HostDeviceBufferPair<float>(size, 2);
+  auto B = HostDeviceBufferPair<float>(size, 3);
+  auto C = HostDeviceBufferPair<float>(size, -1);
+
+  EXPECT_NO_THROW({
+    FBGEMM_LAUNCH_DSA_KERNEL(
+        array_sum_dsa_kernel<float>,
+        8,
+        1024,
+        0,
+        at::cuda::getCurrentCUDAStream(),
+        C.device,
+        A.device,
+        B.device,
+        size);
+
+    C.syncToHost();
+
+    for (const auto x : C.host) {
+      EXPECT_EQ(x, 5.0f);
+    }
+  });
+}
+
 TEST(KernelLauncherTest, tensor_array_kernel_launch) {
   const auto size = 1024;
   // Not using structured bindings bc it fails on ROCm with:
@@ -96,7 +154,7 @@ TEST(KernelLauncherTest, tensor_array_kernel_launch) {
 
   // Test normal kernel launch succeeds
   EXPECT_NO_THROW({
-    FBGEMM_LAUNCH_KERNEL(
+    FBGEMM_LAUNCH_DSA_KERNEL(
         tensor_sum_kernel<float>,
         8,
         1024,
@@ -131,7 +189,7 @@ TEST(KernelLauncherTest, kernel_launch_checks) {
   // Test grid size bounds checking
   EXPECT_THROW(
       {
-        FBGEMM_LAUNCH_KERNEL(
+        FBGEMM_LAUNCH_DSA_KERNEL(
             tensor_sum_kernel<float>,
             // grid dims are too large
             grid_max[0] + 1,
@@ -147,7 +205,7 @@ TEST(KernelLauncherTest, kernel_launch_checks) {
   // Test block size bounds checking
   EXPECT_THROW(
       {
-        FBGEMM_LAUNCH_KERNEL(
+        FBGEMM_LAUNCH_DSA_KERNEL(
             tensor_sum_kernel<float>,
             8,
             // block dims are too large
@@ -164,7 +222,7 @@ TEST(KernelLauncherTest, kernel_launch_checks) {
   // Test max thread count
   EXPECT_THROW(
       {
-        FBGEMM_LAUNCH_KERNEL(
+        FBGEMM_LAUNCH_DSA_KERNEL(
             tensor_sum_kernel<float>,
             // Both grid and block dims conform, but the total number of threads
             // exceeds the max
@@ -182,7 +240,7 @@ TEST(KernelLauncherTest, kernel_launch_checks) {
   // Test shared memory size bounds checking
   EXPECT_THROW(
       {
-        FBGEMM_LAUNCH_KERNEL(
+        FBGEMM_LAUNCH_DSA_KERNEL(
             tensor_sum_kernel<float>,
             8,
             1024,
@@ -195,5 +253,53 @@ TEST(KernelLauncherTest, kernel_launch_checks) {
       },
       std::exception);
 }
+
+// NOTE: This test currently fails in fbcode CI for HIP with the following
+// error:
+//
+// void fbgemm_gpu::utils::always_fail_assertion_kernel(const int,
+// c10::hip::DeviceAssertionsData *const, uint32_t): Device-side assertion `(a
+// != a) && "This assertion should always fail"' failed. :0:rocdevice.cpp :2984:
+// 1311044151769 us: [pid:1082329 tid:0x7fc06c9ff640] Callback: Queue
+// 0x7fc06b500000 aborting with error : HSA_STATUS_ERROR_EXCEPTION: An HSAIL
+// operation resulted in a hardware exception. code: 0x1016
+//
+// Disabled for now until we can figure out why this is happening.
+#ifndef __HIPCC__
+
+TEST(KernelLauncherTest, throws_dsa_exception) {
+  FBGEMM_LAUNCH_DSA_KERNEL(
+      always_fail_assertion_kernel,
+      1,
+      1,
+      0,
+      at::cuda::getCurrentCUDAStream(),
+      42);
+
+  EXPECT_NO_THROW({
+    try {
+      c10::cuda::device_synchronize();
+      throw std::runtime_error("Test didn't fail, but should have.");
+
+    } catch (const c10::Error& err) {
+      const auto err_str = std::string(err.what());
+
+      ASSERT_THAT(
+          err_str,
+          HasSubstr(
+              "CUDA device-side assertion failures were found on GPU #0!"));
+
+      ASSERT_THAT(
+          err_str, HasSubstr("File containing kernel launch = " __FILE__));
+
+      ASSERT_THAT(
+          err_str,
+          HasSubstr(
+              "Name of kernel launched that led to failure = always_fail_assertion_kernel"));
+    }
+  });
+}
+
+#endif // __HIPCC__
 
 } // namespace fbgemm_gpu::utils
