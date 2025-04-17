496ca280b4628d60fe5e1135fd560a701a19bbd3
Supadchaya Puangpontip <supadchaya@meta.com>
2025-04-17 06:39:52 -0700

    Fix split_embeddings_utils in CMake (#3974)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3974
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1060
    
    Usage of `get_infos_metadata` in CPU package causes error:
    ```
    NotImplementedError: fbgemm::get_infos_metadata: attempted to run this operator with Meta tensors, but there was no fake impl or Meta kernel registered. You may have run into this message while using an operator with PT2 compilation APIs (torch.compile/torch.export); in order to use this operator with those APIs you'll need to add a fake impl. Please see the following for next steps:  https://pytorch.org/tutorials/advanced/custom_ops_landing_page.html
    ```
    
    This is because `src/split_embeddings_utils/split_embeddings_utils.cpp` which contains meta registration is not included in CPU build in Cmake.
    
    Reviewed By: q10
    
    Differential Revision: D73150650
    
    fbshipit-source-id: 9c3da959587f054414149f556664d31d03b9b8f5

--- fbgemm_gpu/src/split_embeddings_utils/split_embeddings_utils.cpp
+++ fbgemm_gpu/src/split_embeddings_utils/split_embeddings_utils.cpp
@@ -14,39 +14,8 @@
 using Tensor = at::Tensor;
 using namespace fbgemm_gpu;
 
-namespace {
-
-std::tuple<Tensor /*row_output_offsets*/, Tensor /*b_t_map*/>
-generate_vbe_metadata_meta(
-    const Tensor& B_offsets,
-    const Tensor& B_offsets_rank_per_feature,
-    const Tensor& output_offsets_feature_rank,
-    const Tensor& D_offsets,
-    const int64_t D,
-    const bool nobag,
-    const c10::SymInt max_B_feature_rank,
-    const int64_t info_B_num_bits,
-    const c10::SymInt total_B) {
-  Tensor row_output_offsets =
-      at::empty_symint({total_B}, output_offsets_feature_rank.options());
-  Tensor b_t_map = at::empty_symint({total_B}, B_offsets.options());
-  return {row_output_offsets, b_t_map};
-}
-
-std::tuple<int64_t, int64_t>
-get_infos_metadata_meta(Tensor /*unused*/, int64_t /*B*/, int64_t /*T*/) {
-  return {-1, -1};
-}
-
-} // namespace
-
 TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
   DISPATCH_TO_CUDA("transpose_embedding_input", transpose_embedding_input);
   DISPATCH_TO_CUDA("get_infos_metadata", get_infos_metadata);
   DISPATCH_TO_CUDA("generate_vbe_metadata", generate_vbe_metadata);
-}
-
-TORCH_LIBRARY_IMPL(fbgemm, Meta, m) {
-  m.impl("generate_vbe_metadata", &generate_vbe_metadata_meta);
-  m.impl("get_infos_metadata", &get_infos_metadata_meta);
 }
