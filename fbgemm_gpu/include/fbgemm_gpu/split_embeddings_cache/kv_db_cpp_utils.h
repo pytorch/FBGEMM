a9b05ea6603e6c089986206961a8cd662d64a704
Joe Wang <wangj@meta.com>
2025-04-17 14:25:44 -0700

    add c++ bucket sorted id utils (#3982)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3982
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1065
    
    add util function to return a id in bucket ascending order and bucket tensor.
    
    implemented in c++ to allow parallelism
    
    Reviewed By: q10
    
    Differential Revision: D72576540
    
    fbshipit-source-id: f61bc6da927f13a39515e7805e9cb758c3e20729

--- fbgemm_gpu/include/fbgemm_gpu/split_embeddings_cache/kv_db_cpp_utils.h
+++ fbgemm_gpu/include/fbgemm_gpu/split_embeddings_cache/kv_db_cpp_utils.h
@@ -8,9 +8,11 @@
 
 #pragma once
 
+#include <ATen/ATen.h>
 #include <folly/hash/Hash.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <optional>
 
 /// @defgroup embedding-ssd Embedding SSD Operators
 ///
@@ -31,5 +33,36 @@ inline size_t hash_shard(int64_t id, size_t num_shards) {
   __uint128_t wide = __uint128_t{num_shards} * hash;
   return static_cast<size_t>(wide >> 64);
 }
+
+/// @ingroup embedding-ssd
+///
+/// @brief given a tensor containing ids in random order, returns 2 tensors
+/// tensor1 contains ids sorted in bucket ascending order, for example given
+/// [1,2,3,4] with 2 buckets [1, 4) and [4, 7), the output will be [1,2,3,4] or
+/// [2, 1, 3, 4], id 1, 2, 3 must be prior to 4, but 1 2 3 can be in any order
+/// tensor2 contains number of embeddings in each bucket id(tensor offset), in
+/// the above example tensor2 will be [3, 1] where first item corresponds to the
+/// first bucket id, value 3 means there are 3 ids in the first bucket id
+///
+/// @param unordered_indices unordered ids, the id here might be
+/// original(unlinearized) id
+/// @param hash_mode 0 for hash by mod, 1 for hash by interleave
+/// @param bucket_start global bucket id, the start of the bucket range
+/// @param bucket_end global bucket id, the end of the bucket range
+/// @param bucket_size an optional, virtual size(input space, e.g. 2^50) of a
+/// bucket
+/// @param total_num_buckets an optional with the total number of buckets per
+/// training model
+///
+/// @return list of 2 tensors, first tensor is bucket sorted ids, second tensor
+/// is bucket size
+
+std::tuple<at::Tensor, at::Tensor> get_bucket_sorted_indices_and_bucket_tensor(
+    const at::Tensor& unordered_indices,
+    int64_t hash_mode,
+    int64_t bucket_start,
+    int64_t bucket_end,
+    std::optional<int64_t> bucket_size,
+    std::optional<int64_t> total_num_buckets);
 
 }; // namespace kv_db_utils
