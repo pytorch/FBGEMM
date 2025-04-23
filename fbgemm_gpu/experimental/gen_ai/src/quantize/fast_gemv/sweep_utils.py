1816d4fa06308da9f0c42aead45b4c255ecfffff
Rachel Guo <guorachel@meta.com>
2025-04-23 10:38:44 -0700

    refine the heuristics sweeping scripts for llama4 17b_128e shapes (#3983)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/3983
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1068
    
    as title.
    
    sample heurisitics tuning result paste: P1793001678
    
    Reviewed By: ipiszy
    
    Differential Revision: D73213894
    
    fbshipit-source-id: 4e62be37908d783fe50421dd704e62cef374ef49

--- fbgemm_gpu/experimental/gen_ai/src/quantize/fast_gemv/sweep_utils.py
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/fast_gemv/sweep_utils.py
@@ -36,31 +36,56 @@ class SweepHeuristics:
             (512, 2),
             (1024, 1),
         ]
+        self.block_dim_xs = [
+            (32, 1),
+            (64, 1),
+            (128, 1),
+            (256, 1),
+            (512, 1),
+            (1024, 1),
+        ]
         self.nks = [(1280, 8192), (8192, 1024), (7168, 8192), (8192, 3584)]
+        # 17bx128e dense model shapes grabbed from here:
+        # https://www.internalfb.com/code/fbsource/[26a75e239633]/fbcode/accelerators/workloads/microbench/bench_gemm.py?lines=269
+        self.nks_l4 = [(4096, 5120), (5120, 2048), (896, 5120), (5120, 640)]
 
-    def sweep_heuristics(self, fn, quantize_w=False, quantize_x=False) -> None:
+    def sweep_heuristics(
+        self,
+        fn,
+        quantize_w: bool = False,
+        quantize_x: bool = False,
+        llama_4: bool = False,
+    ) -> None:
+        """Sweep heuristics for a given kernel"""
+        block_dims = self.block_dims
+        if llama_4:
+            nks = self.nks_l4
+            block_dims = self.block_dim_xs
+        else:
+            nks = self.nks
+            if quantize_w and quantize_x:
+                block_dims = self.block_dim_xs
         for m in self.ms:
-            for n, k in self.nks:
+            for n, k in nks:
                 x = torch.randn(size=(m, k), dtype=torch.bfloat16, device="cuda")
                 w = torch.randn(size=(n, k), dtype=torch.bfloat16, device="cuda")
 
                 best_elapsed_time, best_block_dim_x, best_block_dim_y = None, None, None
 
-                for block_dim_x, block_dim_y in self.block_dims:
-                    if (
-                        (k % block_dim_x != 0)
-                        or (n % block_dim_x != 0)
-                        or ((k / block_dim_x) % 8 != 0)
-                    ):
+                for block_dim_x, block_dim_y in block_dims:
+                    if (k % block_dim_x != 0) or (n % block_dim_x != 0):
                         continue
-
                     res = 0.0
+                    # Currently this requires manual changes to the kernel code as listed below:
+                    # 1. update for "testing purpose" the pytorch custom gemv op to accept additional params block_dim_x and block_dim_y
+                    # 2. modify the corresponding `{precision}_fast_gemv.cu` kernel signature to reflect the block_dim_x and block_dim_y heuristics
+                    # e.g. https://www.internalfb.com/code/fbsource/[208a27f25373]/fbcode/deeplearning/fbgemm/fbgemm_gpu/experimental/gen_ai/bench/quantize_ops.py?lines=375
                     if quantize_w and quantize_x:
                         xq, x_scale = torch.ops.fbgemm.quantize_fp8_per_tensor(x)
                         wq, w_scale = torch.ops.fbgemm.quantize_fp8_per_tensor(w)
                         res = do_bench_using_profiling(
-                            lambda func=fn, x=xq, w=wq, scale=x_scale * w_scale, block_dim_x=block_dim_x, block_dim_y=block_dim_y: func(
-                                x, w, scale, block_dim_x, block_dim_y
+                            lambda func=fn, x=xq, w=wq, x_scale=x_scale, w_scale=w_scale, block_dim_x=block_dim_x: func(
+                                x, w, x_scale, w_scale, block_dim_x
                             )
                         )
                     elif quantize_w:
@@ -71,10 +96,6 @@ class SweepHeuristics:
                             )
                         )
                     else:
-                        # This requires
-                        # 1. update for "testing purpose" the pytorch custom gemv op to accept additional params block_dim_x and block_dim_y
-                        # 2. modify the corresponding `{precision}_fast_gemv.cu` kernel signature to reflect the block_dim_x and block_dim_y heuristics
-                        # e.g. https://www.internalfb.com/code/fbsource/[208a27f25373]/fbcode/deeplearning/fbgemm/fbgemm_gpu/experimental/gen_ai/bench/quantize_ops.py?lines=375
                         res = do_bench_using_profiling(
                             lambda func=fn, x=x, w=w, block_dim_x=block_dim_x, block_dim_y=block_dim_y: func(
                                 x, w, block_dim_x, block_dim_y
@@ -123,4 +144,12 @@ sweep_instance.sweep_heuristics(
 )
 sweep_instance.sweep_heuristics(
     fn=torch.ops.fbgemm.fp8fp8bf16_fast_gemv, quantize_w=True, quantize_x=True
+)
+
+# 17bx128e dense l4 model shapes
+sweep_instance.sweep_heuristics(
+    fn=torch.ops.fbgemm.fp8fp8bf16_fast_gemv,
+    quantize_w=True,
+    quantize_x=True,
+    llama_4=True,
 )
