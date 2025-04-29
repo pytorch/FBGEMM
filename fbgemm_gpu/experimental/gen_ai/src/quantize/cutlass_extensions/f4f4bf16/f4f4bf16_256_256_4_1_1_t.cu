14520d12a16d5c1f685faa180d39918b598442d6
Jiawen Liu <jiawenl@meta.com>
2025-04-28 17:09:32 -0700

    Enable FP4 CUTLASS GEMM and CUDA quantization kernels (#4004)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1091
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/4004
    
    Enable MXFP4 and NVFP4 CUTLASS GEMM and NVFP4 CUDA quantization kernels
    
    Reviewed By: jianyuh
    
    Differential Revision: D69505435
    
    fbshipit-source-id: 7b438628663efec47851bb3908ae2f74ee9e8261

---
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f4f4bf16/f4f4bf16_256_256_4_1_1_t.cu
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "f4f4bf16_common.cuh"
+
+namespace fbgemm_gpu {
+
+#if defined(CUDA_VERSION) && (CUDA_VERSION >= 12080)
+
+at::Tensor f4f4bf16_256_256_4_1_1_t(
+    at::Tensor XQ, // FP4
+    at::Tensor WQ, // FP4
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor global_scale) {
+  // Dispatch this kernel to the correct underlying implementation.
+  return _f4f4bf16<256, 256, 4, 1, 1, true>(
+      XQ, WQ, x_scale, w_scale, global_scale);
+}
+
+#endif
+
+} // namespace fbgemm_gpu
