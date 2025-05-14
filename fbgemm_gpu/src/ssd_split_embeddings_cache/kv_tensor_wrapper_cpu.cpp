3cc11d354a3f1ed9217b16cb81c01c6344409338
Joe Wang <wangj@meta.com>
2025-05-14 11:47:44 -0700

    Support ordered read based on weight id in KVT (#4108)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/4108
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1192
    
    if kvt is created with id, KVT will return weights following id order, instead of based on offset.
    this feature can help to integrate with model state_dict, to make sure the weight and optimizer tensors are all ordered consistently with weight_id tensor
    
    Reviewed By: emlin
    
    Differential Revision: D74303407
    
    fbshipit-source-id: ee97bd2ff406d564f5c8f5698c2c795ca2d3ab68

--- fbgemm_gpu/src/ssd_split_embeddings_cache/kv_tensor_wrapper_cpu.cpp
+++ fbgemm_gpu/src/ssd_split_embeddings_cache/kv_tensor_wrapper_cpu.cpp
@@ -32,8 +32,9 @@ KVTensorWrapper::KVTensorWrapper(
     std::vector<int64_t> shape,
     [[maybe_unused]] int64_t dtype,
     int64_t row_offset,
-    [[maybe_unused]] std::optional<
-        c10::intrusive_ptr<EmbeddingSnapshotHandleWrapper>> snapshot_handle)
+    [[maybe_unused]] const std::optional<
+        c10::intrusive_ptr<EmbeddingSnapshotHandleWrapper>> snapshot_handle,
+    [[maybe_unused]] const std::optional<at::Tensor> sorted_indices)
     // @lint-ignore CLANGTIDY clang-diagnostic-missing-noreturn
     : shape_(std::move(shape)), row_offset_(row_offset) {
   FBEXCEPTION("Not implemented");
