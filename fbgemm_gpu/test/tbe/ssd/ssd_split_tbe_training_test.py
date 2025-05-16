175d2e62a914e6cdea066b7a553b6ae64bb7d70d
Joe Wang <wangj@meta.com>
2025-05-16 00:01:34 -0700

    reduce UT emb table size
    Summary:
    We are seeing UT timeout and get disabled for test_kv_emb_state_dict
    
    https://fburl.com/test/prow1lrs
    
    Differential Revision: D74861166

--- fbgemm_gpu/test/tbe/ssd/ssd_split_tbe_training_test.py
+++ fbgemm_gpu/test/tbe/ssd/ssd_split_tbe_training_test.py
@@ -1558,6 +1558,7 @@ class SSDSplitTableBatchedEmbeddingsTest(unittest.TestCase):
         lr = 0.5
         eps = 0.2
         ssd_shards = 2
+        log_E = 3 if log_E > 3 else log_E
 
         trigger_bounds_check = False  # don't stimulate boundary check cases
         assume(not weighted or pooling_mode == PoolingMode.SUM)
