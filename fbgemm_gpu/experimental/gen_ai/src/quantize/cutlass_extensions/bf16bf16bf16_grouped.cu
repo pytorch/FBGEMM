05a77f35b60b16932860ffd9d453a98490a3d15e
Chris Thi <cthi@meta.com>
2025-05-16 07:14:07 -0700

    Update heuristic for Cutlass BF16 Grouped GEMM
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1220
    
    This diff updates the heuristic used for Cutlass BF16 grouped gemm, improving performance in some important shapes.
    
    Differential Revision: D74836650

--- fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/bf16bf16bf16_grouped.cu
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/bf16bf16bf16_grouped.cu
@@ -15,10 +15,13 @@ namespace fbgemm_gpu {
 
 #if CUDART_VERSION >= 12000
 
-// FP8 Tensorwise grouped cutlass kernel dispatch.
+// BF16 grouped cutlass kernel dispatch.
 template <typename InputType>
 at::Tensor dispatch_bf16_grouped_kernel(
+    int G,
     int total_M,
+    int N,
+    int K,
     InputType X, // BF16
     InputType W, // BF16
     at::Tensor output,
@@ -26,6 +29,92 @@ at::Tensor dispatch_bf16_grouped_kernel(
     std::optional<at::Tensor> M_sizes = std::nullopt) {
   // Use heuristics to pick best kernel implementation.
 
+  // Llama4 128E
+  if (G == 128) {
+    if (N == 5120 && K == 1024) {
+      if (total_M <= 128) {
+        return bf16bf16bf16_grouped_128_16_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 256) {
+        return bf16bf16bf16_grouped_128_32_128_2_1_1_t(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 2048) {
+        return bf16bf16bf16_grouped_128_16_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 4096) {
+        return bf16bf16bf16_grouped_128_32_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 8192) {
+        return bf16bf16bf16_grouped_128_64_128_1_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 16384) {
+        return bf16bf16bf16_grouped_128_128_128_2_1_1_t(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else {
+        return bf16bf16bf16_grouped_128_256_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      }
+    }
+
+    if (N == 2048 && K == 5120) {
+      if (total_M <= 2048) {
+        return bf16bf16bf16_grouped_128_16_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else {
+        return bf16bf16bf16_grouped_128_128_128_2_1_1_t(
+            X, W, output, zero_start_index_M, M_sizes);
+      }
+    }
+  }
+
+  // Llama4 64E
+  if (G == 16) {
+    if (N == 5120 && K == 1024) {
+      if (total_M <= 32) {
+        return bf16bf16bf16_grouped_128_16_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 64) {
+        return bf16bf16bf16_grouped_128_32_128_2_1_1_t(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 256) {
+        return bf16bf16bf16_grouped_128_16_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 512) {
+        return bf16bf16bf16_grouped_128_32_128_2_1_1_t(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 1024) {
+        return bf16bf16bf16_grouped_128_64_128_2_1_1_t(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else {
+        return bf16bf16bf16_grouped_128_256_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      }
+    }
+
+    if (N == 2048 && K == 5120) {
+      if (total_M <= 16) {
+        return bf16bf16bf16_grouped_128_16_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 64) {
+        return bf16bf16bf16_grouped_128_32_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 256) {
+        return bf16bf16bf16_grouped_128_16_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 512) {
+        return bf16bf16bf16_grouped_128_32_128_2_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else if (total_M <= 1024) {
+        return bf16bf16bf16_grouped_128_64_128_1_1_1_f(
+            X, W, output, zero_start_index_M, M_sizes);
+      } else {
+        return bf16bf16bf16_grouped_128_128_128_2_1_1_t(
+            X, W, output, zero_start_index_M, M_sizes);
+      }
+    }
+  }
+
+  // Fallback to legacy heuristic for now.
   if (total_M <= 16) {
     return bf16bf16bf16_grouped_128_16_128_1_1_1_f(
         X, W, output, zero_start_index_M, M_sizes);
@@ -52,6 +141,8 @@ OutputType _bf16bf16bf16_grouped(at::TensorList X, at::TensorList W) {
   at::Tensor Y;
   int64_t total_M = 0;
   int64_t G = X.size();
+  int64_t max_N = 0;
+  int64_t max_K = 0;
 
   // Allocate output tensor.
   std::vector<int64_t> output_sizes;
@@ -59,6 +150,9 @@ OutputType _bf16bf16bf16_grouped(at::TensorList X, at::TensorList W) {
   for (int i = 0; i < G; ++i) {
     int64_t M = X[i].size(0);
     int64_t N = W[i].size(0);
+    int64_t K = W[i].size(1);
+    max_N = std::max(max_N, N);
+    max_K = std::max(max_K, K);
     total_M += M;
     const int64_t output_size = M * N;
     total_output_size += output_size;
@@ -67,8 +161,8 @@ OutputType _bf16bf16bf16_grouped(at::TensorList X, at::TensorList W) {
   Y = at::empty(total_output_size, X[0].options().dtype(at::kBFloat16));
 
   // Run kernel.
-  at::Tensor g_out =
-      dispatch_bf16_grouped_kernel<at::TensorList>(total_M, X, W, Y);
+  at::Tensor g_out = dispatch_bf16_grouped_kernel<at::TensorList>(
+      G, total_M, max_N, max_K, X, W, Y);
 
   // Return appropriate output type.
   if constexpr (std::is_same_v<OutputType, at::Tensor>) {
@@ -98,6 +192,7 @@ at::Tensor
 bf16bf16bf16_grouped_stacked(at::Tensor X, at::Tensor W, at::Tensor M_sizes) {
   int64_t total_M = X.size(0);
   int64_t N = W.size(1);
+  int64_t K = W.size(2);
   int64_t G = M_sizes.size(0);
   TORCH_CHECK(
       M_sizes.device() == X.device(),
@@ -111,7 +206,7 @@ bf16bf16bf16_grouped_stacked(at::Tensor X, at::Tensor W, at::Tensor M_sizes) {
   }
   // Return continuous view of output.
   at::Tensor out = dispatch_bf16_grouped_kernel<at::Tensor>(
-      total_M, X, W, Y, std::nullopt, M_sizes);
+      G, total_M, N, K, X, W, Y, std::nullopt, M_sizes);
   return out.view({total_M, N});
 }
 
@@ -125,13 +220,14 @@ at::Tensor bf16bf16bf16_grouped_dynamic(
   int64_t G = X.size(0);
   int64_t M = X.size(1);
   int64_t N = W.size(1);
+  int64_t K = W.size(2);
   int64_t total_output_size = G * M * N;
   at::Tensor Y;
   Y = at::zeros(total_output_size, X.options().dtype(at::kBFloat16));
 
   // Return continuous view of output.
   at::Tensor output = dispatch_bf16_grouped_kernel<at::Tensor>(
-      G * M, X, W, Y, zero_start_index_M);
+      G, G * M, N, K, X, W, Y, zero_start_index_M);
   // View as proper shape.
   return output.view({G, M, N});
 }
