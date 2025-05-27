590d642155164fbcaa4bcc68f4ee2c3a8076973d
pytorchbot <pytorchbot@pytorch.com>
2025-05-27 11:35:48 +0000

    2025-05-27 nightly release (966dff2f945e3875e2fa64107f20bb8405dddb26)

--- fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_rowwise.cu
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_rowwise.cu
@@ -29,95 +29,133 @@ at::Tensor dispatch_fp8_rowwise_kernel(
   int N = size_to_dim_(WQ.dim() - 1, WQ.sizes());
   int K = XQ.size(-1);
 
+  int arch = 9;
+  cudaDeviceProp prop;
+  cudaGetDeviceProperties(&prop, 0);
+  if (prop.major >= 10) {
+    arch = 10;
+  }
+
   // Use shape heuristics to dispatch to optimized kernel configuration.
-  if (M <= 16) {
-    return f8f8bf16_rowwise_64_16_128_1_1_1_f_f(
-        XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-  } else if (M <= 32) {
-    if (N <= 4096) {
-      return f8f8bf16_rowwise_64_16_128_1_1_1_f_f(
+  if (arch == 10) {
+    if (M <= 128) {
+      if (N <= 1024) {
+        return f8f8bf16_rowwise_128_32_128_1_1_1_10_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else {
+        return f8f8bf16_rowwise_128_64_128_1_1_1_10_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      }
+    } else if (M <= 1024) {
+      if (N <= 1024) {
+        return f8f8bf16_rowwise_128_256_128_2_1_1_10_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else {
+        return f8f8bf16_rowwise_128_128_128_2_2_1_10_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      }
+    } else if (M <= 2048) {
+      return f8f8bf16_rowwise_128_256_128_2_1_1_10_f_f(
           XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
     } else {
-      return f8f8bf16_rowwise_64_32_128_2_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    }
-  } else if (M <= 64) {
-    if (N <= 2048) {
-      return f8f8bf16_rowwise_64_16_128_1_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else if (N <= 4096) {
-      return f8f8bf16_rowwise_64_32_128_2_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else {
-      return f8f8bf16_rowwise_64_64_128_2_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    }
-  } else if (M <= 128) {
-    if (N <= 1024) {
-      return f8f8bf16_rowwise_64_16_128_1_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else if (N <= 2048) {
-      return f8f8bf16_rowwise_64_32_128_2_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else if (N <= 4096) {
-      return f8f8bf16_rowwise_64_64_128_2_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else {
-      return f8f8bf16_rowwise_64_128_128_1_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    }
-  } else if (M <= 256) {
-    if (N <= 1024) {
-      return f8f8bf16_rowwise_64_32_128_2_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else if (N <= 2048) {
-      return f8f8bf16_rowwise_64_64_128_2_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else if (N <= 4096) {
-      return f8f8bf16_rowwise_64_128_128_1_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else {
-      return f8f8bf16_rowwise_64_256_128_1_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    }
-  } else if (M <= 512) {
-    if (N <= 1024) {
-      return f8f8bf16_rowwise_64_64_128_2_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else if (N <= 2048) {
-      return f8f8bf16_rowwise_64_128_128_1_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else if (N <= 4096 || use_fast_accum == false) {
-      return f8f8bf16_rowwise_64_256_128_1_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else {
-      return f8f8bf16_rowwise_128_256_128_2_1_1_f_t(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    }
-  } else if (M <= 1024) {
-    if (N <= 1024) {
-      return f8f8bf16_rowwise_64_128_128_1_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else if (N <= 2048 || use_fast_accum == false) {
-      return f8f8bf16_rowwise_64_256_128_1_1_1_f_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else {
-      return f8f8bf16_rowwise_128_256_128_2_1_1_f_t(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      if (N <= 1024) {
+        return f8f8bf16_rowwise_128_256_128_1_2_1_10_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else {
+        return f8f8bf16_rowwise_128_256_128_2_1_1_10_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      }
     }
   } else {
-    if (M <= 2048 && N <= 1024) {
-      return f8f8bf16_rowwise_64_256_128_2_1_1_f_f(
+    if (M <= 16) {
+      return f8f8bf16_rowwise_64_16_128_1_1_1_9_f_f(
           XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else if (K <= 4096 || use_fast_accum == false) {
-      return f8f8bf16_rowwise_128_128_128_2_1_1_t_f(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
-    } else if (M > 8192 && N > 8192) {
-      return f8f8bf16_rowwise_128_256_128_4_4_1_f_t(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+    } else if (M <= 32) {
+      if (N <= 4096) {
+        return f8f8bf16_rowwise_64_16_128_1_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else {
+        return f8f8bf16_rowwise_64_32_128_2_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      }
+    } else if (M <= 64) {
+      if (N <= 2048) {
+        return f8f8bf16_rowwise_64_16_128_1_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else if (N <= 4096) {
+        return f8f8bf16_rowwise_64_32_128_2_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else {
+        return f8f8bf16_rowwise_64_64_128_2_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      }
+    } else if (M <= 128) {
+      if (N <= 1024) {
+        return f8f8bf16_rowwise_64_16_128_1_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else if (N <= 2048) {
+        return f8f8bf16_rowwise_64_32_128_2_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else if (N <= 4096) {
+        return f8f8bf16_rowwise_64_64_128_2_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else {
+        return f8f8bf16_rowwise_64_128_128_1_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      }
+    } else if (M <= 256) {
+      if (N <= 1024) {
+        return f8f8bf16_rowwise_64_32_128_2_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else if (N <= 2048) {
+        return f8f8bf16_rowwise_64_64_128_2_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else if (N <= 4096) {
+        return f8f8bf16_rowwise_64_128_128_1_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else {
+        return f8f8bf16_rowwise_64_256_128_1_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      }
+    } else if (M <= 512) {
+      if (N <= 1024) {
+        return f8f8bf16_rowwise_64_64_128_2_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else if (N <= 2048) {
+        return f8f8bf16_rowwise_64_128_128_1_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else if (N <= 4096 || use_fast_accum == false) {
+        return f8f8bf16_rowwise_64_256_128_1_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else {
+        return f8f8bf16_rowwise_128_256_128_2_1_1_9_f_t(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      }
+    } else if (M <= 1024) {
+      if (N <= 1024) {
+        return f8f8bf16_rowwise_64_128_128_1_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else if (N <= 2048 || use_fast_accum == false) {
+        return f8f8bf16_rowwise_64_256_128_1_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else {
+        return f8f8bf16_rowwise_128_256_128_2_1_1_9_f_t(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      }
     } else {
-      return f8f8bf16_rowwise_128_256_128_2_1_1_f_t(
-          XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      if (M <= 2048 && N <= 1024) {
+        return f8f8bf16_rowwise_64_256_128_2_1_1_9_f_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else if (K <= 4096 || use_fast_accum == false) {
+        return f8f8bf16_rowwise_128_128_128_2_1_1_9_t_f(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else if (M > 8192 && N > 8192) {
+        return f8f8bf16_rowwise_128_256_128_4_4_1_9_f_t(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      } else {
+        return f8f8bf16_rowwise_128_256_128_2_1_1_9_f_t(
+            XQ, WQ, x_scale, w_scale, use_fast_accum, bias, output);
+      }
     }
   }
 }
---
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_rowwise_grouped/f8f8bf16_rowwise_grouped_manifest.cuh
@@ -0,0 +1,247 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <ATen/ATen.h>
+
+namespace fbgemm_gpu {
+
+at::Tensor f8f8bf16_rowwise_grouped_128_16_128_1_1_1_9_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_16_128_1_1_1_9_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_32_128_1_1_1_9_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_32_128_2_1_1_10_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_32_128_2_1_1_10_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_32_128_1_1_1_9_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_64_128_1_1_1_9_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_64_128_1_1_1_9_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_64_128_2_1_1_10_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_64_128_2_1_1_10_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_128_128_1_1_1_9_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_128_128_1_1_1_9_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_128_128_2_1_1_10_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_128_128_2_1_1_10_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_256_128_2_1_1_9_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_256_128_2_1_1_9_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_256_32_128_2_1_1_10_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_256_32_128_2_1_1_10_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_256_64_128_2_1_1_10_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_256_64_128_2_1_1_10_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_256_128_128_2_1_1_9_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_256_128_128_2_1_1_9_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_256_128_128_2_1_1_10_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_256_128_128_2_1_1_10_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_256_256_128_2_1_1_10_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_256_256_128_2_1_1_10_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+} // namespace fbgemm_gpu
