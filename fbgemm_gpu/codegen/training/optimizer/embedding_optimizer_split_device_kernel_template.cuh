9ba0bf539c1dc91130aa90786003632d71691e13
Benson Ma <bensonma415@meta.com>
2025-05-29 12:48:25 -0700

    Update the rowwise adagrad optimizer to leverage optimizer state offloading, v4, backend (#4195)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1271
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/4195
    
    Update the rowwise adagrad optimizer to leverage optimizer state offloading, v4.  It is a revision of D74827718 to make the flag an SSD-specific flag, as opposed to optimizer-specific flag.
    
    By making this an SSD-specific flag, we are expressing clear intent on the flag's use.
    
    This diff adds support for leveraging optimizer state offloading to make optimizer state updates, starting with the rowwise adagrad optimizer.
    
    - Add ssd-specific flag `enable_optimizer_offloading` to the table update kernel to enable handling optimizer offloading, starting with the rowwise adagrad case
    - Propagate the flag upwards to `torch.ops.fbgemm.{{ mdesc }}_embedding_codegen_lookup_{{ optimizer }}_function_pt2`
    
    Reviewed By: spcyppt, duduyi2013
    
    Differential Revision: D75329024
    
    fbshipit-source-id: 037089a1562631f649223b034b5d41afc604c348

--- fbgemm_gpu/codegen/training/optimizer/embedding_optimizer_split_device_kernel_template.cuh
+++ fbgemm_gpu/codegen/training/optimizer/embedding_optimizer_split_device_kernel_template.cuh
@@ -55,6 +55,9 @@ DEVICE_INLINE void {{ mdesc }}_{{ optimizer }}_table_update_kernel(
     {%- endif %}
     const uint32_t shfl_sync_mask,
     const int32_t max_vecs_per_thread,
+    {%- if ssd %}
+    const bool enable_optimizer_offloading,
+    {%- endif %}
     {{ args.split_ref_kernel_args | replace_pta_namespace() | join(",\n    ") }}
 ) {
     constexpr auto kIsInt8 = std::is_same_v<emb_t, uint8_t>;
@@ -112,6 +115,10 @@ DEVICE_INLINE void {{ mdesc }}_{{ optimizer }}_table_update_kernel(
             qparams_template = weight_row_template.load_qparams();
         }
     }
+
+    {%- if not ssd %}
+    constexpr auto enable_optimizer_offloading = false;
+    {%- endif %}
 
     {{ split_precomputation }}
 
