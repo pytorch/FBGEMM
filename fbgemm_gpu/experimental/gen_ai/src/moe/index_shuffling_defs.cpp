5eb7d9704d727bcceffeedd948a4ed03243bbef7
Pat Vignola <patvig@meta.com>
2025-06-02 11:58:19 -0700

    Decouple some operator defs from operator impl (#4175)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/4175
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1254
    
    Original commit changeset: 1e40b95d17c9
    
    Original Phabricator Diff: D75011175
    
    Differential Revision: D75037710
    
    fbshipit-source-id: d47bdcb3e05a63d5a9b3f01aa7a6f8710d0add2d

---
+++ fbgemm_gpu/experimental/gen_ai/src/moe/index_shuffling_defs.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <torch/library.h>
+
+namespace fbgemm_gpu {
+
+TORCH_LIBRARY_FRAGMENT(fbgemm, m) {
+  m.set_python_module("fbgemm_gpu.experimental.gen_ai.moe");
+  m.def(
+      "index_shuffling(Tensor routing_scores,             "
+      "                int? expert_index_start=None,      "
+      "                int? expert_index_end=None,        "
+      "                Tensor? valid_token_count=None) -> "
+      "(Tensor, Tensor, Tensor)");
+}
+
+} // namespace fbgemm_gpu
