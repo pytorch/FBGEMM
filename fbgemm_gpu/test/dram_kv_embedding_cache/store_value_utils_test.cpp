6035cb0167af63cd10eb62d6c1394d3fe7f20da7
pytorchbot <pytorchbot@pytorch.com>
2025-06-04 11:36:27 +0000

    2025-06-04 nightly release (ee0264c59fc6a403100c18f9c676f787c84ccf5b)

--- fbgemm_gpu/test/dram_kv_embedding_cache/store_value_utils_test.cpp
+++ fbgemm_gpu/test/dram_kv_embedding_cache/store_value_utils_test.cpp
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include "deeplearning/fbgemm/fbgemm_gpu/src/dram_kv_embedding_cache/store_value_utils.h"
+#include "deeplearning/fbgemm/fbgemm_gpu/src/dram_kv_embedding_cache/store_value_utils.h" // @manual
 
 #include <common/time/Time.h>
 #include "gtest/gtest.h"
