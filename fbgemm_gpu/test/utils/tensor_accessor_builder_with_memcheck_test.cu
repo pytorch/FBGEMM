fe446d8e84e0fb453881178287f8fa2698dfaee8
Benson Ma <bensonma415@meta.com>
2025-06-04 21:10:25 -0700

    Clean up tensor accessor tests
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1336
    
    - Clean up tensor accessor tests and remove duplicate test definitions
    - Re-enable assertion checks formerly gated by `__HIPCC__` now that `C10_USE_ROCM_KERNEL_ASSERT` is used
    - Add tests for validating inf and nan checks
    - Simplify templates that require DType
    
    Reviewed By: r-barnes
    
    Differential Revision: D75969033
    
    fbshipit-source-id: c0d90dc343573edc8530d656f7c40d088940a661

--- fbgemm_gpu/test/utils/tensor_accessor_builder_with_memcheck_test.cu
+++ fbgemm_gpu/test/utils/tensor_accessor_builder_with_memcheck_test.cu
@@ -6,6 +6,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// Enable kernel asserts on ROCm as it is disabled by default
+// https://github.com/pytorch/pytorch/blob/main/c10/macros/Macros.h#L407
+#define C10_USE_ROCM_KERNEL_ASSERT
+
 #include <ATen/ATen.h>
 #include <gtest/gtest.h>
 #include <torch/types.h> // @manual=//caffe2:torch-cpp-cpu
@@ -17,97 +21,38 @@
 
 #include "fbgemm_gpu/utils/tensor_accessor_builder.h"
 
+namespace fbgemm_gpu::utils {
+
 template <typename T>
-void test_ta_create_1(const at::Tensor& tensor) {
-  TA_B(tensor, T, 1, 64).build("test_ta_create");
-}
-
-template <size_t N>
-void test_ta_create_2(const at::Tensor& tensor) {
-  TA_B(tensor, float, N, 64).build("test_ta_create");
-}
-
-void test_ta_create_3(const at::Tensor& tensor) {
-  TA_B(tensor, float, 1, 64).build("test_ta_create");
-}
-
-TEST(TensorAccessorBuilderWithMemcheckTest, test_ta_create) {
-  const auto tensor = torch::tensor(
-      {1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f}, torch::kFloat32);
-  // Test mismatched types
-  EXPECT_THROW({ test_ta_create_1<int32_t>(tensor); }, std::exception);
-  EXPECT_THROW({ test_ta_create_1<int64_t>(tensor); }, std::exception);
-  EXPECT_THROW({ test_ta_create_1<double>(tensor); }, std::exception);
-
-  // Test invalid dimensions
-  EXPECT_THROW({ test_ta_create_2<2>(tensor); }, std::exception);
-  EXPECT_THROW({ test_ta_create_2<3>(tensor); }, std::exception);
-  EXPECT_THROW({ test_ta_create_2<4>(tensor); }, std::exception);
-
-  // Test valid type and dimension
-  EXPECT_NO_THROW({ test_ta_create_3(tensor); });
-}
-
-template <c10::ScalarType DType, typename T>
 void test_ta_access() {
+  constexpr auto DType = c10::CppTypeToScalarType<T>::value;
+
   const auto tensor = at::empty({0}, at::TensorOptions().dtype(DType));
   const auto accessor = TA_B(tensor, T, 1, 64).build("ta_access");
 
   EXPECT_DEATH({ accessor.at(10); }, "idx < numel_");
 }
 
-// NOTE: CUDA_KERNEL_ASSERT appears to be a no-op when HIPified
-#ifndef __HIPCC__
 TEST(TensorAccessorBuilderWithMemcheckTest, test_ta_access) {
-  test_ta_access<torch::kInt32, int32_t>();
-  test_ta_access<torch::kInt64, int64_t>();
+  test_ta_access<int32_t>();
+  test_ta_access<int64_t>();
 }
-#endif
 
 template <typename T>
-void test_pta_create_1(const at::Tensor& tensor) {
-  PTA_B(tensor, T, 1, 64).build("test_pta_create");
-}
-
-template <size_t N>
-void test_pta_create_2(const at::Tensor& tensor) {
-  PTA_B(tensor, float, N, 64).build("test_pta_create");
-}
-
-void test_pta_create_3(const at::Tensor& tensor) {
-  PTA_B(tensor, float, 1, 64).build("test_pta_create");
-}
-
-TEST(PackedTensorAccessorTest, test_pta_create) {
-  const auto tensor = torch::tensor(
-      {1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f}, torch::kFloat32);
-  // Test mismatched types
-  EXPECT_THROW({ test_pta_create_1<int32_t>(tensor); }, std::exception);
-  EXPECT_THROW({ test_pta_create_1<int64_t>(tensor); }, std::exception);
-  EXPECT_THROW({ test_pta_create_1<double>(tensor); }, std::exception);
-
-  // Test invalid dimensions
-  EXPECT_THROW({ test_pta_create_2<2>(tensor); }, std::exception);
-  EXPECT_THROW({ test_pta_create_2<3>(tensor); }, std::exception);
-  EXPECT_THROW({ test_pta_create_2<4>(tensor); }, std::exception);
-
-  // Test valid type and dimension
-  EXPECT_NO_THROW({ test_pta_create_3(tensor); });
-}
-
-template <c10::ScalarType DType, typename T>
 void test_pta_access() {
+  constexpr auto DType = c10::CppTypeToScalarType<T>::value;
+
   const auto tensor = at::empty({0}, at::TensorOptions().dtype(DType));
   const auto accessor = PTA_B(tensor, T, 1, 64).build("test_pta_access");
 
   EXPECT_DEATH({ accessor.at(10); }, "idx < numel_");
 }
 
-#ifndef __HIPCC__
 TEST(PackedTensorAccessorWithMemcheckTest, test_pta_access) {
-  test_pta_access<torch::kInt32, int32_t>();
-  test_pta_access<torch::kInt64, int64_t>();
+  test_pta_access<int32_t>();
+  test_pta_access<int64_t>();
 }
-#endif
+
+} // namespace fbgemm_gpu::utils
 
 #undef FBGEMM_GPU_MEMCHECK
