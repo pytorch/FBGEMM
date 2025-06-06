8080980715b72c778569f0ff9004dfe3e0204e85
amcamd <andrew.chapman@amd.com>
2025-06-06 11:45:42 -0400

    third correction to hipBLAS V3 API changes

--- fbgemm_gpu/src/sparse_ops/sparse_permute102.cu
+++ fbgemm_gpu/src/sparse_ops/sparse_permute102.cu
@@ -76,7 +76,7 @@ DLL_PUBLIC Tensor permute102_baddbmm_permute102_cuda(
   auto strideC = n;
 
   // computeType is hipblasComputeType_t (e.g., HIPBLAS_COMPUTE_32F) instead of
-  // hipblasDataType (e.g., HIPBLAS_R_32F) and matrixType is hipDataType (e.g.,
+  // hipblasDatatype_t (e.g., HIPBLAS_R_32F) and matrixType is hipDataType (e.g.,
   // HIP_R_16F) instead of hipblasDatatype_t (e.g., HIPBLAS_R_16F) for 
   // hipBLAS 2 and defined(HIPBLAS_V2) or hipBLAS 3 and later
 #if defined(USE_ROCM) && ((hipblasVersionMajor == 2 && defined(HIPBLAS_V2)) || hipblasVersionMajor >= 3)
