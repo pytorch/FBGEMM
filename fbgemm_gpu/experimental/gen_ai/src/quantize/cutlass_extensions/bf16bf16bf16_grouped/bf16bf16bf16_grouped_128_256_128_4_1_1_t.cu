7b86f0d9c2fdcaa5d0dce1e6b367ffee20627143
Chris Thi <cthi@meta.com>
2025-06-13 14:27:03 -0700

    Add new kernels for Cutlass BF16 grouped GEMM for tuning cache (#4300)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/4300
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1376
    
    This diff adds the new kernels, so that one can use the auto tuning without having to generate them each time.
    
    I also slightly changed the kernels used in the heuristic. I tested they are similar in performance to the old kernels, but this makes it slightly easier to maintain since we will generate cooperative schedule for smaller tiles to reduce number of kernels included.
    
    Reviewed By: jiawenliu64
    
    Differential Revision: D75806957

--- fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/bf16bf16bf16_grouped/bf16bf16bf16_grouped_128_256_128_4_1_1_t.cu
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/bf16bf16bf16_grouped/bf16bf16bf16_grouped_128_256_128_4_1_1_t.cu
@@ -10,24 +10,31 @@
 
 namespace fbgemm_gpu {
 
-at::Tensor bf16bf16bf16_grouped_128_32_128_2_1_1_t(
+at::Tensor bf16bf16bf16_grouped_128_256_128_4_1_1_t(
     at::Tensor X, // BF16
     at::Tensor W, // BF16
     at::Tensor output,
     std::optional<at::Tensor> zero_start_index_M,
     std::optional<at::Tensor> M_sizes) {
-  return bf16bf16bf16_grouped_impl<at::Tensor, 128, 32, 128, 2, 1, 1, true>(
+  return bf16bf16bf16_grouped_impl<at::Tensor, 128, 256, 128, 4, 1, 1, true>(
       X, W, output, zero_start_index_M, M_sizes);
 }
 
-at::Tensor bf16bf16bf16_grouped_128_32_128_2_1_1_t(
+at::Tensor bf16bf16bf16_grouped_128_256_128_4_1_1_t(
     at::TensorList X, // BF16
     at::TensorList W, // BF16
     at::Tensor output,
     std::optional<at::Tensor> zero_start_index_M,
     std::optional<at::Tensor> M_sizes) {
-  return bf16bf16bf16_grouped_impl<at::TensorList, 128, 32, 128, 2, 1, 1, true>(
-      X, W, output, zero_start_index_M, M_sizes);
+  return bf16bf16bf16_grouped_impl<
+      at::TensorList,
+      128,
+      256,
+      128,
+      4,
+      1,
+      1,
+      true>(X, W, output, zero_start_index_M, M_sizes);
 }
 
 } // namespace fbgemm_gpu
