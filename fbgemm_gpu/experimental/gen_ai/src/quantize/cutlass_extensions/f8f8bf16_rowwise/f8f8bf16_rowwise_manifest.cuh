4c9313f39684b98d5a4dc6e48d57d8aab814ea01
Chris Thi <cthi@meta.com>
2025-06-13 20:26:16 -0700

    Support tuning cache for Cutlass FP8 GEMM (#4301)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/4301
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1377
    
    This diff adds support for the tuning cache to the kernel. There should be no performance changes to the existing heuristics.
    - I refactored the kernel dispatch logic to instead return the kernel function, as it removes some duplication of the kernel invoke.
    - The next diff in this stack will add the new kernels D75820688, to make the review easier
      - Note that we are having some issues with adding the new kernels, as I have found this kernel is actually compiling 12 variants for each configuration, see D75820688 for more context. So for now we won't add the new kernels in D75820688, but we can just onboard it to auto tuning incase someone wants to compile them locally. Will revisit D75820688 later.
    
    Reviewed By: q10, jiawenliu64
    
    Differential Revision: D75541025
    
    fbshipit-source-id: df74cf2dd953cbaa817514098cae54ee733ea88c

--- fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_rowwise/f8f8bf16_rowwise_manifest.cuh
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_rowwise/f8f8bf16_rowwise_manifest.cuh
@@ -135,4 +135,27 @@ at::Tensor f8f8bf16_rowwise_128_256_128_2_1_1_10_f_f(
     bool use_fast_accum = true,
     std::optional<at::Tensor> bias = std::nullopt,
     std::optional<at::Tensor> output = std::nullopt);
+
+using Kernel_f8f8bf16_rowwise = at::Tensor (*)(
+    at::Tensor,
+    at::Tensor,
+    at::Tensor,
+    at::Tensor,
+    bool,
+    std::optional<at::Tensor>,
+    std::optional<at::Tensor>);
+
+inline const std::unordered_map<std::string, Kernel_f8f8bf16_rowwise>&
+get_f8f8bf16_rowwise_kernels(int arch) {
+  static const std::unordered_map<std::string, Kernel_f8f8bf16_rowwise>
+      kernelsSM90 = {};
+  static const std::unordered_map<std::string, Kernel_f8f8bf16_rowwise>
+      kernelsSM100 = {};
+  if (arch == 10) {
+    return kernelsSM100;
+  } else {
+    return kernelsSM90;
+  }
+}
+
 } // namespace fbgemm_gpu
