6ee96465ad4cb65a520ff1fc657d81ee28ec88c9
Jiawen Liu <jiawenl@meta.com>
2025-06-16 10:02:55 -0700

    Tune FP8 grouped GEMM for Llama4 shapes (#4326)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/4326
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1399
    
    Further tune FP8 grouped GEMM for Llama4 shapes. Found there is 13%-30% perf gain for Llama4 memory-bound cases. For other shapes in future models, I think there will be room to further improve perf by adding more heuristics like this Diff, that we will revisit and add offline cache file loading instead of adding under the kernel file.
    
    Reviewed By: cthi
    
    Differential Revision: D76460456
    
    fbshipit-source-id: 879dc5377502ade67f411cbd9e08b46896556081

--- fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_rowwise_grouped_sm100/f8f8bf16_rowwise_grouped_manifest.cuh
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_rowwise_grouped_sm100/f8f8bf16_rowwise_grouped_manifest.cuh
@@ -64,6 +64,24 @@ at::Tensor f8f8bf16_rowwise_grouped_128_128_128_2_1_1_10_f(
     std::optional<at::Tensor> zero_start_index_M,
     std::optional<at::Tensor> M_sizes);
 
+at::Tensor f8f8bf16_rowwise_grouped_128_256_128_2_1_1_10_f(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
+at::Tensor f8f8bf16_rowwise_grouped_128_256_128_2_1_1_10_f(
+    at::TensorList XQ,
+    at::TensorList WQ,
+    at::TensorList x_scale,
+    at::TensorList w_scale,
+    at::Tensor output,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes);
+
 at::Tensor f8f8bf16_rowwise_grouped_256_32_128_2_1_1_10_f(
     at::Tensor XQ,
     at::Tensor WQ,
