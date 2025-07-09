86aafe0aa1e5ca3ec7f3b0a0c28193e790bfd9c5
Prashanth Palaniappan <prashanth.palaniappan@amd.com>
2025-07-09 00:26:15 +0000

    remove c10_retrieve_device_side_assertion_info

--- fbgemm_gpu/include/fbgemm_gpu/utils/kernel_launcher.cuh
+++ fbgemm_gpu/include/fbgemm_gpu/utils/kernel_launcher.cuh
@@ -276,8 +276,9 @@ struct KernelLauncher {
 #else
         c10::cuda::get_cuda_check_suffix(),
 #endif
-        "\n",
-        c10::cuda::c10_retrieve_device_side_assertion_info());
+        "\n");
+        //"\n",
+        //c10::cuda::c10_retrieve_device_side_assertion_info());
   }
 
   template <typename KernelFunc, typename... Args>
