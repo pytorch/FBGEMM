10363dba37ebed8c22ef445a6560eb871c775a92
Chenyu Zhang <chenyuz@meta.com>
2025-07-16 08:06:36 -0700

    inference eviction interfaces (#4500)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/4500
    
    Add a few inference eviction interfaces. The are interfaces breaking changes, but since model is not in production and it's not affecting other models so it should be fine.
    
    Reviewed By: duduyi2013
    
    Differential Revision: D78010843
    
    fbshipit-source-id: 2ca2459907cbac1cb840cd158ef1ba3b5623ee9d

--- fbgemm_gpu/src/dram_kv_embedding_cache/dram_kv_embedding_inference_wrapper.cpp
+++ fbgemm_gpu/src/dram_kv_embedding_cache/dram_kv_embedding_inference_wrapper.cpp
@@ -15,17 +15,16 @@ namespace fbgemm_gpu {
 DramKVEmbeddingInferenceWrapper::DramKVEmbeddingInferenceWrapper(
     int64_t num_shards,
     double uniform_init_lower,
-    double uniform_init_upper,
-    int64_t evict_trigger_mode)
+    double uniform_init_upper)
     : num_shards_(num_shards),
       uniform_init_lower_(uniform_init_lower),
-      uniform_init_upper_(uniform_init_upper),
-      evict_trigger_mode_(evict_trigger_mode) {}
+      uniform_init_upper_(uniform_init_upper) {}
 
 void DramKVEmbeddingInferenceWrapper::init(
     const std::vector<SerializedSepcType>& specs,
     const int64_t row_alignment,
-    const int64_t scale_bias_size_in_bytes) {
+    const int64_t scale_bias_size_in_bytes,
+    const std::optional<at::Tensor>& hash_size_cumsum) {
   int64_t max_D = 0;
   for (auto i = 0; i < specs.size(); ++i) {
     max_D = std::max(max_D, std::get<1>(specs[i]));
@@ -35,13 +34,16 @@ void DramKVEmbeddingInferenceWrapper::init(
       static_cast<fbgemm_gpu::SparseType>(std::get<2>(specs[0])),
       static_cast<int32_t>(row_alignment),
       static_cast<int32_t>(scale_bias_size_in_bytes));
-  dram_kv_ = std::make_unique<kv_mem::DramKVEmbeddingCache<uint8_t>>(
+  if (dram_kv_ != nullptr) {
+    return;
+  }
+  dram_kv_ = std::make_shared<kv_mem::DramKVEmbeddingCache<uint8_t>>(
       max_row_bytes_,
       uniform_init_lower_,
       uniform_init_upper_,
       c10::make_intrusive<kv_mem::FeatureEvictConfig>(
-          evict_trigger_mode_,
-          0 /* evict_trigger_strategy */,
+          3 /* EvictTriggerMode.MANUAL */,
+          4 /* EvictTriggerStrategy::BY_TIMESTAMP_THRESHOLD */,
           0 /* trigger_step_intervals */,
           0 /* mem_util_threshold_in_GB */,
           std::nullopt /* ttls_in_mins */,
@@ -51,7 +53,11 @@ void DramKVEmbeddingInferenceWrapper::init(
           std::nullopt /* embedding_dims */),
       num_shards_ /* num_shards */,
       num_shards_ /* num_threads */,
-      8 /* row_storage_bitwidth */);
+      8 /* row_storage_bitwidth */,
+      false /* backend_return_whole_row */,
+      false /* enable_async_update */,
+      std::nullopt /* table_dims */,
+      hash_size_cumsum);
   return;
 }
 
@@ -86,10 +92,18 @@ at::Tensor DramKVEmbeddingInferenceWrapper::get_embeddings(
   return weights;
 }
 
+void DramKVEmbeddingInferenceWrapper::trigger_evict() {
+  dram_kv_->trigger_feature_evict();
+  // dram_kv_->resume_ongoing_eviction();
+}
+
+void DramKVEmbeddingInferenceWrapper::wait_evict_completion() {
+  dram_kv_->wait_until_eviction_done();
+}
+
 c10::List<at::Tensor> DramKVEmbeddingInferenceWrapper::serialize() const {
   c10::List<at::Tensor> results;
-  results.push_back(
-      torch::tensor({num_shards_, evict_trigger_mode_}, torch::kInt64));
+  results.push_back(torch::tensor({num_shards_}, torch::kInt64));
   results.push_back(torch::tensor(
       {uniform_init_lower_, uniform_init_upper_}, torch::kDouble));
   return results;
@@ -103,9 +117,8 @@ void DramKVEmbeddingInferenceWrapper::deserialize(
   TORCH_CHECK(states.size() >= 2);
 
   auto* intPtr = states[0].data_ptr<int64_t>();
-  TORCH_CHECK(states[0].numel() >= 2)
+  TORCH_CHECK(states[0].numel() >= 1)
   num_shards_ = intPtr[0];
-  evict_trigger_mode_ = intPtr[1];
 
   TORCH_CHECK(states[1].numel() >= 2)
   auto* floatPtr = states[1].data_ptr<double>();
@@ -119,7 +132,7 @@ static auto dram_kv_embedding_inference_wrapper =
     torch::class_<fbgemm_gpu::DramKVEmbeddingInferenceWrapper>(
         "fbgemm",
         "DramKVEmbeddingInferenceWrapper")
-        .def(torch::init<int64_t, double, double, int64_t>())
+        .def(torch::init<int64_t, double, double>())
         .def("init", &fbgemm_gpu::DramKVEmbeddingInferenceWrapper::init)
         .def(
             "set_embeddings",
@@ -127,6 +140,12 @@ static auto dram_kv_embedding_inference_wrapper =
         .def(
             "get_embeddings",
             &fbgemm_gpu::DramKVEmbeddingInferenceWrapper::get_embeddings)
+        .def(
+            "trigger_evict",
+            &fbgemm_gpu::DramKVEmbeddingInferenceWrapper::trigger_evict)
+        .def(
+            "wait_evict_completion",
+            &fbgemm_gpu::DramKVEmbeddingInferenceWrapper::wait_evict_completion)
         .def(
             "serialize",
             &fbgemm_gpu::DramKVEmbeddingInferenceWrapper::serialize)
