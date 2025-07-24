e893f878337532739b7f396a1e9c9a8f4b52ea4d
cyy <cyyever@outlook.com>
2025-07-24 18:06:11 +0800

    Tidy code
    Signed-off-by: cyy <cyyever@outlook.com>

--- src/EmbeddingSpMDM.cc
+++ src/EmbeddingSpMDM.cc
@@ -1029,8 +1029,8 @@ typename EmbeddingSpMDMKernelSignature<inType, indxType, offsetType, outType>::
         bool is_bf16_out /*=false*/,
         bool is_bf16_in /*=false*/) {
 #if defined(__APPLE__) || defined(_WIN32)
-  if (std::is_same<inType, uint16_t>::value && is_bf16_in &&
-      std::is_same<outType, float>::value) {
+  if (std::is_same_v<inType, uint16_t> && is_bf16_in &&
+      std::is_same_v<outType, float>) {
     throw std::runtime_error(
         "Bfloat16 input with float32 output is not yet supported on Apple or Windows");
   }
