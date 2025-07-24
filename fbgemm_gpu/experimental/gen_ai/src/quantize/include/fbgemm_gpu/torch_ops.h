1a7d1ac32d51945aedbc4e947e6e37f5504f7bf6
Chris Thi <cthi@meta.com>
2025-07-24 08:53:19 -0700

    Add torch_ops.h for ops being built with torch core
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1600
    
    For ops we plan to build directly with torch we will need to expose only the header declarations required, as otherwise we would encounter undefined symbols during linking. In FBGEMM build the declarations are all in [quantize.cpp](https://www.internalfb.com/code/fbsource/fbcode/deeplearning/fbgemm/fbgemm_gpu/experimental/gen_ai/src/quantize/quantize.cpp) (which should also ideally be broken out).
    
    We can have a separate header file and also rely on these in quantize.cpp.
    
    Differential Revision: D78897613

---
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/include/fbgemm_gpu/torch_ops.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// For FBGEMM ops being exposed into torch core add them here.
+// The reason is that we need to only expose the declarations we will build,
+// otherwise we will have undefined symbols during linking.
+
+#pragma once
+
+#include <ATen/core/Tensor.h>
+
+namespace fbgemm_gpu {
+
+#ifdef USE_ROCM
+
+// Generic PyTorch grouped GEMM API is only available on AMD for now.
+at::Tensor f8f8bf16_rowwise_grouped_mm(
+    at::Tensor XQ,
+    at::Tensor WQ,
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    std::optional<at::Tensor> offsets,
+    at::Tensor& output);
+
+#endif
+
+} // namespace fbgemm_gpu
