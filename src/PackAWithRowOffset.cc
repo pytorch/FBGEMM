c7c6d1c2bf67b65d04a6076ba864da43c152951d
Benson Ma <bensonma415@meta.com>
2025-08-03 22:39:33 -0700

    Enable ARM build, pt 2 (#4632)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1669
    
    
    
    
    Differential Revision: D79516108
    
    Pulled By: q10

--- src/PackAWithRowOffset.cc
+++ src/PackAWithRowOffset.cc
@@ -13,7 +13,10 @@
 #include <iomanip>
 #include <iostream>
 #include <stdexcept>
+
+#if defined(FBGEMM_FBCODE) || !defined(__aarch64__)
 #include "./OptimizedKernelsAvx2.h" // @manual
+#endif // __aarch64__
 #include "fbgemm/Fbgemm.h"
 
 namespace fbgemm {
@@ -151,6 +154,12 @@ void PackAWithRowOffset<T, accT>::pack(const block_type_t& block) {
     static_assert(
         std::is_same_v<T, uint8_t>,
         "PackAWithRowOffset<T, accT>::pack only works for T == uint8_t");
+
+#if !defined(FBGEMM_FBCODE) && defined(__aarch64__)
+    throw std::runtime_error(
+        "PackAWithRowOffset<T, accT>::pack(): No fallback available for aarch64");
+#else
+
     for (int i = block.row_start; i < block.row_start + block.row_size; ++i) {
       int buf_idx = i - block.row_start;
       memcpy(
@@ -165,6 +174,8 @@ void PackAWithRowOffset<T, accT>::pack(const block_type_t& block) {
       row_sum += reduceAvx2(smat_ + i * ld_ + block.col_start, block.col_size);
       row_offset_buf[buf_idx] = row_sum;
     }
+
+#endif // __aarch64__
   }
 }
 
