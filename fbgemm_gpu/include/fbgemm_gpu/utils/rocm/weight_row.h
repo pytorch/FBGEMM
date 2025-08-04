8b9eae95095e428c5616d9e419aaae66a41a4178
Josh Fromm <jwfromm@meta.com>
2025-08-04 13:19:25 -0700

    FBGEMM TBE FP8 Embedding Weights (Part 1 Backend) (#4614)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1647
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/4614
    
    This diff enables embeddings to be stored as FP8 during training. To prevent interaction with the inference version of FP8, I instead added a new FP8 datatype `NFP8` that is aligned with the officially supported E4M3 format in pytorch.
    
    Current Limitations:
    * I have not yet added rowwise scaling, I think this should be easy to add but thought it might be worth experimenting without it since it allows us to do much more efficient vectorized reads and writes to the table.
    * I hardcoded in E4M3 but there is also an E5M2 format that would be easy to support. I was thinking instead of `NFP8` we could have two custom datatypes like `FP8_E4M3` and `FP8_E5M2` so that the proper one could be chosen for specific use cases. For gradient accumulation, I would expect E5M2 to be a bit better for example.
    * I havent yet added AMD compatibility but it should be doable without too much work.
    
    Hopefully despite these limitations, this is a useful starting point for experimentation. Certainly the performance looks initially compelling.
    
    Performance measurements suggest that FP8 weights are quite fast compared to FP32, which is a good sign that this may be a useful optimization.
    
    FP32:
    ```
    INFO:root:Embedding tables: 3200000 rows,  0.41 GParam,  1.64 GB
    INFO:root:Accessed weights per batch: 327680 rows,  0.17 GB
    INFO:root:ForwardBackward (UVM), B: 512, E: 100000, T: 32, D: 128, L: 20, BW:  54.06 GB/s, T: 9310us
    INFO:root:Exchanged cache lines -- mean:  225378.69, max: 225973, min: 224687
    INFO:root:Cache miss -- mean: 2.99, max: 12, min: 0
    INFO:root:ForwardBackward (LXU), reuse: 0.1, alpha: 1.0, B: 512, E: 100000, T: 32, D: 128, L: 20, BW:  66.53 GB/s, Tprefetch: 7050us,  32.73 GB/s, Tfwdbwd: 515us,  977.75 GB/s, Te2e: 7565us,
    ```
    
    FP8:
    ```
    INFO:root:Embedding tables: 3200000 rows,  0.41 GParam,  0.41 GB
    INFO:root:Accessed weights per batch: 327680 rows,  0.04 GB
    INFO:root:ForwardBackward (UVM), B: 512, E: 100000, T: 32, D: 128, L: 20, BW:  50.38 GB/s, T: 2498us
    INFO:root:Exchanged cache lines -- mean:  225378.69, max: 225973, min: 224687
    INFO:root:Cache miss -- mean: 2.99, max: 12, min: 0
    INFO:root:ForwardBackward (LXU), reuse: 0.1, alpha: 1.0, B: 512, E: 100000, T: 32, D: 128, L: 20, BW:  47.43 GB/s, Tprefetch: 2139us,  26.98 GB/s, Tfwdbwd: 514us,  244.77 GB/s, Te2e: 2653us,
    ```
    
    Reviewed By: q10, spcyppt
    
    Differential Revision: D63492414
    
    fbshipit-source-id: 5b78dc18917db51cd117c4966bd4db8b8b224823

--- fbgemm_gpu/include/fbgemm_gpu/utils/rocm/weight_row.h
+++ fbgemm_gpu/include/fbgemm_gpu/utils/rocm/weight_row.h
@@ -73,6 +73,38 @@ DEVICE_INLINE Vec2T<at::Half> dequantize_load(
   return out;
 }
 
+template <>
+DEVICE_INLINE Vec2T<float> dequantize_load(
+    const at::Float8_e4m3fnuz* value,
+    const float2 /* currently unused */) {
+#if (defined(USE_ROCM) && ROCM_VERSION >= 60200) || \
+    (defined(CUDA_VERSION) && CUDA_VERSION >= 12000)
+  Vec2T<float> out;
+  const __nv_fp8x2_e4m3* fp8_ptr =
+      reinterpret_cast<const __nv_fp8x2_e4m3*>(value);
+  out.acc = static_cast<float2>(fp8_ptr[0]);
+  return out;
+#else
+  CUDA_KERNEL_ASSERT(false);
+#endif
+}
+
+template <>
+DEVICE_INLINE Vec2T<at::Half> dequantize_load(
+    const at::Float8_e4m3fnuz* value,
+    const float2 /* currently unused */) {
+#if (defined(USE_ROCM) && ROCM_VERSION >= 60200) || \
+    (defined(CUDA_VERSION) && CUDA_VERSION >= 12000)
+  Vec2T<at::Half> out;
+  const __nv_fp8x2_e4m3* fp8_ptr =
+      reinterpret_cast<const __nv_fp8x2_e4m3*>(value);
+  out.acc = static_cast<float2>(fp8_ptr[0]);
+  return out;
+#else
+  CUDA_KERNEL_ASSERT(false);
+#endif
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Weight Row Accessor for Vec2T
 ////////////////////////////////////////////////////////////////////////////////
