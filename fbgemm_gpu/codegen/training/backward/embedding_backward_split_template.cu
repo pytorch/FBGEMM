3ef8e5605536071cfffc3bb2d13e333c5609b283
Andrey Bokovoy <avbokovoy@gmail.com>
2025-08-13 13:21:38 +0000

    Fix mixed_D frontend to backend connection

--- fbgemm_gpu/codegen/training/backward/embedding_backward_split_template.cu
+++ fbgemm_gpu/codegen/training/backward/embedding_backward_split_template.cu
@@ -1205,7 +1205,7 @@ Tensor {{ embedding_cuda_op }}(
                     const auto supported_weights_type = dev_weights.scalar_type() == at::ScalarType::Half
                                                       || dev_weights.scalar_type() == at::ScalarType::Float;
 
-                    if (use_hip_kernel && supported_weights_type && rocm::is_supported_cdna())
+                    if (use_hip_kernel && !mixed_D && supported_weights_type && rocm::is_supported_cdna())
                     {
                         constexpr int segments_per_workgroup = 4;
                         {%- for kDimSize in [64, 128, 160, 192, 256] %}
