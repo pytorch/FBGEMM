a22ddebacc0aef64c2fb0569835eaed9786b5f0e
Andrey Bokovoy <avbokovoy@gmail.com>
2025-08-13 13:21:38 +0000

    Fix mixed_D frontend to backend connection

--- fbgemm_gpu/fbgemm_gpu/split_table_batched_embeddings_ops_training.py
+++ fbgemm_gpu/fbgemm_gpu/split_table_batched_embeddings_ops_training.py
@@ -820,7 +820,7 @@ class SplitTableBatchedEmbeddingBagsCodegen(nn.Module):
             assert (
                 self.pooling_mode != PoolingMode.NONE
             ), "Mixed dimension tables only supported for pooling tables."
-
+        self.mixed_D = mixed_D
         assert all(
             cd == compute_devices[0] for cd in compute_devices
         ), "Heterogenous compute_devices are NOT supported!"
@@ -2517,6 +2517,7 @@ class SplitTableBatchedEmbeddingBagsCodegen(nn.Module):
                         row_counter,
                         iter_int,
                         self.max_counter.item(),
+                        mixed_D=self.mixed_D,
                     ),
                 )
             elif self._used_rowwise_adagrad_with_global_weight_decay:
@@ -2535,6 +2536,7 @@ class SplitTableBatchedEmbeddingBagsCodegen(nn.Module):
                         #  `Optional[Tensor]` but got `Union[Module, Tensor]`.
                         prev_iter_dev=self.prev_iter_dev,
                         gwd_lower_bound=self.gwd_lower_bound,
+                        mixed_D=self.mixed_D,
                     ),
                 )
             else:
@@ -2544,6 +2546,7 @@ class SplitTableBatchedEmbeddingBagsCodegen(nn.Module):
                         common_args,
                         self.optimizer_args,
                         momentum1,
+                        mixed_D=self.mixed_D,
                     ),
                 )
 
