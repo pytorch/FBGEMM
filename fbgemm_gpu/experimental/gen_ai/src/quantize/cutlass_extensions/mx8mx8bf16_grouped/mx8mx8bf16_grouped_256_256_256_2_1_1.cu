9ec5069bca85560311cc9cd2b922c325d7ab788d
Jiawen Liu <jiawenl@meta.com>
2025-08-15 14:22:12 -0700

    Enable MXFP8 grouped GEMM (#4710)
    Summary:
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1733
    
    As title
    
    Reviewed By: cthi
    
    Differential Revision: D80350093

---
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/mx8mx8bf16_grouped/mx8mx8bf16_grouped_256_256_256_2_1_1.cu
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "mx8mx8bf16_grouped_common.cuh"
+
+namespace fbgemm_gpu {
+
+#if defined(CUDA_VERSION) && (CUDA_VERSION >= 12080)
+
+at::Tensor mx8mx8bf16_grouped_256_256_256_2_1_1(
+    at::Tensor XQ, // FP8
+    at::Tensor WQ, // FP8
+    at::Tensor x_scale,
+    at::Tensor w_scale,
+    at::Tensor output,
+    int64_t G,
+    std::optional<at::Tensor> zero_start_index_M,
+    std::optional<at::Tensor> M_sizes,
+    std::optional<at::Tensor> starting_row_after_padding) {
+  return mx8mx8bf16_grouped_impl<at::Tensor, 256, 256, 256, 2, 1, 1>(
+      XQ,
+      WQ,
+      x_scale,
+      w_scale,
+      output,
+      G,
+      zero_start_index_M,
+      M_sizes,
+      starting_row_after_padding);
+}
+
+#endif
+
+} // namespace fbgemm_gpu
