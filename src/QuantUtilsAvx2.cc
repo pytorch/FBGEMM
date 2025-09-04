7bdc8cc4bb29f5cf4d202d7538cc9260b718d129
Nikita Shulga <nshulga@fb.com>
2025-09-04 15:30:20 -0700

    Revert D81052327: Fix Arm64 OSS pytorch build with FBGEMM
    Differential Revision:
    D81052327
    
    Original commit changeset: c50ac43329d9
    
    Original Phabricator Diff: D81052327
    
    fbshipit-source-id: f77d8e6b1a12082d6a15febb4ecc9fcbb26f74e8

--- src/QuantUtilsAvx2.cc
+++ src/QuantUtilsAvx2.cc
@@ -278,6 +278,43 @@ SPECIALIZE_FUSEDDQAVX2(int8_t)
 
 #undef SPECIALIZE_FUSEDDQAVX2
 
+void FindMinMax(const float* m, float* min, float* max, int64_t len) {
+  if (len <= 0) {
+    *min = 0.0f;
+    *max = 0.0f;
+    return;
+  }
+
+  float temp_min = *m, temp_max = *m;
+  int64_t i = 0;
+
+#ifdef __AVX__
+  __m256 min_v = _mm256_set1_ps(*m), max_v = _mm256_set1_ps(*m);
+  constexpr int VLEN = 8;
+  if (len >= VLEN) {
+    for (; i < len / VLEN * VLEN; i += VLEN) {
+      min_v = _mm256_min_ps(min_v, _mm256_loadu_ps(m + i));
+      max_v = _mm256_max_ps(max_v, _mm256_loadu_ps(m + i));
+    }
+
+    float min_buf[VLEN], max_buf[VLEN];
+    _mm256_storeu_ps(min_buf, min_v);
+    _mm256_storeu_ps(max_buf, max_v);
+    for (int j = 0; j < VLEN; ++j) {
+      temp_min = std::min(temp_min, min_buf[j]);
+      temp_max = std::max(temp_max, max_buf[j]);
+    }
+  }
+#endif
+
+  for (; i < len; i++) {
+    temp_min = std::min(temp_min, m[i]);
+    temp_max = std::max(temp_max, m[i]);
+  }
+  *min = temp_min;
+  *max = temp_max;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Requantization (with floats)
 
