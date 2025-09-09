bc75dfd188cc7badcbff18a881d96cd38b20d081
Chris Thi <cthi@meta.com>
2025-09-09 15:15:18 -0700

    Fix tuning cache for f8f8bf16_rowwise_grouped on SM100 (#4843)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/4843
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/1871
    
    It would run SM90 kernels before by accident.
    
    Reviewed By: q10
    
    Differential Revision: D82022651
    
    fbshipit-source-id: ee739499faf61f73e5e9fbdb9d244cacb50c92e0

--- fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_rowwise_grouped_sm100/f8f8bf16_rowwise_grouped_manifest.cuh
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_rowwise_grouped_sm100/f8f8bf16_rowwise_grouped_manifest.cuh
@@ -154,4 +154,31 @@ at::Tensor f8f8bf16_rowwise_grouped_256_256_128_2_1_1_10_f(
     std::optional<at::Tensor> zero_start_index_M,
     std::optional<at::Tensor> M_sizes);
 
+template <typename InputType>
+const std::
+    unordered_map<std::string, Kernel_f8f8bf16_rowwise_grouped<InputType>>&
+    get_f8f8bf16_rowwise_grouped_kernels_sm100() {
+  static const std::
+      unordered_map<std::string, Kernel_f8f8bf16_rowwise_grouped<InputType>>
+          kernels = {
+              {"f8f8bf16_rowwise_grouped_128_32_128_2_1_1_10_f",
+               f8f8bf16_rowwise_grouped_128_32_128_2_1_1_10_f},
+              {"f8f8bf16_rowwise_grouped_128_64_128_2_1_1_10_f",
+               f8f8bf16_rowwise_grouped_128_64_128_2_1_1_10_f},
+              {"f8f8bf16_rowwise_grouped_128_128_128_2_1_1_10_f",
+               f8f8bf16_rowwise_grouped_128_128_128_2_1_1_10_f},
+              {"f8f8bf16_rowwise_grouped_128_256_128_2_1_1_10_f",
+               f8f8bf16_rowwise_grouped_128_256_128_2_1_1_10_f},
+              {"f8f8bf16_rowwise_grouped_256_32_128_2_1_1_10_f",
+               f8f8bf16_rowwise_grouped_256_32_128_2_1_1_10_f},
+              {"f8f8bf16_rowwise_grouped_256_64_128_2_1_1_10_f",
+               f8f8bf16_rowwise_grouped_256_64_128_2_1_1_10_f},
+              {"f8f8bf16_rowwise_grouped_256_128_128_2_1_1_10_f",
+               f8f8bf16_rowwise_grouped_256_128_128_2_1_1_10_f},
+              {"f8f8bf16_rowwise_grouped_256_256_128_2_1_1_10_f",
+               f8f8bf16_rowwise_grouped_256_256_128_2_1_1_10_f},
+          };
+  return kernels;
+}
+
 } // namespace fbgemm_gpu
