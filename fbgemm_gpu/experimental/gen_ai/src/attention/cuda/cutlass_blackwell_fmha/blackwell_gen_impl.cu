bd8eec0733f193ae36c933016a7189ce6ea0e61e
Aya Ibrahim <ayaoibrahim1123@fb.com>
2025-10-14 15:53:22 -0700

    BF16 re-enabled w/ Cutlass update
    Summary:
    This diff updates the code to enable BF16 enablement with latest Cutlass version.
    
    The changes include updating the code in the `blackwell_gen_impl.cu` and `collective/sm100_fmha_gen_mainloop_warpspecialized.hpp` files to support BF16 data type.
    
    The `fmha.hpp` file also includes a check to ensure that the SMEM usage does not exceed the capacity.
    
    Differential Revision: D84624233

--- fbgemm_gpu/experimental/gen_ai/src/attention/cuda/cutlass_blackwell_fmha/blackwell_gen_impl.cu
+++ fbgemm_gpu/experimental/gen_ai/src/attention/cuda/cutlass_blackwell_fmha/blackwell_gen_impl.cu
@@ -258,11 +258,13 @@ struct GenRunner {
 };
 
 // Dispatch macros for different element types
-// TODO(henrylhtsang / ayaoibrahim1123): Add support for other data types.
 #define DISPATCH_ELEMENT_TYPE(DTYPE, ELEMENT_TYPE, ...)                     \
   [&] {                                                                     \
     if (DTYPE == at::kFloat8_e4m3fn) {                                      \
       using ELEMENT_TYPE = cutlass::float_e4m3_t;                           \
+      return __VA_ARGS__();                                                 \
+    } else if (DTYPE == at::kBFloat16) {                                    \
+      using ELEMENT_TYPE = cutlass::bfloat16_t;                             \
       return __VA_ARGS__();                                                 \
     } else {                                                                \
       throw std::runtime_error(                                             \
