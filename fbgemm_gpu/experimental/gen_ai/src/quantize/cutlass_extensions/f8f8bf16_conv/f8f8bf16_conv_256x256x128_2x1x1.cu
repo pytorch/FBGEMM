4d25e92d96aeed45e4a57294668fc4fd4bddf212
pytorchbot <pytorchbot@pytorch.com>
2025-10-24 11:35:47 +0000

    2025-10-24 nightly release (bd91d439ceb5bc1f4ff4c5faf5440b85c46fecc9)

---
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_conv/f8f8bf16_conv_256x256x128_2x1x1.cu
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "f8f8bf16_conv_common.cuh"
+
+namespace fbgemm_gpu {
+
+at::Tensor f8f8bf16_conv_256x256x128_2x1x1(
+    at::Tensor activation, // FP8 - NDHWC layout
+    at::Tensor filter, // FP8 - KTRSC layout
+    at::Tensor scale,
+    std::vector<int64_t> padding, // [pad_d, pad_h, pad_w]
+    std::vector<int64_t> stride, // [stride_d, stride_h, stride_w]
+    std::vector<int64_t> dilation) { // [dilation_d, dilation_h, dilation_w]
+
+  return f8f8bf16_conv_impl<
+      256,
+      256,
+      128,
+      2,
+      1,
+      1,
+      cutlass::conv::KernelImplicitTmaWarpSpecialized2SmSm100>(
+      activation, filter, scale, padding, stride, dilation);
+}
+
+} // namespace fbgemm_gpu
