7c1a4de77ad0ab9b0a11cd2a78708e5772bf038d
Joey Yang <friedcosey@meta.com>
2025-11-17 15:39:24 -0800

    Add table_names as a instance variable of SplitTableBatchedEmbeddingBagsCodegen
    Differential Revision: D87089649

--- fbgemm_gpu/fbgemm_gpu/split_table_batched_embeddings_ops_training.py
+++ fbgemm_gpu/fbgemm_gpu/split_table_batched_embeddings_ops_training.py
@@ -710,6 +710,7 @@ class SplitTableBatchedEmbeddingBagsCodegen(nn.Module):
             f"Feature Gates: {[(feature.name, feature.is_enabled()) for feature in FeatureGateName]}"
         )
 
+        self.table_names: Optional[list[str]] = table_names
         self.logging_table_name: str = self.get_table_name_for_logging(table_names)
         self.enable_raw_embedding_streaming: bool = enable_raw_embedding_streaming
         self.pooling_mode = pooling_mode
