5d869a7c5f97446055604f416dfdaf46d6c730f9
Benson Ma <bensonma415@meta.com>
2025-11-25 11:32:12 +0900

    [fbgemm_gpu] Remove old architectures from build to reduce build size

--- .github/scripts/fbgemm_gpu_build.bash
+++ .github/scripts/fbgemm_gpu_build.bash
@@ -302,21 +302,21 @@ __configure_fbgemm_gpu_build_cuda () {
         local arch_list="9.0a;10.0a;12.0a"
       else
         # NOTE: HSTU requires sm_75 or higher
-        local arch_list="7.5;8.0"
+        local arch_list="8.0"
       fi
 
     elif  [[ $cuda_version_nvcc == *"V13.0"* ]] ||
           [[ $cuda_version_nvcc == *"V12.9"* ]] ||
           [[ $cuda_version_nvcc == *"V12.8"* ]]; then
-      local arch_list="7.5;8.0;9.0a;10.0a;12.0a"
+      local arch_list="9.0a;10.0a;12.0a"
 
     elif  [[ $cuda_version_nvcc == *"V12.6"* ]] ||
           [[ $cuda_version_nvcc == *"V12.4"* ]] ||
           [[ $cuda_version_nvcc == *"V12.1"* ]]; then
-      local arch_list="7.5;8.0;9.0a"
+      local arch_list="9.0a"
 
     else
-      local arch_list="7.5;8.0;9.0"
+      local arch_list="9.0"
       echo "[BUILD] Unknown NVCC version $cuda_version_nvcc - setting TORCH_CUDA_ARCH_LIST to: ${arch_list}"
     fi
   fi
