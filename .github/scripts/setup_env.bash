e242dadd35410a0d0602726ae0bdc7900aec7715
Benson Ma <bensonma415@meta.com>
2025-12-15 15:45:43 -0800

    Re-organize integration scripts (#5227)
    Summary:
    
    - Re-organize integration scripts
    
    Differential Revision: D89230049

--- .github/scripts/setup_env.bash
+++ .github/scripts/setup_env.bash
@@ -5,37 +5,30 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/utils_base.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/utils_system.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/utils_build.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/utils_conda.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/utils_cuda.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/utils_pip.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/utils_rocm.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/utils_pytorch.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/utils_triton.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/utils_torchrec.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/fbgemm_build.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/fbgemm_gpu_build.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/fbgemm_gpu_docs.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/fbgemm_gpu_install.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/fbgemm_gpu_lint.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/fbgemm_gpu_test.bash"
-# shellcheck disable=SC1091,SC2128
-. "$( dirname -- "$BASH_SOURCE"; )/fbgemm_gpu_benchmarks.bash"
+# Define the list of script files to source
+utils_scripts=(
+    utils_base.bash
+    utils_system.bash
+    utils_build.bash
+    utils_conda.bash
+    utils_cuda.bash
+    utils_pip.bash
+    utils_rocm.bash
+    utils_pytorch.bash
+    utils_triton.bash
+    utils_torchrec.bash
+    fbgemm_build.bash
+    fbgemm_gpu_build.bash
+    fbgemm_gpu_docs.bash
+    fbgemm_gpu_install.bash
+    fbgemm_gpu_lint.bash
+    fbgemm_gpu_test.bash
+    fbgemm_gpu_benchmarks.bash
+    fbgemm_gpu_integration.bash
+)
+
+# Loop and source each script
+for script in "${utils_scripts[@]}"; do
+    # shellcheck disable=SC1091,SC2128,SC1090
+    . "$( dirname -- "$BASH_SOURCE"; )/${script}"
+done
