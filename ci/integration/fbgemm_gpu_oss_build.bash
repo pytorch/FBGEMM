090c59056cf8ab9a0370da6e6133b7c5b7fe5cd2
Benson Ma <bensonma415@meta.com>
2025-12-18 11:02:42 -0800

    Update OSS build script to support AMD and CPU variants (#5257)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/2250
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/5257
    
    - Update OSS build script to support AMD and CPU variants
    
    Reviewed By: cthi
    
    Differential Revision: D89438912
    
    fbshipit-source-id: 4bd3207e2785955efe91b1476fed9f5a185826ae

--- ci/integration/fbgemm_gpu_oss_build.bash
+++ ci/integration/fbgemm_gpu_oss_build.bash
@@ -11,14 +11,26 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 # Set Python and CUDA versions, along with Conda environment name
 PYTHON_VERSION=${PYTHON_VERSION:-3.14}
 PYTORCH_VERSION=${PYTORCH_VERSION:-nightly}
+BUILD_VARIANT=${BUILD_VARIANT:-cuda}
 BUILD_CUDA_VERSION=${BUILD_CUDA_VERSION:-12.9.1}
+BUILD_ROCM_VERSION=${BUILD_ROCM_VERSION:-7.0}
 BUILD_TARGET=${BUILD_TARGET:-default}
 
 # Set build environment name
-BUILD_ENV=${BUILD_ENV:-"build-py$PYTHON_VERSION-torch$PYTORCH_VERSION-cu$BUILD_CUDA_VERSION"}
+if [[ "$BUILD_VARIANT" == "rocm" ]]; then
+  BUILD_VARIANT_VERSION=${BUILD_ROCM_VERSION}
+elif [[ "$BUILD_VARIANT" == "cuda" ]]; then
+  BUILD_VARIANT_VERSION=${BUILD_CUDA_VERSION}
+elif [[ "$BUILD_VARIANT" == "cpu" ]]; then
+  BUILD_VARIANT_VERSION=none
+else
+  echo "[FBGEMM_GPU CI] Invalid build variant: $BUILD_VARIANT"
+  exit 1
+fi
+BUILD_ENV=${BUILD_ENV:-"build-py${PYTHON_VERSION}-torch${PYTORCH_VERSION}-${BUILD_VARIANT}${BUILD_VARIANT_VERSION}"}
 
 echo "################################################################################"
-echo "# FBGEMM_GPU OSS Build and Install (CUDA)"
+echo "# FBGEMM_GPU OSS Build and Install"
 echo "#"
 echo "# [$(date --utc +%FT%T.%3NZ)] + ${FUNCNAME[0]} ${*}"
 echo "################################################################################"
@@ -27,8 +39,14 @@ echo "REPO_ROOT                 : $REPO_ROOT"
 echo ""
 echo "PYTHON_VERSION            : $PYTHON_VERSION"
 echo "PYTORCH_VERSION           : $PYTORCH_VERSION"
-echo "BUILD_CUDA_VERSION        : $BUILD_CUDA_VERSION"
+echo ""
+echo "BUILD_VARIANT             : $BUILD_VARIANT"
 echo "BUILD_TARGET              : $BUILD_TARGET"
+if [[ "$BUILD_VARIANT" == "rocm" ]]; then
+echo "BUILD_ROCM_VERSION        : $BUILD_ROCM_VERSION"
+elif [[ "$BUILD_VARIANT" == "cuda" ]]; then
+echo "BUILD_CUDA_VERSION        : $BUILD_CUDA_VERSION"
+fi
 echo ""
 echo "Target Conda Enviroment   : $BUILD_ENV"
 echo ""
@@ -50,9 +68,9 @@ fi
 
 # Set up the Conda enviroment for building the package
 echo "[FBGEMM_GPU CI] Setting up the conda environment ..."
-integration_setup_conda_environment "$BUILD_ENV" gcc "$PYTHON_VERSION" "$PYTORCH_VERSION" "cuda/$BUILD_CUDA_VERSION"
+integration_setup_conda_environment "$BUILD_ENV" gcc "$PYTHON_VERSION" "$PYTORCH_VERSION" "$BUILD_VARIANT/$BUILD_VARIANT_VERSION"
 
 # Build and install package into the Conda environment
 echo "[FBGEMM_GPU CI] Building the package ..."
-integration_fbgemm_gpu_build_and_install "$BUILD_ENV" "$BUILD_TARGET/cuda" "$REPO_ROOT"
+integration_fbgemm_gpu_build_and_install "$BUILD_ENV" "$BUILD_TARGET/$BUILD_VARIANT" "$REPO_ROOT"
 echo "[FBGEMM_GPU CI] Package is now installed into $BUILD_ENV"
