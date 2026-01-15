51e7bd9f6fc30b18c17dbaeddacd536db8d13bb1
Yu Guo <yuguo@meta.com>
2026-01-14 16:17:38 -0800

    use codesign ck for fbgemm ck moe (#5314)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/2305
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/5314
    
    update the ck moe dependences to be consistent with other kernels
    
    facebook
    see the summary in D88991893
    
    Reviewed By: cthi
    
    Differential Revision: D88991886
    
    fbshipit-source-id: bb04afb5102dab205919010bfeb40f8e733954c4

--- fbgemm_gpu/experimental/gen_ai/src/quantize/ck_extensions/fused_moe/fused_moe.hpp
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/ck_extensions/fused_moe/fused_moe.hpp
@@ -16,7 +16,6 @@ struct fused_moe_args {
   const void*
       y_smooth_scale_ptr; // [e, 1, n], smooth-quant-scale for 2nd gemm input
   const void* local_expert_mask_ptr; // [e], local_expert_mask_ptr for EP
-  const void* local_tokens; // [1] if not nullptr, tokens read from here
   void* o_ptr; // [m, k], output token (no need to do zeroing)
   void* ws_ptr; // size is moe_sorting_get_workspace_size()
                 // if return zero, then could be nullptr
