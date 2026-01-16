4a08ff69349369c255e3f2d062a8ed45abfc09bd
Supadchaya Puangpontip <supadchaya@meta.com>
2026-01-15 18:42:01 -0800

    Back out "Add CPU support for cumem_utils to remove GPU dep on MTIA" (#5317)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/5317
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/2310
    
    Original commit changeset: 684e3a775235
    
    Original Phabricator Diff: D90525136
    
    Reviewed By: q10, crypt3lx2k
    
    Differential Revision: D90799168
    
    fbshipit-source-id: 319309b9c870e14db3bccaa30b736ca5eb15cd77

--- fbgemm_gpu/src/memory_utils/common.h
+++ fbgemm_gpu/src/memory_utils/common.h
@@ -9,7 +9,6 @@
 #pragma once
 
 #include <ATen/ATen.h>
-#include "fbgemm_gpu/cumem_utils.h"
 
 using Tensor = at::Tensor;
 
@@ -19,11 +18,5 @@ Tensor new_unified_tensor_cpu(
     const Tensor& self,
     const std::vector<std::int64_t>& sizes,
     bool is_host_mapped);
-
-Tensor uvm_to_cpu_cpu(const Tensor& t);
-
-Tensor uvm_to_cpu_clone_cpu(const Tensor& t);
-
-bool is_uvm_tensor_cpu(const Tensor& t);
 
 } // namespace fbgemm_gpu
