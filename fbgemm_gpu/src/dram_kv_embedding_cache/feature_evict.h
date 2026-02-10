2c57fad4c1d2ba5fb0fcf63d432a9d2790ca485f
Benson Ma <bensonma415@meta.com>
2026-02-10 10:53:37 -0800

    Back out "More modernization fixes"
    Summary:
    Original commit changeset: 5914332bd6ea
    
    Original Phabricator Diff: D92645749
    
    Reviewed By: tonykao8080
    
    Differential Revision: D92856605

--- fbgemm_gpu/src/dram_kv_embedding_cache/feature_evict.h
+++ fbgemm_gpu/src/dram_kv_embedding_cache/feature_evict.h
@@ -13,7 +13,6 @@
 #include <chrono>
 #include <cstddef>
 #include <memory_resource>
-#include <ranges>
 #include <stdexcept>
 #include <vector>
 
@@ -319,9 +318,12 @@ struct FeatureEvictMetrics {
   }
 
   void reset() {
-    std::ranges::fill(evicted_counts, 0);
-    std::ranges::fill(processed_counts, 0);
-    std::ranges::fill(eviction_threshold_with_dry_run, 0.0);
+    std::fill(evicted_counts.begin(), evicted_counts.end(), 0);
+    std::fill(processed_counts.begin(), processed_counts.end(), 0);
+    std::fill(
+        eviction_threshold_with_dry_run.begin(),
+        eviction_threshold_with_dry_run.end(),
+        0.0);
     exec_duration_ms = 0;
     full_duration_ms = 0;
     start_time_ms =
@@ -808,7 +810,8 @@ class FeatureEvict {
   }
 
   [[nodiscard]] int get_sub_table_id(int64_t key) const {
-    auto it = std::ranges::upper_bound(sub_table_hash_cumsum_, key);
+    auto it = std::upper_bound(
+        sub_table_hash_cumsum_.begin(), sub_table_hash_cumsum_.end(), key);
     if (it == sub_table_hash_cumsum_.end()) {
       CHECK(false) << "key " << key << " doesn't belong to any feature";
     }
