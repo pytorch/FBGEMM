9b22ade3d931f4e369a8b77251f546f367ca8965
Supadchaya Puangpontip <supadchaya@meta.com>
2026-03-17 02:36:33 -0700

    Add missing copyright headers to Meta-authored files (#5482)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/5482
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/2455
    
    The opensource automated checkup (P2238882218) flagged files in pytorch/FBGEMM
    missing copyright headers. This diff adds the standard Meta BSD copyright header
    to all Meta-authored files that were missing it:
    
    - Blackwell FMHA forward/backward dispatch, template, and instantiation files
    - fused_moe_kernel.hip (ROCm fused MoE kernel wrapper)
    - static_switch.h (HSTU ampere and hopper variants)
    - ExampleGraph.dot (documentation)
    - FbgemmFP32.h (added "Copyright" keyword to existing header)
    
    Reviewed By: q10
    
    Differential Revision: D96794875
    
    fbshipit-source-id: 33a80b183626fc1f5ad40f4a483c527c9b644ae9

--- fbgemm_gpu/experimental/hstu/src/hstu_ampere/static_switch.h
+++ fbgemm_gpu/experimental/hstu/src/hstu_ampere/static_switch.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // Inspired by
 // https://github.com/NVIDIA/DALI/blob/main/include/dali/core/static_switch.h
 // and https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/Dispatch.h
