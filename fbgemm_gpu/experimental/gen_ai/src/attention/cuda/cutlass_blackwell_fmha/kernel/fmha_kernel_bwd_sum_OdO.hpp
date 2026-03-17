1e9607ffee4577a84e6bdaaa4ca17a028a0d4249
Supadchaya Puangpontip <supadchaya@meta.com>
2026-03-17 11:51:54 -0700

    Add Meta copyright headers to modified NVIDIA CUTLASS files (#5483)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/5483
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/2456
    
    Add Meta BSD copyright header alongside existing NVIDIA BSD-3-Clause headers
    in 39 Blackwell FMHA CUTLASS files. These files were originally copied from
    NVIDIA CUTLASS examples but have been extensively modified by Meta engineers
    (adding sliding window attention, split-K decode, MLA support, torch.compile
    integration, etc.).
    
    Reviewed By: gchalump
    
    Differential Revision: D96035462
    
    fbshipit-source-id: 51a210a1ef519cf18230662c65437dee92c5f319

--- fbgemm_gpu/experimental/gen_ai/src/attention/cuda/cutlass_blackwell_fmha/kernel/fmha_kernel_bwd_sum_OdO.hpp
+++ fbgemm_gpu/experimental/gen_ai/src/attention/cuda/cutlass_blackwell_fmha/kernel/fmha_kernel_bwd_sum_OdO.hpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // @nolint
 /***************************************************************************************************
  * Copyright (c) 2025 - 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
