a7c081f2b76c9ffe990a2ac2a4d677ecd1700d51
Supadchaya Puangpontip <supadchaya@meta.com>
2026-03-17 00:15:31 -0700

    Add Portions Copyright headers to modified Arm KleidiAI files
    Summary:
    Add "Portions Copyright" Meta BSD header to Arm KleidiAI and Neon utility
    files. These files were originally from Arm but have been modified by Meta
    engineers.
    
    Reviewed By: gchalump, q10
    
    Differential Revision: D96861205

--- src/TransposeUtilsNeon.h
+++ src/TransposeUtilsNeon.h
@@ -1,4 +1,12 @@
 /*
+ * Portions Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/*
  * SPDX-FileCopyrightText: Copyright 2024 Arm Limited and/or its affiliate
  * <open-source-office@arm.com> SPDX-License-Identifier: BSD-3-Clause
  */
