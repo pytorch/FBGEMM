ce03e8f8610aaa0bc0b2dc71131f5267baa93993
Supadchaya Puangpontip <supadchaya@meta.com>
2026-03-17 00:15:12 -0700

    Add Portions Copyright headers to modified third-party include files
    Summary:
    Add "Portions Copyright" Meta BSD header to AMD ROCm headers (cdna_guard.h,
    split_embeddings_common.h, stochastic_rounding.h, vec2.h, weight_row.h) and
    the spaCy-derived common_utils.cuh. These files were originally from third
    parties but have been modified by Meta engineers.
    
    Reviewed By: gchalump
    
    Differential Revision: D95329597

--- fbgemm_gpu/include/fbgemm_gpu/utils/rocm/weight_row.h
+++ fbgemm_gpu/include/fbgemm_gpu/utils/rocm/weight_row.h
@@ -1,3 +1,11 @@
+/*
+ * Portions Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 /*******************************************************************************
  * Copyright (c) 2016 - 2024 Advanced Micro Devices, Inc. All rights reserved.
  *
