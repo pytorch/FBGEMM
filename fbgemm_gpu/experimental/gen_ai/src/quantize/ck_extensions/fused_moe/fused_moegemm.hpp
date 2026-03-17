22e214b4b62eb761a08a425e999cadb1cbdcda15
Supadchaya Puangpontip <supadchaya@meta.com>
2026-03-17 11:51:54 -0700

    Add Portions Copyright headers to modified AMD CK/ROCm gen_ai files (#5484)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/5484
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/2457
    
    Add "Portions Copyright" Meta BSD header to AMD CK fused MoE files and the
    ROCm embedding backward kernel template. These files were originally from AMD
    but have been modified by Meta engineers.
    
    Reviewed By: gchalump
    
    Differential Revision: D96035536
    
    fbshipit-source-id: 4b888ad8df1c8642c2dffd31bf369992aec7e55c

--- fbgemm_gpu/experimental/gen_ai/src/quantize/ck_extensions/fused_moe/fused_moegemm.hpp
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/ck_extensions/fused_moe/fused_moegemm.hpp
@@ -1,3 +1,11 @@
+/*
+ * Portions Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2018-2024, Advanced Micro Devices, Inc. All rights reserved.
 
