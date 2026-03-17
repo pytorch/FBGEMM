e1124168a57fac535bf25bf5752ed50a7d470fc4
Supadchaya Puangpontip <supadchaya@meta.com>
2026-03-17 11:51:54 -0700

    Add Portions Copyright headers to modified Arm KleidiAI files (#5486)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/5486
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/2459
    
    Add "Portions Copyright" Meta BSD header to Arm KleidiAI and Neon utility
    files. These files were originally from Arm but have been modified by Meta
    engineers.
    
    Reviewed By: gchalump, q10
    
    Differential Revision: D96861205
    
    fbshipit-source-id: 7c750a2f8780f6798fe1fe988a33ed8167f68af7

--- src/KleidiAIFP16UKernelsNeon.cc
+++ src/KleidiAIFP16UKernelsNeon.cc
@@ -1,3 +1,11 @@
+/*
+ * Portions Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // @lint-ignore-every LICENSELINT
 //
 // SPDX-FileCopyrightText: Copyright 2024-2025 Arm Limited and/or its affiliates
--- src/fp32/KleidiAIFP32UKernelsNeon.cc
+++ src/fp32/KleidiAIFP32UKernelsNeon.cc
@@ -1,3 +1,11 @@
+/*
+ * Portions Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 //
 // SPDX-FileCopyrightText: Copyright 2025 Arm Limited and/or its affiliates
 // <open-source-office@arm.com>
