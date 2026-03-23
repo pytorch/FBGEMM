0dfaed7f0e423ea9e67925965a62fb4a504d6843
Aryaman Gupta <aryaman.gupta@amd.com>
2026-03-23 13:58:40 +0000

    backward_adagrad_test.py: updates unit test to vary params

--- fbgemm_gpu/test/tbe/training/backward_adagrad_test.py
+++ fbgemm_gpu/test/tbe/training/backward_adagrad_test.py
@@ -23,6 +23,7 @@ from fbgemm_gpu.split_embedding_configs import (
 from fbgemm_gpu.split_table_batched_embeddings_ops_training import (
     ComputeDevice,
     SplitTableBatchedEmbeddingBagsCodegen,
+    WeightDecayMode,
 )
 from hypothesis import given, settings
 
@@ -38,6 +39,7 @@ from .backward_adagrad_common import (
     gpu_unavailable,
     optests,
     PoolingMode,
+    skipIfNotRocm,
     SparseType,
     st,
 )
@@ -234,28 +236,61 @@ class BackwardAdagradTest(unittest.TestCase):
             **kwargs,
         )
 
+    @given(
+        T=st.integers(min_value=1, max_value=5),
+        D=st.sampled_from([16, 32, 40, 48, 64, 80]),
+        B=st.integers(min_value=1, max_value=128),
+        log_E=st.integers(min_value=3, max_value=5),
+        L=st.integers(min_value=2, max_value=20),
+        weights_precision=st.sampled_from([SparseType.FP16, SparseType.FP32]),
+        weighted=st.booleans(),
+        weight_decay_mode=st.sampled_from(
+            [
+                WeightDecayMode.NONE,
+                WeightDecayMode.L2,
+                WeightDecayMode.DECOUPLE,
+            ]
+        ),
+    )
+    @settings(**common_settings)
     @unittest.skipIf(*gpu_unavailable)
-    def test_backward_adagrad_fp16_pmSUM_D320(self) -> None:
-        execute_backward_adagrad(
-            T=2,
-            # using D=80 since the test harness multiplies D by 4, so 80*4=320
-            D=80,
-            B=16,
-            log_E=4,
-            L=4,
-            D_gradcheck=1,
-            weights_precision=SparseType.FP16,
-            stochastic_rounding=False,
-            weighted=False,
-            row_wise=True,
-            mixed=False,
-            mixed_B=False,
-            use_cache=False,
-            cache_algorithm=CacheAlgorithm.LRU,
-            pooling_mode=PoolingMode.SUM,
-            use_cpu=False,
-            output_dtype=SparseType.FP16,
-        )
+    @skipIfNotRocm("Test evaluates ROCm backward kernels")
+    def test_backward_adagrad_rocm_optimized(
+        self,
+        T: int,
+        D: int,
+        B: int,
+        log_E: int,
+        L: int,
+        weights_precision: SparseType,
+        weighted: bool,
+        weight_decay_mode: WeightDecayMode,
+    ) -> None:
+        for use_optimized_kernel in ["0", "1"]:
+            os.environ["FBGEMM_TBE_ROCM_HIP_BACKWARD_KERNEL"] = use_optimized_kernel
+            logging.info(
+                f"Testing ROCm backward kernel with FBGEMM_TBE_ROCM_HIP_BACKWARD_KERNEL={use_optimized_kernel}"
+            )
+            execute_backward_adagrad(
+                T=T,
+                D=D,
+                B=B,
+                log_E=log_E,
+                L=L,
+                D_gradcheck=1,
+                weights_precision=weights_precision,
+                stochastic_rounding=False,
+                weighted=weighted,
+                row_wise=True,
+                mixed=False,
+                mixed_B=False,
+                use_cache=False,
+                cache_algorithm=CacheAlgorithm.LRU,
+                pooling_mode=PoolingMode.SUM,
+                use_cpu=False,
+                output_dtype=weights_precision,
+                weight_decay_mode=weight_decay_mode,
+            )
 
     @unittest.skipIf(*gpu_unavailable)
     @unittest.skipIf(*gpu_memory_lt_gb(40))
