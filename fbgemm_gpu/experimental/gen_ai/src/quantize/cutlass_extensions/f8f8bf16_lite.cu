a027572108460df7030ccb1f8e5c5c51d8820d23
Supadchaya Puangpontip <supadchaya@meta.com>
2026-03-27 13:33:04 -0700

    Add Portions Copyright headers to modified third-party files
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/2512
    
    Add "Portions Copyright" Meta BSD header to files originally from third
    parties (NVIDIA, AMD, Triton) that have been modified by Meta engineers.
    These files were missing the required Meta copyright header.
    
    Files modified:
    - `fbgemm_gpu/experimental/gemm/triton_gemm/matmul_perf_model.py` (from triton-lang/kernels)
    - `fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_lite.cu` (from NVIDIA)
    - `fbgemm_gpu/include/fbgemm_gpu/utils/rocm/half2.h` (from AMD)
    
    Reviewed By: gchalump, q10
    
    Differential Revision: D98453024

--- fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_lite.cu
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/cutlass_extensions/f8f8bf16_lite.cu
@@ -1,4 +1,12 @@
 /*
+ * Portions Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/*
  * Copyright (c) 2022-2024, NVIDIA CORPORATION.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
