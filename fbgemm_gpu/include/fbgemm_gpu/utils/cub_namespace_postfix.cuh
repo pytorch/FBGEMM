1fbf26b6d6aeb7e7cd7f622c10c6b21bed977b47
cyy <cyyever@outlook.com>
2026-04-09 11:58:56 +0800

    Use CUB_WRAPPED_NAMESPACE instead of legacy CUB_NS_PREFIX

--- fbgemm_gpu/include/fbgemm_gpu/utils/cub_namespace_postfix.cuh
+++ fbgemm_gpu/include/fbgemm_gpu/utils/cub_namespace_postfix.cuh
@@ -10,22 +10,7 @@
 
 #ifdef FBGEMM_CUB_USE_NAMESPACE
 
-#undef CUB_NS_PREFIX
-#undef CUB_NS_POSTFIX
-
-#include <cuda.h> // for CUDA_VERSION
-#if defined(CUDA_VERSION) && CUDA_VERSION >= 11000
-#include <cub/version.cuh>
-#else
-#define CUB_VERSION 0
-#endif
-
-// PR https://github.com/NVIDIA/cub/pull/350 introduced breaking change.
-// When the CUB_NS_[PRE|POST]FIX macros are set,
-// CUB_NS_QUALIFIER must also be defined to the fully qualified CUB namespace
-#if CUB_VERSION >= 101301
-#undef CUB_NS_QUALIFIER
-#endif
+#undef CUB_WRAPPED_NAMESPACE
 
 #define FBGEMM_GPU_CUB_NS_PREFIX fbgemm_gpu::
 
