a0e1105552f26d12dff222970be8d3259d80d4ac
Yuanyuan Chen <cyyever@outlook.com>
2026-04-12 17:09:58 +0800

    Unify duplicated cmake code between CPU and GPU builds
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>

--- cmake/modules/Utilities.cmake
+++ cmake/modules/Utilities.cmake
@@ -19,51 +19,8 @@ function(BLOCK_PRINT)
   message("")
 endfunction()
 
-function(LIST_FILTER)
-  set(flags)
-  set(singleValueArgs OUTPUT REGEX)
-  set(multiValueArgs INPUT)
-
-  cmake_parse_arguments(
-    args
-    "${flags}" "${singleValueArgs}" "${multiValueArgs}"
-    ${ARGN})
-
-  set(${args_OUTPUT})
-
-  foreach(value ${args_INPUT})
-    if("${value}" MATCHES "${args_REGEX}")
-      list(APPEND ${args_OUTPUT} ${value})
-    endif()
-  endforeach()
-
-  set(${args_OUTPUT} ${${args_OUTPUT}} PARENT_SCOPE)
-endfunction()
-
-function(prepend_filepaths)
-  set(flags)
-  set(singleValueArgs PREFIX OUTPUT)
-  set(multiValueArgs INPUT)
-
-  cmake_parse_arguments(
-    args
-    "${flags}" "${singleValueArgs}" "${multiValueArgs}"
-    ${ARGN})
-
-  set(${args_OUTPUT})
-
-  foreach(filepath ${args_INPUT})
-    list(APPEND ${args_OUTPUT} "${args_PREFIX}/${filepath}")
-  endforeach()
-
-  set(${args_OUTPUT} ${${args_OUTPUT}} PARENT_SCOPE)
-endfunction()
-
 macro(handle_genfiles variable)
-  prepend_filepaths(
-    PREFIX ${CMAKE_BINARY_DIR}
-    INPUT ${${variable}}
-    OUTPUT ${variable})
+  list(TRANSFORM ${variable} PREPEND "${CMAKE_BINARY_DIR}/")
 endmacro()
 
 macro(handle_genfiles_rocm variable)
