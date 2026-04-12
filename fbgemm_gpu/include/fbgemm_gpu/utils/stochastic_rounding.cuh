16e8ba28906e05f63d30ebb0c6b5de328cbceb2e
Yuanyuan Chen <cyyever@outlook.com>
2026-04-12 13:08:37 -0700

    Cleanup stale code for ROCM < 6.2 and CUDA < 12 (#5616)
    Summary: Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/5616
    
    Differential Revision: D100440190
    
    Pulled By: q10

--- fbgemm_gpu/include/fbgemm_gpu/utils/stochastic_rounding.cuh
+++ fbgemm_gpu/include/fbgemm_gpu/utils/stochastic_rounding.cuh
@@ -235,13 +235,8 @@ DEVICE_INLINE void nearest_rounding_vector(
     at::Float8_e4m3fn* output,
     const Vec4T<float>& value,
     const float2 /* Not used yet */) {
-#if (defined(USE_ROCM) && ROCM_VERSION >= 60200) || \
-    (defined(CUDA_VERSION) && CUDA_VERSION >= 12000)
   __nv_fp8x4_e4m3* fp8_ptr = reinterpret_cast<__nv_fp8x4_e4m3*>(output);
   fp8_ptr[0] = static_cast<__nv_fp8x4_e4m3>(value.acc);
-#else
-  CUDA_KERNEL_ASSERT(false);
-#endif
 }
 
 template <>
@@ -249,13 +244,8 @@ DEVICE_INLINE void nearest_rounding_vector(
     at::Float8_e4m3fnuz* output,
     const Vec4T<float>& value,
     const float2 /* Not used yet */) {
-#if (defined(USE_ROCM) && ROCM_VERSION >= 60200) || \
-    (defined(CUDA_VERSION) && CUDA_VERSION >= 12000)
   __nv_fp8x4_e4m3* fp8_ptr = reinterpret_cast<__nv_fp8x4_e4m3*>(output);
   fp8_ptr[0] = static_cast<__nv_fp8x4_e4m3>(value.acc);
-#else
-  CUDA_KERNEL_ASSERT(false);
-#endif
 }
 
 template <>
@@ -263,13 +253,8 @@ DEVICE_INLINE void nearest_rounding_vector(
     at::Float8_e4m3fn* output,
     const Vec4T<at::Half>& value,
     const float2 /* Not used yet */) {
-#if (defined(USE_ROCM) && ROCM_VERSION >= 60200) || \
-    (defined(CUDA_VERSION) && CUDA_VERSION >= 12000)
   __nv_fp8x4_e4m3* fp8_ptr = reinterpret_cast<__nv_fp8x4_e4m3*>(output);
   fp8_ptr[0] = static_cast<__nv_fp8x4_e4m3>(value.acc);
-#else
-  CUDA_KERNEL_ASSERT(false);
-#endif
 }
 
 template <>
@@ -277,13 +262,8 @@ DEVICE_INLINE void nearest_rounding_vector(
     at::Float8_e4m3fnuz* output,
     const Vec4T<at::Half>& value,
     const float2 /* Not used yet */) {
-#if (defined(USE_ROCM) && ROCM_VERSION >= 60200) || \
-    (defined(CUDA_VERSION) && CUDA_VERSION >= 12000)
   __nv_fp8x4_e4m3* fp8_ptr = reinterpret_cast<__nv_fp8x4_e4m3*>(output);
   fp8_ptr[0] = static_cast<__nv_fp8x4_e4m3>(value.acc);
-#else
-  CUDA_KERNEL_ASSERT(false);
-#endif
 }
 
 template <>
@@ -292,14 +272,9 @@ DEVICE_INLINE void stochastic_rounding_vector(
     const Vec4T<float>& value,
     StochasticRoundingRNGState& /* state */,
     const float2 /* qparams */) {
-// TODO, make this actually stochastic later.
-#if (defined(USE_ROCM) && ROCM_VERSION >= 60200) || \
-    (defined(CUDA_VERSION) && CUDA_VERSION >= 12000)
+  // TODO, make this actually stochastic later.
   __nv_fp8x4_e4m3* fp8_ptr = reinterpret_cast<__nv_fp8x4_e4m3*>(output);
   fp8_ptr[0] = static_cast<__nv_fp8x4_e4m3>(value.acc);
-#else
-  CUDA_KERNEL_ASSERT(false);
-#endif
 }
 
 template <>
@@ -308,14 +283,9 @@ DEVICE_INLINE void stochastic_rounding_vector(
     const Vec4T<float>& value,
     StochasticRoundingRNGState& /* state */,
     const float2 /* qparams */) {
-// TODO, make this actually stochastic later.
-#if (defined(USE_ROCM) && ROCM_VERSION >= 60200) || \
-    (defined(CUDA_VERSION) && CUDA_VERSION >= 12000)
+  // TODO, make this actually stochastic later.
   __nv_fp8x4_e4m3* fp8_ptr = reinterpret_cast<__nv_fp8x4_e4m3*>(output);
   fp8_ptr[0] = static_cast<__nv_fp8x4_e4m3>(value.acc);
-#else
-  CUDA_KERNEL_ASSERT(false);
-#endif
 }
 
 template <>
@@ -324,14 +294,9 @@ DEVICE_INLINE void stochastic_rounding_vector(
     const Vec4T<at::Half>& value,
     StochasticRoundingRNGState& /* state */,
     const float2 /* qparams */) {
-// TODO, make this stochastic later.
-#if (defined(USE_ROCM) && ROCM_VERSION >= 60200) || \
-    (defined(CUDA_VERSION) && CUDA_VERSION >= 12000)
+  // TODO, make this stochastic later.
   __nv_fp8x4_e4m3* fp8_ptr = reinterpret_cast<__nv_fp8x4_e4m3*>(output);
   fp8_ptr[0] = static_cast<__nv_fp8x4_e4m3>(value.acc);
-#else
-  CUDA_KERNEL_ASSERT(false);
-#endif
 }
 
 template <>
@@ -340,14 +305,9 @@ DEVICE_INLINE void stochastic_rounding_vector(
     const Vec4T<at::Half>& value,
     StochasticRoundingRNGState& /* state */,
     const float2 /* qparams */) {
-// TODO, make this stochastic later.
-#if (defined(USE_ROCM) && ROCM_VERSION >= 60200) || \
-    (defined(CUDA_VERSION) && CUDA_VERSION >= 12000)
+  // TODO, make this stochastic later.
   __nv_fp8x4_e4m3* fp8_ptr = reinterpret_cast<__nv_fp8x4_e4m3*>(output);
   fp8_ptr[0] = static_cast<__nv_fp8x4_e4m3>(value.acc);
-#else
-  CUDA_KERNEL_ASSERT(false);
-#endif
 }
 
 } // namespace fbgemm_gpu
