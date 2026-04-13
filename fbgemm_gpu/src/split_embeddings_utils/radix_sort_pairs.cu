80a41381aa88d710992bea7d25fd2260e663fdbb
Yuanyuan Chen <cyyever@outlook.com>
2026-04-13 10:59:09 -0700

    Cleanup stale code for ROCM < 6.2 and CUDA < 12 (#5625)
    Summary:
    X-link: https://github.com/facebookresearch/FBGEMM/pull/2571
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/5625
    
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/5616
    
    Reviewed By: cthi
    
    Differential Revision: D100440190
    
    Pulled By: q10
    
    fbshipit-source-id: 24f43215f8e0fc9de790bc2c4a508b7639dc01c3

--- fbgemm_gpu/src/split_embeddings_utils/radix_sort_pairs.cu
+++ fbgemm_gpu/src/split_embeddings_utils/radix_sort_pairs.cu
@@ -24,7 +24,6 @@
 using Tensor = at::Tensor;
 using namespace fbgemm_gpu;
 
-#if defined(CUDA_VERSION) && CUDA_VERSION >= 12000
 #define DEF_RADIX_SORT_PAIRS_FN(KeyT, ValueT)                        \
   DLL_PUBLIC cudaError_t radix_sort_pairs(                           \
       void* d_temp_storage,                                          \
@@ -49,33 +48,6 @@ using namespace fbgemm_gpu;
         end_bit,                                                     \
         stream);                                                     \
   }
-#else
-#define DEF_RADIX_SORT_PAIRS_FN(KeyT, ValueT)                        \
-  DLL_PUBLIC cudaError_t radix_sort_pairs(                           \
-      void* d_temp_storage,                                          \
-      size_t& temp_storage_bytes,                                    \
-      const KeyT* d_keys_in,                                         \
-      KeyT* d_keys_out,                                              \
-      const ValueT* d_values_in,                                     \
-      ValueT* d_values_out,                                          \
-      const int num_items,                                           \
-      const int begin_bit,                                           \
-      const int end_bit,                                             \
-      cudaStream_t stream) {                                         \
-    return FBGEMM_GPU_CUB_NS_PREFIX cub::DeviceRadixSort::SortPairs( \
-        d_temp_storage,                                              \
-        temp_storage_bytes,                                          \
-        d_keys_in,                                                   \
-        d_keys_out,                                                  \
-        d_values_in,                                                 \
-        d_values_out,                                                \
-        num_items,                                                   \
-        begin_bit,                                                   \
-        end_bit,                                                     \
-        stream,                                                      \
-        false);                                                      \
-  }
-#endif
 
 DEF_RADIX_SORT_PAIRS_FN(int64_t, int32_t);
 DEF_RADIX_SORT_PAIRS_FN(int64_t, int64_t);
