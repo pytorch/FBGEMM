c880a5aa60b2f4a1fa44b4dfc288b82fbfb800f5
pytorchbot <pytorchbot@pytorch.com>
2026-04-29 12:03:55 +0000

    2026-04-29 nightly release (1da5df4e3eea0d9156ef06bbeccf88019ab51e53)

--- bench/EmbeddingQuantizeNBitToFloatOrHalfBenchmark.cc
+++ bench/EmbeddingQuantizeNBitToFloatOrHalfBenchmark.cc
@@ -89,6 +89,66 @@ static void performance_test() {
   } // for each bit_rate
 } // performance_test
 
+static void performance_test_bf16() {
+  constexpr int NWARMUP = 4;
+  constexpr int NITER = 256;
+
+  cout << "With result as bfloat16" << '\n';
+  cout << setw(8) << "bit_rate" << ", " << setw(6) << "rows" << "," << setw(6)
+       << "cols" << "," << setw(16) << "elems_per_usec" << "," << setw(10)
+       << "GB/Sec" << '\n';
+  std::vector<int> bit_rates = {2, 4, 8};
+  for (int bit_rate : bit_rates) {
+    for (int rowSize : {100, 120, 1000}) {
+      for (int colSize : {16, 64, 128, 256, 512, 1024, 2048}) {
+        int elem_per_byte = 8 / bit_rate;
+        int bytes_per_row = colSize / elem_per_byte + 2 * sizeof(float16);
+
+        aligned_vector<uint8_t> inpVec(rowSize * bytes_per_row);
+        randFill<uint8_t>(inpVec, 0, 20);
+
+        aligned_vector<float16> outVec(rowSize * colSize);
+
+        double duration = 0.0f;
+
+        int constexpr kNumRepeats = 4;
+
+        duration = measureWithWarmup(
+            [&]() {
+              for (int i = 0; i < kNumRepeats; ++i) {
+                FusedNBitRowwiseQuantizedSBHalfToFloatOrHalfRef<float16, true>(
+                    bit_rate,
+                    inpVec.data(),
+                    rowSize,
+                    bytes_per_row,
+                    outVec.data());
+              }
+            },
+            NWARMUP,
+            NITER,
+            [&]() {
+              cache_evict(inpVec);
+              cache_evict(outVec);
+            });
+
+        float elements_per_usec =
+            rowSize * colSize * kNumRepeats / (duration * 1e6);
+
+        duration *= 1e9; // convert to ns
+        long bytes_read = rowSize * colSize * sizeof(float) * kNumRepeats;
+        float gigabyes_per_sec = bytes_read / duration;
+
+        cout << setw(8) << bit_rate << "," << setw(6) << rowSize << ", "
+             << setw(6) << colSize << ",";
+        cout << setw(16) << std::fixed << std::setprecision(2)
+             << elements_per_usec << ", ";
+        cout << setw(10) << std::fixed << std::setprecision(2)
+             << gigabyes_per_sec << '\n';
+      } // for each cols
+    } // for each rows
+  } // for each bit_rate
+} // performance_test_bf16
+
 int main() {
 #ifdef _OPENMP
   // Use 1 thread unless OMP_NUM_THREADS is explicit set.
@@ -99,5 +159,6 @@ int main() {
 #endif
   performance_test<float16>();
   performance_test<float>();
+  performance_test_bf16();
   return 0;
 }
